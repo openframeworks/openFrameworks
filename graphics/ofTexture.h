59a4638aa3a2988b95c2911f7151b30c55ddaadc
arturo <arturo@5aee0c02-c622-0410-bcf2-d8652e674aa7>
2009-01-16 00:39:10 +0000

    change pointers in textureData
    git-svn-id: http://svn.openframeworks.cc/openFrameworks/trunk/openFrameworks@175 5aee0c02-c622-0410-bcf2-d8652e674aa7

--- graphics/ofTexture.h
+++ graphics/ofTexture.h
@@ -6,7 +6,7 @@
 
 
 typedef struct{
-	
+
 	bool bAllocated;
 	int glType;
 	int glTypeInternal;
@@ -18,51 +18,51 @@ typedef struct{
 	int width;
 	int height;
 	bool bFlipTexture;
-	
+
 	//we do this because openGL expects an array
 	//but we don't want people to work with textureName[1]
 	//so we make textureID point to the same location
 	union{
 		struct{
-			unsigned int * textureID;	//use me
-		};		
-		unsigned int * textureName[1];  //don't use me
+			unsigned int textureID;	//use me
+		};
+		unsigned int textureName[1];  //don't use me
 	};
-		
+
 }ofTextureData;
 
 
 class ofTexture : public ofBaseDraws{
 
 	public :
-		
+
 		ofTexture();
 		~ofTexture();
-		
+
 		// -----------------------------------------------------------------------
 		// we allow pass by copy and assignment operator
 		// it does a straight copy but you are getting the textureID of mom's texture
 		// so this means that your texture and mom's texture are the same thing
 		// so in other words be careful! calling clear on your texture will trash mom's
 		// texture and vice versa.
-		ofTexture(const ofTexture& mom); 							
-		ofTexture& operator=(const ofTexture& mom); 	
+		ofTexture(const ofTexture& mom);
+		ofTexture& operator=(const ofTexture& mom);
 		// -----------------------------------------------------------------------
-		
-		void allocate(int w, int h, int internalGlDataType, bool bUseARBExtention = true);	 
+
+		void allocate(int w, int h, int internalGlDataType, bool bUseARBExtention = true);
 		void clear();
-		void loadData(unsigned char * data, int w, int h, int glDataType); 
+		void loadData(unsigned char * data, int w, int h, int glDataType);
 		void loadScreenData(int x, int y, int w, int h);
 		void draw(float x, float y, float w, float h);
 		void draw(float x, float y);
-		
+
 		bool bAllocated();
-		
+
 		ofTextureData getTextureData();
-		
-	protected:		
+
+	protected:
 		ofTextureData   texData;
-			
-}; 
+
+};
 
 #endif
