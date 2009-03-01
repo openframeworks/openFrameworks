7a94c729700bba11fa9b9a1233200311656011ed
arturo <arturo@0457777e-be4a-0410-a59e-cd2fa5f2afb7>
2009-03-01 17:43:41 +0000

    CreateMutex -> CreateMutexA
    git-svn-id: http://svn.openframeworks.cc/addons/ofxOsc/trunk/ofxOsc@72 0457777e-be4a-0410-a59e-cd2fa5f2afb7

--- src/ofxOscReceiver.cpp
+++ src/ofxOscReceiver.cpp
@@ -1,20 +1,20 @@
 /*
- 
+
  Copyright 2007, 2008 Damian Stewart damian@frey.co.nz
  Distributed under the terms of the GNU Lesser General Public License v3
- 
+
  This file is part of the ofxOsc openFrameworks OSC addon.
- 
+
  ofxOsc is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  ofxOsc is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU Lesser General Public License
  along with ofxOsc.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,7 +30,7 @@
 ofxOscReceiver::ofxOscReceiver()
 {
 #ifdef TARGET_WIN32
-	mutex = CreateMutex( NULL, FALSE, NULL );
+	mutex = CreateMutexA( NULL, FALSE, NULL );
 #else
 	pthread_mutex_init( &mutex, NULL );
 #endif
