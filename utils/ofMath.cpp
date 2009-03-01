80389ba16a2cfe8977ddc991fd934ded39355c40
arturo <arturo@5aee0c02-c622-0410-bcf2-d8652e674aa7>
2009-03-01 22:26:46 +0000

    correcting breaklines: svn propset svn:eol-style native
    git-svn-id: http://svn.openframeworks.cc/openFrameworks/trunk/openFrameworks@240 5aee0c02-c622-0410-bcf2-d8652e674aa7

--- utils/ofMath.cpp
+++ utils/ofMath.cpp
@@ -3,15 +3,16 @@
 #include <sys/time.h>
 
 #ifdef TARGET_WIN32
-    #include <process.h>
-    #define getpid _getpid
+	#include <process.h>
+	#define getpid _getpid
 #endif
 
 //--------------------------------------------------
-int ofNextPow2(int a){
+int ofNextPow2(int a) {
 	// from nehe.gamedev.net lesson 43
-	int rval=1;
-	while(rval<a) rval<<=1;
+	int rval = 1;
+	while (rval < a)
+		rval <<= 1;
 	return rval;
 }
 
@@ -38,24 +39,25 @@ float ofRandom(float x, float y) {
 	float low = 0;
 	float randNum = 0;
 	// if there is no range, return the value
-	if (x == y) return x; 			// float == ?, wise? epsilon?
+	if (x == y)
+		return x; // float == ?, wise? epsilon?
 	high = MAX(x,y);
 	low = MIN(x,y);
-	randNum = low + ((high-low) * rand()/(RAND_MAX + 1.0)); 
+	randNum = low + ((high - low) * rand() / (RAND_MAX + 1.0));
 	return randNum;
 }
 
 //--------------------------------------------------
 float ofRandomf() {
 	float randNum = 0;
-	randNum = (rand()/(RAND_MAX + 1.0)) * 2.0 - 1.0; 
+	randNum = (rand() / (RAND_MAX + 1.0)) * 2.0 - 1.0;
 	return randNum;
 }
 
 //--------------------------------------------------
 float ofRandomuf() {
 	float randNum = 0;
-	randNum = rand()/(RAND_MAX + 1.0); 
+	randNum = rand() / (RAND_MAX + 1.0);
 	return randNum;
 }
 
@@ -63,65 +65,68 @@ float ofRandomuf() {
 //from the forums http://www.openframeworks.cc/forum/viewtopic.php?t=1413
 
 //--------------------------------------------------
-float ofNormalize(float value, float min, float max){ 
-   return ofClamp( (value - min) / (max - min), 0, 1);
-} 
+float ofNormalize(float value, float min, float max) {
+	return ofClamp((value - min) / (max - min), 0, 1);
+}
 
 //check for division by zero???
 //--------------------------------------------------
-float ofMap(float value, float inputMin, float inputMax, float outputMin, float outputMax) { 
-   return ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin); 
-} 
+float ofMap(float value, float inputMin, float inputMax, float outputMin, float outputMax) {
+	return ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
+}
 
 //--------------------------------------------------
-float ofDist(float x1, float y1, float x2, float y2) { 
-   return sqrt(double((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))); 
-} 
+float ofDist(float x1, float y1, float x2, float y2) {
+	return sqrt(double((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+}
 
 //--------------------------------------------------
-float ofDistSquared(float x1, float y1, float x2, float y2) { 
-   return ( (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) ); 
-} 
+float ofDistSquared(float x1, float y1, float x2, float y2) {
+	return ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
 
 //--------------------------------------------------
-float ofClamp(float value, float min, float max) { 
-   return value < min ? min : value > max ? max : value; 
-} 
+float ofClamp(float value, float min, float max) {
+	return value < min ? min : value > max ? max : value;
+}
 
-// return sign of the number 
+// return sign of the number
 //--------------------------------------------------
-int ofSign(float n) { 
-  if( n > 0 ) return 1;
-  else if(n < 0) return -1;
-  else return 0;
-} 
-
-//--------------------------------------------------
-bool ofInRange(float t, float min, float max) { 
-   return t>=min && t<=max; 
-} 
+int ofSign(float n) {
+	if (n > 0)
+		return 1;
+	else if (n < 0)
+		return -1;
+	else
+		return 0;
+}
 
 //--------------------------------------------------
-float ofRadToDeg(float radians) { 
-   return radians * RAD_TO_DEG; 
-} 
+bool ofInRange(float t, float min, float max) {
+	return t >= min && t <= max;
+}
 
 //--------------------------------------------------
-float ofDegToRad(float degrees) { 
-    return degrees * DEG_TO_RAD; 
-} 
+float ofRadToDeg(float radians) {
+	return radians * RAD_TO_DEG;
+}
 
 //--------------------------------------------------
-float ofLerp(float start, float stop, float amt) { 
-    return start + (stop-start) * amt; 
-} 
+float ofDegToRad(float degrees) {
+	return degrees * DEG_TO_RAD;
+}
 
 //--------------------------------------------------
-float ofRandomWidth() { 
-   return ofRandom(0, ofGetWidth()); 
-} 
+float ofLerp(float start, float stop, float amt) {
+	return start + (stop - start) * amt;
+}
 
 //--------------------------------------------------
-float ofRandomHeight() { 
-   return ofRandom(0, ofGetHeight()); 
-} 
+float ofRandomWidth() {
+	return ofRandom(0, ofGetWidth());
+}
+
+//--------------------------------------------------
+float ofRandomHeight() {
+	return ofRandom(0, ofGetHeight());
+}
