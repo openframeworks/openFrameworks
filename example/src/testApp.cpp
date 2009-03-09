cf32a0b2dcea1dcce42c1c53a40f3911949b874c
arturo <arturo@0457777e-be4a-0410-a59e-cd2fa5f2afb7>
2009-03-09 20:25:07 +0000

    resized -> windowResized
    git-svn-id: http://svn.openframeworks.cc/addons/ofxXmlSettings/trunk/ofxXmlSettings@125 0457777e-be4a-0410-a59e-cd2fa5f2afb7

--- example/src/testApp.cpp
+++ example/src/testApp.cpp
@@ -253,7 +253,7 @@ void testApp::mouseReleased(int x, int y, int button){
 
 
 //--------------------------------------------------------------
-void testApp::resized(int w, int h){
+void testApp::windowResized(int w, int h){
 }
 
 
