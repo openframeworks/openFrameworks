4e9a12fada200c13267ddef9835f7bcf7806c60c
arturo <arturo@5aee0c02-c622-0410-bcf2-d8652e674aa7>
2009-03-09 13:57:08 +0000

    resized to windowResized
    git-svn-id: http://svn.openframeworks.cc/openFrameworks/trunk/openFrameworks@254 5aee0c02-c622-0410-bcf2-d8652e674aa7

--- app/ofBaseApp.h
+++ app/ofBaseApp.h
@@ -4,37 +4,37 @@
 #include "ofTypes.h"
 
 class ofBaseApp{
-	
+
 	public:
         ofBaseApp() {
             mouseX = mouseY = 0;
         }
-    	
+
 		virtual ~ofBaseApp(){}
-		
+
 		virtual void setup(){}
 		virtual void update(){}
 		virtual void draw(){}
 		virtual void exit(){}
-		
 
-		virtual void resized(int w, int h){}
+
+		virtual void windowResized(int w, int h){}
 
 		virtual void keyPressed( int key ){}
 		virtual void keyReleased( int key ){}
-		
+
 		virtual void mouseMoved( int x, int y ){}
 		virtual void mouseDragged( int x, int y, int button ){}
 		virtual void mousePressed( int x, int y, int button ){}
 		virtual void mouseReleased(){}
 		virtual void mouseReleased(int x, int y, int button ){}
-	
-		virtual void audioReceived( float * input, int bufferSize, int nChannels ){} 
-		virtual void audioRequested( float * output, int bufferSize, int nChannels ){}	
+
+		virtual void audioReceived( float * input, int bufferSize, int nChannels ){}
+		virtual void audioRequested( float * output, int bufferSize, int nChannels ){}
 
 		int mouseX, mouseY;			// for processing heads
 };
 
-#endif	
+#endif
 
 
