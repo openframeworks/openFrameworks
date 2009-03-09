a36ba97fffd54cbc8fe55857246aba6be87bfaaa
arturo <arturo@0457777e-be4a-0410-a59e-cd2fa5f2afb7>
2009-03-09 20:11:55 +0000

    update addons examples to the last version with mouseReleased(x,y,button), resized and window in main
    git-svn-id: http://svn.openframeworks.cc/addons/ofxOsc/trunk/ofxOsc@104 0457777e-be4a-0410-a59e-cd2fa5f2afb7

--- examples/oscSendExample/src/main.cpp
+++ examples/oscSendExample/src/main.cpp
@@ -1,17 +1,16 @@
 #include "ofMain.h"
 #include "testApp.h"
+#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
 
+    ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 640,480, OF_WINDOW);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofSetupOpenGL(640,480, OF_WINDOW );			// <-------- setup the GL context	
-	
-	// this is my "app" :
-	testApp APP;
-	
-	// this kicks off the running of my app
-	ofRunApp(&APP);
-	
+	ofRunApp( new testApp());
+
 }
--- examples/oscSendExample/src/testApp.cpp
+++ examples/oscSendExample/src/testApp.cpp
@@ -1,24 +1,3 @@
-/*
- 
- Copyright 2007, 2008 Damian Stewart damian@frey.co.nz
- Distributed under the terms of the GNU Lesser General Public License v3
- 
- This file is part of the ofxOsc openFrameworks OSC addon.
- 
- ofxOsc is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- ofxOsc is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public License
- along with ofxOsc.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "testApp.h"
 
 //--------------------------------------------------------------
@@ -32,7 +11,7 @@ void testApp::setup(){
 
 //--------------------------------------------------------------
 void testApp::update(){
-	
+
 }
 
 
@@ -45,12 +24,11 @@ void testApp::draw(){
 	ofDrawBitmapString( "move the mouse to send osc message [/mouse/position <x> <y>]", 10, 50 );
 	ofDrawBitmapString( "click to send osc message [/mouse/button <button> <\"up\"|\"down\">]", 10, 65 );
 	ofDrawBitmapString( "press A to send osc message [/test 1 3.5 hello <time>]", 10, 80 );
-	ofDrawBitmapString( "press B to send osc bundle [ [/bundle1 3] [/bundle2 5.5]\n    [ [/bundle3a nested_bundle_test] [/bundle3b test2] ] ]", 10, 95 );
 }
 
 
 //--------------------------------------------------------------
-void testApp::keyPressed  (int key){ 
+void testApp::keyPressed  (int key){
 	if ( key =='a' || key == 'A' )
 	{
 		ofxOscMessage m;
@@ -61,42 +39,6 @@ void testApp::keyPressed  (int key){
 		m.addFloatArg( ofGetElapsedTimef() );
 		sender.sendMessage( m );
 	}
-	else if ( key == 'b' || key == 'B' )
-	{
-		ofxOscBundle b;
-		
-		// make message
-		ofxOscMessage m;
-		m.setAddress( "/bundle1" );
-		m.addIntArg( 3 );
-		// add to bundle
-		b.addMessage( m );
-
-		// and repeat
-		m.clear();
-		m.setAddress( "/bundle2" );
-		m.addFloatArg( 5.5f );
-		// add to bundle
-		b.addMessage( m );
-		
-		// make nested bundle
-		ofxOscBundle nested_b;
-		m.clear();
-		m.setAddress( "/bundle3a" );
-		m.addStringArg( "nested bundle test" );
-		nested_b.addMessage( m );
-		m.clear();
-		m.setAddress( "/bundle3b" );
-		m.addStringArg( "test2" );
-		nested_b.addMessage( m );
-		
-		// add to outer bundle
-		b.addBundle( nested_b );
-
-		// send
-		sender.sendBundle( b );
-	}
-		
 }
 
 //--------------------------------------------------------------
@@ -110,7 +52,7 @@ void testApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-	
+
 }
 
 //--------------------------------------------------------------
@@ -122,9 +64,16 @@ void testApp::mousePressed(int x, int y, int button){
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(){
+void testApp::mouseReleased(int x, int y, int button){
 	ofxOscMessage m;
 	m.setAddress( "/mouse/button" );
 	m.addStringArg( "up" );
-	sender.sendMessage( m );	
+	sender.sendMessage( m );
+
 }
+
+//--------------------------------------------------------------
+void testApp::resized(int w, int h){
+
+}
+
