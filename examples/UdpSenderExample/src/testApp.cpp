d06c87a7480fec173669628582b907f8e00435bd
arturo <arturo@0457777e-be4a-0410-a59e-cd2fa5f2afb7>
2009-03-09 20:24:14 +0000

    resized -> windowResized
    git-svn-id: http://svn.openframeworks.cc/addons/ofxNetwork/trunk/ofxNetwork@122 0457777e-be4a-0410-a59e-cd2fa5f2afb7

--- examples/UdpSenderExample/src/testApp.cpp
+++ examples/UdpSenderExample/src/testApp.cpp
@@ -76,5 +76,5 @@ void testApp::mouseReleased(int x, int y, int button){
 }
 
 //--------------------------------------------------------------
-void testApp::resized(int w, int h){
+void testApp::windowResized(int w, int h){
 }
