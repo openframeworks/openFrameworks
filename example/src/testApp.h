9f290c66c4a5e07155e46c54cf0a4a0ac4420f45
arturo <arturo@0457777e-be4a-0410-a59e-cd2fa5f2afb7>
2009-03-09 20:16:45 +0000

    update addons examples to the last version with mouseReleased(x,y,button), resized and window in main
    git-svn-id: http://svn.openframeworks.cc/addons/ofxVectorMath/trunk/ofxVectorMath@112 0457777e-be4a-0410-a59e-cd2fa5f2afb7

--- example/src/testApp.h
+++ example/src/testApp.h
@@ -2,47 +2,35 @@
 #define _TEST_APP
 
 
+#include "ofxVectorGraphics.h"
+
 #include "ofMain.h"
 
-#define OF_ADDON_USING_OFXVECTORMATH
 
-#include "ofAddons.h"
+class testApp : public ofBaseApp{
 
-#define MAX_N_PTS         1500
-
-class testApp : public ofSimpleApp{
-	
 	public:
-		
+
 		void setup();
 		void update();
 		void draw();
-		
-		void keyPressed  (int key);
-		void keyReleased (int key);
-		
+
+		void keyPressed(int key);
+		void keyReleased(int key);
 		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
-		void mouseReleased();
-		
-		ofxVec3f	pts[MAX_N_PTS];
-		int			nPts;
-		
-		ofxVec3f	rotateAmount;  // amount to rotate in x,y,z;
-		float		speedOfRotation; // speed;
-		
-		// a grid helpful for seeing the rotation
-		ofxVec3f	xAxisMin;
-		ofxVec3f	xAxisMax;
-		ofxVec3f	yAxisMin;
-		ofxVec3f	yAxisMax;
-		ofxVec3f	zAxisMin;
-		ofxVec3f	zAxisMax;
-		
-		bool		bDrawnAnything;
-		
+		void mouseReleased(int x, int y, int button);
+		void resized(int w, int h);
+
+		ofxVectorGraphics output;
+		bool capture;
+		bool bFill;
+
+		vector <ofPoint> pts;
+
+
 };
 
 #endif
-	
+
