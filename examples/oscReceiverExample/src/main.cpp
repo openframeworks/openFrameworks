057d729bfa964a0a4bafa76a2162b813d3fbd649
theo <theo@0457777e-be4a-0410-a59e-cd2fa5f2afb7>
2009-03-25 01:23:54 +0000

    rename osc example
    git-svn-id: http://svn.openframeworks.cc/addons/ofxOsc/trunk/ofxOsc@165 0457777e-be4a-0410-a59e-cd2fa5f2afb7

---
+++ examples/oscReceiverExample/src/main.cpp
@@ -0,0 +1,15 @@
+#include "ofMain.h"
+#include "testApp.h"
+
+//========================================================================
+int main( ){
+
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofSetupOpenGL(640,480, OF_WINDOW);			// <-------- setup the GL context
+	
+	// this kicks off the running of my app
+	ofRunApp(new testApp());
+	
+}
+
--- examples/oscReceiverExample/src/testApp.h
+++ examples/oscReceiverExample/src/testApp.h
@@ -4,7 +4,9 @@
 
 #include "ofMain.h"
 
-#include "ofxOsc.h"
+#define OF_ADDON_USING_OFXOSC
+
+#include "ofAddons.h"
 
 // listen on port 12345
 #define PORT 12345
@@ -12,33 +14,32 @@
 
 
 //--------------------------------------------------------
-class testApp : public ofBaseApp{
-
+class testApp : public ofSimpleApp{
+	
 	public:
-
+		
 		void setup();
 		void update();
 		void draw();
-
+		
 		void keyPressed  (int key);
 		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-
+		void mouseReleased();
+		
 		ofTrueTypeFont		font;
 
-
+		
 private:
 		ofxOscReceiver	receiver;
 
 		int				current_msg_string;
-		string		msg_strings[NUM_MSG_STRINGS];
+		std::string		msg_strings[NUM_MSG_STRINGS];
 		float			timers[NUM_MSG_STRINGS];
-
+		
 		int				mouseX, mouseY;
-		string			mouseButtonState;
+		char			mouseButtonState[128];
 };
 
-#endif
+#endif	
