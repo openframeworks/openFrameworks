9cc3f23759b6f2e19e26a85a5335b7576e4fae62
arturo <arturo@5aee0c02-c622-0410-bcf2-d8652e674aa7>
2009-05-26 18:15:50 +0000

    delete should be delete[] in allocatePixels
    git-svn-id: http://svn.openframeworks.cc/openFrameworks/trunk/openFrameworks@276 5aee0c02-c622-0410-bcf2-d8652e674aa7

--- graphics/ofImage.cpp
+++ graphics/ofImage.cpp
@@ -264,7 +264,7 @@ void ofImage::grabScreen(int _x, int _y, int _w, int _h){
 	#endif
 
 	int sizeOfOneLineOfPixels = myPixels.width * myPixels.bytesPerPixel;
-	unsigned char * tempLineOfPix = new unsigned char[sizeOfOneLineOfPixels]; 
+	unsigned char * tempLineOfPix = new unsigned char[sizeOfOneLineOfPixels];
 	unsigned char * linea;
 	unsigned char * lineb;
 	for (int i = 0; i < myPixels.height/2; i++){
@@ -345,7 +345,7 @@ inline void  ofImage::allocatePixels(ofPixels &pix, int width, int height, int b
 			//ofLog(OF_LOG_NOTICE,"we are good, no reallocation needed");
 			bNeedToAllocate = false;
 		 } else {
-			delete pix.pixels;
+			delete[] pix.pixels;
 			bNeedToAllocate = true;
 		 }
 	} else {
