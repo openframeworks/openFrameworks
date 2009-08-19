0712ba008549ae50403e88824fb44434106c83e4
arturo <arturo@adcd5791-1b6c-4787-a4a6-ffef2f616413>
2009-08-19 15:48:32 +0000

    freetype includes for macosx

---
+++ libs/freetype/include_macosx/freetype2/freetype/internal/internal.h
@@ -0,0 +1,50 @@
+/***************************************************************************/
+/*                                                                         */
+/*  internal.h                                                             */
+/*                                                                         */
+/*    Internal header files (specification only).                          */
+/*                                                                         */
+/*  Copyright 1996-2001, 2002, 2003, 2004 by                               */
+/*  David Turner, Robert Wilhelm, and Werner Lemberg.                      */
+/*                                                                         */
+/*  This file is part of the FreeType project, and may only be used,       */
+/*  modified, and distributed under the terms of the FreeType project      */
+/*  license, LICENSE.TXT.  By continuing to use, modify, or distribute     */
+/*  this file you indicate that you have read the license and              */
+/*  understand and accept it fully.                                        */
+/*                                                                         */
+/***************************************************************************/
+
+
+  /*************************************************************************/
+  /*                                                                       */
+  /* This file is automatically included by `ft2build.h'.                  */
+  /* Do not include it manually!                                           */
+  /*                                                                       */
+  /*************************************************************************/
+
+
+#define FT_INTERNAL_OBJECTS_H             "ftobjs.h"
+#define FT_INTERNAL_STREAM_H              "ftstream.h"
+#define FT_INTERNAL_MEMORY_H              "ftmemory.h"
+#define FT_INTERNAL_DEBUG_H               "ftdebug.h"
+#define FT_INTERNAL_CALC_H                "ftcalc.h"
+#define FT_INTERNAL_DRIVER_H              "ftdriver.h"
+#define FT_INTERNAL_TRACE_H               "fttrace.h"
+#define FT_INTERNAL_GLYPH_LOADER_H        "ftgloadr.h"
+#define FT_INTERNAL_SFNT_H                "sfnt.h"
+#define FT_INTERNAL_SERVICE_H             "ftserv.h"
+#define FT_INTERNAL_RFORK_H               "ftrfork.h"
+#define FT_INTERNAL_VALIDATE_H            "ftvalid.h"
+
+#define FT_INTERNAL_TRUETYPE_TYPES_H      "tttypes.h"
+#define FT_INTERNAL_TYPE1_TYPES_H         "t1types.h"
+
+#define FT_INTERNAL_POSTSCRIPT_AUX_H      "psaux.h"
+#define FT_INTERNAL_POSTSCRIPT_HINTS_H    "pshints.h"
+#define FT_INTERNAL_POSTSCRIPT_GLOBALS_H  "psglobal.h"
+
+#define FT_INTERNAL_AUTOHINT_H            "autohint.h"
+
+
+/* END */
