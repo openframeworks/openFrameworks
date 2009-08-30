52374e23d64c89d970346ea5bdcfd36f45680499
arturo <arturo@0457777e-be4a-0410-a59e-cd2fa5f2afb7>
2009-08-30 15:09:33 +0000

    prama message for vs instead of #warning
    git-svn-id: http://svn.openframeworks.cc/addons/ofxVectorMath/trunk/ofxVectorMath@205 0457777e-be4a-0410-a59e-cd2fa5f2afb7

--- src/ofxPoint2f.h
+++ src/ofxPoint2f.h
@@ -9,7 +9,11 @@
 #ifndef _OFX_POINT2f
 #define _OFX_POINT2f
 
+#if (_MSC_VER)
+#pragma message("warning: ofxPoint2f is deprecated, use ofxVec2f.h for both points and vectors")
+#else
 #warning "ofxPoint2f is deprecated, use ofxVec2f.h for both points and vectors"
+#endif
 
 
 #include "ofxVec2f.h"
--- src/ofxPoint4f.h
+++ src/ofxPoint4f.h
@@ -9,8 +9,11 @@
 #ifndef _OFX_POINT4f
 #define _OFX_POINT4f
 
+#if (_MSC_VER)
+#pragma message("warning: ofxPoint4f is deprecated, use ofxVec4f.h for both points and vectors")
+#else
 #warning "ofxPoint4f is deprecated, use ofxVec4f.h for both points and vectors"
-
+#endif
 
 #include "ofxVec4f.h"
 
