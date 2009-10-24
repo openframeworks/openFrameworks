defec06c5b21ce0590efba3fcb383f6f9db9900b
ofTheo <theo@openframeworks.cc>
2009-10-24 15:40:36 +0200

    ofxOsc fixes from damian. Closes #9

--- addons/ofxOsc/src/ofxOscReceiver.cpp
+++ addons/ofxOsc/src/ofxOscReceiver.cpp
@@ -1,22 +1,29 @@
 /*
-
- Copyright 2007, 2008 Damian Stewart damian@frey.co.nz
- Distributed under the terms of the GNU Lesser General Public License v3
-
- This file is part of the ofxOsc openFrameworks OSC addon.
-
- ofxOsc is free software: you can redistribute it and/or modify
- it under the terms of the GNU Lesser General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- ofxOsc is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public License
- along with ofxOsc.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ Copyright (c) 2007-2009, Damian Stewart
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the developer nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY DAMIAN STEWART ''AS IS'' AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL DAMIAN STEWART BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "ofxOscReceiver.h"
@@ -29,62 +36,88 @@
 
 ofxOscReceiver::ofxOscReceiver()
 {
-listen_socket = NULL;
-#ifdef TARGET_WIN32
-	mutex = CreateMutexA( NULL, FALSE, NULL );
-#else
-	pthread_mutex_init( &mutex, NULL );
-#endif
+	listen_socket = NULL;
 }
 
 void ofxOscReceiver::setup( int listen_port )
 {
-	if(listen_socket) delete listen_socket;
+	// if we're already running, shutdown before running again
+	if ( listen_socket )
+		shutdown();
+	
+	// create the mutex
+	#ifdef TARGET_WIN32
+	mutex = CreateMutexA( NULL, FALSE, NULL );
+	#else
+	pthread_mutex_init( &mutex, NULL );
+	#endif
+	
+	// create socket
+	socketHasShutdown = false;
 	listen_socket = new UdpListeningReceiveSocket( IpEndpointName( IpEndpointName::ANY_ADDRESS, listen_port ), this );
-#ifdef TARGET_WIN32
+
+	// start thread
+	#ifdef TARGET_WIN32
 	thread	= CreateThread(
 							   NULL,              // default security attributes
 							   0,                 // use default stack size
 							&ofxOscReceiver::startThread,        // thread function
-							   (void*)(listen_socket),             // argument to thread function
+							   (void*)this,             // argument to thread function
 							   0,                 // use default creation flags
 							   NULL);             // we don't the the thread id
 
-#else
-	pthread_create( &thread, NULL, &ofxOscReceiver::startThread, (void*)(listen_socket) );
+	#else
+	pthread_create( &thread, NULL, &ofxOscReceiver::startThread, (void*)this );
+	#endif
+}
 
-
-#endif
+void ofxOscReceiver::shutdown()
+{
+	if ( listen_socket )
+	{
+		// tell the socket to shutdown
+		listen_socket->AsynchronousBreak();
+		// wait for shutdown to complete
+		while (!socketHasShutdown)
+			// sleep 0.1ms
+			usleep(100);
+		
+		// thread will clean up itself
+		
+		// clean up the mutex
+		#ifdef TARGET_WIN32
+		ReleaseMutex( mutex );
+		#else
+		pthread_mutex_destroy( &mutex );	
+		#endif
+		
+		// delete the socket
+		delete listen_socket;
+		listen_socket = NULL;
+	}
 }
 
 ofxOscReceiver::~ofxOscReceiver()
 {
-//	delete listen_socket;
+	shutdown();
 }
-
-
-/*
-#if defined TARGET_OSX
-#error target-osx
-#elif defined TARGET_WIN32
-#error target-win32
-#else
-#error no target
-#endif
- */
 
 #ifdef TARGET_WIN32
 DWORD WINAPI
 #else
 void*
 #endif
-
-		ofxOscReceiver::startThread( void* _socket )
+		ofxOscReceiver::startThread( void* receiverInstance )
 {
-	UdpListeningReceiveSocket* socket = (UdpListeningReceiveSocket*)_socket;
-	socket->Run();
+	// cast the instance
+	ofxOscReceiver* instance = (ofxOscReceiver*)receiverInstance;
+	// start the socket listener
+	instance->listen_socket->Run();
+	// socket listener has finished - remember this fact
+	instance->socketHasShutdown = true;
+	// return
     #ifdef TARGET_WIN32
-    	return 0;
+	return 0;
     #else
 	return NULL;
     #endif
