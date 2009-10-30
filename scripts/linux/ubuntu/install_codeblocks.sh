19471aea90416418cc786c208b84f00a7287e75e
reactable <reactable@reactable-desktop.(none)>
2009-10-30 23:07:44 +0100

    changed permissions for linux scripts to 775 changed to jaunty the wx-widgets repository in install_codeblocks

--- scripts/linux/ubuntu/install_codeblocks.sh
+++ scripts/linux/ubuntu/install_codeblocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DISTRIB_CODENAME=`cat /etc/lsb-release | grep DISTRIB_CODENAME | sed 's/DISTRIB_CODENAME\=\(\.*\)/\1/g'`
-WX_DEB="deb http://apt.wxwidgets.org/ hardy-wx main"
+WX_DEB="deb http://apt.wxwidgets.org/ jaunty-wx main"
 CB_DEB="deb http://lgp203.free.fr/ubuntu/ $DISTRIB_CODENAME universe"
 
 echo $WX_DEB > /etc/apt/sources.list.d/wx.list
