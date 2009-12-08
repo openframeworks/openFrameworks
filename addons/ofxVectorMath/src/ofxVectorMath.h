c50400ab34c59aebfcba9b61b48367abcf0362f3
Kyle McDonald <kyle@kylemcdonald.net>
2009-12-08 18:20:59 -0500

    Added Aaron Meyers' Quaternion and Matrix4x4 implementations with C::B autoformat applied. Original code available from http://aaron-meyers.com/osg-ofx.zip

--- addons/ofxVectorMath/src/ofxVectorMath.h
+++ addons/ofxVectorMath/src/ofxVectorMath.h
@@ -6,6 +6,8 @@
 #include "ofxVec3f.h"
 #include "ofxVec4f.h"
 #include "ofxMatrix3x3.h"
+#include "ofxMatrix4x4.h"
+#include "ofxQuat.h"
 
 typedef ofxVec2f  ofxPoint2f;
 typedef ofxVec3f  ofxPoint3f;
