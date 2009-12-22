3fe5742e92a3427ce80593e704068383f0efacfd
Zachary Lieberman <zach@zachary-liebermans-imac-2.local>
2009-12-22 11:17:36 -0500

    made some small mods to license info and getPtr

--- addons/ofxVectorMath/src/ofxMatrix4x4.cpp
+++ addons/ofxVectorMath/src/ofxMatrix4x4.cpp
@@ -1,11 +1,3 @@
-/*
- *  ofxMatrix4x4.cpp
- *  splineTest
- *
- *  Created by Aaron Meyers on 6/22/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
 
 #include "ofxMatrix4x4.h"
 #include <limits>
