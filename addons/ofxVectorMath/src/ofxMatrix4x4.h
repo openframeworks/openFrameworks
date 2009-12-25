3b261ea3c7755031171c9a28020feba133e70efa
Zach Lieberman <zach@openframeworks.cc>
2009-12-25 09:29:35 -0500

    changes for VS 2008 happiness again

--- addons/ofxVectorMath/src/ofxMatrix4x4.h
+++ addons/ofxVectorMath/src/ofxMatrix4x4.h
@@ -19,8 +19,10 @@
 #include <cmath>
 
 #if (_MSC_VER)       
-		// make microsoft visual studio complain less about double / float conversion.
+		// make microsoft visual studio complain less about double / float conversion and
+		// truncation
 		#pragma warning(disable : 4244)
+		#pragma warning(disable : 4305)
 #endif
 
 
