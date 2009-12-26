4ed073ef0bded3d306b3e03e9ed9d2c44961407a
ofTheo <theo@openframeworks.cc>
2009-12-26 19:15:05 +0100

    All xcode projects build apps with the project name

--- scripts/osx/buildAndRunAllAddonsExamples.sh
+++ scripts/osx/buildAndRunAllAddonsExamples.sh
@@ -6,9 +6,9 @@ do
 echo "-----------------------------------------------------------------"
 echo "building " $example
 cd $example/
-xcodebuild -configuration Debug -target "openFrameworks" -project "$example.xcodeproj"
+xcodebuild -configuration Debug -target "$example" -project "$example.xcodeproj"
 echo "example " $example " built - running example"
-./bin/openFrameworksDebug.app/Contents/MacOS/openFrameworksDebug
+./bin/${example}Debug.app/Contents/MacOS/${example}Debug
 cd ../
 echo "-----------------------------------------------------------------"
 echo ""
