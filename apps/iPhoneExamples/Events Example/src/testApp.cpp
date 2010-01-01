70dcd07c732a6225d13f134391963212c71404a0
jonbroFERrealz <jonbro5556@jonathan-brodskys-macbook-pro-44.local>
2010-01-01 18:24:40 -0500

    added the poco callbacks on draw update setup and exit

--- apps/iPhoneExamples/Events Example/src/testApp.cpp
+++ apps/iPhoneExamples/Events Example/src/testApp.cpp
@@ -28,7 +28,6 @@ void testApp::setup(){
 
 //--------------------------------------------------------------
 void testApp::update() {
-	ofNotifyEvent( ofEvents.update, voidEventArgs);
 }
 
 //--------------------------------------------------------------
