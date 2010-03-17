53a9cd300f0c9fe8e09a88b0751476a9359be079
ofTheo <theo@openframeworks.cc>
2010-03-17 14:30:53 -0400

    Revert "Small fixes to iPhone bitmap type."
    This reverts commit bfd3f87901ef4878d3e4750e8c42732d86b018f8.

---
+++ apps/iPhoneExamples/audioInputExample/src/testApp.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiPhone.h"
+
+class testApp : public ofxiPhoneApp{
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void touchDown(ofTouchEventArgs &touch);
+	void touchMoved(ofTouchEventArgs &touch);
+	void touchUp(ofTouchEventArgs &touch);
+	void touchDoubleTap(ofTouchEventArgs &touch);
+
+	void lostFocus();
+	void gotFocus();
+	void gotMemoryWarning();
+	void deviceOrientationChanged(int newOrientation);
+	
+	void audioReceived( float * input, int bufferSize, int nChannels );
+
+	int		initialBufferSize;
+	int		sampleRate;
+	int		drawCounter, bufferCounter;
+	float 	* buffer;
+	
+};
+
---
+++ apps/iPhoneExamples/polygonExample/src/testApp.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiPhone.h"
+
+typedef struct {
+
+	float 	x;
+	float 	y;
+	bool 	bBeingDragged;
+	bool 	bOver;
+	float 	radius;
+	
+}	draggableVertex;
+
+
+class testApp : public ofxiPhoneApp {
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void touchDown(ofTouchEventArgs &touch);
+	void touchMoved(ofTouchEventArgs &touch);
+	void touchUp(ofTouchEventArgs &touch);
+	void touchDoubleTap(ofTouchEventArgs &touch);
+
+	void lostFocus();
+	void gotFocus();
+	void gotMemoryWarning();
+	void deviceOrientationChanged(int newOrientation);
+
+	int nCurveVertexes;
+	draggableVertex curveVertices[7];
+	draggableVertex bezierVertices[4];
+
+	float appIphoneScale;
+
+};
+
+
