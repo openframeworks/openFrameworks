29a2780c89aa708af4f495236fa3f3512bef432a
arturo <arturo@openframeworks.cc>
2010-04-27 21:57:37 +0200

    bezier & curve interpolation. Closes #108

--- libs/openFrameworks/utils/ofPolyUtils.h
+++ libs/openFrameworks/utils/ofPolyUtils.h
@@ -10,4 +10,9 @@ bool ofInsidePoly(const ofPoint & p, const vector<ofPoint> & poly);
 bool ofLineSegmentCrosses(ofPoint line1Start, ofPoint line1End, ofPoint line2Start, ofPoint line2End);
 ofPoint ofLineSegmentIntersection(ofPoint line1Start, ofPoint line1End, ofPoint line2Start, ofPoint line2End);
 
+ofPoint ofBezierPoint( ofPoint a, ofPoint b, ofPoint c, ofPoint d, float t);
+ofPoint ofCurvePoint( ofPoint a, ofPoint b, ofPoint c, ofPoint d, float t);
+ofPoint ofBezierTangent( ofPoint a, ofPoint b, ofPoint c, ofPoint d, float t);
+ofPoint ofCurveTangent( ofPoint a, ofPoint b, ofPoint c, ofPoint d, float t);
+
 #endif /* OFPOLYUTILS_H_ */
