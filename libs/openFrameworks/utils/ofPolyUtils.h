cd2841721c0884d263f7e26b202acda93bb5c4fc
arturo <arturo@openframeworks.cc>
2010-04-27 21:53:04 +0200

    ofLineSegmentCrosses & ofLineSegmentIntersection. Closes #106

--- libs/openFrameworks/utils/ofPolyUtils.h
+++ libs/openFrameworks/utils/ofPolyUtils.h
@@ -7,4 +7,7 @@
 bool ofInsidePoly(float x, float y, const vector<ofPoint> & poly);
 bool ofInsidePoly(const ofPoint & p, const vector<ofPoint> & poly);
 
+bool ofLineSegmentCrosses(ofPoint line1Start, ofPoint line1End, ofPoint line2Start, ofPoint line2End);
+ofPoint ofLineSegmentIntersection(ofPoint line1Start, ofPoint line1End, ofPoint line2Start, ofPoint line2End);
+
 #endif /* OFPOLYUTILS_H_ */
