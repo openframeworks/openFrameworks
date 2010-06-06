d92eab6e285c73ce3d594b6ed65f88fadfda3aa0
Mehmet Akten <memo@Memos-MacBook-Pro.local>
2010-06-06 14:11:53 +0100

    added emptyUniversalExample to iPhoneExamples

---
+++ apps/iPhoneExamples/emptyUniversalExample/src/testApp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiPhone.h"
+#include "ofxiPhoneExtras.h"
+
+class testApp : public ofxiPhoneApp {
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void touchDown(ofTouchEventArgs &touch);
+	void touchMoved(ofTouchEventArgs &touch);
+	void touchUp(ofTouchEventArgs &touch);
+	void touchDoubleTap(ofTouchEventArgs &touch);
+
+	void lostFocus();
+	void gotFocus();
+	void gotMemoryWarning();
+	void deviceOrientationChanged(int newOrientation);
+
+};
+
+
