4f6c91c221a51e4d9b0f494e7c39fd73ea7c42ed
arturo <arturo@openframeworks.cc>
2010-07-11 16:42:27 +0200

    ofxAndroid: - added soundstream and videograbber - manages pause/resume application life cycle

--- addons/ofxAndroid/src/ofAppAndroidWindow.cpp
+++ addons/ofxAndroid/src/ofAppAndroidWindow.cpp
@@ -10,11 +10,13 @@
 #include <jni.h>
 #include "ofGraphics.h"
 #include "ofAppRunner.h"
+#include "ofUtils.h"
+#include "ofxAndroidApp.h"
+#include "ofxAndroidUtils.h"
 #include <android/log.h>
 
-#define  LOG_TAG    "libgl2jni"
-#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+static bool paused=true;
+
 
 static int  sWindowWidth  = 320;
 static int  sWindowHeight = 480;
@@ -36,9 +38,35 @@ float 			frameRate;
 
 double			lastFrameTime;
 
+static JavaVM *ofJavaVM=0;
+
 
 static ofBaseApp * OFApp = NULL;
+static ofxAndroidApp * androidApp = NULL;
 //static ofAppAndroidWindow window;
+
+JavaVM * ofGetJavaVMPtr(){
+	return ofJavaVM;
+}
+
+JNIEnv * ofGetJNIEnv(){
+	JNIEnv *env;
+	JavaVM * vm = ofGetJavaVMPtr();
+	if(!vm){
+		ofLog(OF_LOG_ERROR,"couldn't get java vm");
+		return NULL;
+	}
+	if (vm->GetEnv((void**) &env, JNI_VERSION_1_4) != JNI_OK) {
+		ofLog(OF_LOG_ERROR,"Failed to get the environment using GetEnv()");
+		return NULL;
+	}
+	return env;
+}
+
+void ofRunApp( ofxAndroidApp * app){
+	androidApp = app;
+	ofRunApp((ofBaseApp*)app);
+}
 
 ofAppAndroidWindow::ofAppAndroidWindow() {
 	// TODO Auto-generated constructor stub
@@ -51,7 +79,7 @@ ofAppAndroidWindow::~ofAppAndroidWindow() {
 
 void ofAppAndroidWindow::runAppViaInfiniteLoop(ofBaseApp * appPtr){
 	OFApp = appPtr;
-	LOGI("runAppViaInfiniteLoop");
+	ofLog(OF_LOG_NOTICE,"runAppViaInfiniteLoop");
 }
 
 ofPoint	ofAppAndroidWindow::getWindowSize(){
@@ -79,10 +107,29 @@ void ofAppAndroidWindow::disableSetupScreen(){
 	bSetupScreen = false;
 }
 
+void reloadTextures(){
+	ofUpdateBitmapCharacterTexture();
+	ofReloadAllImageTextures();
+	//ofReloadAllFontTextures();
+	ofResumeVideoGrabbers();
+}
+
 extern "C"{
 
+jint JNI_OnLoad(JavaVM* vm, void* reserved)
+{
+	JNIEnv *env;
+	ofJavaVM = vm;
+	ofLog(OF_LOG_NOTICE,"JNI_OnLoad called");
+	if (vm->GetEnv((void**) &env, JNI_VERSION_1_4) != JNI_OK) {
+		ofLog(OF_LOG_ERROR,"Failed to get the environment using GetEnv()");
+		return -1;
+	}
+	return JNI_VERSION_1_4;
+}
+
 void
-Java_cc_openframeworks_OFAndroidWindow_setAppName( JNIEnv*  env, jobject  thiz, jstring app_name )
+Java_cc_openframeworks_OFAndroid_setAppName( JNIEnv*  env, jobject  thiz, jstring app_name )
 {
 	jboolean iscopy;
 	const char *mfile = env->GetStringUTFChars(app_name, &iscopy);
@@ -91,34 +138,89 @@ Java_cc_openframeworks_OFAndroidWindow_setAppName( JNIEnv*  env, jobject  thiz, 
 }
 
 void
-Java_cc_openframeworks_OFAndroidWindow_setup( JNIEnv*  env, jobject  thiz )
-{
+Java_cc_openframeworks_OFAndroid_onRestart( JNIEnv*  env, jobject  thiz ){
 
+}
+
+void
+Java_cc_openframeworks_OFAndroid_onPause( JNIEnv*  env, jobject  thiz ){
+	paused = true;
+
+	if(androidApp) androidApp->pause();
+	//ofUnloadAllFontTextures();
+	//ofPauseVideoGrabbers();
+}
+
+void
+Java_cc_openframeworks_OFAndroid_onResume( JNIEnv*  env, jobject  thiz ){
+
+	//reloadTextures();
+	if(androidApp){
+		androidApp->resume();
+		//androidApp->reloadTextures();
+	}
+	paused = false;
+}
+
+void
+Java_cc_openframeworks_OFAndroid_onStop( JNIEnv*  env, jobject  thiz ){
+
+}
+
+
+void
+Java_cc_openframeworks_OFAndroid_onDestroy( JNIEnv*  env, jobject  thiz ){
+
+}
+
+void
+Java_cc_openframeworks_OFAndroid_onSurfaceDestroyed( JNIEnv*  env, jobject  thiz ){
+	paused = true;
+	ofLog(OF_LOG_NOTICE,"onSurfaceDestroyed");
+	ofUnloadAllFontTextures();
+	ofPauseVideoGrabbers();
+}
+
+void
+Java_cc_openframeworks_OFAndroid_onSurfaceCreated( JNIEnv*  env, jobject  thiz ){
+	ofLog(OF_LOG_NOTICE,"onSurfaceCreated");
+	reloadTextures();
+	if(androidApp){
+		androidApp->resume();
+		androidApp->reloadTextures();
+	}
+	paused = false;
+}
+
+void
+Java_cc_openframeworks_OFAndroid_setup( JNIEnv*  env, jobject  thiz )
+{
     //initAndroidOF();
-    LOGI("setup");
+	ofLog(OF_LOG_NOTICE,"setup");
     if(OFApp) OFApp->setup();
 }
 
 void
-Java_cc_openframeworks_OFAndroidWindow_resize( JNIEnv*  env, jobject  thiz, jint w, jint h )
+Java_cc_openframeworks_OFAndroid_resize( JNIEnv*  env, jobject  thiz, jint w, jint h )
 {
     sWindowWidth  = w;
     sWindowHeight = h;
-    LOGI("resize");
+    ofLog(OF_LOG_NOTICE,"resize");
     if(OFApp)OFApp->windowResized(w,h);
 }
 
 /* Call to finalize the graphics state */
 void
-Java_cc_openframeworks_OFAndroidWindow_exit( JNIEnv*  env )
+Java_cc_openframeworks_OFAndroid_exit( JNIEnv*  env )
 {
    if(OFApp) OFApp->exit();
 }
 
 /* Call to render the next GL frame */
 void
-Java_cc_openframeworks_OFAndroidWindow_render( JNIEnv*  env )
+Java_cc_openframeworks_OFAndroid_render( JNIEnv*  env )
 {
+	if(paused) return;
 	//LOGI("update");
 	if(OFApp) OFApp->update();
 
@@ -159,9 +261,10 @@ Java_cc_openframeworks_OFAndroidWindow_render( JNIEnv*  env )
 }
 
 void
-Java_cc_openframeworks_OFGLSurfaceView_onTouchDown(JNIEnv*  env, jobject  thiz, int id,float x,float y,float pressure){
+Java_cc_openframeworks_OFAndroid_onTouchDown(JNIEnv*  env, jobject  thiz, jint id,jfloat x,jfloat y,jfloat pressure){
 	if(OFApp)OFApp->mousePressed(x,y,0);
 	ofTouchEventArgs touch;
+	touch.id = id;
 	touch.x = x;
 	touch.y = y;
 	touch.pressure = pressure;
@@ -169,9 +272,10 @@ Java_cc_openframeworks_OFGLSurfaceView_onTouchDown(JNIEnv*  env, jobject  thiz, 
 }
 
 void
-Java_cc_openframeworks_OFGLSurfaceView_onTouchUp(JNIEnv*  env, jobject  thiz, int id,float x,float y,float pressure){
+Java_cc_openframeworks_OFAndroid_onTouchUp(JNIEnv*  env, jobject  thiz, jint id,jfloat x,jfloat y,jfloat pressure){
 	if(OFApp)OFApp->mouseReleased(x,y,0);
 	ofTouchEventArgs touch;
+	touch.id = id;
 	touch.x = x;
 	touch.y = y;
 	touch.pressure = pressure;
@@ -179,12 +283,15 @@ Java_cc_openframeworks_OFGLSurfaceView_onTouchUp(JNIEnv*  env, jobject  thiz, in
 }
 
 void
-Java_cc_openframeworks_OFGLSurfaceView_onTouchMoved(JNIEnv*  env, jobject  thiz, int id,float x,float y,float pressure){
+Java_cc_openframeworks_OFAndroid_onTouchMoved(JNIEnv*  env, jobject  thiz, jint id,jfloat x,jfloat y,jfloat pressure){
 	if(OFApp)OFApp->mouseMoved(x,y);
 	ofTouchEventArgs touch;
+	touch.id = id;
 	touch.x = x;
 	touch.y = y;
 	touch.pressure = pressure;
 	ofNotifyEvent(ofEvents.touchMoved,touch);
 }
 }
+
+
---
+++ addons/ofxAndroid/src/ofxAndroidSoundStream.cpp
@@ -0,0 +1,135 @@
+/*
+ * ofxAndroidSoundStream.cpp
+ *
+ *  Created on: 04/07/2010
+ *      Author: arturo
+ */
+
+#include <jni.h>
+#include "ofBaseApp.h"
+#include "ofSoundStream.h"
+#include "ofUtils.h"
+#include "ofxAndroidUtils.h"
+
+static short * out_buffer, * in_buffer;
+static float * out_float_buffer=NULL, * in_float_buffer=NULL;
+static int outBufferSize=0, outChannels=0, inBufferSize=0, inChannels=0;
+static ofBaseApp * OFApp;
+
+
+
+void ofSoundStreamSetup(int nOutputChannels, int nInputChannels, ofBaseApp * OFSA, int sampleRate, int bufferSize, int nBuffers){
+	OFApp = OFSA;
+	if(!ofGetJavaVMPtr()){
+		ofLog(OF_LOG_ERROR,"ofSoundStreamSetup: Cannot find java virtual machine");
+		return;
+	}
+	JNIEnv *env;
+	if (ofGetJavaVMPtr()->GetEnv((void**) &env, JNI_VERSION_1_4) != JNI_OK) {
+		ofLog(OF_LOG_ERROR,"Failed to get the environment using GetEnv()");
+		return;
+	}
+	jclass javaClass = env->FindClass("cc.openframeworks.OFAndroidSoundStream");
+
+	if(javaClass==0){
+		ofLog(OF_LOG_ERROR,"cannot find OFAndroidSoundStream java class");
+		return;
+	}
+
+	jmethodID soundStreamSingleton = env->GetStaticMethodID(javaClass,"getInstance","()Lcc/openframeworks/OFAndroidSoundStream;");
+	if(!soundStreamSingleton){
+		ofLog(OF_LOG_ERROR,"cannot find OFAndroidSoundStream singleton method");
+		return;
+	}
+	jobject javaObject = env->CallStaticObjectMethod(javaClass,soundStreamSingleton);
+	jmethodID javaSetup = env->GetMethodID(javaClass,"setup","(IIIII)V");
+	if(javaObject && javaSetup)
+		env->CallVoidMethod(javaObject,javaSetup,nOutputChannels,nInputChannels,sampleRate,bufferSize,nBuffers);
+	else
+		ofLog(OF_LOG_ERROR, "cannot get OFAndroidSoundStream instance or setup method");
+}
+
+void ofSoundStreamSetup(int nOutputChannels, int nInputChannels, ofBaseApp * OFSA){
+	ofSoundStreamSetup(nOutputChannels,nInputChannels,OFSA,44100,256,2);
+}
+
+void ofSoundStreamSetup(int nOutputChannels, int nInputChannels, int sampleRate, int bufferSize, int nBuffers){
+	ofSoundStreamSetup(nOutputChannels,nInputChannels,NULL,sampleRate,bufferSize,nBuffers);
+}
+
+void ofSoundStreamStop(){
+
+}
+
+void ofSoundStreamStart(){
+
+}
+
+void ofSoundStreamClose(){
+
+}
+
+void ofSoundStreamListDevices(){
+
+}
+
+extern "C"{
+
+/*
+void
+Java_cc_openframeworks_OFAndroidSoundStream_initAudioOutput(JNIEnv*  env, jobject  thiz, jobject javaSoundStream){
+	jclass javaClass = env->FindClass("cc.openframeworks.OFAndroidSoundStream");
+	if(javaClass!=0){
+		javaObject = javaSoundStream;
+		javaSetup = env->GetMethodID(javaClass,"setup","(IIIII)V");
+	}
+	javaEnv = env;
+}*/
+
+jint
+Java_cc_openframeworks_OFAndroidSoundStream_audioRequested(JNIEnv*  env, jobject  thiz, jshortArray array, jint numChannels, jint bufferSize){
+	if(!out_float_buffer || numChannels!=outChannels || bufferSize!=outBufferSize){
+		if(out_float_buffer) delete[] out_float_buffer;
+		out_float_buffer = new float[bufferSize*numChannels];
+		outBufferSize = bufferSize;
+		outChannels   = numChannels;
+	}
+	if(OFApp){
+		out_buffer = (short*)env->GetPrimitiveArrayCritical(array, NULL);
+		if(!out_buffer) return 1;
+		OFApp->audioRequested(out_float_buffer,bufferSize,numChannels);
+
+		for(int i=0;i<bufferSize*numChannels;i++){
+			short tempf = (out_float_buffer[i] * 32767.5f) - 0.5;
+			out_buffer[i]=tempf;//lrintf( tempf - 0.5 );
+		}
+
+		env->ReleasePrimitiveArrayCritical(array,out_buffer,0);
+		return 0;
+	}
+	return 1;
+}
+
+static float conv_factor = 1/32767.5f;
+
+jint
+Java_cc_openframeworks_OFAndroidSoundStream_audioReceived(JNIEnv*  env, jobject  thiz, jshortArray array, jint numChannels, jint bufferSize){
+	if(!in_float_buffer || numChannels!=inChannels || bufferSize!=inBufferSize){
+		if(in_float_buffer) delete[] in_float_buffer;
+		in_float_buffer = new float[bufferSize*numChannels];
+		inBufferSize = bufferSize;
+		inChannels   = numChannels;
+	}
+	if(OFApp){
+		in_buffer = (short*)env->GetPrimitiveArrayCritical(array, NULL);
+		if(!in_buffer) return 1;
+		for(int i=0;i<bufferSize*numChannels;i++){
+			in_float_buffer[i] = (float(in_buffer[i]) + 0.5) * conv_factor;
+		}
+		OFApp->audioReceived(in_float_buffer,bufferSize,numChannels);
+		env->ReleasePrimitiveArrayCritical(array,in_buffer,0);
+		return 0;
+	}
+	return 1;
+}
+}
