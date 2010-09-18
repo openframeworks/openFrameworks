07a4bf097e54f021a7c74b0e861742d5f3094075
arturo <arturo@openframeworks.cc>
2010-09-18 14:10:19 +0200

    ubuntu maverick has codeblocks 10.05, no need to use nightly builds any more

--- scripts/linux/ubuntu/install_codeblocks.sh
+++ scripts/linux/ubuntu/install_codeblocks.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 DISTRIB_CODENAME=`cat /etc/lsb-release | grep DISTRIB_CODENAME | sed 's/DISTRIB_CODENAME\=\(\.*\)/\1/g'`
-WX_DEB=""
-ARCH=$(uname -m)
-
-wget "http://openframeworks.cc/wx-dist.php?dist=${DISTRIB_CODENAME}&arch=${ARCH}" -q -O /tmp/oFtemp
-ret=$?
 
 
 CB_DEB="deb http://lgp203.free.fr/ubuntu/ $DISTRIB_CODENAME universe"
-if [ $ret -eq 0 ]; then
-    WX_DEB=$(cat /tmp/oFtemp)
+if [ $DISTRIB_CODENAME = "maverick" ]; then
+    CB_DEB=""
+elif [ $DISTRIB_CODENAME = "karmic" ]; then
+    CB_DEB="deb http://ppa.launchpad.net/ubuntu-backports-testers/ppa/ubuntu lucid main"
+elif [ $DISTRIB_CODENAME = "lucid" ] || [ $DISTRIB_CODENAME = "jaunty" ]; then
+    CB_DEB="ppa:ubuntu-backports-testers/ppa"
 fi
 
-echo $WX_DEB
-echo $WX_DEB > /etc/apt/sources.list.d/wx.list
-echo $CB_DEB > /etc/apt/sources.list.d/cb-nightly.list
+if [ ! $CB_DEB = "" ]; then
+    add-apt-repository $CB_DEB
+fi
 
-wget -q http://apt.wxwidgets.org/key.asc -O- | apt-key add -
-wget -q http://lgp203.free.fr/public.key -O- | apt-key add -
+#wget -q http://lgp203.free.fr/public.key -O- | apt-key add -
 
 apt-get update
 
 apt-get install libcodeblocks0 codeblocks libwxsmithlib0 codeblocks-contrib libwxgtk2.8-0
+
+if [ ! $CB_DEB = "" ]; then
+    add-apt-repository -r $CB_DEB
+fi
 
 ## fix pkg-config by installing missing dependencies
 
