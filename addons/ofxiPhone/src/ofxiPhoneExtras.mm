f9344cd6f2d5a25eeaedcd2766ccd59c3dfd4af0
ofTheo <theo@openframeworks.cc>
2010-09-20 12:56:21 +0100

    poco iphone libs renamed from armv6 to iphone. setup screen aware of orientation. Closes #219. Fixed get device rev mem leak. Closes #220

--- addons/ofxiPhone/src/ofxiPhoneExtras.mm
+++ addons/ofxiPhone/src/ofxiPhoneExtras.mm
@@ -42,13 +42,20 @@ ofxiPhoneDeviceType ofxiPhoneGetDeviceType() {
 string ofxiPhoneGetDeviceRevision() {
 	size_t size;
     sysctlbyname("hw.machine", NULL, &size, NULL, 0);
-    char * machine;
+	
+	if( size > 0 ){
+    	char * machine;
 	machine = new char[size];
 	sysctlbyname("hw.machine", machine, &size, NULL, 0);
 	
 	string device(machine);
 	
+		delete[] machine;
+			
 	return device;
+	}else{
+		return "";
+	}
 }
 
 //--------------------------------------------------------------
@@ -249,6 +256,61 @@ bool ofxiPhoneUIImageToOFImage(UIImage *uiImage, ofImage &outImage, int targetWi
 			
 	ofLog(OF_LOG_VERBOSE, "about to setFromPixels");
 	outImage.setFromPixels(pixels, width, height, ofImageMode, true);
+
+	free(pixels);
+	
+	return true;
+}
+
+//--------------------------------------------------------------
+bool ofxiPhoneUIImageToOFTexture(UIImage *uiImage, ofTexture &outTexture, int targetWidth, int targetHeight) {
+	if(!uiImage) return false;
+	
+	CGContextRef spriteContext;
+	CGImageRef	cgImage = uiImage.CGImage;
+
+	int bytesPerPixel	= CGImageGetBitsPerPixel(cgImage)/8;
+	if(bytesPerPixel == 3) bytesPerPixel = 4;
+	
+	int width			= targetWidth > 0 ? targetWidth : CGImageGetWidth(cgImage);
+	int height			= targetHeight > 0 ? targetHeight : CGImageGetHeight(cgImage);
+	
+	// Allocated memory needed for the bitmap context
+	GLubyte *pixels		= (GLubyte *) malloc(width * height * bytesPerPixel);
+	
+	// Uses the bitmatp creation function provided by the Core Graphics framework. 
+	spriteContext = CGBitmapContextCreate(pixels, width, height, CGImageGetBitsPerComponent(cgImage), width * bytesPerPixel, CGImageGetColorSpace(cgImage), bytesPerPixel == 4 ? kCGImageAlphaPremultipliedLast : kCGImageAlphaNone);
+	
+	if(spriteContext == NULL) {
+		ofLog(OF_LOG_ERROR, "iPhoneUIImageToOFImage - CGBitmapContextCreate returned NULL");
+		free(pixels);
+		return false;
+	}
+
+	// After you create the context, you can draw the sprite image to the context.
+	ofLog(OF_LOG_VERBOSE, "about to CGContextDrawImage");
+	CGContextDrawImage(spriteContext, CGRectMake(0.0, 0.0, (CGFloat)width, (CGFloat)height), cgImage);
+	
+	// You don't need the context at this point, so you need to release it to avoid memory leaks.
+	ofLog(OF_LOG_VERBOSE, "about to CGContextRelease");
+	CGContextRelease(spriteContext);
+		
+	int glMode;
+	
+	switch(bytesPerPixel) {
+		case 1:
+			glMode = GL_LUMINANCE;
+			break;
+		case 3: 
+			glMode = GL_RGB;
+			break;
+		case 4: 
+		default:
+			glMode = GL_RGBA; break;
+	}
+			
+	outTexture.allocate(width, height, glMode);
+	outTexture.loadData(pixels, width, height, glMode);
 
 	free(pixels);
 	
