04375ee36cc5154d286f570cb9880d50081fc0e1
arturo <arturo@openframeworks.cc>
2010-09-25 20:51:50 +0200

    ofxNetwork: added type of error detection and logging + better disconnection detection for tcp. Closes #122

--- addons/ofxNetwork/src/ofxTCPClient.cpp
+++ addons/ofxNetwork/src/ofxTCPClient.cpp
@@ -94,6 +94,10 @@ bool ofxTCPClient::send(string message){
 	// note that you will receive a trailing [/TCP]\0
 	// if sending from here and receiving from receiveRaw or
 	// other applications
+	if(!connected){
+		if(verbose)printf("ofxTCPClient: trying to send while not connected\n");
+		return false;
+	}
 	message = partialPrevMsg + message + STR_END_MSG;
 	message += (char)0; //for flash
 	int ret = TCPClient.SendAll( message.c_str(), message.length() );
