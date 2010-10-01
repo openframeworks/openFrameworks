bd53ec97415519b67ab1cc95135d6a9ba43db71b
ofTheo <theo@openframeworks.cc>
2010-10-01 14:36:52 -0400

    added ofxiPhoneVideoGrabber example app for iPhone

---
+++ apps/iPhoneSpecificExamples/CameraCaptureExample/src/testApp.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiPhone.h"
+#include "ofxiPhoneExtras.h"
+
+#include "ofxiPhoneVideoGrabber.h"
+
+class testApp : public ofxiPhoneApp{
+	
+	public:
+		
+		void setup();
+		void update();
+		void draw();
+		
+		void touchDown(ofTouchEventArgs &touch);
+		void touchMoved(ofTouchEventArgs &touch);
+		void touchUp(ofTouchEventArgs &touch);
+		void touchDoubleTap(ofTouchEventArgs &touch);
+		
+		ofxiPhoneVideoGrabber grabber;
+		ofTexture tex;
+		
+		unsigned char * pix;
+};
