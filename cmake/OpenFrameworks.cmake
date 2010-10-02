d88b23a411288aa5e0525562217a1c81a95a2ba1
Francois Visconte <f.visconte@gmail.com>
2010-10-02 14:57:07 +0200

    Modular openframeworks cmake files

---
+++ cmake/OpenFrameworks.cmake
@@ -0,0 +1,196 @@
+# - Find openFrameworks dependencies 
+#
+# Find openframeworks dependencies on Linux and OS X 
+# This code set the following variables
+# Configuration variables:
+#
+# Search:
+# -includes
+# -libraries
+# With the following parameters :
+# - library_name
+# - include_files
+# - prefixes
+#
+# INPUT_VARIABLES :
+#   FOR LINUX:
+#     OPENFRAMEWORKS_VIDCAP_METHOD
+#
+#   OPENFRAMEWORKS_LIBRARIES_SEARCH_PATHS 
+#   OPENFRAMEWORKS_INCLUDES_SEARCH_PATHS
+# 
+# OUTPUT VARIABLES:
+#   OPENFRAMEWORKS_INCLUDES
+#   OPENFRAMEWORKS_LIBRARIES
+#
+
+#######################################################
+#######################################################
+#--- Common libraries
+#######################################################
+#######################################################
+
+message(STATUS "Looking for libraries in : ${OPENFRAMEWORKS_LIBRARIES_SEARCH_PATHS}")
+message(STATUS "Logging for includes in  : ${OPENFRAMEWORKS_INCLUDES_SEARCH_PATHS}")
+
+if ( APPLE )
+    set (CMAKE_FIND_LIBRARY_PREFIXES "" "lib")
+    set (CMAKE_FIND_LIBRARY_SUFFIXES ".dylib" ".a" ".lib", ".so")
+else()
+# if ( CMAKE_SYSTEM_NAME MATCHES "Linux" ) 
+    message(STATUS "RUNNING LINUX")
+    set (CMAKE_FIND_LIBRARY_PREFIXES "lib" "")
+    set (CMAKE_FIND_LIBRARY_SUFFIXES ".a" ".so")
+endif ()
+
+set (CMAKE_LIBRARY_PATH ${OPENFRAMEWORKS_LIBRARIES_SEARCH_PATHS} /usr/lib /usr/local/lib )
+set (CMAKE_INCLUDE_PATH ${OPENFRAMEWORKS_INCLUDES_SEARCH_PATHS} /usr/include /usr/local/include )
+
+
+#######################################################
+#
+# Find include file and fail if not found 
+# 
+macro(of_find_include includefile)
+  find_path(${includefile}_INCPATH NAMES ${includefile} PATHS ${OPENFRAMEWORKS_INCLUDES_SEARCH_PATHS})
+  if ( ${${includefile}_INCPATH} STREQUAL "${includefile}_INCPATH-NOTFOUND")
+    message(FATAL_ERROR "Cannot find ${includefile}")
+  else()
+    message(STATUS "Found ${includefile} in ${${includefile}_INCPATH}")
+  endif()
+  set (OPENFRAMEWORKS_INCLUDES ${OPENFRAMEWORKS_INCLUDES} ${${includefile}_INCPATH})
+endmacro(of_find_include)
+
+
+#######################################################
+####
+# Find library and fail if not found 
+#
+macro(of_find_library libname)
+  string(TOLOWER ${libname} libnamelow)
+     
+  find_library(${libname}_LIB NAMES ${libname} PATHS ${OPENFRAMEWORKS_LIBRARIES_SEARCH_PATHS})
+  if (NOT ${libname}_LIB)
+    find_library(${libname}_LIB NAMES ${libnamelow} PATHS ${OPENFRAMEWORKS_LIBRARIES_SEARCH_PATHS})
+  endif()
+  
+  if (NOT ${libname}_LIB)
+    message(FATAL_ERROR "Cannot find '${libname}' library")
+  else()
+    message(STATUS "Found '${libname}' library in ${${libname}_LIB}")
+    set(OPENFRAMEWORKS_LIBRARIES ${OPENFRAMEWORKS_LIBRARIES} ${${libname}_LIB})
+  endif()
+endmacro()
+
+
+#######################################################
+####
+# Find library and include 
+#
+macro(of_find_lib_and_include libname incname)
+  of_find_library(${libname})
+  of_find_include(${incname})
+endmacro(of_find_lib_and_include)
+
+
+#######################################################
+###
+# Use pkgconfig to find module and fail if not found 
+#
+macro(of_find_pkgconfig_lib pkgname)
+  include(FindPkgConfig)
+  pkg_check_modules(${pkgname}_pkg REQUIRED ${pkgname})
+  if ( DEFINED ${pkgname}_pkg_FOUND )
+    set (OPENFRAMEWORKS_LIBRARIES ${OPENFRAMEWORKS_LIBRARIES} ${${pkgname}_pkg_LIBRARIES}
+                                  ${${pkgname}_pkg_LDFLAGS}  ${${pkgname}_pkg_LDFLAGS_OTHER})
+    set (OPENFRAMEWORKS_INCLUDES ${OPENFRAMEWORKS_INCLUDES} ${${pkgname}_pkg_INCLUDE_DIRS})
+    set (OPENFRAMEWORKS_CXX_FLAGS ${OPENFRAMEWORKS_CXX_FLAGS} ${${pkgname}_pkg_CFLAGS})
+  else()
+    message( FATAL_ERROR "Cannot find pkgconfig file for ${pkgconfig}. Check your PKG_CONFIG_PATH environment variable.")
+  endif()
+endmacro(of_find_pkgconfig_lib)
+
+
+#######################################################
+####
+# Find glu library
+#
+macro(of_find_glu)
+  include(FindGLU)
+  message(STATUS "GLUT ${GLU_LIBRARY}")
+  if ( GLU_LIBRARY )
+    message (STATUS "Found GLU")
+    set(OPENFRAMEWORKS_INCLUDES ${OPENFRAMEWORKS_INCLUDES} ${GLU_INCLUDE_PATH})
+    set(OPENFRAMEWORKS_LIBRARIES ${OPENFRAMEWORKS_LIBRARIES} ${GLU_LIBRARY})
+  else ( GLU_LIBRARY )
+    message ( WARNING "Cannot find GLU")
+  endif (GLU_LIBRARY)
+endmacro(of_find_glu)
+
+
+#######################################################
+####
+# Find glut library
+#
+macro(of_find_glut)
+  # find GLUT 
+  include(FindGLUT)
+  if ( GLUT_FOUND )
+      message ( STATUS "Found GLUT ${GLUT_LIBRARIES}")
+      set(OPENFRAMEWORKS_INCLUDES ${OPENFRAMEWORKS_INCLUDES} ${GLUT_INCLUDE_DIR})
+      set(OPENFRAMEWORKS_LIBRARIES ${OPENFRAMEWORKS_LIBRARIES} ${GLUT_LIBRARIES})
+  else ( GLUT_FOUND )
+      message ( FATAL_ERROR  "Cannot find GLUT")
+  endif (GLUT_FOUND)
+endmacro(of_find_glut)
+
+macro(of_find_freetype)
+  include (FindFreetype)
+  if (NOT FREETYPE_FOUND)
+      message( FATAL_ERROR "Cannot find freetype 2 library")
+  else()
+      message( STATUS "Found Freetype2" )
+      set(OPENFRAMEWORKS_INCLUDES ${OPENFRAMEWORKS_INCLUDES} ${FREETYPE_INCLUDE_DIRS})
+      set(OPENFRAMEWORKS_LIBRARIES ${OPENFRAMEWORKS_LIBRARIES} ${FREETYPE_LIBRARIES})
+  endif()
+endmacro()
+
+
+
+   
+#######################################################
+#######################################################
+#######################################################
+#######################################################
+
+of_find_lib_and_include(fmodex "fmod.h")
+of_find_lib_and_include(PocoFoundation "Poco/Poco.h")
+of_find_lib_and_include(GLee GLee.h)
+of_find_lib_and_include(RtAudio RtAudio.h)
+of_find_lib_and_include(FreeImage FreeImage.h)
+of_find_lib_and_include(avcodec avcodec.h)
+
+of_find_library(asound)
+of_find_library(raw1394)
+
+if ( LINUX ) 
+  if ( ${OPENFRAMEWORKS_VIDCAP_METHOD} MATCHES "unicap" )
+    of_find_pkgconfig_lib(unicap)
+  elseif( ${OPENFRAMEWORKS_VIDCAP_METHOD} MATCHES "gstreamer" )
+    of_find_pkgconfig_lib(gstreamer-app-0.10)
+    of_find_pkgconfig_lib(gstreamer-video-0.10)
+  endif()
+endif( LINUX )
+
+of_find_pkgconfig_lib(libudev)
+of_find_pkgconfig_lib(gstreamer-app-0.10)
+of_find_pkgconfig_lib(gstreamer-video-0.10)
+of_find_pkgconfig_lib(libavformat)
+of_find_pkgconfig_lib(libswscale)
+
+of_find_freetype()
+
+of_find_glu()
+of_find_glut()
+
+
---
+++ cmake/OpenFrameworksConfig.cmake
@@ -0,0 +1,39 @@
+
+get_filename_component(OPENFRAMEWORKS_CMAKE_DIR_ROOT ${CMAKE_CURRENT_LIST_FILE} PATH)
+get_filename_component(OPENFRAMEWORKS_ROOT "${OPENFRAMEWORKS_CMAKE_DIR_ROOT}/../" ABSOLUTE)
+
+set(OPENFRAMEWORKS_INCLUDES_SEARCH_PATHS 
+                                 ${OPENFRAMEWORKS_ROOT}/libs/fmodex/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/FreeImage/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/freetype/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/glee/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/glu/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/glut/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/gstappsink/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/poco/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/quicktime/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/rtAudio/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/unicap/include
+                                 ${OPENFRAMEWORKS_ROOT}/libs/videoInput/include
+                                 /usr/include/*
+                                 
+                                 CACHE STRING "OpenFrameworks includes search path")
+
+set(OPENFRAMEWORKS_LIBRARIES_SEARCH_PATHS 
+                                 ${OPENFRAMEWORKS_ROOT}/libs/fmodex/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/FreeImage/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/freetype/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/glee/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/glu/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/glut/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/gstappsink/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/poco/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/quicktime/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/rtAudio/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/unicap/lib/linux64
+                                 ${OPENFRAMEWORKS_ROOT}/libs/videoInput/lib/linux64
+                                 
+                                 CACHE STRING "OpenFrameworks library search paths")
+
+
+
