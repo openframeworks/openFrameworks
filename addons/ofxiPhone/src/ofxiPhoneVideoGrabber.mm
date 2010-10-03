f1a2ccf9513485885ec5c976169bc88dfee40248
zach lieberman <zach@eyebeam.org>
2010-10-03 14:47:52 -0400

    #defines for ofxIphoneVideoGrabber to make compiling on sim / 3.2 sdk ok

--- addons/ofxiPhone/src/ofxiPhoneVideoGrabber.mm
+++ addons/ofxiPhone/src/ofxiPhoneVideoGrabber.mm
@@ -7,8 +7,14 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 #import <CoreGraphics/CoreGraphics.h>
+
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED > __IPHONE_3_2
+
 #import <CoreVideo/CoreVideo.h>
 #import <CoreMedia/CoreMedia.h>
+
+#if defined  __arm__
 
 @implementation iPhoneVideoGrabber
 
@@ -277,3 +283,11 @@ bool ofxiPhoneVideoGrabber::convertCGImageToPixels(CGImageRef & ref, unsigned ch
 	free(pixelsTmp);
 	return true;
 }
+
+#endif	// (__arm__) compile only for ARM
+
+#else   // (> 3.2 SDK) compile for 4.0+
+
+#warning "skipping ofxIphoneVideoGrabber compilation because you need > 3.2 iOS SDK"
+
+#endif
