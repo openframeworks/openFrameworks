ea0eec5024b52adbd43bc11c612b1ea7233029f4
zach lieberman <zach@eyebeam.org>
2011-01-09 13:16:40 -0500

    created a types folder.  breaking up .h into .cpp files.

--- libs/openFrameworks/types/ofBaseTypes.h
+++ libs/openFrameworks/types/ofBaseTypes.h
@@ -1,101 +1,18 @@
-#ifndef _OF_TYPES
-#define _OF_TYPES
+/*
+ *  ofBaseTypes.h
+ *  openFrameworksLib
+ *
+ *  Created by zachary lieberman on 1/9/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
 
+
+
+#pragma once
 #include "ofConstants.h"
-#include "ofUtils.h"
-#include "ofRectangle.h"
 #include "ofPoint.h"
-#include "ofColor.h"
-
-//----------------------------------------------------------
-// ofStyle
-//----------------------------------------------------------
-
-class ofStyle{
-	public:
-		ofStyle(){
-			bFill				= true;
-			blending			= false;
-			smoothing			= false;
-			circleResolution	= 20;
-			lineWidth			= 1.0;
-			polyMode			= OF_POLY_WINDING_ODD;
-			rectMode			= OF_RECTMODE_CORNER;
-		}
-
-		virtual ~ofStyle(){}
-
-		ofColor color;
-		int polyMode;
-		int rectMode;
-		bool bFill;
-		bool blending;
-		bool smoothing;
-		int circleResolution;
-		float lineWidth;
-};
-
-//----------------------------------------------------------
-// ofBuffer
-//----------------------------------------------------------
-
-class ofBuffer{
-
-	long 	size;
-	char * 	buffer;
-	long 	nextLinePos;
-public:
-
-	ofBuffer(){
-		size 	= 0;
-		buffer 	= NULL;
-		nextLinePos = 0;
-	}
-
-	ofBuffer(int _size, char * _buffer){
-		size 	= _size;
-		buffer 	= _buffer;
-		nextLinePos = 0;
-	}
-
-	ofBuffer(const ofBuffer & mom){
-		size = mom.size;
-		nextLinePos = mom.nextLinePos;
-		memcpy(buffer,mom.buffer,size);
-	}
-
-	~ofBuffer(){
-		if(buffer) delete[] buffer;
-	}
-
-	void allocate(long _size){
-		if(buffer) delete[] buffer;
-		buffer = new char[_size];
-		size = _size;
-	}
-
-	char * getBuffer(){
-		return buffer;
-	}
-
-	long getSize(){
-		return size;
-	}
-
-	string getNextLine(){
-		if( size <= 0 ) return "";
-		long currentLinePos = nextLinePos;
-		while(nextLinePos<size && buffer[nextLinePos]!='\n') nextLinePos++;
-		string line(buffer + currentLinePos,nextLinePos-currentLinePos);
-		if(nextLinePos<size-1) nextLinePos++;
-		return line;
-	}
-
-	string getFirstLine(){
-		nextLinePos = 0;
-		return getNextLine();
-	}
-};
+#include "ofRectangle.h"
 
 
 //----------------------------------------------------------
@@ -115,14 +32,14 @@ public:
 	virtual void draw(const ofRectangle & rect){
 		draw(rect.x, rect.y, rect.width, rect.height); 
 	}
-
+	
 	virtual float getHeight()=0;
 	virtual float getWidth()=0;
-
+	
 	virtual void setAnchorPercent(float xPct, float yPct){};
     virtual void setAnchorPoint(float x, float y){};
 	virtual void resetAnchor(){};
-
+	
 };
 
 //----------------------------------------------------------
@@ -162,7 +79,7 @@ public:
 //----------------------------------------------------------
 class ofBaseImage: public ofBaseDraws, public ofBaseHasTexture, public ofBaseHasPixels{
 public:
-
+	
 };
 
 //----------------------------------------------------------
@@ -174,4 +91,3 @@ public:
 	virtual bool isFrameNew()=0;
 	virtual void close()=0;
 };
-#endif
