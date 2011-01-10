67561b445d3e138278800bf6f73349f007690e2d
kylemcdonald <kyle@kylemcdonald.net>
2011-01-10 12:48:55 -0500

    fixed emptyExample

--- apps/examples/emptyExample/src/testApp.cpp
+++ apps/examples/emptyExample/src/testApp.cpp
@@ -7,21 +7,6 @@ void testApp::update() {
 }
 
 void testApp::draw() {
-	float ox = ofGetWidth() / 2;
-	float oy = ofGetHeight() / 2;
-	float tx = mouseX - ox;
-	float ty = mouseY - oy;
-	
-	ofBackground(0, 0, 0);
-	ofSetColor(255);
-	ofTranslate(ox, oy);
-	ofLine(0, 0, tx, ty);
-	
-	ofTranslate(tx, ty);
-	ofDrawBitmapString(ofToString((int) tx) + ", " + ofToString((int) ty), 10, 20);
-	float angle = atan2f(ty, tx);
-	ofDrawBitmapString(ofToString(angle / PI, 2) + " PI", 10, 40);
-	ofDrawBitmapString(ofToString(ofRadToDeg(angle), 2), 10, 60);
 }
 
 void testApp::keyPressed(int key) {
