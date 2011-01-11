201059de6f00c54ce322f3ed204a44891a1e1962
ofTheo <theo@openframeworks.cc>
2011-01-11 14:04:40 -0500

    rest of stub file which was missing

---
+++ addons/ofxVectorMath/src/ofxVec2f.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "ofVec2f.h"
+
+#warning ofxVec2f has been deprecated. 
+#warning Remove all references to ofxVec2f.h and use ofVec2f instead.
+
+typedef ofVec2f ofxVec2f;
---
+++ addons/ofxVectorMath/src/ofxVec4f.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "ofVec4f.h"
+
+#warning ofxVec4f has been deprecated. 
+#warning Remove all references to ofxVec4f.h and use ofVec4f instead.
+
+typedef ofVec4f ofxVec4f;
