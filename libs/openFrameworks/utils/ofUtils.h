fe0cf838d3fe53af3cfc36b88acb0cdf6976e25f
kylemcdonald <kyle@kylemcdonald.net>
2011-01-11 15:39:37 -0500

    added ofToHex() which works for a variety of datatypes (char, int, float, string, etc).

--- libs/openFrameworks/utils/ofUtils.h
+++ libs/openFrameworks/utils/ofUtils.h
@@ -48,11 +48,28 @@ template <class T> string ofToString(T value, int precision){
 	return out.str();
 }
 
+template <class T> string ofToHex(const T& value) {
+	ostringstream out;
+	// pretend that the value is a bunch of bytes
+	unsigned char* valuePtr = (unsigned char*) &value;
+	// the number of bytes is determined by the datatype
+	int numBytes = sizeof(T);
+	// the bytes are stored backwards (least significant first)
+	for(int i = numBytes - 1; i >= 0; i--) {
+		// print each byte out as a 2-character wide hex value
+		out << setfill('0') << setw(2) << hex << (int) valuePtr[i];
+	}
+	return out.str();
+}
+
+template <> string ofToHex(const string& value);
+string ofToHex(const char* value);
 int ofToInt(const string& intString);
 int ofHexToInt(const string& intHexString);
 float ofToFloat(const string& floatString);
 bool ofToBool(const string& boolString);
 char ofToChar(const string& charString);
+
 
 string 	ofGetVersionInfo();
 
