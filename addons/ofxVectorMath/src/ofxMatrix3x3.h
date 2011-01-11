b3db0227266db90bd17f26f34b047a8e5055f4cb
ofTheo <theo@openframeworks.cc>
2011-01-11 14:02:35 -0500

    ofxMatrix3x3 was missing

---
+++ addons/ofxVectorMath/src/ofxMatrix3x3.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "ofMatrix3x3.h"
+
+#warning ofxMatrix3x3 has been deprecated. 
+#warning Remove all references to ofxMatrix3x3.h and use ofMatrix3x3 instead.
+
+typedef ofMatrix3x3 ofxMatrix3x3;
+
