5f143940c10adb179c40ec45d118e320bcb0201d
Jonathan Brodsky <jonbro@gmail.com>
2011-01-13 22:16:13 -0500

    removed the dependencies on ofxMaximillian

--- addons/ofxSynth/src/ofxSynth.h
+++ addons/ofxSynth/src/ofxSynth.h
@@ -1,17 +1,77 @@
+/*  Uses portions of
+ * 
+ *  maximilian
+ *  platform independent synthesis library using portaudio or rtaudio
+ *
+ *  Created by Mick Grierson on 29/12/2009.
+ *  Copyright 2009 Mick Grierson & Strangeloop Limited. All rights reserved.
+ *	Thanks to the Goldsmiths Creative Computing Team.
+ *	Special thanks to Arturo Castro for the PortAudio implementation.
+ * 
+ *	Permission is hereby granted, free of charge, to any person
+ *	obtaining a copy of this software and associated documentation
+ *	files (the "Software"), to deal in the Software without
+ *	restriction, including without limitation the rights to use,
+ *	copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the
+ *	Software is furnished to do so, subject to the following
+ *	conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be
+ *	included in all copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,	
+ *	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *	OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+ 
 #pragma once
 
 #include "ofMain.h"
-#include "ofxMaxim.h"
+
 #include "ofxSynthDelayLine.h"
 #include "ofxSynthFilter.h"
-#include "ofxSynthFilter.h"
-#include "ofxSynthSampler.h"
 
 #define OFXSYNTHONESHOT 1
 #define OFXSYNTHADR 2
+#define TWOPI 3.14159*2
+
 
 const int wav_header_size = 0x2C;
 
+class maxiOsc {
+	
+	double frequency;
+	double phase;
+	double startphase;
+	double endphase;
+	double output;
+	double tri;
+
+	
+public:
+	maxiOsc();
+	double sinewave(double frequency);
+	double coswave(double frequency);
+	double phasor(double frequency);
+	double phasor(double frequency, double startphase, double endphase);
+	double saw(double frequency);
+	double triangle(double frequency,double phase);
+	double square(double frequency);
+	double pulse(double frequency, double duty);
+	double noise();
+	double sinebuf(double frequency);
+	double sinebuf4(double frequency);
+	
+};
+
+typedef maxiOsc ofxMaxiOsc;
 
 /** ofxSynthADSR
  
@@ -50,6 +110,11 @@ class ofxSynthOsc {
 		double	output;
 		double	tri;
 };
+/** ofxSynth
+ 
+ A classic subtractive synth
+ 
+*/
 class ofxSynth : public ofSoundSource{
 	public:
 		ofxSynth();
