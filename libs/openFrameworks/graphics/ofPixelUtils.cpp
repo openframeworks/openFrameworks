775524fd83d8bed9fdb8b4ebc1c3098b7b348bcc
zach lieberman <zach@eyebeam.org>
2011-01-15 15:17:21 -0500

    fixed a silly error in ofPixelUtils

--- libs/openFrameworks/graphics/ofPixelUtils.cpp
+++ libs/openFrameworks/graphics/ofPixelUtils.cpp
@@ -3,7 +3,7 @@
 #include "ofPixels.h"
 
 
-
+// TODO: more optimized non in place versions of these functions. 
 
 void ofPixelUtils::crop(ofPixels & pix, int x, int y, int _width, int _height){
 	
@@ -14,10 +14,10 @@ void ofPixelUtils::crop(ofPixels & pix, int x, int y, int _width, int _height){
 		int height = pix.height;
 		int bytesPerPixel = pix.bytesPerPixel;
 		
-		unsigned char * newPixels = new unsigned char [width*height*bytesPerPixel];
 		int newWidth = _width;
 		int newHeight = _height;
-		memset(newPixels, 0, width*height*pix.bytesPerPixel);
+		unsigned char * newPixels = new unsigned char [newWidth*newHeight*bytesPerPixel];
+		memset(newPixels, 0, newWidth*newHeight*pix.bytesPerPixel);
 		
 		// this prevents having to do a check for bounds in the for loop;
 		int minX = MAX(x, 0);
@@ -43,16 +43,43 @@ void ofPixelUtils::crop(ofPixels & pix, int x, int y, int _width, int _height){
 		pix.width = newWidth;
 		pix.height = newHeight;
 	}
-		
-	
 }
 
 void ofPixelUtils::rotate90(ofPixels & pix, int nClockwiseRotations){
-	
-	
+
+		
 }
 
 void ofPixelUtils::mirror(ofPixels & pix, bool vertically, bool horizontal){
 	
 	
 }
+
+//
+////--------------------------------------------------------------
+//void setFromSubImage(ofImage &src, ofImage &dst, ofRectangle subRect) {
+//	int oldw = src.width;
+//	int oldh = src.height;
+//	int w = subRect.width;
+//	int h = subRect.height;
+//	int x = (int)subRect.x;
+//	int y = (int)subRect.y;
+//	if (w > oldw) return;
+//	if (h > oldh) return; 
+//	int nPix = w * h * 3;
+//	unsigned char * tempPixels = new unsigned char[ nPix ];
+//	memset(tempPixels, 0, nPix);
+//	//if (dst.width != subRect.width || dst.height != subRect.height){
+//	//		dst.clear();	// clean up if necessary?
+//	//		dst.allocate(subRect.width, subRect.height, OF_IMAGE_COLOR);
+//	//	}
+//	unsigned char * srcPixel = src.getPixels() + (y * oldw + x) * 3;
+//	for (int i = 0; i < h; i++){
+//		memcpy(tempPixels + i*w*3, srcPixel + i*oldw*3, w*3);
+//		//memset(tempPixels + i*w*3, 0, w*3);
+//	}
+//	
+//	dst.setFromPixels(tempPixels, w, h, OF_IMAGE_COLOR);
+//	dst.update();
+//	delete tempPixels;
+//}
