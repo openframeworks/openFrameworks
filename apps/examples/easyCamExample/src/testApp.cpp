a0b961086e28ff6c0c192c32bdf5fa733f0e6f55
Memo Akten <memo@memo.tv>
2011-01-15 00:10:10 -0500

    easyCamExample and basic3dExample

---
+++ apps/examples/easyCamExample/src/testApp.cpp
@@ -0,0 +1,89 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_COLOR_MATERIAL);
+	
+	
+	// start camera, with radius of 100
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+}
+
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+	cam.begin();
+	
+	// draw world axis
+	float axisLength = 100;
+	// draw world x axis
+	ofSetColor(255, 0, 0);
+	glPushMatrix();
+	glTranslatef(axisLength/2, 0, 0);
+	glScalef(axisLength, 2, 2);
+	ofBox(0, 0, 0, 1);
+	glPopMatrix();
+	
+	// draw world y axis
+	ofSetColor(0, 255, 0);
+	glPushMatrix();
+	glTranslatef(0, axisLength/2, 0);
+	glScalef(2, axisLength, 2);
+	ofBox(0, 0, 0, 1);
+	glPopMatrix();
+	
+	// draw world z axis
+	ofSetColor(0, 0, 255);
+	glPushMatrix();
+	glTranslatef(0, 0, axisLength/2);
+	glScalef(2, 2, axisLength);
+	ofBox(0, 0, 0, 1);
+	glPopMatrix();
+	
+	
+	ofSetColor(255, 255, 0);
+	ofBox(20);
+	
+	
+	cam.end();
+	
+	ofSetColor(255, 255, 255);
+	ofDrawBitmapString("This text is not affected by camera because it is after cam.end()", 10, 30);
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+}
+
