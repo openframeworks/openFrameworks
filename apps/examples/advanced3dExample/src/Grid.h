1ed1dffd381e5f46cd964cc07979aad8cd02ab18
elliotwoods <elliot.woods@gmail.com>
2011-01-19 20:53:17 +0000

    added advanced3dExample

---
+++ apps/examples/advanced3dExample/src/Grid.h
@@ -0,0 +1,16 @@
+/*
+ *  Grid.h
+ *  advanced3dExample
+ *
+ *  Created by Elliot Woods on 19/01/2011.
+ *  Copyright 2011 Kimchi and Chips. All rights reserved.
+ *
+ */
+
+#include "ofMain.h"
+
+class Grid : public ofNode
+{
+public:
+	void	customDraw();
+};
