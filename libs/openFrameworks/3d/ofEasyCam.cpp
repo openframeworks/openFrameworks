6c7ccc2c159804eb37b03e2edbdde63dd625100f
elliotwoods <elliot.woods@gmail.com>
2011-01-23 22:30:03 +0000

    Added momentum and dolly controls to ofEasyCam

--- libs/openFrameworks/3d/ofEasyCam.cpp
+++ libs/openFrameworks/3d/ofEasyCam.cpp
@@ -13,27 +13,32 @@
 
 //----------------------------------------
 ofEasyCam::ofEasyCam():
-distance(100),
+distance(OF_EASYCAM_DEFAULT_DISTANCE),
 mousePosViewPrev(0, 0), 
-oldMousePress(false),
 lastMouseActionFrame(0),
 speed(0.1),
-drag(0.1)
+drag(0.1),
+distanceScaleVelocity(0)
 {
 	target.setPosition(0, 0, 0);
 	setPosition(0, 0, distance);
 	setParent(target);
+	
+	oldMousePress[0]=false;
+	oldMousePress[1]=false;
 }
 
 
 //----------------------------------------
 void ofEasyCam::begin(ofRectangle rect) {
 	
+	//check whether we've already interacted
+	//with this camera this frame
 	if (lastMouseActionFrame != ofGetFrameNum())
 	{
 		lastMouseActionFrame = ofGetFrameNum();
 		
-		if(ofGetMousePressed(0)) {
+		if(ofGetMousePressed(0) || ofGetMousePressed(2)) {
 			ofVec3f targetPos =  target.getGlobalPosition();
 			ofVec3f mousePos(ofGetMouseX() - rect.width/2, rect.height/2 - ofGetMouseY(), targetPos.z);
 			ofVec3f mousePosView;
@@ -48,24 +53,52 @@ void ofEasyCam::begin(ofRectangle rect) {
 			mousePos.z += targetPos.z;
 			mousePosView = ofMatrix4x4::getInverseOf(target.getGlobalTransformMatrix()) * mousePos;
 			
-			if(oldMousePress) {
-				ofQuaternion rotAmount;
-				rotAmount.makeRotate(mousePosViewPrev, mousePosView);
-				target.rotate(rotAmount.conj());
-				//target.rotate(rotAmount);
+			if(oldMousePress[0]) {
+				rotation.makeRotate(mousePosViewPrev, mousePosView);
+				target.rotate(rotation.conj());
 			}
 			
-			
-			printf("mousePos: %f %f %f\n", mousePos.x, mousePos.y, mousePos.z);
-			
-			if(oldMousePress) {
-				//			vel -= (mousePos - mousePrev) * speed;
+			if(oldMousePress[1]) {
+				//maths for dolly				
+				distanceScaleVelocity = 2 * (mousePos.y - mousePosScreenPrevY) / rect.height;
+				setDistance(distance * (1.0f + distanceScaleVelocity));
 			}
+			
 			mousePosViewPrev = ofMatrix4x4::getInverseOf(target.getGlobalTransformMatrix()) * mousePos;
+			mousePosScreenPrevY = mousePos.y;
+		}
+		else
+		{
+			// no mouse button pressed
+			//
+			// we keep values from previous frame
+			// and damp the values
+			//
+			float dampFactor = 1.0f - drag;
+			rotation.x() *= dampFactor;
+			rotation.y() *= dampFactor;
+			rotation.z() *= dampFactor;
+			rotation.w() = 1;
+			
+			distanceScaleVelocity = exp(log((distanceScaleVelocity * dampFactor) + 1.0f)) - 1.0f;
+			
+			//apply transforms if they're big enough
+			if (rotation.asVec3().lengthSquared() > 1e-5)
+				target.rotate(rotation.conj());
+			//
+			if (abs(distanceScaleVelocity - 1.0f) > 1e-5)
+				setDistance(distance * (1.0f + distanceScaleVelocity));
 		}
 		
-		oldMousePress = ofGetMousePressed(0);
+		oldMousePress[0] = ofGetMousePressed(0);
+		oldMousePress[1] = ofGetMousePressed(2);
+		
+		//reset view on press key 'r'
+		if (ofGetKeyPressed('r'))
+			reset();
+		
 	}
+
 	
 	ofCamera::begin(rect);
 }
@@ -73,7 +106,7 @@ void ofEasyCam::begin(ofRectangle rect) {
 //----------------------------------------
 void ofEasyCam::reset() {
 	target.resetTransform();
-	distance = 100;
+	distance = OF_EASYCAM_DEFAULT_DISTANCE;
 }
 
 
@@ -96,7 +129,11 @@ ofNode& ofEasyCam::getTarget() {
 
 //----------------------------------------
 void ofEasyCam::setDistance(float f) {
-	distance = f;
+	if (f > 0.0f)
+	{
+		distance = f;
+		setPosition(0, 0, f);
+	}
 }
 
 //----------------------------------------
