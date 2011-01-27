a3ccbce6423f50a524ad6fabc85d419f0a26f477
Kyle McDonald <kyle@kylemcdonald.net>
2011-01-27 18:46:02 -0500

    removed old commented out variables, removed memo's default header/license

--- libs/openFrameworks/3d/ofEasyCam.cpp
+++ libs/openFrameworks/3d/ofEasyCam.cpp
@@ -1,12 +1,3 @@
-/*
- *  ofEasyCam.cpp
- *  openFrameworksLib
- *
- *  Created by Memo Akten on 14/01/2011.
- *  Copyright 2011 MSA Visuals Ltd. All rights reserved.
- *
- */
-
 #include "ofEasyCam.h"
 #include "ofEvents.h"
 #include "ofMain.h"
