72b13ef893add4ee51deb64a85c90d89b3059f05
Vanderlin <todd@vanderlin.cc>
2011-01-29 21:04:25 -0500

    init shader att test

--- apps/devApps/BillboardTester/src/testApp.cpp
+++ apps/devApps/BillboardTester/src/testApp.cpp
@@ -1,5 +1,40 @@
 #include "testApp.h"
 
+
+//--------------------------------------------------------------
+//---------------------   THOUGHTS    --------------------------
+//--------------------------------------------------------------
+/*
+ just some thought on how we could wrap this
+ into ofShader...
+ shader.setVertexAttribute1fv("name", data);  
+ shader.setVertexAttribute2fv("name", data);
+ shader.setVertexAttribute3fv("name", data);
+ shader.setVertexAttribute4fv("name", data);
+ 
+ shader.setVertexAttribute1fv("name", data);  
+ shader.setVertexAttribute2fv("name", data);
+ shader.setVertexAttribute3fv("name", data);
+ shader.setVertexAttribute4fv("name", data);
+ 
+ shader.bindAttribute("name");
+ shader.enableVertexAttrib("name");
+ 
+ // GL Functions... 
+ void glVertexAttrib1f(GLint location, GLfloat v0);
+ void glVertexAttrib2f(GLint location, GLfloat v0, GLfloat v1);
+ void glVertexAttrib3f(GLint location, GLfloat v0, GLfloat v1,GLfloat v2);
+ void glVertexAttrib4f(GLint location, GLfloat v0, GLfloat v1,,GLfloat v2, GLfloat v3); 
+ 
+ GLint glVertexAttrib{1,2,3,4}fv(GLint location, GLfloat *v); 
+ glBindAttribLocation(shader.getProgram(), pointAttLocation, "pointSize");
+ */
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+
+
+
 //--------------------------------------------------------------
 void testApp::setup() {
 	
@@ -7,11 +42,8 @@ void testApp::setup() {
 	ofSetVerticalSync(true);
 	ofSetLineWidth(3.0);
 	
-	cameraRotation = 0;
-	zoom		   = -500;
-	zoomTarget     = 200;
 	
-	// ------------------------- billboard particles
+	// billboard particles
 	for (int i=0; i<NUM_BILLBOARDS; i++) {
 		pos[i].x = ofRandomWidth();
 		pos[i].y = ofRandomHeight();
@@ -21,16 +53,11 @@ void testApp::setup() {
 	
 	// set the vertex data
 	vbo.setVertexData(pos, NUM_BILLBOARDS, GL_DYNAMIC_DRAW);
-	
-	// load the bilboard shader 
-	// this is used to change the
-	// size of the particle
 	shader.setup("Billboard");
 	
-	// we need to disable ARB textures
-	// because we are binding ourselfs
+	
 	ofDisableArbTex();
-	texture.loadImage("dot.png");
+	texture.loadImage("fly.png");
 }
 
 //--------------------------------------------------------------
@@ -39,9 +66,10 @@ void testApp::update() {
 	ofVec2f mouse(ofGetMouseX(), ofGetMouseY());
 	ofVec2f mouseVec(ofGetPreviousMouseX()-ofGetMouseX(), ofGetPreviousMouseY()-ofGetMouseY());
 	mouseVec.limit(10.0);
+	
 	for (int i=0; i<NUM_BILLBOARDS; i++) {
 		
-		rotations[i]  += 0.003;
+		rotations[i]  += 0.03;
 		
 		if(mouse.distance(pos[i]) < 100.0) {
 			vel[i] -= mouseVec;	
@@ -68,41 +96,40 @@ void testApp::draw() {
 	ofEnablePointSprites();
 	shader.begin();
 	
+	
 	// we are getting the location of the point size attribute
-	// we then set the point of the pointSizes to the vertex att
+	// we then set the  pointSizes to the vertex attritbute
 	// we then bind and then enable
-	int pointAttLocation = shader.getAttributeLocation("pointSize");
-	glVertexAttribPointer(pointAttLocation, 1, GL_FLOAT, false, 0, pointSizes);
-	glBindAttribLocation(shader.getProgram(), pointAttLocation, "pointSize");
-	glEnableVertexAttribArray(pointAttLocation);
+	int pointAttLoc = shader.getAttributeLocation("pointSize");
+	glVertexAttribPointer(pointAttLoc, 1, GL_FLOAT, false, 0, pointSizes);
+	glBindAttribLocation(shader.getProgram(), pointAttLoc, "pointSize");
+	glEnableVertexAttribArray(pointAttLoc);
 
-	//shader.setUniform2fv("rot", &vel[0].x, NUM_BILLBOARDS);
-	shader.setUniform1fv("timer", rotations, NUM_BILLBOARDS);
 	
-	// bind the texture for point replace
+	// rotate the fly based on the velocity
+	int angleLoc = shader.getAttributeLocation("angle");
+	
+	
+	
+	
 	texture.getTextureReference().bind();
-	
-	// draw the vbo as points
-	vbo.bind();
-	vbo.setVertexData(pos, NUM_BILLBOARDS, GL_DYNAMIC_DRAW);
+	vbo.updateVertexData(pos, NUM_BILLBOARDS);
 	vbo.draw(GL_POINTS, 0, NUM_BILLBOARDS);
-	vbo.unbind();
-
-	// unbind the texture
 	texture.getTextureReference().unbind();
+
 	
 	shader.end();
-	
 	ofDisablePointSprites();
-	glDisableVertexAttribArray(pointAttLocation); // disable to point array
+	
+	// disable vertex attributes
+	glDisableVertexAttribArray(pointAttLoc); 
+	glDisableVertexAttribArray(angleLoc);
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
 	
 	if(key == 'f') ofToggleFullscreen();
-	if(key == OF_KEY_UP) 	zoomTarget +=10;
-	if(key == OF_KEY_DOWN) 	zoomTarget -=10;
 }
 
 //--------------------------------------------------------------
