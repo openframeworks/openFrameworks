0d94904ee17f7e2ebd16756e8e2bf30e9fcd86fb
Keith Pasko <keith@myThing.(none)>
2011-01-30 13:00:55 -0800

    modified vbo to take number of coordinates in ofVec(3,2)f when passing float*, changed ofMesh to accomodate

--- libs/openFrameworks/3d/ofVbo.h
+++ libs/openFrameworks/3d/ofVbo.h
@@ -43,18 +43,19 @@ public:
 	void setTexCoordData(const ofVec2f * texCoords, int total, int usage);	
 	void setIndexData(const GLuint * indices, int total, int usage);
 
-	void setVertexData(const float * vert0x, int total, int usage);
+	void setVertexData(const float * vert0x, int numCoords, int total, int usage);
 	void setColorData(const float * color0r, int total, int usage);	
 	void setNormalData(const float * normal0x, int total, int usage);	
 	void setTexCoordData(const float * texCoord0x, int total, int usage);	
 	
 	void updateVertexData(const ofVec3f * verts, int total);
+	void updateVertexData(const ofVec2f * verts, int total);
 	void updateColorData(const ofColor * colors, int total);	
 	void updateNormalData(const ofVec3f * normals, int total);	
 	void updateTexCoordData(const ofVec2f * texCoords, int total);	
 	void updateIndexData(const GLuint * indices, int total);
 	
-	void updateVertexData(const float * ver0x, int total);
+	void updateVertexData(const float * ver0x, int numCoords, int total);
 	void updateColorData(const float * color0r, int total);	
 	void updateNormalData(const float * normal0x, int total);	
 	void updateTexCoordData(const float * texCoord0x, int total);	
