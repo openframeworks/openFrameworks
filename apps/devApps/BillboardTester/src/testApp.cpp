14b06a041e9fba066207e802c5f2e265840170b1
Vanderlin <todd@vanderlin.cc>
2011-01-29 21:48:53 -0500

    vert att demo

--- apps/devApps/BillboardTester/src/testApp.cpp
+++ apps/devApps/BillboardTester/src/testApp.cpp
@@ -40,22 +40,24 @@ void testApp::setup() {
 	
 	ofBackground(0, 0, 0);
 	ofSetVerticalSync(true);
-	ofSetLineWidth(3.0);
 	
 	
 	// billboard particles
 	for (int i=0; i<NUM_BILLBOARDS; i++) {
-		pos[i].x = ofRandomWidth();
-		pos[i].y = ofRandomHeight();
-		pointSizes[i] = ofNextPow2(ofRandom(2, 64));
-		rotations[i]  = ofRandom(0, 1);
+		pos[i].x	  = ofRandomWidth();
+		pos[i].y	  = ofRandomHeight();
+		vel[i].x	  = ofRandomf();
+		vel[i].y	  = ofRandomf();
+		home[i]		  = pos[i];
+		pointSizes[i] = ofNextPow2(ofRandom(2, 40));
+		rotations[i]  = ofRandom(0, 360);
 	}
 	
 	// set the vertex data
 	vbo.setVertexData(pos, NUM_BILLBOARDS, GL_DYNAMIC_DRAW);
 	shader.setup("Billboard");
 	
-	
+	// yes its a fly!
 	ofDisableArbTex();
 	texture.loadImage("fly.png");
 }
@@ -67,21 +69,37 @@ void testApp::update() {
 	ofVec2f mouseVec(ofGetPreviousMouseX()-ofGetMouseX(), ofGetPreviousMouseY()-ofGetMouseY());
 	mouseVec.limit(10.0);
 	
+	
 	for (int i=0; i<NUM_BILLBOARDS; i++) {
 		
-		rotations[i]  += 0.03;
-		
-		if(mouse.distance(pos[i]) < 100.0) {
+
+		if(mouse.distance(pos[i]) < 200.0) {
 			vel[i] -= mouseVec;	
 		}
+	
 		
 		pos[i] += vel[i];
-		vel[i] *= 0.94f;
+		vel[i] *= 0.84f;
 		
 		if(pos[i].x < 0) pos[i].x = ofGetWidth();
 		if(pos[i].x > ofGetWidth()) pos[i].x = 0;
 		if(pos[i].y < 0) pos[i].y = ofGetHeight();
 		if(pos[i].y > ofGetHeight()) pos[i].y = 0;
+
+	
+		ofVec2f center(ofGetWidth()/2, ofGetHeight()/2);
+		ofVec2f frc = home[i] - pos[i];
+		if(frc.length() > 20.0) {
+			frc.normalize();
+			frc *= 0.84;
+			vel[i] += frc;
+		}
+		
+		
+		// get the 2d heading
+		float angle = (float)atan2(-vel[i].y, vel[i].x) + PI;
+		rotations[i] = (angle * -1.0);
+		
 	}
 		
 }
@@ -106,11 +124,12 @@ void testApp::draw() {
 	glEnableVertexAttribArray(pointAttLoc);
 
 	
-	// rotate the fly based on the velocity
+	// rotate the fly based on the velocity...(well tried need help with 2d tex rot see shader)
 	int angleLoc = shader.getAttributeLocation("angle");
-	
-	
-	
+	glVertexAttribPointer(angleLoc, 1, GL_FLOAT, false, 0, rotations);
+	glBindAttribLocation(shader.getProgram(), angleLoc, "angle");
+	glEnableVertexAttribArray(angleLoc);
+		
 	
 	texture.getTextureReference().bind();
 	vbo.updateVertexData(pos, NUM_BILLBOARDS);
@@ -158,6 +177,9 @@ void testApp::mouseReleased(int x, int y, int button) {
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-	
+	for (int i=0; i<NUM_BILLBOARDS; i++) {
+		home[i].x = ofRandomWidth();
+		home[i].y = ofRandomHeight();
+	}
 }
 
