9c3d0a9c1252507459c435a0d1f66ddbe6ff0f7a
arturo <arturo@openframeworks.cc>
2011-01-30 13:11:36 +0100

    ofModelLoader/ofModel: ofModelLoader should set useIndices in meshes since it's generating the indices

--- libs/openFrameworks/3d/ofModel.cpp
+++ libs/openFrameworks/3d/ofModel.cpp
@@ -21,9 +21,9 @@ ofModel::~ofModel(){
 void ofModel::drawWireframe(){
 	for (int i =0; i < meshes.size();i++){
 		
-		if(!meshes.at(i).bEnableIndices){
+		/*if(!meshes.at(i).bEnableIndices){
 			meshes.at(i).setUseIndices();
-		}
+		}*/
 		
 		/*
 		if(meshes.at(i).renderMethod!=OF_MESH_USING_VBO){
@@ -39,9 +39,9 @@ void ofModel::drawWireframe(){
 void ofModel::drawFaces(){
 	for (int i =0; i < meshes.size();i++){
 		
-		if(!meshes.at(i).bEnableIndices){
+		/*if(!meshes.at(i).bEnableIndices){
 			meshes.at(i).setUseIndices();
-		}
+		}*/
 		
 		/*
 		if(meshes.at(i).renderMethod!=OF_MESH_USING_VBO){
@@ -57,9 +57,9 @@ void ofModel::drawFaces(){
 void ofModel::drawVertices(){
 	for (int i =0; i < meshes.size();i++){
 		
-		if(!meshes.at(i).bEnableIndices){
+		/*if(!meshes.at(i).bEnableIndices){
 			meshes.at(i).setUseIndices();
-		}
+		}*/
 		
 		/*
 		if(meshes.at(i).renderMethod!=OF_MESH_USING_VBO){
