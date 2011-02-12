4091ac346392a7c6056a84f755582d8db1f5d50f
arturo castro <arturocastro@arturo.local>
2011-02-12 12:50:12 +0100

    remove the const version of getPixelsRef from iphone videograbber

--- libs/openFrameworks/video/ofiPhoneVideoGrabber.h
+++ libs/openFrameworks/video/ofiPhoneVideoGrabber.h
@@ -20,7 +20,6 @@ class ofiPhoneVideoGrabber : public ofBaseVideoGrabber {
 	
 	unsigned char * getPixels();
 	ofPixelsRef		getPixelsRef();
-	const ofPixels&	getPixelsRef() const;
 	
 	void close();	
 	
