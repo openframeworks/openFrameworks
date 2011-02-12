23b06467ba6dfe21a00311e99320d52051d6bfce
roxlu <diederick@apollomedia.nl>
2011-02-12 14:12:18 +0100

    Added some settings to ofMaterial

--- libs/openFrameworks/3d/ofMaterial.h
+++ libs/openFrameworks/3d/ofMaterial.h
@@ -14,8 +14,7 @@
  *			http://assimp.sourceforge.net/lib_html/ai_material_8h.html#7dd415ff703a2cc53d1c22ddbbd7dde0
  */
 enum ofTextureMapType {
-	 OF_DUMMY_MAP
-	,OF_DIFFUSE_MAP
+	 OF_DIFFUSE_MAP
 	,OF_SPECULAR_MAP
 	,OF_AMBIENT_MAP
 	,OF_HEIGHT_MAP
@@ -34,16 +33,33 @@ typedef std::map<ofTextureMapType, ofTexture*> ofTextureMaps;
 class ofMaterial {
 public:
 	ofMaterial();
-	ofMaterial& setDiffuse(ofColor oDiffuse);
-	ofMaterial& setAmbient(ofColor oAmbient);
-	ofMaterial& setSpecular(ofColor oSpecular);
+	
+	// set colors
+	ofMaterial& setColors(ofColor oDiffuse, ofColor oAmbient, ofColor oSpecular);
+	ofMaterial& setDiffuseColor(ofColor oDiffuse);
+	ofMaterial& setAmbientColor(ofColor oAmbient);
+	ofMaterial& setSpecularColor(ofColor oSpecular);
 	ofMaterial& setShininess(float nShininess);
 	
+	
+	// set maps.
+	ofMaterial& setDiffuseMap(ofTexture& oDiffuseMap) {	return addTexture(oDiffuseMap, OF_DIFFUSE_MAP);	}
+	ofMaterial& setSpecularMap(ofTexture& oSpecularMap) { return addTexture(oSpecularMap, OF_SPECULAR_MAP); }
+	ofMaterial& setAmbientMap(ofTexture& oAmbientMap) { return addTexture(oAmbientMap, OF_AMBIENT_MAP); }
+	ofMaterial& setHeightMap(ofTexture& oHeightMap) { return addTexture(oHeightMap, OF_HEIGHT_MAP); }
+	ofMaterial& setNormalMap(ofTexture& oNormalMap) { return addTexture(oNormalMap, OF_NORMAL_MAP); }
+	ofMaterial& setShininessMap(ofTexture& oShininessMap) { return addTexture(oShininessMap, OF_SHININESS_MAP);}
+	ofMaterial& setOpactiyMap(ofTexture& oOpacityMap) { return addTexture(oOpacityMap, OF_OPACITY_MAP); }
+	ofMaterial& setDisplacementMap(ofTexture& oDispMap) { return addTexture(oDispMap, OF_DISPLACEMENT_MAP); }
+	ofMaterial& setLightMap(ofTexture& oLightMap) { return addTexture(oLightMap, OF_LIGHT_MAP); }
+	ofMaterial& setReflectionMap(ofTexture& oReflectionMap) { return addTexture(oReflectionMap, OF_REFLECTION_MAP); }
+		
 	ofMaterial& addTexture(
-		 ofTexture* pTexture
-		,ofTextureMapType nType = OF_DUMMY_MAP
+		 ofTexture& pTexture
+		,ofTextureMapType nType = OF_DIFFUSE_MAP
 	);
 
+	
 	ofColor diffuse;
 	ofColor ambient;
 	ofColor specular;
