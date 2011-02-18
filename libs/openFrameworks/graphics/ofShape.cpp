84794328e952ad5d30a9790c3f623480ba94e699
arturo <arturo@openframeworks.cc>
2011-02-18 01:51:47 +0100

    ofShape: on close don't create a new shape

--- libs/openFrameworks/graphics/ofShape.cpp
+++ libs/openFrameworks/graphics/ofShape.cpp
@@ -174,7 +174,7 @@ void ofShape::close(){
 		lastPolyline().setClosed(true);
 		bNeedsTessellation = true;
 	}
-	newPath();
+	//newPath();
 }
 
 void ofShape::setPolyWindingMode(ofPolyWindingMode newMode){
