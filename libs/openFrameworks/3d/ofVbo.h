67b89a84b5bd467e1149d8582a0e7cb454eda560
arturo <arturo@openframeworks.cc>
2011-02-25 19:47:40 +0000

    added ofVboMesh + ofPrimitive accepts POINT mode for rendering

--- libs/openFrameworks/3d/ofVbo.h
+++ libs/openFrameworks/3d/ofVbo.h
@@ -4,6 +4,57 @@
 #include "ofConstants.h"
 #include "ofVec3f.h"
 #include "ofColor.h"
+#include "ofUtils.h"
+
+enum polyMode{
+	OF_MESH_POINTS,
+	OF_MESH_WIREFRAME,
+	OF_MESH_FILL
+};
+
+inline GLuint ofGetGLPolyMode(polyMode m){
+#ifndef TARGET_OPENGLES
+	switch(m){
+		case(OF_MESH_POINTS):
+			return GL_POINT;
+			break;
+		case(OF_MESH_WIREFRAME):
+			return GL_LINE;
+			break;
+		case(OF_MESH_FILL):
+			return GL_FILL;
+			break;
+		default:
+			ofLog(OF_LOG_ERROR,"asked for unsupported or non existant poly mode " + ofToString(m) + " returning GL_FILL");
+			return GL_FILL;
+			break;
+	}
+#else
+	return 0;
+#endif
+}
+
+inline polyMode ofGetOFPolyMode(GLuint m){
+#ifndef TARGET_OPENGLES
+	switch(m){
+		case(GL_POINT):
+			return OF_MESH_POINTS;
+			break;
+		case(GL_LINE):
+			return OF_MESH_WIREFRAME;
+			break;
+		case(GL_FILL):
+			return OF_MESH_FILL;
+			break;
+		default:
+			ofLog(OF_LOG_ERROR,"asked for non existant glPolygonMode " + ofToString(m) + " returning OF_MESH_FILL");
+			return OF_MESH_FILL;
+			break;
+	}
+#else
+	return OF_MESH_FILL;
+#endif
+}
 
 class ofVbo {
 public:
