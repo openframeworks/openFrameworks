f28a3e749750ed4daedaf145fd80aa4083bd8467
zach lieberman <zach@eyebeam.org>
2011-03-13 21:55:39 -0400

    added golan's resize functionality from OF dev week

--- libs/openFrameworks/graphics/ofPixelUtils.h
+++ libs/openFrameworks/graphics/ofPixelUtils.h
@@ -2,22 +2,37 @@
 
 #pragma once
 
+//---------------------------------------
+enum ofInterpolationMethod {
+	OF_INTERPOLATE_NEAREST_NEIGHBOR =1,
+	OF_INTERPOLATE_BILINEAR			=2,
+	OF_INTERPOLATE_BICUBIC			=3
+};
+
+
+//---------------------------------------
 
 class ofPixels;	
 
 class ofPixelUtils{
 	
-
-	public:
-		
-		// crop to a new width and height, this reallocates memory.
-		static void crop(ofPixels &pix, int x, int y, int width, int height);
-		// not in place
-		static void cropFromTo(ofPixels &frompix, ofPixels &topix, int x, int y, int width, int height);
-
-		// crop to a new width and height, this reallocates memory.
-		static void rotate90(ofPixels &pix, int nClockwiseRotations);
-		
-		static void mirror(ofPixels &pix, bool vertically, bool horizontal);
-
+	
+public:
+	
+	// crop to a new width and height, this reallocates memory.
+	static void crop(ofPixels &pix, int x, int y, int width, int height);	
+	// not in place
+	static void cropFromTo(ofPixels &frompix, ofPixels &topix, int x, int y, int width, int height);	
+	
+	// crop to a new width and height, this reallocates memory.
+	static void rotate90(ofPixels &pix, int nClockwiseRotations); 
+	
+	static void mirror(ofPixels &pix, bool vertically, bool horizontal);
+	
+	static bool resize(ofPixels &pix, int dstWidth, int dstHeight, ofInterpolationMethod interpMethod);
+	
+protected:
+	
+	static float bicubicInterpolate (const int *patch, float x, float y, float x2,float y2, float x3,float y3);
+	
 };
