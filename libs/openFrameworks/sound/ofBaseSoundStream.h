7b91097d99586ad11ebde91ac404a9ce08a30191
arturo <arturo@openframeworks.cc>
2011-03-20 13:17:40 +0100

    SoundStream: modified api to be able to have full duplex streams + cleaner callback

--- libs/openFrameworks/sound/ofBaseSoundStream.h
+++ libs/openFrameworks/sound/ofBaseSoundStream.h
@@ -10,15 +10,14 @@ class ofBaseSoundStream{
 		
 		virtual void listDevices() = 0;
 		virtual void setDeviceID(int deviceID) = 0;
-	
-		virtual bool setupInput(ofBaseSoundInput * soundInput, int numChannels, int sampleRate, int bufferSize, int nBuffers) = 0;
-		virtual bool setupOutput(ofBaseSoundOutput * soundOutput, int numChannels, int sampleRate, int bufferSize, int nBuffers) = 0;
+		virtual bool setup(int outChannels, int inChannels, int sampleRate, int bufferSize, int nBuffers)=0;
+		virtual bool setup(ofBaseApp * app, int outChannels, int inChannels, int sampleRate, int bufferSize, int nBuffers)=0;
+		virtual void setInput(ofBaseSoundInput * soundInput) = 0;
+		virtual void setOutput(ofBaseSoundOutput * soundOutput) = 0;
 		
 		virtual void start() = 0;
 		virtual void stop() = 0;
 		virtual void close() = 0;
 
 		virtual long unsigned long getTickCount() = 0;
-
-		virtual void incrementTickCount() = 0;
 };
