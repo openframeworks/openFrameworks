be5abc789673b5fa70e650dbd79b84b3c20862b2
damian <damian@frey.NOSPAMco.nz>
2011-04-20 12:04:16 +0200

    some more working ios examples

--- libs/openFrameworks/communication/ofSerial.cpp
+++ libs/openFrameworks/communication/ofSerial.cpp
@@ -456,7 +456,7 @@ int ofSerial::writeBytes(unsigned char * buffer, int length){
 			return OF_SERIAL_ERROR;
 		}
 
-		ofLog(OF_LOG_VERBOSE,"ofSerial: numWritten %i", numWritten);
+		//ofLog(OF_LOG_VERBOSE,"ofSerial: numWritten %i", numWritten);
 
 	    return numWritten;
     #endif
@@ -626,6 +626,18 @@ void ofSerial::flush(bool flushIn, bool flushOut){
 	#endif
 	//---------------------------------------------
 
+}
+
+void ofSerial::drain()
+{
+    if (!bInited){
+		ofLog(OF_LOG_ERROR,"ofSerial: serial not inited");
+		return;
+	}
+
+    #if defined( TARGET_OSX ) || defined( TARGET_LINUX )
+        tcdrain( fd );
+    #endif
 }
 
 //-------------------------------------------------------------
