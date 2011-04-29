befa14fc6b23ea2b9e269b61c62db65f0c59f832
ofTheo <theo@openframeworks.cc>
2011-04-29 17:13:23 -0400

    removed base project from examples/ and addonexamples/ for xcode - added in libtess2

--- apps/examples/blendingExample/src/testApp.cpp
+++ apps/examples/blendingExample/src/testApp.cpp
@@ -86,22 +86,27 @@ void testApp::keyPressed  (int key){
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){ 
+
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
+
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
+
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
+
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
+
 }
 
 //--------------------------------------------------------------
