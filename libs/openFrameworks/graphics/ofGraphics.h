d1febd9ca182e6ec3fdcf29f6711b3f278795312
ofTheo <theo@openframeworks.cc>
2011-04-29 14:00:17 -0400

    removed << ofDrawBitmapString.cpp - this needs to be looked at first and done as a seperate pull

--- libs/openFrameworks/graphics/ofGraphics.h
+++ libs/openFrameworks/graphics/ofGraphics.h
@@ -7,7 +7,6 @@
 #include "ofRectangle.h"
 #include "ofTypes.h"
 #include "ofBaseTypes.h"
-#include "ofDrawBitmapString.h"
 
 #define  	CIRC_RESOLUTION		    22				// 22 pts for a circle...
 
@@ -127,7 +126,7 @@ void ofEnableSmoothing();
 void ofDisableSmoothing();
 
 // drawing style - combines color, fill, blending and smoothing
-ofStyle& ofGetStyle();
+ofStyle ofGetStyle();
 void ofSetStyle(ofStyle style);
 void ofPushStyle();
 void ofPopStyle();
@@ -225,7 +224,12 @@ void ofBox(float size);
 
 
 // bitmapped type
+// ** note, this uses glDrawPixels and may be S L 0 W on some graphics cards
 void ofSetDrawBitmapMode(ofDrawBitmapMode mode);
+void ofDrawBitmapString(string textString, const ofPoint & p);
+void ofDrawBitmapString(string textString, float x, float y);
+void ofDrawBitmapString(string textString, float x, float y, float z);
+
 
 // end primitives
 //-------------------------------------------
