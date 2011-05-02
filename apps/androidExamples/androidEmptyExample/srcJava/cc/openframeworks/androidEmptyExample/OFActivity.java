3f2fce1d07c2cd571dfe828067b030e07564e2bb
arturo <arturo@reactable.com>
2011-05-02 20:01:20 +0200

    android: added menu stub code to emptyExample

--- apps/androidExamples/androidEmptyExample/srcJava/cc/openframeworks/androidEmptyExample/OFActivity.java
+++ apps/androidExamples/androidEmptyExample/srcJava/cc/openframeworks/androidEmptyExample/OFActivity.java
@@ -2,6 +2,10 @@ package cc.openframeworks.androidEmptyExample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import cc.openframeworks.OFAndroid;
 
 
@@ -31,8 +35,38 @@ public class OFActivity extends Activity{
         super.onResume();
         ofApp.resume();
     }
-	
+
 	OFAndroid ofApp;
+    
+	
+	
+    // Menus
+    // http://developer.android.com/guide/topics/ui/menus.html
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	// Create settings menu options from here, one by one or infalting an xml
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// This passes the menu option string to OF
+    	// you can add additional behavior from java modifying this method
+    	if(OFAndroid.menuItemSelected(item)){
+    		
+    		return true;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
+    
+
+    @Override
+    public boolean onPrepareOptionsMenu (Menu menu){
+    	// This method is called every time the menu is opened
+    	//  you can add or remove menu options from here
+    	return  super.onPrepareOptionsMenu(menu);
+    }
+	
 }
 
 
