f1219eed333d7ee3952037498e490637b39794f3
Kyle McDonald <kyle@kylemcdonald.net>
2011-05-11 13:37:11 -0400

    added placeholder for blobs example, changed naming conventions for examples

---
+++ example-blobs/src/testApp.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+using namespace ofxCv;
+using namespace cv;
+
+class testApp : public ofBaseApp {
+public:
+	void setup();
+	void update();
+	void draw();
+};
