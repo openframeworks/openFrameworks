81d169a06c4ac4c3f673957ae491f066d9bd9c89
arturo castro <arturocastro@arturo.local>
2011-05-20 23:37:42 +0200

    cairoRenderer & rendererCollection: added methods to render float and short images

--- libs/openFrameworks/graphics/ofCairoRenderer.h
+++ libs/openFrameworks/graphics/ofCairoRenderer.h
@@ -33,6 +33,8 @@ public:
 	void draw(ofMesh & vertexData, ofPolyRenderMode mode);
 	void draw(vector<ofPoint> & vertexData, ofPrimitiveMode drawMode);
 	void draw(ofImage & img, float x, float y, float z, float w, float h);
+	void draw(ofFloatImage & image, float x, float y, float z, float w, float h);
+	void draw(ofShortImage & image, float x, float y, float z, float w, float h);
 
 	bool rendersPathPrimitives(){
 		return true;
