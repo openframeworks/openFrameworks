a1381dd1879ae10c90ce0d8c1e7460e5330fee3d
ofTheo <theo@openframeworks.cc>
2011-05-23 18:20:17 -0400

    using xcconfig for Empty Example too.

--- libs/openFrameworks/graphics/ofCairoRenderer.h
+++ libs/openFrameworks/graphics/ofCairoRenderer.h
@@ -1,10 +1,10 @@
 #pragma once
 
 
-#include "cairo/cairo-features.h"
-#include "cairo/cairo-pdf.h"
-#include "cairo/cairo-svg.h"
-#include "cairo/cairo.h"
+#include "cairo-features.h"
+#include "cairo-pdf.h"
+#include "cairo-svg.h"
+#include "cairo.h"
 #include <deque>
 #include <stack>
 #include "ofMatrix4x4.h"
