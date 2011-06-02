99f3fdb381ae24d75cc0764d74cabd0d6da9fe44
unknown <arturo@.(none)>
2011-06-02 21:03:15 +0200

    createProjects.py: win_cb script + linux updates for multiplatform

--- scripts/win_cb/createProjects.py
+++ scripts/win_cb/createProjects.py
@@ -8,10 +8,10 @@ import glob
 
 
 of_root = os.path.realpath(__file__)[0:-(len(os.path.join('scripts','linux','createProject.py'))+2)]
-platform = 'linux'
-arch = 'linux64'
+platform = 'win_cb'
+arch = 'win_cb'
 templates_path = os.path.join(of_root,'apps','devApps','win_cb')
-template = {'cbp': os.path.join(templates_path , 'emptyExample_' + arch + '.cbp'), 'full_cbp': os.path.join(templates_path , 'emptyExample_' + arch + '_fullCBP.cbp'), 'makefile': templates_path + 'Makefile', 'config.make': templates_path + 'config.make'}
+template = {'cbp': os.path.join(templates_path , 'emptyExample_' + arch + '.cbp'), 'full_cbp': os.path.join(templates_path , 'emptyExample_' + arch + '.cbp'), 'makefile': templates_path + 'Makefile', 'config.make': templates_path + 'config.make'}
 fullCBP = True
 
 def addCBPIncludePath(project,dirpath):
@@ -182,11 +182,11 @@ def createProject(project_path):
 
 parser = argparse.ArgumentParser(description='OF linux project generator')
 parser.add_argument('project_path', metavar='project_path', nargs='?')
-parser.add_argument('-n', '--not_mk', dest='not_mk', action='store_const',
-        default=False, const=True, help='create cbp not dependent on Makefile')
+#parser.add_argument('-n', '--not_mk', dest='not_mk', action='store_const',
+#        default=False, const=True, help='create cbp not dependent on Makefile')
 
 project_path = parser.parse_args().project_path
-fullCBP = parser.parse_args().not_mk
+fullCBP = True #parser.parse_args().not_mk
 
 if project_path==None: #parse all examples
     for example in os.listdir(os.path.join(of_root,'apps','examples')):
