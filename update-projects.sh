3860001a7e301fe0e3fa2b19861c153fd00bdf99
Kyle McDonald <kyle@kylemcdonald.net>
2011-09-10 02:00:48 +0900

    updated project generator script with suggestion from arturo

--- update-projects.sh
+++ update-projects.sh
@@ -1,41 +1,15 @@
 #!/bin/bash
 
-directories=(
-"example-background"
-"example-bayer"
-"example-contours-advanced"
-"example-contours-basic"
-"example-contours-color"
-"example-contours-tracking"
-"example-difference"
-"example-face"
-"example-flow"
-"example-homography"
-"example-threshold"
-"example-undistortion")
+examples=$(ls -1d example*/*.xcodeproj)
 
-projects=(
-"BackgroundExample"
-"BayerExample"
-"ContoursAdvancedExample"
-"ContoursBasicExample"
-"ContoursColorExample"
-"ContoursTrackingExample"
-"DifferenceExample"
-"FaceExample"
-"FlowExample"
-"HomographyExample"
-"ThresholdExample"
-"UndistortionExample")
-
-n=${#directories[@]}
-
-for ((i=0; i<$n; i++));
+for cur in $examples
 do
-	curDirectory=${directories[i]}
-	curProject=${projects[i]}
-	target=$curDirectory/$curProject.xcodeproj/project.pbxproj
-	replace="s/EmptyExample/"$curProject"/g"
-	cp example-empty/EmptyExample.xcodeproj/project.pbxproj $target
-	sed -i "" -e $replace $target
+	projectName=`echo "$cur" | sed -e 's/example-[^\/]*\/\([^.]*\)\(\.xcodeproj\)/\1/g'`
+	if [ "$projectName" != "EmptyExample" ]
+	then
+		projectFile=$cur"/project.pbxproj"
+		replace="s/EmptyExample/"$projectName"/g"
+		cp example-empty/EmptyExample.xcodeproj/project.pbxproj $projectFile
+		sed -i "" -e $replace $projectFile
+	fi
 done
