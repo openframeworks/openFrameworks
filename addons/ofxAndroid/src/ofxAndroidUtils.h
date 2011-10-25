051c0ef257ee443e4c709bd3d137f26b5047edf3
Falcon4ever <laurence.muller@gmail.com>
2011-10-25 23:18:25 +0200

    ofxAndroid from the Development branch

--- addons/ofxAndroid/src/ofxAndroidUtils.h
+++ addons/ofxAndroid/src/ofxAndroidUtils.h
@@ -52,6 +52,7 @@ void  ofUpdateBitmapCharacterTexture();
 void ofxAndroidSoundStreamPause();
 void ofxAndroidSoundStreamResume();
 
+bool ofxAndroidCheckSDCardMounted();
 
 //this is just to fix a problem with undefined symbols
 inline void ofFixSoundStreamInclude(){
@@ -71,6 +72,6 @@ inline void ofxAndroidSetViewItemChecked(string item_name, bool checked){
 		ofLog(OF_LOG_ERROR,"cannot find OFAndroid setViewItemChecked method");
 		return;
 	}
-	ofGetJNIEnv()->CallStaticObjectMethod(javaClass,setViewItemChecked,ofGetJNIEnv()->NewStringUTF(item_name.c_str()),checked);
+	ofGetJNIEnv()->CallStaticVoidMethod(javaClass,setViewItemChecked,ofGetJNIEnv()->NewStringUTF(item_name.c_str()),checked);
 }
 #endif /* OFANDROIDUTILS_H_ */
