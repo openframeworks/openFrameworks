d8ea98789eb8c013759036467d543b6508dd3938
Falcon4ever <laurence.muller@gmail.com>
2011-10-25 23:18:25 +0200

    ofxAndroid from the Development branch

--- addons/ofxAndroid/ofAndroidLib/src/cc/openframeworks/OFAndroidObject.java
+++ addons/ofxAndroid/ofAndroidLib/src/cc/openframeworks/OFAndroidObject.java
@@ -24,24 +24,24 @@ public abstract class OFAndroidObject {
 	}
 	
     public void onPause(){
-    	pause();
+    	appPause();
     	state = State.Paused;
     }
     public void onResume(){
-    	resume();
+    	appResume();
     	state = State.Running;
     }
     public void onStop(){
-    	stop();
+    	appStop();
     	state = State.Stopped;
     }
     public void release(){
     	ofObjects.remove(this);
     }
     
-    abstract protected void pause();
-    abstract protected void resume();
-    abstract protected void stop();
+    abstract protected void appPause();
+    abstract protected void appResume();
+    abstract protected void appStop();
     
     public static void setActivity(Activity activity){
     	OFAndroidObject.activity = activity;
