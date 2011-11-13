26f920b702ee657c82644e52740baaef3404a498
arturo <arturo@openframeworks.cc>
2011-11-13 23:52:13 +0100

    ofFbo: added option to use texture for the depth buffer instead of a render buffer

--- libs/openFrameworks/gl/ofFbo.h
+++ libs/openFrameworks/gl/ofFbo.h
@@ -64,6 +64,7 @@ public:
 		int		numColorbuffers;		// how many color buffers to create
 		bool	useDepth;				// whether to use depth buffer or not
 		bool	useStencil;				// whether to use stencil buffer or not
+		bool	depthAsTexture;			// use a texture instead of a renderbuffer for depth (useful to draw it or use it in a shader later)
 		GLenum	textureTarget;			// GL_TEXTURE_2D or GL_TEXTURE_RECTANGLE_ARB
 		GLint	internalformat;			// GL_RGBA, GL_RGBA16F_ARB, GL_RGBA32F_ARB, GL_LUMINANCE32F_ARB etc.
 		int		wrapModeHorizontal;		// GL_REPEAT, GL_MIRRORED_REPEAT, GL_CLAMP_TO_EDGE, GL_CLAMP_TO_BORDER etc.
