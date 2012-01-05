9e32b16bb9b3cd719563df37683fdedb9dd4730c
Kyle McDonald <kyle@kylemcdonald.net>
2012-01-05 13:41:04 -0500

    added windows code::blocks projects and workspaces. switched from bash to python for project generation.

---
+++ update-projects.py
@@ -0,0 +1,48 @@
+#!/bin/python
+
+import glob, re, shutil, fileinput, os
+
+def toUpper(pattern):
+	return pattern.group(1).upper()
+	
+def replaceInFile(filename, pattern, replacement):
+	for line in fileinput.FileInput(filename, inplace=1):
+		print re.sub(pattern, replacement, line),
+
+sourceProjectName = "EmptyExample"
+
+# windows code::blocks
+sourceCbp = "example-empty/EmptyExample.cbp"
+sourceWorkspace = "example-empty/EmptyExample.workspace"
+
+# xcode osx
+sourceXcconfig = "example-empty/Project.xcconfig"
+sourcePlist = "example-empty/openFrameworks-Info.plist"
+sourcePbxproj = "example-empty/EmptyExample.xcodeproj/project.pbxproj"
+
+examples = glob.glob("example*")
+for example in examples:
+	if example != "example-empty":
+		sansExample = re.sub("^example", "", example)
+		upper = re.sub("-([a-z])", toUpper, sansExample)
+		targetProjectName = upper + "Example"
+		
+		# windows code::blocks
+		targetCbp = "{}/{}.cbp".format(example, targetProjectName)
+		targetWorkspace = "{}/{}.workspace".format(example, targetProjectName)
+		shutil.copy(sourceCbp, targetCbp)
+		shutil.copy(sourceWorkspace, targetWorkspace)
+		replaceInFile(targetCbp, sourceProjectName, targetProjectName)
+		replaceInFile(targetWorkspace, sourceProjectName, targetProjectName)
+
+		# xcode osx
+		targetXcodeproj = "{}/{}.xcodeproj".format(example, targetProjectName)
+		targetPbxproj = "{}/project.pbxproj".format(targetXcodeproj)
+		shutil.copy(sourceXcconfig, example)
+		shutil.copy(sourcePlist, example)
+		if not os.path.exists(targetXcodeproj):
+			os.mkdir(targetXcodeproj)
+		shutil.copy(sourcePbxproj, targetPbxproj)
+		replaceInFile(targetPbxproj, sourceProjectName, targetProjectName)
+		
+		print "Copied into " + targetProjectName + "."
