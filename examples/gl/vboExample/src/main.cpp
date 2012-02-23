a34b945aa6c0ebbc7b6ce5c3250af004fe38ae8d
Kyle McDonald <kyle@kylemcdonald.net>
2012-02-23 17:47:42 -0500

    fixed ofApp->testApp

--- examples/gl/vboExample/src/main.cpp
+++ examples/gl/vboExample/src/main.cpp
@@ -1,8 +1,8 @@
-#include "ofApp.h"
+#include "testApp.h"
 #include "ofAppGlutWindow.h"
 
 int main() {
 	ofAppGlutWindow window;
 	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
-	ofRunApp(new ofApp());
+	ofRunApp(new testApp());
 }
--- examples/gl/vboExample/src/testApp.h
+++ examples/gl/vboExample/src/testApp.h
@@ -2,7 +2,7 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp {
+class testApp : public ofBaseApp {
 public:
 	void setup();
 	void update();
