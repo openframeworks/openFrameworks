867d81f04d2a153daca11e55d837dcc01ed8e00b
Todd Vanderlin <todd@todd.local>
2012-02-24 13:38:21 -0500

    adding utils examples

---
+++ examples/utils/sortingExample/src/main.cpp
@@ -0,0 +1,8 @@
+#include "ofApp.h"
+#include "ofAppGlutWindow.h"
+
+int main() {
+	ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 1440, 900, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
---
+++ examples/utils/systemSpeakExample/src/main.cpp
@@ -0,0 +1,8 @@
+#include "ofApp.h"
+#include "ofAppGlutWindow.h"
+
+int main() {
+	ofAppGlutWindow window;
+	ofSetupOpenGL(&window, 500, 500, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
