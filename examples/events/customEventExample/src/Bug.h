3337896e123b3a5abcced6e6cc9263a7e837bde1
Todd Vanderlin <todd@todd.(none)>
2012-02-25 14:10:08 -0500

    Update for ImageSequenceExample
    bug fix for timing

---
+++ examples/events/customEventExample/src/Bug.h
@@ -0,0 +1,14 @@
+//
+//  Mover.h
+//  customEventExample
+//
+//  Created by Todd Vanderlin on 2/24/12.
+//  Copyright (c) 2012 vanderlin.cc. All rights reserved.
+//
+
+#ifndef customEventExample_Mover_h
+#define customEventExample_Mover_h
+
+
+
+#endif
---
+++ examples/events/customEventExample/src/Bullet.h
@@ -0,0 +1,14 @@
+//
+//  Bullets.h
+//  customEventExample
+//
+//  Created by Todd Vanderlin on 2/24/12.
+//  Copyright (c) 2012 vanderlin.cc. All rights reserved.
+//
+
+#ifndef customEventExample_Bullets_h
+#define customEventExample_Bullets_h
+
+
+
+#endif
