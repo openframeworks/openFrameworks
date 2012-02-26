2429a3e1fc4ed2e985e4674b63757bdd468ebbe5
Nick Hardeman <nickhardeman@gmail.com>
2012-02-26 16:05:43 -0500

    Moved enable init functionality into constructor

--- libs/openFrameworks/gl/ofLight.h
+++ libs/openFrameworks/gl/ofLight.h
@@ -69,13 +69,7 @@ public:
 	
 	int getLightID() const;
 
-	void customDraw() {
-		ofPushMatrix();
-		ofTranslate(getPosition());
-		ofBox(10);
-		ofDrawAxis(20);
-		ofPopMatrix();
-	}
+	void customDraw();
 	
 	
 	// this method overrides ofNode to catch the changes and update glLightv(GL_POSITION)
