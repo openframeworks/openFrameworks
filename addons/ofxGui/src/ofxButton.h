eae58f4dda1582ee0f5d13800d7d7f492efce94d
arturoc <arturo@openframeworks.cc>
2012-02-27 01:17:21 -0500

    ofxGui implementation moved to cpp

--- addons/ofxGui/src/ofxButton.h
+++ addons/ofxGui/src/ofxButton.h
@@ -7,22 +7,12 @@ class ofxButton : public ofxToggle{
 	friend class ofPanel;
 	
 public:
-	ofxButton* setup(string toggleName, float width = defaultWidth, float height = defaultHeight){
-		name = toggleName;
-		b.x = 0;
-		b.y = 0;
-		b.width = width;
-		b.height = height;
-		currentFrame = 0;			
-		bGuiActive = false;
-		value = false;
-		checkboxRect.set(1, 1, b.height - 2, b.height - 2);
-		return this;
-	}
-	
-	virtual void mouseReleased(ofMouseEventArgs & args){
-		setValue(args.x, args.y, false);
-		bGuiActive = false;		
-	}	
+
+	ofxButton* setup(string toggleName, float width = defaultWidth, float height = defaultHeight);
+
+	virtual void mouseReleased(ofMouseEventArgs & args);
+	virtual void mouseMoved(ofMouseEventArgs & args);
+	virtual void mousePressed(ofMouseEventArgs & args);
+	virtual void mouseDragged(ofMouseEventArgs & args);
 	
 };
