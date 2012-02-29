48ce79557f53f9f5f89d04129687c868d4639644
Todd Vanderlin <todd@todd.local>
2012-02-29 17:35:59 -0500

    IOS working on develop branch

--- addons/ofxiPhone/src/EAGLView.mm
+++ addons/ofxiPhone/src/EAGLView.mm
@@ -156,8 +156,8 @@
 		touchArgs.x = touchPoint.x;
 		touchArgs.y = touchPoint.y;
 		touchArgs.id = touchIndex;
-		if([touch tapCount] == 2) ofNotifyEvent(ofEvents.touchDoubleTap,touchArgs);	// send doubletap
-		ofNotifyEvent(ofEvents.touchDown,touchArgs);	// but also send tap (upto app programmer to ignore this if doubletap came that frame)
+		if([touch tapCount] == 2) ofNotifyEvent(ofEvents().touchDoubleTap,touchArgs);	// send doubletap
+		ofNotifyEvent(ofEvents().touchDown,touchArgs);	// but also send tap (upto app programmer to ignore this if doubletap came that frame)
 	}
 	
 }
@@ -185,7 +185,7 @@
 		touchArgs.x = touchPoint.x;
 		touchArgs.y = touchPoint.y;
 		touchArgs.id = touchIndex;
-		ofNotifyEvent(ofEvents.touchMoved, touchArgs);
+		ofNotifyEvent(ofEvents().touchMoved, touchArgs);
 	}
 	
 }
@@ -214,7 +214,7 @@
 		touchArgs.x = touchPoint.x;
 		touchArgs.y = touchPoint.y;
 		touchArgs.id = touchIndex;
-		ofNotifyEvent(ofEvents.touchUp, touchArgs);
+		ofNotifyEvent(ofEvents().touchUp, touchArgs);
 	}
 }
 
@@ -237,7 +237,7 @@
 		touchArgs.x = touchPoint.x;
 		touchArgs.y = touchPoint.y;
 		touchArgs.id = touchIndex;
-		ofNotifyEvent(ofEvents.touchCancelled, touchArgs);
+		ofNotifyEvent(ofEvents().touchCancelled, touchArgs);
 	}
 	
 	[self touchesEnded:touches withEvent:event];
