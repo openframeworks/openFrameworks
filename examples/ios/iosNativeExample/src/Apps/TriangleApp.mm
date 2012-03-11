9513c9b0b79741ec1c4c5c4897382203c997fb73
lukasz.karluk <julapy@googlemail.com>
2012-03-11 11:55:10 +1100

    added iosNativeExample to show to get OF apps running inside a native ios app.

---
+++ examples/ios/iosNativeExample/src/Apps/TriangleApp.mm
@@ -0,0 +1,103 @@
+#include "TriangleApp.h"
+
+TriangleApp :: TriangleApp () {
+    cout << "creating TriangleApp" << endl;
+}
+
+TriangleApp :: ~TriangleApp () {
+    cout << "destroying TriangleApp" << endl;
+}
+
+//--------------------------------------------------------------
+void TriangleApp::setup() {	
+	ofRegisterTouchEvents(this);
+	ofxAccelerometer.setup();
+	ofxiPhoneAlerts.addListener(this);
+	ofBackground(127);
+    
+    int fontSize = 8;
+    if (ofxiPhoneGetOFWindow()->isRetinaSupported())
+        fontSize *= 2;
+    
+    font.loadFont("fonts/mono0755.ttf", fontSize);
+}
+
+//--------------------------------------------------------------
+void TriangleApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void TriangleApp::draw() {
+    int a = MIN(ofGetWidth(), ofGetHeight()) * 0.3;
+    int b = sqrt( a * a + a * a );
+    int x = ofGetWidth()  * 0.5;
+    int y = ofGetHeight() * 0.5;
+    int p = 0;
+    
+	ofSetColor(ofColor::red);
+    ofTriangle(x, y - a, x + b, y + b, x - b, y + b);
+    
+    x = ofGetWidth()  * 0.2;
+    y = ofGetHeight() * 0.11;
+    p = ofGetHeight() * 0.035;
+    
+    ofSetColor(ofColor::white);
+    font.drawString("frame num      = " + ofToString( ofGetFrameNum() ),    x, y+=p);
+    font.drawString("frame rate     = " + ofToString( ofGetFrameRate() ),   x, y+=p);
+    font.drawString("screen width   = " + ofToString( ofGetWidth() ),       x, y+=p);
+    font.drawString("screen height  = " + ofToString( ofGetHeight() ),      x, y+=p);
+}
+
+//--------------------------------------------------------------
+void TriangleApp::exit() {
+    ofUnregisterTouchEvents(this);
+    ofxiPhoneAlerts.removeListener(this);
+}
+
+//--------------------------------------------------------------
+void TriangleApp::touchDown(ofTouchEventArgs &touch){
+
+}
+
+//--------------------------------------------------------------
+void TriangleApp::touchMoved(ofTouchEventArgs &touch){
+
+}
+
+//--------------------------------------------------------------
+void TriangleApp::touchUp(ofTouchEventArgs &touch){
+
+}
+
+//--------------------------------------------------------------
+void TriangleApp::touchDoubleTap(ofTouchEventArgs &touch){
+
+}
+
+//--------------------------------------------------------------
+void TriangleApp::lostFocus(){
+
+}
+
+//--------------------------------------------------------------
+void TriangleApp::gotFocus(){
+
+}
+
+//--------------------------------------------------------------
+void TriangleApp::gotMemoryWarning(){
+
+}
+
+//--------------------------------------------------------------
+void TriangleApp::deviceOrientationChanged(int newOrientation){
+
+}
+
+
+//--------------------------------------------------------------
+void TriangleApp::touchCancelled(ofTouchEventArgs& args){
+
+}
+
