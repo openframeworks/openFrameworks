7a4d658b3f1375ff0be2ef5149918ac306f463b5
bilderbuchi <bilderbuchi@phononoia.at>
2012-03-14 23:41:01 +0100

    Apply code style on OSC and logging examples.

--- examples/addons/oscReceiveExample/src/testApp.cpp
+++ examples/addons/oscReceiveExample/src/testApp.cpp
@@ -4,14 +4,14 @@
 void testApp::setup(){
 	// listen on the given port
 	cout << "listening for osc messages on port " << PORT << "\n";
-	receiver.setup( PORT );
+	receiver.setup(PORT);
 
 	current_msg_string = 0;
 	mouseX = 0;
 	mouseY = 0;
 	mouseButtonState = "";
 
-	ofBackground( 30, 30, 130 );
+	ofBackground(30, 30, 130);
 
 }
 
@@ -19,57 +19,56 @@ void testApp::setup(){
 void testApp::update(){
 
 	// hide old messages
-	for ( int i=0; i<NUM_MSG_STRINGS; i++ )
-	{
-		if ( timers[i] < ofGetElapsedTimef() )
+	for(int i = 0; i < NUM_MSG_STRINGS; i++){
+		if(timers[i] < ofGetElapsedTimef()){
 			msg_strings[i] = "";
+		}
 	}
 
 	// check for waiting messages
-	while( receiver.hasWaitingMessages() )
-	{
+	while(receiver.hasWaitingMessages()){
 		// get the next message
 		ofxOscMessage m;
-		receiver.getNextMessage( &m );
+		receiver.getNextMessage(&m);
 
 		// check for mouse moved message
-		if ( m.getAddress() == "/mouse/position" )
-		{
+		if(m.getAddress() == "/mouse/position"){
 			// both the arguments are int32's
-			mouseX = m.getArgAsInt32( 0 );
-			mouseY = m.getArgAsInt32( 1 );
+			mouseX = m.getArgAsInt32(0);
+			mouseY = m.getArgAsInt32(1);
 		}
 		// check for mouse button message
-		else if ( m.getAddress() == "/mouse/button" )
-		{
+		else if(m.getAddress() == "/mouse/button"){
 			// the single argument is a string
-			mouseButtonState = m.getArgAsString( 0 ) ;
+			mouseButtonState = m.getArgAsString(0);
 		}
-		else
-		{
+		else{
 			// unrecognized message: display on the bottom of the screen
 			string msg_string;
 			msg_string = m.getAddress();
 			msg_string += ": ";
-			for ( int i=0; i<m.getNumArgs(); i++ )
-			{
+			for(int i = 0; i < m.getNumArgs(); i++){
 				// get the argument type
-				msg_string += m.getArgTypeName( i );
+				msg_string += m.getArgTypeName(i);
 				msg_string += ":";
 				// display the argument - make sure we get the right type
-				if( m.getArgType( i ) == OFXOSC_TYPE_INT32 )
-					msg_string += ofToString( m.getArgAsInt32( i ) );
-				else if( m.getArgType( i ) == OFXOSC_TYPE_FLOAT )
-					msg_string += ofToString( m.getArgAsFloat( i ) );
-				else if( m.getArgType( i ) == OFXOSC_TYPE_STRING )
-					msg_string += m.getArgAsString( i );
-				else
+				if(m.getArgType(i) == OFXOSC_TYPE_INT32){
+					msg_string += ofToString(m.getArgAsInt32(i));
+				}
+				else if(m.getArgType(i) == OFXOSC_TYPE_FLOAT){
+					msg_string += ofToString(m.getArgAsFloat(i));
+				}
+				else if(m.getArgType(i) == OFXOSC_TYPE_STRING){
+					msg_string += m.getArgAsString(i);
+				}
+				else{
 					msg_string += "unknown";
+				}
 			}
 			// add to the list of strings to display
 			msg_strings[current_msg_string] = msg_string;
 			timers[current_msg_string] = ofGetElapsedTimef() + 5.0f;
-			current_msg_string = ( current_msg_string + 1 ) % NUM_MSG_STRINGS;
+			current_msg_string = (current_msg_string + 1) % NUM_MSG_STRINGS;
 			// clear the next line
 			msg_strings[current_msg_string] = "";
 		}
@@ -82,17 +81,16 @@ void testApp::update(){
 void testApp::draw(){
 
 	string buf;
-	buf = "listening for osc messages on port" + ofToString( PORT );
-	ofDrawBitmapString( buf, 10, 20 );
+	buf = "listening for osc messages on port" + ofToString(PORT);
+	ofDrawBitmapString(buf, 10, 20);
 
 	// draw mouse state
-	buf = "mouse: " + ofToString( mouseX, 4) +  " " + ofToString( mouseY, 4 );
-	ofDrawBitmapString( buf, 430, 20 );
-	ofDrawBitmapString( mouseButtonState, 580, 20 );
+	buf = "mouse: " + ofToString(mouseX, 4) +  " " + ofToString(mouseY, 4);
+	ofDrawBitmapString(buf, 430, 20);
+	ofDrawBitmapString(mouseButtonState, 580, 20);
 
-	for ( int i=0; i<NUM_MSG_STRINGS; i++ )
-	{
-		ofDrawBitmapString( msg_strings[i], 10, 40+15*i );
+	for(int i = 0; i < NUM_MSG_STRINGS; i++){
+		ofDrawBitmapString(msg_strings[i], 10, 40 + 15 * i);
 	}
 
 
@@ -100,45 +98,28 @@ void testApp::draw(){
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed  (int key){
-
-}
+void testApp::keyPressed(int key){}
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-}
+void testApp::keyReleased(int key){}
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
+void testApp::mouseMoved(int x, int y){}
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
+void testApp::mouseDragged(int x, int y, int button){}
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-}
+void testApp::mousePressed(int x, int y, int button){}
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
+void testApp::mouseReleased(int x, int y, int button){}
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
+void testApp::windowResized(int w, int h){}
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
+void testApp::gotMessage(ofMessage msg){}
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
+void testApp::dragEvent(ofDragInfo dragInfo){}
