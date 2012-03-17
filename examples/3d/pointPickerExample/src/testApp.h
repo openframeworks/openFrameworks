5c7974d34394008c55da8e62a62dcfeb97bc5b7e
Kyle McDonald <kyle@kylemcdonald.net>
2012-03-17 01:20:19 -0400

    fixed indentation on pointPickerExample

--- examples/3d/pointPickerExample/src/testApp.h
+++ examples/3d/pointPickerExample/src/testApp.h
@@ -3,21 +3,21 @@
 #include "ofMain.h"
 
 class testApp : public ofBaseApp{
-public:
-	void setup();
-	void update();
-	void draw();
-	
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
-	
-	ofMesh mesh;
-	ofEasyCam cam;
+	public:
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+		ofMesh mesh;
+		ofEasyCam cam;
 };
