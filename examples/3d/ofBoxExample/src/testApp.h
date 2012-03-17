8b2a19a404f327b7e61c1fb6f00359625140e0f7
Kyle McDonald <kyle@kylemcdonald.net>
2012-03-17 01:18:57 -0400

    fixed indentation on ofBoxExample

--- examples/3d/ofBoxExample/src/testApp.h
+++ examples/3d/ofBoxExample/src/testApp.h
@@ -3,22 +3,22 @@
 #include "ofMain.h"
 
 class testApp : public ofBaseApp {
-public:
-	void setup();
-	void update();
-	void draw();
-	
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
-	
-	ofImage ofLogo; // the OF logo
-	ofLight light; // creates a light and enables lighting
-	ofEasyCam cam; // add mouse controls for camera movement
+	public:
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+		ofImage ofLogo; // the OF logo
+		ofLight light; // creates a light and enables lighting
+		ofEasyCam cam; // add mouse controls for camera movement
 };
