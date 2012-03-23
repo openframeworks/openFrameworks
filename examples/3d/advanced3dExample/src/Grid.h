65d659b682b7b852d61a530571df3b9ebff178b2
elliotwoods <elliot.woods@gmail.com>
2012-03-23 11:51:21 +0000

    viewport example and applied lots of style changes to advanced3dExample

--- examples/3d/advanced3dExample/src/Grid.h
+++ examples/3d/advanced3dExample/src/Grid.h
@@ -1,23 +1,15 @@
-/*
- *  Grid.h
- *  advanced3dExample
- *
- *  Created by Elliot Woods on 19/01/2011.
- *  Copyright 2011 Kimchi and Chips. All rights reserved.
- *
- */
+#pragma once
+#include "ofMain.h"
 
-// Grid is a custom 3D
-// object that we've
-// created in this
-// example.
+// grid is a custom 3D
+//  object that we've
+//  created in this
+//  example.
 //
 // We inherit from ofNode
 
-#include "ofMain.h"
-
-class Grid : public ofNode
+class grid : public ofNode
 {
-public:
-	void	customDraw();
+	public:
+		void	customDraw();
 };
--- examples/3d/advanced3dExample/src/OrthoCamera.cpp
+++ examples/3d/advanced3dExample/src/OrthoCamera.cpp
@@ -1,28 +1,22 @@
-/*
- *  OrthoCamera.cpp
- *  advanced3dExample
- *
- *  Created by Elliot Woods on 19/01/2011.
- *  Copyright 2011 Kimchi and Chips. All rights reserved.
- *
- */
+#include "orthoCamera.h"
 
-#include "OrthoCamera.h"
+// Ortho camera is a custom
+//	camera we've created in
+//  this example
+//
+// We inherit from ofCamera
 
-OrthoCamera::OrthoCamera()
-{
+orthoCamera::orthoCamera(){
 	enableOrtho();
 	scale = 1;
 }
 
-void OrthoCamera::begin(ofRectangle rect)
-{
+void orthoCamera::begin(ofRectangle rect){
 	ofCamera::begin(rect);
 	
-	//////////////////////////////
-	// CALCULATE ASPECT RATIO
-	//////////////////////////////
-	//
+	//--
+	// Calculate aspect ratio
+
 	float vw = ofGetViewportWidth();
 	float vh = ofGetViewportHeight();
 	
@@ -31,27 +25,29 @@ void OrthoCamera::begin(ofRectangle rect)
 	
 	float scalex, scaley;
 	
-	if (ar > 1.0f)
-	{
+	if (ar > 1.0f){
 		//wide
 		scalex = scale * ar;
 		scaley = scale;
-	} else {
+	}else{
 		//tall
 		scalex = scale;
 		scaley = scale / ar;
 	}
-	//
-	//////////////////////////////
 	
-	//////////////////////////////
-	// SETUP PROJECTION
-	//////////////////////////////
 	//
+	//--
+	
+	
+	
+	//--
+	// Setup projection
+
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-scalex, scalex, -scaley, scaley, -20 * scale, 20 * scale);
 	glMatrixMode(GL_MODELVIEW);
+
 	//
-	//////////////////////////////
+	//--
 }
