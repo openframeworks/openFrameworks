55a5cca122d5cdac37b3de5536b7b19f4933219b
elliotwoods <elliot.woods@gmail.com>
2012-03-23 11:59:55 +0000

    ran uncrustify script

--- examples/3d/advanced3dExample/src/OrthoCamera.cpp
+++ examples/3d/advanced3dExample/src/OrthoCamera.cpp
@@ -13,33 +13,34 @@ orthoCamera::orthoCamera(){
 
 void orthoCamera::begin(ofRectangle rect){
 	ofCamera::begin(rect);
-	
+
 	//--
 	// Calculate aspect ratio
 
 	float vw = ofGetViewportWidth();
 	float vh = ofGetViewportHeight();
-	
+
 	//aspect ratio
-	float ar = vw/vh;
-	
+	float ar = vw / vh;
+
 	float scalex, scaley;
-	
-	if (ar > 1.0f){
+
+	if(ar > 1.0f){
 		//wide
 		scalex = scale * ar;
 		scaley = scale;
-	}else{
+	}
+	else{
 		//tall
 		scalex = scale;
 		scaley = scale / ar;
 	}
-	
+
 	//
 	//--
-	
-	
-	
+
+
+
 	//--
 	// Setup projection
 
