84c224dc29fb5da9de48f6c1cdd591c6d8e746bd
Jesus Gollonet <jesusgollonet@gmail.com>
2012-03-30 10:07:22 +0200

    fix in indentation

--- libs/openFrameworks/graphics/ofPixels.cpp
+++ libs/openFrameworks/graphics/ofPixels.cpp
@@ -473,7 +473,7 @@ void ofPixels_<PixelType>::rotate90To(ofPixels_<PixelType> & dst, int nClockwise
 	}
 
 	// otherwise, we will need to do some new allocaiton.
-    dst.allocate(height,width,getImageType());
+	dst.allocate(height,width,getImageType());
     
 	int strideSrc = width * channels;
 	int strideDst = dst.width * channels;
