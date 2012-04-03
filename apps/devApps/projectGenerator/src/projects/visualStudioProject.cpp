4d5e24149994c8f43286430f850c0eb3595d31ec
Matthew Gingold <m@gingold.com.au>
2012-04-03 16:02:34 +1000

    Ghetto hack to add debug and release lib files to correct AdditionalDependencies nodes - makes VS projects work - for now...

--- apps/devApps/projectGenerator/src/projects/visualStudioProject.cpp
+++ apps/devApps/projectGenerator/src/projects/visualStudioProject.cpp
@@ -13,32 +13,32 @@ void visualStudioProject::setup() {
 }
 
 bool visualStudioProject::createProjectFile(){
-    
+
     ofFile project(projectDir + projectName + ".vcxproj");
     ofFile user(projectDir + projectName + ".vcxproj.user");
     ofFile solution(projectDir + projectName + ".sln");
-    
+
     ofFile::copyFromTo(ofFilePath::join(templatePath,"emptyExample_vs2010.vcxproj"),project.path(),false, true);
     ofFile::copyFromTo(ofFilePath::join(templatePath,"emptyExample_vs2010.vcxproj.user"),user.path(), false, true);
     ofFile::copyFromTo(ofFilePath::join(templatePath,"emptyExample_vs2010.sln"),solution.path(), false, true);
 
     findandreplaceInTexfile(solution.path(),"emptyExample_vs2010",projectName);
     findandreplaceInTexfile(project.path(),"emptyExample",projectName);
-    
+
     string relRoot = getOFRelPath(ofFilePath::removeTrailingSlash(projectDir));
     if (relRoot != "../../../"){
-        
+
         string relRootWindows = relRoot;
         // let's make it windows friendly:
-        for(int i = 0; i < relRootWindows.length(); i++) { 
-            if( relRootWindows[i] == '/' ) 
-                relRootWindows[i] = '\\'; 
-        } 
-        
-        // sln has windows paths: 
+        for(int i = 0; i < relRootWindows.length(); i++) {
+            if( relRootWindows[i] == '/' )
+                relRootWindows[i] = '\\';
+        }
+
+        // sln has windows paths:
         findandreplaceInTexfile(solution.path(), "..\\..\\..\\", relRootWindows);
-        
-        // vcx has unixy paths: 
+
+        // vcx has unixy paths:
         //..\..\..\libs
         findandreplaceInTexfile(project.path(), "../../../", relRoot);
     }
@@ -46,9 +46,9 @@ bool visualStudioProject::createProjectFile(){
     return true;
 }
 
-    
+
 bool visualStudioProject::loadProjectFile(){
-    
+
     ofFile project(projectDir + projectName + ".vcxproj");
 	if(!project.exists()){
 		ofLogError(LOG_NAME) << "error loading" << project.path() << "doesn't exist";
@@ -60,29 +60,29 @@ bool visualStudioProject::loadProjectFile(){
 }
 
 
-bool visualStudioProject::saveProjectFile(){    
+bool visualStudioProject::saveProjectFile(){
     return doc.save_file((projectDir + projectName + ".vcxproj").c_str());
 }
 
 void visualStudioProject::addSrc(string srcFile, string folder){
-    
+
     // TODO: no folder love here...
-    
+
     fixSlashOrder(srcFile);
-    
+
     if (ofIsStringInString(srcFile, ".h") || ofIsStringInString(srcFile, ".hpp")){
         appendValue(doc, "ClInclude", "Include", srcFile);
     } else {
         appendValue(doc, "ClCompile", "Include", srcFile);
     }
-    
-} 
+
+}
 
 void visualStudioProject::addInclude(string includeName){
-   
-    
+
+
     fixSlashOrder(includeName);
-    
+
     pugi::xpath_node_set source = doc.select_nodes("//ClCompile/AdditionalIncludeDirectories");
     for (pugi::xpath_node_set::const_iterator it = source.begin(); it != source.end(); ++it){
         //printf("here ?? \n");
@@ -100,22 +100,22 @@ void visualStudioProject::addInclude(string includeName){
             string includesNew = unsplitString(strings, ";");
             node.node().first_child().set_value(includesNew.c_str());
         }
-       
+
     }
     //appendValue(doc, "Add", "directory", includeName);
-}  
+}
 void visualStudioProject::addLibrary(string libraryName){
 
-    
+
     fixSlashOrder(libraryName);
-    
-    // ok first, split path and library name.  
-    size_t found = libraryName.find_last_of("/");
+
+    // ok first, split path and library name.
+    size_t found = libraryName.find_last_of("\\");
     string libFolder = libraryName.substr(0,found);
     string libName = libraryName.substr(found+1);
-    
+
     // do the path, then the library
-    
+
     // paths for libraries
     pugi::xpath_node_set source = doc.select_nodes("//Link/AdditionalLibraryDirectories");
     for (pugi::xpath_node_set::const_iterator it = source.begin(); it != source.end(); ++it){
@@ -135,9 +135,10 @@ void visualStudioProject::addLibrary(string libraryName){
             node.node().first_child().set_value(libPathsNew.c_str());
         }
     }
-    
+
     // libs
     source = doc.select_nodes("//Link/AdditionalDependencies");
+    int platformCounter = 0;
     for (pugi::xpath_node_set::const_iterator it = source.begin(); it != source.end(); ++it){
         pugi::xpath_node node = *it;
         string includes = node.node().first_child().value();
@@ -148,13 +149,47 @@ void visualStudioProject::addLibrary(string libraryName){
                 bAdd = false;
             }
         }
+
+        // ok everything about this next bit is totally ghetto:
+        // FIRST, using a weak assumption that VS projects do Debug|Win32 then Release|Win32
+        // TODO: iterate by <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='BUILD TYPE|SOME PLATFORM'">
+        // and SECOND making the assumtion that debug libs end in d.lib, both because
+        // you might have a lib called libodd.lib/liboddd.lib
+        // and because many libraries don't even compile with the d at the end;
+        // I can see two ways around this:
+        // 1) check the sizes of the lib files and make two seperate lib lists
+        // 2) do a full string compare on the lib names and make two seperate lib lists
+        // ... but for now I'm going to commit this terrible hack:
+
+        if (platformCounter == 0){
+
+            // add debug libraries...
+
+            //cout << platformCounter << " " << libName << " " << ofSplitString(libName, "d.lib").size() << endl;
+            if(ofSplitString(libName, "d.lib").size() == 1){
+                // ... it's not a debug lib
+                bAdd = false;
+            }
+        }else{
+
+            // add release libs
+
+            //cout << platformCounter << " " << libName << " " << ofSplitString(libName, "d.lib").size() << endl;
+            if(ofSplitString(libName, "d.lib").size() > 1){
+                // ... it's a debug lib
+                bAdd = false;
+            }
+        }
+
+        // end ghetto ;-)
+
         if (bAdd == true){
             strings.push_back(libName);
             string libsNew = unsplitString(strings, ";");
             cout << libsNew << endl;
             node.node().first_child().set_value(libsNew.c_str());
         }
-       
+        platformCounter++;
     }
 
 
