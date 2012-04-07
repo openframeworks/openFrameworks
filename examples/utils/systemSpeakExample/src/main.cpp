297ffa36dbbf34311f456861a4eca23e890b2a84
Kyle McDonald <kyle@kylemcdonald.net>
2012-04-07 19:34:35 -0400

    added functions to systemSpeakExample

--- examples/utils/systemSpeakExample/src/main.cpp
+++ examples/utils/systemSpeakExample/src/main.cpp
@@ -1,8 +1,10 @@
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
 
-int main() {
-	ofAppGlutWindow window;
+//--------------------------------------------------------------
+int main(){
+	ofAppGlutWindow window; // create a window
+	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
 	ofSetupOpenGL(&window, 500, 500, OF_WINDOW);
-	ofRunApp(new testApp());
+	ofRunApp(new testApp()); // start the app
 }
