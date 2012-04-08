30d26c6d12c62941dc9c2e8fc0a4eec9d633d5ea
Kyle McDonald <kyle@kylemcdonald.net>
2012-04-08 16:50:30 -0400

    wip on drawSubsection

--- examples/graphics/imageSubsectionExample/src/main.cpp
+++ examples/graphics/imageSubsectionExample/src/main.cpp
@@ -1,16 +1,10 @@
-#include "ofMain.h"
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
 
-//========================================================================
-int main( ){
-
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 527, 640, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new testApp());
-
+//--------------------------------------------------------------
+int main(){
+	ofAppGlutWindow window; // create a window
+	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+	ofSetupOpenGL(&window, 527, 640, OF_WINDOW);
+	ofRunApp(new testApp()); // start the app
 }
--- libs/openFrameworks/graphics/ofImage.h
+++ libs/openFrameworks/graphics/ofImage.h
@@ -172,17 +172,17 @@ class ofImage_ : public ofBaseImage_<PixelType>{
         void				resetAnchor();								// resets the anchor to (0, 0)
 
 		// draw:
-		void 				draw(const ofRectangle & r);
-		void 				draw(const ofPoint & p, float w, float h);
-		void 				draw(float x, float y, float w, float h);
-		void 				draw(float x, float y, float w, float h, float sx, float sy);
-		void 				draw(float x, float y, float w, float h, float sx, float sy, float sw, float sh);
-		void 				draw(float x, float y, float z, float w, float h);
-		void 				draw(float x, float y, float z, float w, float h, float sx, float sy);
-		void 				draw(float x, float y, float z, float w, float h, float sx, float sy, float sw, float sh);
-		void 				draw(const ofPoint & p);
-		void 				draw(float x, float y);
-		void 				draw(float x, float y, float z);
+		void draw(const ofRectangle & r);
+		void draw(const ofPoint & p);
+		void draw(const ofPoint & p, float w, float h);
+		void draw(float x, float y);
+		void draw(float x, float y, float z);
+		void draw(float x, float y, float w, float h);
+		void draw(float x, float y, float z, float w, float h);
+		void draw(float x, float y, float w, float h, float sx, float sy);
+		void draw(float x, float y, float z, float w, float h, float sx, float sy);
+		void draw(float x, float y, float w, float h, float sx, float sy, float sw, float sh);
+		void draw(float x, float y, float z, float w, float h, float sx, float sy, float sw, float sh);
 
 		float 				getHeight();
 		float 				getWidth();
