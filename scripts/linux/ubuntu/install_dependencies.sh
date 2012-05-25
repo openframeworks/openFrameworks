77d647af651d05fd36bda5727dcaf6e02cb4a279
benben <ben@nerdlabor.de>
2012-05-25 13:30:01 +0200

    fixed exit codes for all install_dep scripts

--- scripts/linux/ubuntu/install_dependencies.sh
+++ scripts/linux/ubuntu/install_dependencies.sh
@@ -11,16 +11,18 @@ WHO=`sudo who am i`;ID=`echo ${WHO%% *}`
 GROUP_ID=`id --group -n ${ID}`
 cd ../../../libs/openFrameworksCompiled/project/$LIBSPATH
 make Debug
-if [ $? != 0 ]; then
-	echo "there has been a problem compiling Debug OF library"
-	echo "please report this problem in the forums"
-	exit
+exit_code=$?
+if [ $exit_code != 0 ]; then
+  echo "there has been a problem compiling Debug OF library"
+  echo "please report this problem in the forums"
+  exit $exit_code
 fi
 chown -R $ID:$GROUP_ID obj ../../lib/${LIBSPATH}/*
 make Release
-if [ $? != 0 ]; then
-        echo "there has been a problem compiling Release OF library"
-        echo "please report this problem in the forums"
+exit_code=$?
+if [ $exit_code != 0 ]; then
+  echo "there has been a problem compiling Release OF library"
+  echo "please report this problem in the forums"
+  exit $exit_code
 fi
 chown -R $ID:$GROUP_ID obj ../../lib/${LIBSPATH}/*
-# libpoco-dev 
