707b49d7a2f5c852b30e0a5d11e05081d3951676
Kyle McDonald <kyle@kylemcdonald.net>
2012-05-28 19:10:32 -0400

    refactoring comments. step by step. next up is checking parity with ofxOpenCv.

--- src/ofxCv.h
+++ src/ofxCv.h
@@ -1,34 +1,28 @@
-/*
- there are three types of functions in the ofxCv namespace:
- 1 low-level utility functions like imitate and toCv (Utilities.h)
- 2 wrapper functions that accept toCv-compatible objects (Wrappers.h)
- 3 helper functions that handle more complex tasks (Helpers.h)
- 
- also in the namespace are a few helper classes that make common tasks easier.
- for example: camera calibration (Calibration.h) and contour finding
- (ContourFinder.h).
- 
- all functions guarantee the size of the output with imitate when possible. data
- is returned using arguments when an expensive copy would be required or when
- you want to use a preallocated buffer, and a return value is used when the data
- is small or there is probably no preallocated buffer.
- */
-
 #pragma once
 
 // cv
 #include "opencv2/opencv.hpp"
 
 // ofxCv
-#include "ofxCv/Utilities.h"
-#include "ofxCv/Wrappers.h"
-#include "ofxCv/Helpers.h"
 
-#include "ofxCv/Distance.h"
-#include "ofxCv/Calibration.h"
-#include "ofxCv/ContourFinder.h"
-#include "ofxCv/Tracker.h"
-#include "ofxCv/RunningBackground.h"
-#include "ofxCv/Flow.h"
+// there are three types of functions in the ofxCv namespace
+#include "ofxCv/Utilities.h" // low-level utilities like imitate and toCv
+#include "ofxCv/Wrappers.h" // wrappers that accept toCv-compatible objects
+#include "ofxCv/Helpers.h" // helper functions that handle more complex tasks
+
+/*
+ all functions guarantee the size of the output with imitate when possible. data
+ is returned using arguments when an expensive copy would be required or when
+ you want to use a preallocated buffer, and a return value is used when the data
+ is small or there is probably no preallocated buffer.
+ */
+
+// also in the namespace are a few helper classes that make common tasks easier:
+#include "ofxCv/Distance.h" // edit distance
+#include "ofxCv/Calibration.h" // camera calibration
+#include "ofxCv/Tracker.h" // object tracking
+#include "ofxCv/ContourFinder.h" // contour finding and tracking
+#include "ofxCv/RunningBackground.h" // background subtraction
+#include "ofxCv/Flow.h" // optical flow
 
 // <3 kyle
