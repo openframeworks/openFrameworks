d264b2493d4bcdb97f7d70ee70c18d2f22ae19e8
lukasz.karluk <julapy@googlemail.com>
2012-06-07 18:11:52 +1000

    rewritten version of ofiPhoneVideoPlayer and AVFoundationVideoPlayer.

---
+++ examples/ios/moviePlayerExample/src/VideoPlayerControlsDelegateForOF.h
@@ -0,0 +1,36 @@
+//
+//  VideoPlayerControlsDelegateForOF.h
+//  moviePlayerExample
+//
+//  Created by lukasz karluk on 6/06/12.
+//
+
+#import <Foundation/Foundation.h>
+#import "VideoPlayerControls.h"
+#import "testApp.h"
+
+/**
+ *  this is a bit of obj-c that will help connect the OF app with the VideoPlayerControls UI.
+ *  all this is doing is re-routing UI events from the controls to the OF app.
+ */
+
+@interface VideoPlayerControlsDelegateForOF : NSObject <VideoPlayerControlsDelegate> {
+    testApp * app;
+}
+
+- (id)initWithApp:(testApp *)app;
+- (void)playPressed;
+- (void)pausePressed;
+- (void)scrubBegin;
+- (void)scrubToPosition:(float)position;
+- (void)scrubEnd;
+- (void)loadPressed;
+- (void)unloadPressed;
+- (void)loopOnPressed;
+- (void)loopOffPressed;
+- (void)nativeOnPressed;
+- (void)nativeOffPressed;
+- (void)muteOnPressed;
+- (void)muteOffPressed;
+
+@end
