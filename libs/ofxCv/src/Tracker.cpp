c396e072834e6d136077acaf0c0f3760126bf67a
Kyle McDonald <kyle@kylemcdonald.net>
2012-06-15 18:35:35 -0400

    forgot to git add

--- libs/ofxCv/src/Tracker.cpp
+++ libs/ofxCv/src/Tracker.cpp
@@ -3,8 +3,8 @@
 namespace ofxCv {
 	
 	float trackingDistance(const cv::Rect& a, const cv::Rect& b) {
-		float dx = (a.x + a.width / 2) - (b.x + b.width / 2);
-		float dy = (a.y + a.height / 2) - (b.y + b.height / 2);
+		float dx = (a.x + a.width / 2.) - (b.x + b.width / 2.);
+		float dy = (a.y + a.height / 2.) - (b.y + b.height / 2.);
 		float dw = a.width - b.width;
 		float dh = a.height - b.height;
 		float pd = sqrtf(dx * dx + dy * dy);
