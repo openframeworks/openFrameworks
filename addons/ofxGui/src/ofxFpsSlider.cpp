e3dd31c58a73cb0aa1b6034c5e1a767e5f04e933
Mark Pitchless <markpitchless@gmail.com>
2012-06-30 18:22:33 +0100

    FPS sliders don't need mouse events.

--- addons/ofxGui/src/ofxFpsSlider.cpp
+++ addons/ofxGui/src/ofxFpsSlider.cpp
@@ -3,6 +3,8 @@
 ofxFpsSlider* ofxFpsSlider::setup(string _name, float _max, float width, float height) {
     ofxFloatSlider::setup(_name, 0.0, 0.0, _max, width, height);
     setAutoMax(true);
+    // Clicking the fps slider should do nothing.
+    ofUnregisterMouseEvents((ofxFloatSlider*)this);
     return this;
 }
 
