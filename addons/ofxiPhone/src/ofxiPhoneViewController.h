80fe24a298dcb1dc8b1729b4b8a534a98e5ad875
lukasz.karluk <julapy@googlemail.com>
2012-07-05 23:28:38 +1000

    ofxiPhoneViewController has been completely refactored. it now only creates and sets up the glView. here, it will also rotate the glView to the device orientation - still need to work on this.

--- addons/ofxiPhone/src/ofxiPhoneViewController.h
+++ addons/ofxiPhone/src/ofxiPhoneViewController.h
@@ -4,50 +4,14 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "EAGLView.h"
 
-void ofPauseVideoGrabbers();
-void ofResumeVideoGrabbers();
-void ofReloadAllImageTextures();
-void ofUnloadAllFontTextures();
-void ofReloadAllFontTextures();
-void ofUpdateBitmapCharacterTexture();
+class ofBaseApp;
+@class ofxiOS_EAGLView;
 
-@interface ofxiPhoneViewController : UIViewController {
-    //
-}
+@interface ofxiPhoneViewController : UIViewController
 
-@property (nonatomic, retain) EAGLView* glView;
-@property (nonatomic, retain) NSLock* glLock;
-@property (nonatomic, retain) NSTimer* animTimer;
-@property (nonatomic, assign) float animFrameInterval;
-@property (nonatomic, assign) BOOL animating;
-@property (nonatomic, assign) BOOL displayLinkSupported;
-@property (nonatomic, assign) id displayLink;
+@property (nonatomic, retain) ofxiOS_EAGLView * glView;
 
-
-- (id) initWithFrame:(CGRect)frame 
-                 app:(ofBaseApp*)app;
-
-- (void) initApp:(ofBaseApp*)app;
-- (void) initGLLock;
-- (void) initGLViewWithFrame:(CGRect)frame;
-- (void) initAnimationVars;
-- (void) setupApp;
-- (void) clearBuffers;
-- (void) reloadTextures;
-
-- (void) lockGL;
-- (void) unlockGL;
-
-- (void) timerLoop;
-
-- (void) stopAnimation;
-- (void) startAnimation;
-
-- (void) setAnimationFrameInterval:(float)frameInterval;
-- (void) setFrameRate:(float)frameRate;
-
-- (void) destroy;
+- (id)initWithFrame:(CGRect)frame app:(ofBaseApp *)app;
 
 @end
