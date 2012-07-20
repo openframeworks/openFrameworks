f4d3813b24ab176db84b06ac5f0f9d1568edc12d
arturoc <arturo@openframeworks.cc>
2012-07-20 14:13:37 +0200

    OFAndroidSoundPlayer: release resources on app pause and stop

--- addons/ofxAndroid/ofAndroidLib/src/cc/openframeworks/OFAndroidSoundPlayer.java
+++ addons/ofxAndroid/ofAndroidLib/src/cc/openframeworks/OFAndroidSoundPlayer.java
@@ -3,13 +3,13 @@ package cc.openframeworks;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.util.FloatMath;
 import android.util.Log;
 
 public class OFAndroidSoundPlayer extends OFAndroidObject{
 	OFAndroidSoundPlayer(){
 		pan = 0.5f;
 		volume = leftVolume = rightVolume = 1;
-		player = new MediaPlayer();
 		bIsLoaded = false;
 		fileID = -1;
 	}
@@ -17,12 +17,17 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
 	void loadSound(String fileName, boolean stream){
 		try {
 			if(!multiPlay){
+				if(player!=null) unloadSound();
+				player = new MediaPlayer();
 				player.setDataSource(fileName);
 				player.prepare();
-				bIsLoaded = true;
 			}else{
+				if(pool!=null) unloadSound();
+				pool = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);
 				fileID = pool.load(fileName, 1);
 			}
+			setVolume(volume);
+			bIsLoaded = true;
 			this.fileName = fileName;
 		} catch (Exception e) {
 			Log.e("OF","couldn't load " + fileName,e);
@@ -30,7 +35,15 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
 	}
 	
 	void unloadSound(){
-		player.release();
+		if(player!=null){
+			player.release();
+			player = null;
+		}
+
+		if(pool!=null){
+			pool.release();
+			pool = null;	
+		}
 		fileName = null;
 		fileID = 0;
 		bIsLoaded = false;
@@ -38,16 +51,20 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
 	
 	void play(){
 		if(!multiPlay){
+			if(player==null) return;
 			if(getIsPlaying()) setPosition(0);	
 			player.start();
 		}else{
+			if(pool==null) return;
 			pool.play(fileID,leftVolume,rightVolume,1,loop?-1:0,speed);
 		}
 	}
 	void stop(){
 		if(!multiPlay){
+			if(player==null) return;
 			player.stop();
 		}else{
+			if(pool==null) return;
 			pool.autoPause();
 		}
 	}
@@ -58,11 +75,11 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
         // see: Curtis Roads: Computer Music Tutorial p 460
 		// thanks to jasch
         float angle = pan * 0.7853981633974483f; // in radians from -45. to +45.
-        float cosAngle = (float) Math.cos(angle);
-        float sinAngle = (float) Math.sin(angle);
+        float cosAngle = FloatMath.cos(angle);
+        float sinAngle = FloatMath.sin(angle);
         leftVolume  = (float)((cosAngle - sinAngle) * 0.7071067811865475) * vol; // multiplied by sqrt(2)/2
         rightVolume = (float)((cosAngle + sinAngle) * 0.7071067811865475) * vol; // multiplied by sqrt(2)/2
-		if(!multiPlay) player.setVolume(leftVolume, rightVolume);
+		if(!multiPlay && player!=null) player.setVolume(leftVolume, rightVolume);
 	}
 	
 	float getVolume(){
@@ -70,7 +87,7 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
 	}
 	
 	void setPan(float vol){
-		pan = vol*2-1;
+		pan = vol;
 		setVolume(volume);
 	}
 	
@@ -80,11 +97,13 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
 	
 	void setPaused(boolean bP){
 		if(!multiPlay){
+			if(player==null) return;
 			if(bP)
 				player.pause();
 			else
 				player.start();
 		}else{
+			if(pool==null) return;
 			if(bP){
 				pool.autoPause();
 			}else{
@@ -94,41 +113,39 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
 	}
 	
 	void setLoop(boolean bLp){
-		if(!multiPlay){
+		if(!multiPlay && player!=null){
 			player.setLooping(bLp);
 		}
 		loop = bLp;
 	}
 	
 	void setMultiPlay(boolean bMp){
-		if(bMp){
-			if(fileName!=null){
-				fileID = pool.load(fileName, 1);
-			}
-			player.release();
-		}else{
-			if(fileID!=-1) pool.unload(fileID);
-		}
+		if(multiPlay==bMp) return;
 		multiPlay = bMp;
+		if(fileName!=null && fileName!=null){
+			String currFileName = fileName;
+			unloadSound();
+			loadSound(currFileName, !bMp);
+		}
 	}
 	
 	void setPosition(float pct){
-		if(!multiPlay) player.seekTo((int) (player.getDuration()*pct)); // 0 = start, 1 = end;
+		if(!multiPlay && player!=null) player.seekTo((int) (player.getDuration()*pct)); // 0 = start, 1 = end;
 	}
 	
 	void setPositionMS(int ms){
-		if(!multiPlay) player.seekTo(ms); // 0 = start, 1 = end;
+		if(!multiPlay && player!=null) player.seekTo(ms); // 0 = start, 1 = end;
 	}
 	
 	float getPosition(){
-		if(!multiPlay)
+		if(!multiPlay && player!=null)
 			return ((float)player.getCurrentPosition())/(float)player.getDuration();
 		else
 			return 0;
 	}
 	
 	int getPositionMS(){
-		if(!multiPlay)
+		if(!multiPlay && player!=null)
 			return player.getCurrentPosition();
 		else
 			return 0;
@@ -136,9 +153,9 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
 	
 	boolean getIsPlaying(){
 		if(!multiPlay)
-			return player.isPlaying();
+			return player!=null && player.isPlaying();
 		else
-			return true;
+			return pool!=null;
 	}
 	
 	float getSpeed(){
@@ -156,16 +173,23 @@ public class OFAndroidSoundPlayer extends OFAndroidObject{
 	@Override
 	protected void appPause() {
 		stop();
+		String currFileName = fileName;
+		boolean currIsLoaded = bIsLoaded; 
+		unloadSound();
+		fileName = currFileName;
+		bIsLoaded = currIsLoaded;
 	}
 
 	@Override
 	protected void appResume() {
-		
+		if(bIsLoaded){
+			loadSound(fileName, !multiPlay);
+		}
 	}
 
 	@Override
 	protected void appStop() {
-		stop();
+		appPause();
 	}
 	
 	
