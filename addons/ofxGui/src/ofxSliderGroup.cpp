763a8f474afa02353a5dba4ee6b466dad84faf4d
Elliot Woods <elliot@kimchiandchips.com>
2012-07-23 22:58:50 +0100

    added ofParmaeter files to osx oF project

---
+++ addons/ofxGui/src/ofxSliderGroup.cpp
@@ -0,0 +1,9 @@
+//
+//  ofxSliderGroup.cpp
+//  ofxGrabScene-example
+//
+//  Created by Elliot Woods on 23/07/2012.
+//  Copyright (c) 2012 Kimchi and Chips. All rights reserved.
+//
+
+#include <iostream>
