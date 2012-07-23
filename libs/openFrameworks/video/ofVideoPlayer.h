e073afabebd4c42c4987ee014a7d7f979e224764
obviousjim <james@jamesgeorge.org>
2012-07-23 19:17:11 +0900

    Added variable and getter for movie path in ofVideoPlayer

--- libs/openFrameworks/video/ofVideoPlayer.h
+++ libs/openFrameworks/video/ofVideoPlayer.h
@@ -31,6 +31,7 @@ class ofVideoPlayer : public ofBaseVideoPlayer,public ofBaseVideoDraws{
 		ofPtr<ofBaseVideoPlayer>	getPlayer();
 
 		bool 				loadMovie(string name);
+	    string				getMoviePath();
 		void				setPixelFormat(ofPixelFormat pixelFormat);
 		void 				closeMovie();
 		void 				close();
@@ -94,6 +95,7 @@ class ofVideoPlayer : public ofBaseVideoPlayer,public ofBaseVideoDraws{
 		ofTexture * playerTex; // a seperate texture that may be optionally implemented by the player to avoid excessive pixel copying.
 		bool bUseTexture;
 		ofPixelFormat internalPixelFormat;
+	    string moviePath;
 };
 
 
