dbdc832538775d901acefdf90a1c6475dc3e2b90
George Schneeloch <bostonbusmap@gmail.com>
2012-07-26 11:44:48 -0400

    removing threshold code, using color image

--- examples/android/androidOpenCVFaceExample/src/testApp.h
+++ examples/android/androidOpenCVFaceExample/src/testApp.h
@@ -26,30 +26,17 @@ class testApp : public ofBaseApp{
 		void drawButton(const string& s, int x, int y, int w, int h, bool isToggled);
 
 		ofVideoGrabber grabber;
-		ofxCvGrayscaleImage gray, bg;//, diff;
+		ofxCvColorImage colorCv;
+		ofxCvColorImage colorCvSmall;
+		ofxCvGrayscaleImage grayCv;
 		ofxCvHaarFinder faceFinder;
 		std::vector<ofxCvBlob> faces;
 
-		bool captureBg;
+		void exit();
 
 		int one_second_time;
 		int camera_fps;
 		int frames_one_sec;
-		int threshold;
-
-
-		// TODO: replace this code with ofxGui when it supports big buttons
-		bool minusOn;
-		bool plusOn;
-
-		int minusX;
-		int plusX;
-		int minusY;
-		int plusY;
-
-		int buttonWidth;
-		int buttonHeight;
-
 };
 
 #endif	
