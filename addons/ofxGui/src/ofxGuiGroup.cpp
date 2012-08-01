9baa5fdbf5da91652b1bb5507ee8d6e3ad417acb
arturoc <arturo@openframeworks.cc>
2012-08-01 11:17:01 +0200

    ofxBaseGroup -> ofxGuiGroup + simplified inheritance for VecSlider + added ColorSlider

--- addons/ofxGui/src/ofxGuiGroup.cpp
+++ addons/ofxGui/src/ofxGuiGroup.cpp
@@ -1,18 +1,18 @@
-#include "ofxBaseGroup.h"
+#include "ofxGuiGroup.h"
 #include "ofxPanel.h"
 #include "ofxSliderGroup.h"
 
-ofxBaseGroup::ofxBaseGroup(string collectionName, string filename, float x, float y){
-    this->setup(collectionName, filename, x, y);
+ofxGuiGroup::ofxGuiGroup(string collectionName, string filename, float x, float y){
+    setup(collectionName, filename, x, y);
 }
 
-ofxBaseGroup::ofxBaseGroup(const ofParameterGroup & parameters, string filename, float x, float y){
-    this->setup(parameters, filename, x, y);
+ofxGuiGroup::ofxGuiGroup(const ofParameterGroup & parameters, string filename, float x, float y){
+    setup(parameters, filename, x, y);
 }
 
-ofxBaseGroup * ofxBaseGroup::setup(string collectionName, string filename, float x, float y){
+ofxGuiGroup * ofxGuiGroup::setup(string collectionName, string filename, float x, float y){
 	name = collectionName;
-	this->parameters.setName(name);
+	parameters.setName(name);
 	b.x = x;
 	b.y = y;
 	header = defaultHeight;
@@ -25,25 +25,25 @@ ofxBaseGroup * ofxBaseGroup::setup(string collectionName, string filename, float
 }
 
 
-ofxBaseGroup * ofxBaseGroup::setup(const ofParameterGroup & parameters, string filename, float x, float y){
-	name = parameters.getName();
-	this->parameters.setName(name);
+ofxGuiGroup * ofxGuiGroup::setup(const ofParameterGroup & _parameters, string _filename, float x, float y){
+	name = _parameters.getName();
+	parameters.setName(name);
 	b.x = x;
 	b.y = y;
 	header = defaultHeight;
 	spacing  = 1;
 	b.width = defaultWidth;
-    this->clear();
-	this->filename = filename;
+    clear();
+	filename = _filename;
     
-    this->add(parameters);
+    add(_parameters);
     
 	ofRegisterMouseEvents(this);
     
 	return this;
 }
 
-void ofxBaseGroup::add(ofxBaseGui * element){
+void ofxGuiGroup::add(ofxBaseGui * element){
 	collection.push_back( element );
     
 	element->setPosition(0, b.height);
@@ -61,31 +61,40 @@ void ofxBaseGroup::add(ofxBaseGui * element){
 	parameters.add(element->getParameter());
 }
 
-void ofxBaseGroup::add(const ofParameterGroup & parameters){
+void ofxGuiGroup::add(const ofParameterGroup & parameters){
     for(int i=0;i<parameters.size();i++){
 		string type = parameters.getType(i);
 		if(type==typeid(ofParameter<int>).name()){
 			ofParameter<int> p = parameters.getInt(i);
-			this->add(p);
+			add(p);
 		}else if(type==typeid(ofParameter<float>).name()){
 			ofParameter<float> p = parameters.getFloat(i);
-			this->add(p);
+			add(p);
 		}else if(type==typeid(ofParameter<bool>).name()){
 			ofParameter<bool> p = parameters.getBool(i);
-			this->add(p);
+			add(p);
 		}else if(type==typeid(ofParameter<ofVec2f>).name()){
             ofParameter<ofVec2f> p = parameters.getVec2f(i);
-            this->add(p);
+            add(p);
         }else if(type==typeid(ofParameter<ofVec3f>).name()){
             ofParameter<ofVec3f> p = parameters.getVec3f(i);
-            this->add(p);
+            add(p);
         }else if(type==typeid(ofParameter<ofVec4f>).name()){
             ofParameter<ofVec4f> p = parameters.getVec4f(i);
-            this->add(p);
+            add(p);
+        }else if(type==typeid(ofParameter<ofColor>).name()){
+            ofParameter<ofColor> p = parameters.getColor(i);
+            add(p);
+        }else if(type==typeid(ofParameter<ofShortColor>).name()){
+            ofParameter<ofShortColor> p = parameters.getShortColor(i);
+            add(p);
+        }else if(type==typeid(ofParameter<ofFloatColor>).name()){
+            ofParameter<ofFloatColor> p = parameters.getFloatColor(i);
+            add(p);
         }else if(type==typeid(ofParameterGroup).name()){
 			ofParameterGroup p = parameters.getGroup(i);
 			ofxPanel * panel = new ofxPanel(p);
-			this->add(panel);
+			add(panel);
 		}else{
 			ofLogError() << "ofxBaseGroup; can't add control of type " << type;
 		}
@@ -93,36 +102,48 @@ void ofxBaseGroup::add(const ofParameterGroup & parameters){
 
 }
 
-void ofxBaseGroup::add(ofParameter<float> & parameter){
+void ofxGuiGroup::add(ofParameter<float> & parameter){
 	add(new ofxFloatSlider(parameter.getName(),parameter));
 }
 
-void ofxBaseGroup::add(ofParameter<int> & parameter){
+void ofxGuiGroup::add(ofParameter<int> & parameter){
 	add(new ofxIntSlider(parameter.getName(),parameter));
 }
 
-void ofxBaseGroup::add(ofParameter<bool> & parameter){
+void ofxGuiGroup::add(ofParameter<bool> & parameter){
 	add(new ofxToggle(parameter.getName(),parameter));
 }
 
-void ofxBaseGroup::add(ofParameter<ofVec2f> & parameter){
+void ofxGuiGroup::add(ofParameter<ofVec2f> & parameter){
 	add(new ofxVecSlider<ofVec2f>(parameter.getName(),parameter));
 }
 
-void ofxBaseGroup::add(ofParameter<ofVec3f> & parameter){
+void ofxGuiGroup::add(ofParameter<ofVec3f> & parameter){
 	add(new ofxVecSlider<ofVec3f>(parameter.getName(),parameter));
 }
 
-void ofxBaseGroup::add(ofParameter<ofVec4f> & parameter){
+void ofxGuiGroup::add(ofParameter<ofVec4f> & parameter){
 	add(new ofxVecSlider<ofVec4f>(parameter.getName(),parameter));
 }
 
-void ofxBaseGroup::clear(){
+void ofxGuiGroup::add(ofParameter<ofColor> & parameter){
+	add(new ofxColorSlider<unsigned char>(parameter.getName(),parameter));
+}
+
+void ofxGuiGroup::add(ofParameter<ofShortColor> & parameter){
+	add(new ofxColorSlider<unsigned short>(parameter.getName(),parameter));
+}
+
+void ofxGuiGroup::add(ofParameter<ofFloatColor> & parameter){
+	add(new ofxColorSlider<float>(parameter.getName(),parameter));
+}
+
+void ofxGuiGroup::clear(){
 	collection.clear();
 	b.height = header + spacing; // weird to start out with something arbitrary like this
 }
 
-void ofxBaseGroup::mouseMoved(ofMouseEventArgs & args){
+void ofxGuiGroup::mouseMoved(ofMouseEventArgs & args){
 	ofMouseEventArgs a = args;
 	a.x -= b.x;
 	a.y -= b.y;
@@ -131,7 +152,7 @@ void ofxBaseGroup::mouseMoved(ofMouseEventArgs & args){
 	}
 }
 
-void ofxBaseGroup::mousePressed(ofMouseEventArgs & args){
+void ofxGuiGroup::mousePressed(ofMouseEventArgs & args){
 	setValue(args.x, args.y, true);
 	if( bGuiActive ){
 		ofMouseEventArgs a = args;
@@ -143,7 +164,7 @@ void ofxBaseGroup::mousePressed(ofMouseEventArgs & args){
 	}
 }
 
-void ofxBaseGroup::mouseDragged(ofMouseEventArgs & args){
+void ofxGuiGroup::mouseDragged(ofMouseEventArgs & args){
 	setValue(args.x, args.y, false);
 	if( bGuiActive ){
 		ofMouseEventArgs a = args;
@@ -155,7 +176,7 @@ void ofxBaseGroup::mouseDragged(ofMouseEventArgs & args){
 	}
 }
 
-void ofxBaseGroup::mouseReleased(ofMouseEventArgs & args){
+void ofxGuiGroup::mouseReleased(ofMouseEventArgs & args){
 	bGuiActive = false;
 	for(int k = 0; k < (int)collection.size(); k++){
 		ofMouseEventArgs a = args;
@@ -165,7 +186,7 @@ void ofxBaseGroup::mouseReleased(ofMouseEventArgs & args){
 	}
 }
 
-void ofxBaseGroup::draw(){
+void ofxGuiGroup::draw(){
 	ofPushStyle();
 	ofPushMatrix();
     
@@ -188,7 +209,7 @@ void ofxBaseGroup::draw(){
 	ofPopStyle();
 }
 
-vector<string> ofxBaseGroup::getControlNames(){
+vector<string> ofxGuiGroup::getControlNames(){
 	vector<string> names;
 	for(int i=0; i<(int)collection.size(); i++){
 		names.push_back(collection[i]->getName());
@@ -196,23 +217,23 @@ vector<string> ofxBaseGroup::getControlNames(){
 	return names;
 }
 
-ofxIntSlider ofxBaseGroup::getIntSlider(string name){
+ofxIntSlider ofxGuiGroup::getIntSlider(string name){
 	return getControlType<ofxIntSlider>(name);
 }
 
-ofxFloatSlider ofxBaseGroup::getFloatSlider(string name){
+ofxFloatSlider ofxGuiGroup::getFloatSlider(string name){
 	return getControlType<ofxFloatSlider>(name);
 }
 
-ofxToggle ofxBaseGroup::getToggle(string name){
+ofxToggle ofxGuiGroup::getToggle(string name){
 	return getControlType<ofxToggle>(name);
 }
 
-ofxButton ofxBaseGroup::getButton(string name){
+ofxButton ofxGuiGroup::getButton(string name){
 	return getControlType<ofxButton>(name);
 }
 
-ofxBaseGui * ofxBaseGroup::getControl(string name){
+ofxBaseGui * ofxGuiGroup::getControl(string name){
 	for(int i=0; i<(int)collection.size(); i++){
 		if(collection[i]->getName()==name){
 			return collection[i];
@@ -221,10 +242,10 @@ ofxBaseGui * ofxBaseGroup::getControl(string name){
 	return NULL;
 }
 
-void ofxBaseGroup::registerMouseEvents(){
+void ofxGuiGroup::registerMouseEvents(){
     ofRegisterMouseEvents(this);
 }
-void ofxBaseGroup::setValue(float mx, float my, bool bCheck){
+void ofxGuiGroup::setValue(float mx, float my, bool bCheck){
     
 	if( ofGetFrameNum() - currentFrame > 1 ){
 		bGuiActive = false;
@@ -239,17 +260,17 @@ void ofxBaseGroup::setValue(float mx, float my, bool bCheck){
 }
 
 
-int ofxBaseGroup::getNumControls(){
+int ofxGuiGroup::getNumControls(){
 	return collection.size();
 }
 
-ofxBaseGui * ofxBaseGroup::getControl(int num){
+ofxBaseGui * ofxGuiGroup::getControl(int num){
 	if(num<(int)collection.size())
 		return collection[num];
 	else
 		return NULL;
 }
 
-ofAbstractParameter & ofxBaseGroup::getParameter(){
+ofAbstractParameter & ofxGuiGroup::getParameter(){
 	return parameters;
 }
