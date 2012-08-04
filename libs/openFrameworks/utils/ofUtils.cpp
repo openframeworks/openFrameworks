a8a335b5f5fedfce611cee608698f7404b78a58e
Kyle McDonald <kyle@kylemcdonald.net>
2012-08-04 17:07:31 -0400

    fixes ofGetElapsedTimeMillis limit, fixes ofResetElapsedTimeCounter connection to ofGetElapsedTimeMicros, increases ofGetElapsedTimef accuracy, closes #428

--- libs/openFrameworks/utils/ofUtils.cpp
+++ libs/openFrameworks/utils/ofUtils.cpp
@@ -43,23 +43,24 @@ static unsigned long startTime = ofGetSystemTime();   //  better at the first fr
 static unsigned long startTimeMicros = ofGetSystemTimeMicros();
 
 //--------------------------------------
-int ofGetElapsedTimeMillis(){
-	return (int)(ofGetSystemTime() - startTime);
+unsigned long ofGetElapsedTimeMillis(){
+	return ofGetSystemTime() - startTime;
 }
 
 //--------------------------------------
 unsigned long ofGetElapsedTimeMicros(){
-	return (int)(ofGetSystemTimeMicros() - startTimeMicros);
+	return ofGetSystemTimeMicros() - startTimeMicros;
 }
 
 //--------------------------------------
 float ofGetElapsedTimef(){
-	return ((float) ((int)(ofGetSystemTime() - startTime)) / 1000.0f);
+	return ofGetElapsedTimeMicros() / 1000000.0f;
 }
 
 //--------------------------------------
 void ofResetElapsedTimeCounter(){
 	startTime = ofGetSystemTime();
+	startTimeMicros = ofGetSystemTimeMicros();
 }
 
 //=======================================
