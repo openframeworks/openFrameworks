f7a337228499ab8d91b0ae6b81289828e4f7487d
Philip Whitfield <public@underdoeg.com>
2012-08-08 15:11:05 +0200

    partial cleanup

--- apps/devApps/projectGenerator/build/projectGenerator.build/Debug/emptyExample.build/Script-E4B6FFFD0C3F9AB9008CF71C.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cp -f ../../../libs/fmodex/lib/osx/libfmodex.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/libfmodex.dylib"; install_name_tool -change ./libfmodex.dylib @executable_path/libfmodex.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/$PRODUCT_NAME";
