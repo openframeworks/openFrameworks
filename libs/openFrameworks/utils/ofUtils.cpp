e7f06c67cd07a356ac404aadd97ad02971425531
Elliot Woods <elliot@kimchiandchips.com>
2012-09-01 00:15:19 +0100

    updated comments of ofToDataPath

--- libs/openFrameworks/utils/ofUtils.cpp
+++ libs/openFrameworks/utils/ofUtils.cpp
@@ -258,40 +258,43 @@ string ofToDataPath(string path, bool makeAbsolute){
 		return path;
 	
 	// if our Current Working Directory has changed (e.g. file open dialog)
-	// this could be performed here, or wherever we might think we accidentally change the cwd
 	if (defaultWorkingDirectory().toString() != getWorkingDir().toString()) {
 		// change our cwd back to where it was on app load
 		chdir(defaultWorkingDirectory().toString().c_str());
 	}
+	// this could be performed here, or wherever we might think we accidentally change the cwd, e.g. after file dialogs on windows
 	
 	Poco::Path const  & dataPath(dataPathRoot());
 	Poco::Path inputPath(path);
 	Poco::Path outputPath;
 	
+	// if path is already absolute, just return it
 	if (inputPath.isAbsolute()) {
 		return path;
 	}
 	
 	// here we check whether path already refers to the data folder by looking for common elements
-	// if the path begins with the full contents of dataPathRoot
-	// we compare inputPath.toString() rather that the input var path ensure common formatting against dataPathRoot.toString() to
-	
-	// strip trailing slash from datapath since `path` may be input as a file formatted path even if it is a folder (i.e. missing trailing slash)
+	// if the path begins with the full contents of dataPathRoot then the data path has already been added
+	// we compare inputPath.toString() rather that the input var path to ensure common formatting against dataPath.toString()
 	string strippedDataPath = dataPath.toString();
+	// also, we strip the trailing slash from dataPath since `path` may be input as a file formatted path even if it is a folder (i.e. missing trailing slash)
 	strippedDataPath.resize(strippedDataPath.length() - 1);
 	
 	if (inputPath.toString().find(strippedDataPath) != 0) {
-		// inputPath doesn't contain data path already, so we add it
+		// inputPath doesn't contain data path already, so we build the output path as the inputPath relative to the dataPath
 		outputPath = dataPath;
 		outputPath.resolve(inputPath);
 	} else {
-		// inputPath does contain data path already, no need to change
+		// inputPath already contains data path, so no need to change
 		outputPath = inputPath;
 	}
 	
+	// finally, if we do want an absolute path and we don't already have one
 	if (makeAbsolute) {
+		// then we perform it here
 		return outputPath.absolute().toString();
 	} else {
+		// or output the relative path
 		return outputPath.toString();
 	}
 }
