58f5f19623d0c88ae17cf17f3d4a4b1788c40276
arturoc <arturo@openframeworks.cc>
2012-09-03 22:22:13 +0200

    ofTrueTypeFont: remove UNICODE encoding call which is the default

--- libs/openFrameworks/graphics/ofTrueTypeFont.cpp
+++ libs/openFrameworks/graphics/ofTrueTypeFont.cpp
@@ -278,7 +278,6 @@ bool ofTrueTypeFont::loadFont(string _filename, int _fontSize, bool _bAntiAliase
 		return false;
 	}
 
-	FT_Select_Charmap(face,FT_ENCODING_UNICODE);
 
 	FT_Set_Char_Size( face, fontSize << 6, fontSize << 6, dpi, dpi);
 	lineHeight = fontSize * 1.43f;
