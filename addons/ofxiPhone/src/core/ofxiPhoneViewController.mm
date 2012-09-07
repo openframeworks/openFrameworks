6f7f3d2c4b33286530dc685c425d0334c1ae6510
lukasz.karluk <julapy@googlemail.com>
2012-09-07 19:34:10 -0400

    orientation is now working splendidly. the glViewController now forces the orientation when requested and auto adjusts it when the user rotates the device to the new orientation.

--- addons/ofxiPhone/src/core/ofxiPhoneViewController.mm
+++ addons/ofxiPhone/src/core/ofxiPhoneViewController.mm
@@ -10,6 +10,7 @@
 
 @interface ofxiPhoneViewController() <EAGLViewDelegate> {
     UIInterfaceOrientation currentInterfaceOrientation;
+    UIInterfaceOrientation actualInterfaceOrientation;
 }
 @end
 
@@ -20,7 +21,7 @@
 - (id)initWithFrame:(CGRect)frame app:(ofxiPhoneApp *)app {
 
     if((self = [super init])) {
-        currentInterfaceOrientation = UIInterfaceOrientationPortrait;
+        currentInterfaceOrientation = actualInterfaceOrientation = self.interfaceOrientation;
         self.glView = [[[ofxiOSEAGLView alloc] initWithFrame:frame andApp:app] autorelease];
         self.glView.delegate = self;
     }
@@ -91,37 +92,87 @@
     
     CGSize screenSize = [UIScreen mainScreen].bounds.size;
     
+    NSLog(@"interfaceOrientation = %i", self.interfaceOrientation);
+    
+    CGPoint center;
+    if(UIInterfaceOrientationIsLandscape(actualInterfaceOrientation)) {
+        center.x = screenSize.height * 0.5;
+        center.y = screenSize.width * 0.5;
+    } else {
+        center.x = screenSize.width * 0.5;
+        center.y = screenSize.height * 0.5;
+    }
+    
     CGSize glViewSize = screenSize;
     if(UIInterfaceOrientationIsLandscape(interfaceOrientation)) {
         glViewSize.width = screenSize.height;
         glViewSize.height = screenSize.width;
     }
     
-    CGAffineTransform rotate;
-    if (interfaceOrientation == UIInterfaceOrientationPortrait) {
-        rotate = CGAffineTransformMakeRotation(0);          // 0 degrees.
-    } else if (interfaceOrientation == UIInterfaceOrientationPortraitUpsideDown) {
-        rotate = CGAffineTransformMakeRotation(M_PI);       // 180 degrees.
-    } else if (interfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
-        rotate = CGAffineTransformMakeRotation(M_PI * 1.5); // 270 degrees.
-    } else if (interfaceOrientation == UIInterfaceOrientationLandscapeRight) {
-        rotate = CGAffineTransformMakeRotation(M_PI * 0.5); // 90 degrees.
-    } else {
-        return;
-    }
+    float rot1 = [self rotationForOrinetation:currentInterfaceOrientation];
+    float rot2 = [self rotationForOrinetation:interfaceOrientation];
+    float rot3 = rot2 - rot1;
+    CGAffineTransform rotate = CGAffineTransformMakeRotation(rot3);
+    rotate = CGAffineTransformConcat(rotate, self.glView.transform);
     
     if(animated) {
         NSTimeInterval duration = 0.3;
+        if((UIInterfaceOrientationIsLandscape(currentInterfaceOrientation) && 
+           UIInterfaceOrientationIsLandscape(interfaceOrientation)) ||
+           (UIInterfaceOrientationIsPortrait(currentInterfaceOrientation) &&
+            UIInterfaceOrientationIsPortrait(interfaceOrientation))) {
+               duration = 0.6;
+           }
         [UIView animateWithDuration:duration animations:^{
+            self.glView.center = center;
             self.glView.transform = rotate;
             self.glView.bounds = CGRectMake(0, 0, glViewSize.width, glViewSize.height);
         }];
     } else {
+        self.glView.center = center;
         self.glView.transform = rotate;
         self.glView.bounds = CGRectMake(0, 0, glViewSize.width, glViewSize.height);
     }
     
     currentInterfaceOrientation = interfaceOrientation;
+}
+
+- (float)rotationForOrinetation:(UIInterfaceOrientation)interfaceOrientation {
+    if (interfaceOrientation == UIInterfaceOrientationPortrait) {
+        return 0;           // 0 degrees.
+    } else if (interfaceOrientation == UIInterfaceOrientationLandscapeRight) {
+        return M_PI * 0.5;  // 90 degrees.
+    } else if (interfaceOrientation == UIInterfaceOrientationPortraitUpsideDown) {
+        return M_PI;        // 180 degrees.
+    } else if (interfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
+        return M_PI * 1.5;  // 270 degrees.
+    } else {
+        return 0;
+    }
+}
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation 
+                                         duration:(NSTimeInterval)duration {
+
+    actualInterfaceOrientation = toInterfaceOrientation;
+    
+    CGSize screenSize = [UIScreen mainScreen].bounds.size;
+    
+    CGPoint center;
+    if(UIInterfaceOrientationIsLandscape(toInterfaceOrientation)) {
+        center.x = screenSize.height * 0.5;
+        center.y = screenSize.width * 0.5;
+    } else {
+        center.x = screenSize.width * 0.5;
+        center.y = screenSize.height * 0.5;
+    }
+    
+    self.glView.center = center;
+    self.glView.transform = CGAffineTransformMakeRotation(0);
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    return (toInterfaceOrientation == currentInterfaceOrientation);
 }
 
 @end
