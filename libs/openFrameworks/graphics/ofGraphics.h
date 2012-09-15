3750aa2617d2a8b8e0be6dc296c7c5c4f002d6a2
Christopher Baker <me@christopherbaker.net>
2012-09-15 00:43:24 -0700

    Updated ofRectRounded. -Added support for multiple corner radii. -Use arcs rather than bezier curves. -Respect for z. -Respect circleResolution.

--- libs/openFrameworks/graphics/ofGraphics.h
+++ libs/openFrameworks/graphics/ofGraphics.h
@@ -174,10 +174,26 @@ void ofRect(const ofRectangle & r);
 void ofRect(const ofPoint & p,float w,float h);
 void ofRect(float x,float y,float z,float w,float h);
 
-void ofRectRounded(const ofRectangle & b,float r);
-void ofRectRounded(const ofPoint & p,float w,float h,float r);
-void ofRectRounded(float x,float y,float w,float h,float r);
-void ofRectRounded(float x,float y,float z,float w,float h,float r);
+void ofRectRounded(const ofRectangle & b, float r);
+void ofRectRounded(const ofPoint & p, float w, float h, float r);
+void ofRectRounded(float x, float y, float w, float h, float r);
+void ofRectRounded(float x, float y, float z, float w, float h, float r);
+
+//----------------------------------------------------------
+void ofRectRounded(const ofPoint & p, float w, float h, float topLeftRadius,
+                                                        float topRightRadius,
+                                                        float bottomRightRadius,
+                                                        float bottomLeftRadius);
+
+void ofRectRounded(const ofRectangle & b, float topLeftRadius,
+                                          float topRightRadius,
+                                          float bottomRightRadius,
+                                          float bottomLeftRadius);
+
+void ofRectRounded(float x, float y, float z, float w, float h, float topLeftRadius,
+                                                                float topRightRadius,
+                                                                float bottomRightRadius,
+                                                                float bottomLeftRadius);
 
 void ofCurve(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3);
 void ofCurve(float x0, float y0, float z0, float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3);
