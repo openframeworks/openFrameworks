dfb0e91cd6ebae93b56c6b73a6d3e76d3056dc6a
Christopher Baker <me@christopherbaker.net>
2012-09-18 13:32:12 -0500

    Example comment clarifications and formatting cleanup.

--- examples/video/osxHighPerformanceVideoPlayerExample/src/testApp.cpp
+++ examples/video/osxHighPerformanceVideoPlayerExample/src/testApp.cpp
@@ -1,44 +1,51 @@
 #include "testApp.h"
 
-//High performance OS X Video Playback example
+// High Performance OS X Video Playback Example
 //--------------------------------------------------------------
-// This example shows how to use the OS X platform specific ofQTKitPlayer on its own
-// without the cross platform ofVideoPlayer wrapper
-// Apps you write in this way won't be cross platform,
-// but can be faster for HD videos 
+// This example shows how to use the OS X platform specific
+// ofQTKitPlayer on its own without the cross platform
+// ofVideoPlayer wrapper.  Apps you write in this way won't be
+// cross platform, but can be faster for HD videos .
 //--------------------------------------------------------------
+
 void testApp::setup(){
 	ofBackground(255, 255, 255);
 
 	frameByframe = false;
 
-	//# 1 Videos with alpha ---------------------------
-	// support alpha channel, but the pixel format must be enabled prior to loading!
-	// Turning off alpha will increase performance if it's not needed
+	// #1 Play videos with an alpha channel. ---------------------------
+	// ofQTKitPlayer videos encoded with Alpha channels (e.g. Animation Codec etc).
+    // The pixel format MUST be enabled prior to loading!
+	// If an alpha channels is not used, setting a non-alpha pixel format
+    // (e.g. OF_PIXELS_RGB) will increase performance.
 	fingerMovie.setPixelFormat(OF_PIXELS_RGBA);
 
+	//# 2 Decode Modes and video loading. -------------------------------
+	// If you don't need direct access to pixel data, you can use ofQTKitPlayer's
+    // highly optimized internal texture management system.  These mode are not
+    // available when using the cross-platform ofVideoPlayer.
 
-	//# 2 Decode Modes. -------------------------------
-	// If you don't need access to pixels and aren't using the ofQTKitPlayer inside of an ofVideoPlayer
-	// You can use QTKit's internal texture management system which is streamlined.
-
-	//Texture only is fastest, but no pixel access allowed
+	// Texture only is fastest, but no pixel access allowed.
 	ofQTKitDecodeMode decodeMode = OF_QTKIT_DECODE_TEXTURE_ONLY;
 
-	//pixels and texture together is faster than PIXEL_ONLY and manually upload textures.
-//	ofQTKitDecodeMode decodeMode = OF_QTKIT_DECODE_PIXELS_AND_TEXTURE;
+	// Pixels and texture together is faster than PIXEL_ONLY and manually uploaded textures.
+    // ofQTKitDecodeMode decodeMode = OF_QTKIT_DECODE_PIXELS_AND_TEXTURE;
 	
 	fingerMovie.loadMovie("movies/fingers.mov", decodeMode);
-    // You can even load URLs!
-    //fingerMovie.loadMovie("http://labs.silentlycrashing.net/fingers.mov", decodeMode);
-	//# 3 Synchronous seeking ---------------------------
-	// In many cases you want to be certain a call to movie.setFrame(x) and then a call to movie.update() will result in
-	// immediate calls to movie.getPixels() or movie.draw() containing frame x. This is slow to guarentuee,
-	// and QTKit naturally wants to update on its own time, a few moments later.
-	// If you are working in a way where this small delay is acceptable, then disabling synchronous seeking will
-	// speed things up
-	// Note that this can be turned on and off freely without having to reload the video
-	fingerMovie.setSynchronousSeeking(true);
+
+    // You can also load movies from URLs.
+    // fingerMovie.loadMovie("http://labs.silentlycrashing.net/fingers.mov", decodeMode);
+	
+    //# 3 Synchronous seeking. ---------------------------
+    // Like the legacy player, by default frames are available immediately for display
+    // via setFrame().  In many cases when random access via setFrame() is not required,
+    // significant speed increases can be gained by disabling synchronous seeking
+    // particularly when playing high bitrate HD video.
+    
+    // In "asynchronous" seek mode, individual frames can still be randomly accessed,
+    // but the pixels may not be available immediately without calling update() first.
+    
+    // fingerMovie.setSynchronousSeeking(false);
 
 	fingerMovie.play();
 }
