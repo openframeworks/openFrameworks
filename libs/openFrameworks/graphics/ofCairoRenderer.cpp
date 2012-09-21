972ae7eafb39481a7c4af5f2f636628cf7e16cbe
Nick Hardeman <nickhardeman@gmail.com>
2012-09-21 12:02:12 -0400

    added draw with of3dModel as a parameter, but does not render anything yet

--- libs/openFrameworks/graphics/ofCairoRenderer.cpp
+++ libs/openFrameworks/graphics/ofCairoRenderer.cpp
@@ -5,6 +5,7 @@
 #include "ofUtils.h"
 #include "ofMesh.h"
 #include "ofImage.h"
+#include "of3dPrimitives.h"
 
 //-----------------------------------------------------------------------------------
 static void helper_quadratic_to (cairo_t *cr,
@@ -383,6 +384,29 @@ void ofCairoRenderer::draw(ofMesh & vertexData, ofPolyRenderMode mode, bool useC
         ofLog(OF_LOG_WARNING,"Cairo rendering for meshes doesn't support colors, textures, or normals. drawing wireframe...");
     }
 	draw(vertexData,false,false,false);
+}
+
+//----------------------------------------------------------
+void ofCairoRenderer::draw( of3dModel& model, ofPolyRenderMode renderType  ) {
+    
+    if(model.hasScaling()) {
+        ofLog(OF_LOG_WARNING,"Cairo rendering for meshes doesn't support scaling");
+        //glEnable( GL_NORMALIZE );
+        //glPushMatrix();
+        //ofVec3f scale = model.getScale();
+        //glScalef( scale.x, scale.y, scale.z);
+    }
+    
+    for(int i = 0; i < model.getNumMeshes(); i++) {
+        //ofMesh& mesh = model.getMesh( i );
+        //draw( mesh, renderType, false, false, false );
+    }
+    
+    if(model.hasScaling()) {
+        //glPopMatrix();
+        //glDisable( GL_NORMALIZE );
+    }
+    
 }
 
 void ofCairoRenderer::draw(ofSubPath & path){
