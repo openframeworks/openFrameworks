137fb83b4cbf5b69ea13f30f522be2676fbefa04
Nick Hardeman <nickhardeman@gmail.com>
2012-09-21 11:57:08 -0400

    caching of primitives, moved resolution out of renderer, added draw of3dModel to GL rederer

--- libs/openFrameworks/graphics/of3dGraphics.h
+++ libs/openFrameworks/graphics/of3dGraphics.h
@@ -6,9 +6,49 @@
 //
 //
 
-#ifndef openFrameworksLib_of3dGraphics_h
-#define openFrameworksLib_of3dGraphics_h
+#pragma once
+
+#include "ofConstants.h"
+#include "ofColor.h"
+#include "ofPoint.h"
+#include "ofMatrix4x4.h"
+#include "ofRectangle.h"
+#include "ofTypes.h"
+#include "ofBaseTypes.h"
+#include "ofGLRenderer.h"
+#include "of3dPrimitives.h"
 
 
+// Grid Plane - drawn from the center //
+void ofSetPlaneResolution( int columns, int rows );
+ofVec2f ofGetPlaneResolution();
+void ofPlane(float x, float y, float width, float height);
+void ofPlane(float x, float y, float z, float width, float height);
+void ofPlane(ofPoint& position, float width, float height);
+void ofPlane( float width, float height );
 
-#endif
+// UV Sphere
+void ofSetSphereResolution(int res);
+int ofGetSphereResolution();
+void ofSphere(float x, float y, float radius);
+void ofSphere(float x, float y, float z, float radius);
+void ofSphere(const ofPoint& position, float radius);
+void ofSphere(float radius);
+
+// Ico Sphere
+void ofSetIcoSphereResolution( int res );
+int ofGetIcoSphereResolution();
+void ofIcoSphere(float x, float y, float z, float radius);
+void ofIcoSphere(float x, float y, float radius);
+void ofIcoSphere(const ofPoint& position, float radius);
+void ofIcoSphere(float radius);
+
+void ofBox(float x, float y, float z, float size);
+void ofBox(float x, float y, float size);
+void ofBox(const ofPoint& position, float size);
+void ofBox(float size);
+
+void ofCone(float x, float y, float z, float radius, float height);
+void ofCone(float x, float y, float radius, float height);
+void ofCone(const ofPoint& position, float radius, float height);
+void ofCone(float radius, float height);
