aad828a58739cc20f806020730bb27060c76c89b
arturo <arturo@openframeworks.cc>
2012-10-05 17:30:39 +0200

    ofAppRunner: linux, moved icon creation code to setupOpenGL so it can be overwritten from main

--- libs/openFrameworks/app/ofAppGlutWindow.cpp
+++ libs/openFrameworks/app/ofAppGlutWindow.cpp
@@ -289,6 +289,18 @@ void ofAppGlutWindow::setupOpenGL(int w, int h, int screenMode){
 	}
 	windowW = glutGet(GLUT_WINDOW_WIDTH);
 	windowH = glutGet(GLUT_WINDOW_HEIGHT);
+
+#ifdef TARGET_LINUX
+    ofPixels iconPixels;
+	#ifdef DEBUG
+    	iconPixels.allocate(ofIconDebug.width,ofIconDebug.height,ofIconDebug.bytes_per_pixel);
+    	GIMP_IMAGE_RUN_LENGTH_DECODE(iconPixels.getPixels(),ofIconDebug.rle_pixel_data,iconPixels.getWidth()*iconPixels.getHeight(),ofIconDebug.bytes_per_pixel);
+	#else
+    	iconPixels.allocate(ofIcon.width,ofIcon.height,ofIcon.bytes_per_pixel);
+    	GIMP_IMAGE_RUN_LENGTH_DECODE(iconPixels.getPixels(),ofIcon.rle_pixel_data,iconPixels.getWidth()*iconPixels.getHeight(),ofIcon.bytes_per_pixel);
+	#endif
+    setWindowIcon(iconPixels);
+#endif
 }
 
 //------------------------------------------------------------
@@ -323,18 +335,6 @@ void ofAppGlutWindow::initializeWindow(){
         // this is specific to windows (respond properly to close / destroy)
         fixCloseWindowOnWin32();
     #endif
-
-#ifdef TARGET_LINUX
-    ofPixels iconPixels;
-	#ifdef DEBUG
-    	iconPixels.allocate(ofIconDebug.width,ofIconDebug.height,ofIconDebug.bytes_per_pixel);
-    	GIMP_IMAGE_RUN_LENGTH_DECODE(iconPixels.getPixels(),ofIconDebug.rle_pixel_data,iconPixels.getWidth()*iconPixels.getHeight(),ofIconDebug.bytes_per_pixel);
-	#else
-    	iconPixels.allocate(ofIcon.width,ofIcon.height,ofIcon.bytes_per_pixel);
-    	GIMP_IMAGE_RUN_LENGTH_DECODE(iconPixels.getPixels(),ofIcon.rle_pixel_data,iconPixels.getWidth()*iconPixels.getHeight(),ofIcon.bytes_per_pixel);
-	#endif
-    setWindowIcon(iconPixels);
-#endif
 
 }
 
