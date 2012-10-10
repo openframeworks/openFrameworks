19857d54a74f92a196aec897ab89a26840797b1e
arturo <arturo@openframeworks.cc>
2012-10-10 14:40:35 +0200

    release script

---
+++ scripts/dev/release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+version=$1
+cd $(cat ~/.ofprojectgenerator/config)/scripts/dev
+
+./create_package.sh linux $version
+./create_package.sh linux64 $version
+./create_package.sh win_cb $version
+./create_package.sh vs2010 $version
+./create_package.sh ios $version
+./create_package.sh osx $version
+./create_package.sh android $version
+mv *.tar.gz /var/www/versions/
+mv *.zip /var/www/versions/
+
+wget http://openframeworks.cc/release_hook.php?version=${version} -O /dev/null
