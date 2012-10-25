df79900a9cf95ac71539bd457c98398a69ca489b
Christopher Baker <me@christopherbaker.net>
2012-10-25 17:21:26 -0500

    A working main.

--- apps/devApps/raspberrypi/src/main.cpp
+++ apps/devApps/raspberrypi/src/main.cpp
@@ -5,12 +5,12 @@
 //========================================================================
 int main( ){
 
-    ofxRaspberryPiWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofxRaspberryPiWindow window;
+	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	//ofRunApp( new testApp());
+	ofRunApp( new testApp());
 
 }
