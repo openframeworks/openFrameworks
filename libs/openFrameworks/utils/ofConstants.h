4a178e5917a8b8443f6955133031c4094c100c64
arturo <arturo@openframeworks.cc>
2012-11-07 14:12:51 +0100

    gles2: move includes to constants

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -162,6 +162,10 @@ enum ofTargetPlatform{
 #ifdef TARGET_OF_IPHONE
 	#import <OpenGLES/ES1/gl.h>
 	#import <OpenGLES/ES1/glext.h>
+
+	#import <OpenGLES/ES2/gl.h>
+	#import <OpenGLES/ES2/glext.h>
+
 	
 	#define TARGET_LITTLE_ENDIAN		// arm cpu	
 #endif
@@ -172,6 +176,9 @@ enum ofTargetPlatform{
 	#include <GLES/gl.h>
 	#define GL_GLEXT_PROTOTYPES
 	#include <GLES/glext.h>
+
+	#include <GLES2/gl2.h>
+	#include <GLES2/gl2ext.h>
 
 	#define TARGET_LITTLE_ENDIAN
 #endif
