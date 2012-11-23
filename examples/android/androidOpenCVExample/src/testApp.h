dce8f8fa8321e5b81ecfc3fe4eff8dfa3720bc83
Thiago Hersan <thiago.hersan+github@gmail.com>
2012-11-23 18:12:32 -0200

    updated androidOpenCVExample to use ofxAndroidApp

--- examples/android/androidOpenCVExample/src/testApp.h
+++ examples/android/androidOpenCVExample/src/testApp.h
@@ -3,9 +3,11 @@
 
 
 #include "ofMain.h"
+#include "ofxAndroid.h"
+
 #include "ofxOpenCv.h"
 
-class testApp : public ofBaseApp{
+class testApp : public ofxAndroidApp{
 	
 	public:
 		
@@ -15,12 +17,23 @@ class testApp : public ofBaseApp{
 
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
 
+		void touchDown(int x, int y, int id);
+		void touchMoved(int x, int y, int id);
+		void touchUp(int x, int y, int id);
+		void touchDoubleTap(int x, int y, int id);
+		void touchCancelled(int x, int y, int id);
+		void swipe(ofxAndroidSwipeDir swipeDir, int id);
+
+		void pause();
+		void stop();
+		void resume();
+		void reloadTextures();
+
+		bool backPressed();
+		void okPressed();
+		void cancelPressed();
 
 		ofVideoGrabber grabber;
 		ofxCvGrayscaleImage gray, bg;//, diff;
