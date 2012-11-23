a500f96fc44cdb67ceade460c7ceba6026923265
Thiago Hersan <thiago.hersan+github@gmail.com>
2012-11-23 18:29:12 -0200

    updated androidOpenCVFaceExample to use ofxAndroidApp

--- examples/android/androidOpenCVFaceExample/src/testApp.h
+++ examples/android/androidOpenCVFaceExample/src/testApp.h
@@ -3,10 +3,12 @@
 
 
 #include "ofMain.h"
+#include "ofxAndroid.h"
+
 #include "ofxOpenCv.h"
 #include <vector>
 
-class testApp : public ofBaseApp{
+class testApp : public ofxAndroidApp{
 	
 	public:
 		
@@ -16,14 +18,23 @@ class testApp : public ofBaseApp{
 
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
 
+		void touchDown(int x, int y, int id);
+		void touchMoved(int x, int y, int id);
+		void touchUp(int x, int y, int id);
+		void touchDoubleTap(int x, int y, int id);
+		void touchCancelled(int x, int y, int id);
+		void swipe(ofxAndroidSwipeDir swipeDir, int id);
 
-		void drawButton(const string& s, int x, int y, int w, int h, bool isToggled);
+		void pause();
+		void stop();
+		void resume();
+		void reloadTextures();
+
+		bool backPressed();
+		void okPressed();
+		void cancelPressed();
 
 		ofVideoGrabber grabber;
 		ofxCvColorImage colorCv;
@@ -31,8 +42,6 @@ class testApp : public ofBaseApp{
 		ofxCvGrayscaleImage grayCv;
 		ofxCvHaarFinder faceFinder;
 		std::vector<ofxCvBlob> faces;
-
-		void exit();
 
 		int one_second_time;
 		int camera_fps;
