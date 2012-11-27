bb35b1f5cfd43d69498c686e75bd1d2d86af88e0
arturoc <arturo@openframeworks.cc>
2012-11-27 13:06:56 +0100

    examples main.cpp: use default window so examples are multiplatform

--- examples/graphics/floatingPointImageExample/src/main.cpp
+++ examples/graphics/floatingPointImageExample/src/main.cpp
@@ -1,9 +1,12 @@
 #include "ofMain.h"
 #include "testApp.h"
+#ifndef TARGET_OPENGLES
 #include "ofAppGlutWindow.h"
+#endif
 
 //========================================================================
 int main( ){
+	#ifndef TARGET_OPENGLES
 	ofAppGlutWindow window;
 
 	// antialiasing hacks
@@ -11,6 +14,9 @@ int main( ){
 	//window.setGlutDisplayString("rgb double depth alpha samples>=4") //uncomment for pc fsaa
 
 	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+	#else
+	ofSetupOpenGL(1024,768,OF_WINDOW);
+	#endif
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
