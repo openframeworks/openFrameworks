d4d46ff435d8bc7198c2dbb185da7b46ead2c4db
arturoc <arturo@openframeworks.cc>
2012-11-27 13:06:56 +0100

    examples main.cpp: use default window so examples are multiplatform

--- examples/addons/allAddonsExample/src/main.cpp
+++ examples/addons/allAddonsExample/src/main.cpp
@@ -1,12 +1,12 @@
 #include "ofMain.h"
 #include "testApp.h"
-#include "ofAppGlutWindow.h"
+//#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
 
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 600,320, OF_WINDOW);			// <-------- setup the GL context
+    	//ofAppGlutWindow window;
+	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
