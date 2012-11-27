cb3988132c42109f899648cf012c880b559dd2fa
arturo <arturo@openframeworks.cc>
2012-11-27 13:19:17 +0100

    examples main: remove comments

--- examples/addons/xmlSettingsExample/src/main.cpp
+++ examples/addons/xmlSettingsExample/src/main.cpp
@@ -1,11 +1,9 @@
 #include "ofMain.h"
 #include "testApp.h"
-//#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
 
-    	//ofAppGlutWindow window;
 	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
