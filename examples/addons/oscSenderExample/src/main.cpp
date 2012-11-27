b7140969f7476e9066aed5a28bc6d01c7aff42e7
arturo <arturo@openframeworks.cc>
2012-11-27 13:33:18 +0100

    examples: fix main windows sizes and modes to the originals

--- examples/addons/oscSenderExample/src/main.cpp
+++ examples/addons/oscSenderExample/src/main.cpp
@@ -4,7 +4,7 @@
 //========================================================================
 int main( ){
 
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(640,480, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
