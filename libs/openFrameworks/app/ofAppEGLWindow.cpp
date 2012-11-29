e6769077256b16b2ad7fbddecec4f44109ac6191
arturoc <arturo@openframeworks.cc>
2012-11-29 11:36:58 +0100

    ofAppEGLWindow: simplified mouse texture loading macro

--- libs/openFrameworks/app/ofAppEGLWindow.cpp
+++ libs/openFrameworks/app/ofAppEGLWindow.cpp
@@ -35,19 +35,13 @@
 #define MOUSE_CURSOR_RUN_LENGTH_DECODE(image_buf, rle_data, size, bpp) do \
 { unsigned int __bpp; unsigned char *__ip; const unsigned char *__il, *__rd; \
   __bpp = (bpp); __ip = (image_buf); __il = __ip + (size) * __bpp; \
-  __rd = (rle_data); if (__bpp > 3) { /* RGBA */ \
+  __rd = (rle_data); \
     while (__ip < __il) { unsigned int __l = *(__rd++); \
       if (__l & 128) { __l = __l - 128; \
         do { memcpy (__ip, __rd, 4); __ip += 4; } while (--__l); __rd += 4; \
       } else { __l *= 4; memcpy (__ip, __rd, __l); \
                __ip += __l; __rd += __l; } } \
-  } else { /* RGB */ \
-    while (__ip < __il) { unsigned int __l = *(__rd++); \
-      if (__l & 128) { __l = __l - 128; \
-        do { memcpy (__ip, __rd, 3); __ip += 3; } while (--__l); __rd += 3; \
-      } else { __l *= 3; memcpy (__ip, __rd, __l); \
-               __ip += __l; __rd += __l; } } \
-  } } while (0)
+} while (0)
 static const struct {
   unsigned int 	 width;
   unsigned int 	 height;
