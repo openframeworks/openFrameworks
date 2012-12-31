7f4b68439e5ec6cd600b55be5d69571374fcdb31
Christopher Baker <me@christopherbaker.net>
2012-12-31 13:14:48 -0600

    Added to custom EGLWindow example.

--- examples/gles/customEGLWindowSettings/src/main.cpp
+++ examples/gles/customEGLWindowSettings/src/main.cpp
@@ -8,6 +8,8 @@ int main( ){
 	ofAppEGLWindow::Settings settings;
 
 	settings.eglWindowOpacity = 127;
+        settings.frameBufferAttributes[EGL_DEPTH_SIZE]   = 0; // 0 bits for depth
+        settings.frameBufferAttributes[EGL_STENCIL_SIZE] = 0; // 0 bits for stencil
 	
 	ofAppEGLWindow window(settings);
 
