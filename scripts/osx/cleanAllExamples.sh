dbde1df05d46f12c79ffac4d36f8210e0e96745f
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-01-12 23:06:06 -0500

    fix formatting of osx scripts

--- scripts/osx/cleanAllExamples.sh
+++ scripts/osx/cleanAllExamples.sh
@@ -5,31 +5,30 @@ cd ../../examples
 
 for category in $( ls . )
 do
-    if [ "$category" = "android" -o "$category" = "ios" ]; then
-            continue
-    fi
-    cd $category
-    for example in $( ls . )
-    do
-        echo "-----------------------------------------------------------------"
-        echo cleaning $example Debug
-        cd $example
-	xcodebuild -configuration Debug -target "$example" -project "$example.xcodeproj" clean
-	if [ "$?" != "0" ]; then
-	   echo failed cleaning $example Debug
-	   exit
+	if [ "$category" = "android" -o "$category" = "ios" ]; then
+		continue
 	fi
-        echo "-----------------------------------------------------------------"
-        echo cleaning $example Release
-        cd $example
-	xcodebuild -configuration Release -target "$example" -project "$example.xcodeproj" clean
-	if [ "$?" != "0" ]; then
-	   echo failed cleaning $example Release
-	   exit
-	fi
-        cd ..
-        echo "-----------------------------------------------------------------"
-        echo ""
-    done
-    cd ..
+	cd $category
+	for example in $( ls . )
+	do
+		echo "-----------------------------------------------------------------"
+		echo cleaning $example Debug
+		cd $example
+		xcodebuild -configuration Debug -target "$example" -project "$example.xcodeproj" clean
+		if [ "$?" != "0" ]; then
+			echo failed cleaning $example Debug
+			exit
+		fi
+		echo "-----------------------------------------------------------------"
+		echo cleaning $example Release
+		xcodebuild -configuration Release -target "$example" -project "$example.xcodeproj" clean
+		if [ "$?" != "0" ]; then
+			echo failed cleaning $example Release
+			exit
+		fi
+		cd ..
+		echo "-----------------------------------------------------------------"
+		echo ""
+	done
+	cd ..
 done
--- scripts/osx/testAllExamples.sh
+++ scripts/osx/testAllExamples.sh
@@ -4,28 +4,28 @@ cd ../../examples
 
 for category in $( ls . )
 do
-    if [ "$category" = "android" -o "$category" = "ios" ]; then
-            continue
-    fi
-    cd $category
-    for example in $( ls . )
-    do
-        echo "-----------------------------------------------------------------"
-	echo running $example
-        cd $example
-	if [ ! -d bin/"$example".app ]; then
-	        echo building $example
-		xcodebuild -configuration Release -target "$example" -project "$example.xcodeproj"
-		if [ "$?" != "0" ]; then
-		   echo failed building $example
-		   exit
-		fi
+	if [ "$category" = "android" -o "$category" = "ios" ]; then
+		continue
 	fi
-        cd bin
-        ./"$example".app/Contents/MacOS/"$example"
-        cd ../../
-        echo "-----------------------------------------------------------------"
-        echo ""
-    done
-    cd ..
+	cd $category
+	for example in $( ls . )
+	do
+		echo "-----------------------------------------------------------------"
+		echo running $example
+		cd $example
+		if [ ! -d bin/"$example".app ]; then
+	        echo building $example
+			xcodebuild -configuration Release -target "$example" -project "$example.xcodeproj"
+			if [ "$?" != "0" ]; then
+				echo failed building $example
+				exit
+			fi
+		fi
+		cd bin
+		./"$example".app/Contents/MacOS/"$example"
+		cd ../../
+		echo "-----------------------------------------------------------------"
+		echo ""
+	done
+	cd ..
 done
