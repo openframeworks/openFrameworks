9c75aedac070ef85e88734a1e982ee26baa08f18
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-01-12 23:06:06 -0500

    fix formatting of osx scripts

--- scripts/osx/buildAllExamples.sh
+++ scripts/osx/buildAllExamples.sh
@@ -4,30 +4,30 @@ cd ../../examples
 
 for category in $( ls . )
 do
-    if [ "$category" = "android" -o "$category" = "ios" ]; then
-            continue
-    fi
-    cd $category
-    for example in $( ls . )
-    do
-        echo "-----------------------------------------------------------------"
-        echo building $example Debug
-        cd $example
-	xcodebuild -configuration Debug -target "$example" -project "$example.xcodeproj"
-	if [ "$?" != "0" ]; then
-	   echo failed building $example Debug
-	   exit
+	if [ "$category" = "android" -o "$category" = "ios" ]; then
+		continue
 	fi
-        echo "-----------------------------------------------------------------"
-        echo building $example Release
-	xcodebuild -configuration Release -target "$example" -project "$example.xcodeproj"
-	if [ "$?" != "0" ]; then
-	   echo failed building $example Release
-	   exit
-	fi
-        cd ../
-        echo "-----------------------------------------------------------------"
-        echo ""
-    done
-    cd ..
+	cd $category
+	for example in $( ls . )
+	do
+		echo "-----------------------------------------------------------------"
+		echo building $example Debug
+		cd $example
+		xcodebuild -configuration Debug -target "$example" -project "$example.xcodeproj"
+		if [ "$?" != "0" ]; then
+			echo failed building $example Debug
+			exit
+		fi
+		echo "-----------------------------------------------------------------"
+		echo building $example Release
+		xcodebuild -configuration Release -target "$example" -project "$example.xcodeproj"
+		if [ "$?" != "0" ]; then
+			echo failed building $example Release
+			exit
+		fi
+		cd ../
+		echo "-----------------------------------------------------------------"
+		echo ""
+	done
+	cd ..
 done
