261b92a712f21b9f006e1659020e5870b15921d2
Roy Macdonald <macdonald.roy@gmail.com>
2013-01-14 13:47:47 -0300

    changed circle to Circle. changed listener methods names.

--- examples/events/simpleEventsExample/src/circle.cpp
+++ examples/events/simpleEventsExample/src/circle.cpp
@@ -6,12 +6,12 @@
 //  Copyright (c) 2012 micasa. All rights reserved.
 //
 
-#include "circle.h"
+#include "Circle.h"
 
 // the static event, or any static variable, must be initialized outside of the class definition.
-ofEvent<ofVec2f> circle::clickedInsideGlobal = ofEvent<ofVec2f>(); 
+ofEvent<ofVec2f> Circle::clickedInsideGlobal = ofEvent<ofVec2f>(); 
 
-void circle::setup(int radius, int x, int y, ofColor color){
+void Circle::setup(int radius, int x, int y, ofColor color){
     this->radius = radius;
     this->x = x;
     this->y = y;
@@ -19,7 +19,7 @@ void circle::setup(int radius, int x, int y, ofColor color){
     ofRegisterMouseEvents(this); // this will enable our circle class to listen to the mouse events.
 
 }
-void circle::draw(){
+void Circle::draw(){
     ofPushStyle();
     ofSetColor(color);
     ofCircle(x,y, radius);
@@ -27,10 +27,10 @@ void circle::draw(){
 }
 
 
-void circle::mouseMoved(ofMouseEventArgs & args){}
-void circle::mouseDragged(ofMouseEventArgs & args){}
-void circle::mousePressed(ofMouseEventArgs & args){}
-void circle::mouseReleased(ofMouseEventArgs & args){
+void Circle::mouseMoved(ofMouseEventArgs & args){}
+void Circle::mouseDragged(ofMouseEventArgs & args){}
+void Circle::mousePressed(ofMouseEventArgs & args){}
+void Circle::mouseReleased(ofMouseEventArgs & args){
     if (inside(args.x, args.y)) {
         // if the mouse is pressed over the circle an event will be notified (broadcasted) 
         // the circleEvent object will contain the mouse position, so this values are accesible to any class that is listening.
@@ -41,6 +41,6 @@ void circle::mouseReleased(ofMouseEventArgs & args){
 }
 
 //this function checks if the passed arguments are inside the circle.
-bool circle::inside(float _x, float _y ){
+bool Circle::inside(float _x, float _y ){
     return (ofVec2f(_x, _y).distance(ofVec2f(x, y)) <radius);
 }
