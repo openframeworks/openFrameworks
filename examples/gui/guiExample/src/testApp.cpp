3656511910acd70a823ce414c1a3c6d4669624a5
arturo <arturo@openframeworks.cc>
2013-02-06 19:19:31 +0100

    guiExample: use names with spaces to test serialization

--- examples/gui/guiExample/src/testApp.cpp
+++ examples/gui/guiExample/src/testApp.cpp
@@ -5,15 +5,15 @@
 void testApp::setup(){
 	ofSetVerticalSync(true);
 	
-	gui.setup("panel"); // most of the time you don't need a name
-	gui.add(filled.setup("bFill", true));
+	gui.setup(); // most of the time you don't need a name
+	gui.add(filled.setup("fill", true));
 	gui.add(radius.setup( "radius", 140, 10, 300 ));
 	gui.add(center.setup("center",ofVec2f(ofGetWidth()*.5,ofGetHeight()*.5),ofVec2f(0,0),ofVec2f(ofGetWidth(),ofGetHeight())));
 	gui.add(color.setup("color",ofColor(100,100,140),ofColor(0,0),ofColor(255,255)));
-	gui.add(circleResolution.setup("circleRes", 5, 3, 90));
-	gui.add(twoCircles.setup("twoCircles"));
+	gui.add(circleResolution.setup("circle res", 5, 3, 90));
+	gui.add(twoCircles.setup("two circles"));
 	gui.add(ringButton.setup("ring"));
-	gui.add(screenSize.setup("screenSize", ""));
+	gui.add(screenSize.setup("screen size", ""));
 	
 	ringButton.addListener(this,&testApp::ringButtonPressed);
 
