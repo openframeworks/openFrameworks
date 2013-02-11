56caffc011ac2d8ed8c7b30d628aaf0429d704c3
arturo <arturo@openframeworks.cc>
2013-02-11 17:15:42 +0100

    ubuntu/debian: detect gstreamer 1.0 and install it if exists

--- scripts/linux/ubuntu/install_codecs.sh
+++ scripts/linux/ubuntu/install_codecs.sh
@@ -1,1 +1,21 @@
-apt-get install libmpg123-dev gstreamer0.10-plugins-ugly
+#!/bin/bash
+
+if [ $EUID != 0 ]; then
+	echo "this script must be run using sudo"
+	echo ""
+	echo "usage:"
+	echo "sudo ./install_dependencies.sh"
+	exit $exit_code
+   exit 1
+fi
+
+GSTREAMER_VERSION=0.10
+
+dpkg-query -l libgstreamer2.0-dev >/dev/null
+RET = $?
+
+if [ $RET -eq 0 ]; then
+    GSTREAMER_VERSION=1.0
+fi
+
+apt-get install libmpg123-dev gstreamer${GSTREAMER_VERSION}-plugins-ugly
