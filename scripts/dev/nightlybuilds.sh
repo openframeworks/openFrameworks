e77906fa778c72a493df6ea813061fb4180a91e8
arturo <arturo@openframeworks.cc>
2013-02-12 16:49:08 +0100

    nightlybuilds: delete day by day

--- scripts/dev/nightlybuilds.sh
+++ scripts/dev/nightlybuilds.sh
@@ -8,8 +8,7 @@ fi
 
 # delete packages older than 3 weeks ago
 threeweeksago=$(date --date=@$[$(date +%s)-60*60*24*7*3] +%Y%m%d)
-weektodelete=${threeweeksago:0:7}
-rm /var/www/versions/nightly/of_v${weektodelete}_release.*
+rm /var/www/versions/nightly/of_v${threeweeksago}*_release.*
 
 
 lastversion=$(date +%Y%m%d)
