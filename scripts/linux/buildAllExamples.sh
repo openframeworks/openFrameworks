88ef2c44d21d6763bb617227ad4815b90565afa8
arturo <arturo@openframeworks.cc>
2013-02-28 11:01:54 +0100

    linux: build scripts, don't use cd but make -C path

--- scripts/linux/buildAllExamples.sh
+++ scripts/linux/buildAllExamples.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
 export LC_ALL=C
-cd ../../examples
 
-for category in $( find . -maxdepth 1 -type d )
+for category in $( find ../../examples/ -maxdepth 1 -type d )
 do
-    if [ "$category" = "./android" -o "$category" = "./ios" -o "$category" = "." ]; then
+    if [ "$category" = "../../examples/android" -o "$category" = "../../examples/ios" -o "$category" = "../../examples/" ]; then
             continue
     fi
-    cd $category
-    for example in $( find . -maxdepth 1 -type d | grep -v osx )
+    
+    echo category $category
+    for example in $( find $category -maxdepth 1 -type d | grep -v osx )
     do        
-	if [ "$example" = "." ]; then
+	if [ "$example" = "$category" ]; then
                 continue
         fi
 
         echo "-----------------------------------------------------------------"
-        echo "building " + $category/$example
-        cd $example
-	#projectGenerator .
-        make Debug -j2
+        echo "building " + $example
+        
+	    #projectGenerator .
+        make Debug -j2 -C $example
         ret=$?
         if [ $ret -ne 0 ]; then
           echo error compiling $example
           exit
         fi
-        make Release -j2
+        make Release -j2 -C $example
         ret=$?
         if [ $ret -ne 0 ]; then
           echo error compiling $example
           exit
         fi
-        cd ../
+        
         echo "-----------------------------------------------------------------"
         echo ""
     done
-    cd ..
 done
