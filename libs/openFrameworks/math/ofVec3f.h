680e1c0e25f88817e4281f1c52cc0b3825ca760e
Graham Reeves <graham@grahamreeves.com>
2013-03-01 22:52:00 +0000

    Fixed compile error; =0 in ofVec3f constructor definition

--- libs/openFrameworks/math/ofVec3f.h
+++ libs/openFrameworks/math/ofVec3f.h
@@ -281,7 +281,7 @@ ofVec3f operator/( float f, const ofVec3f& vec );
 /////////////////
 
 
-inline ofVec3f::ofVec3f( const ofVec2f& vec, float _z=0 ):x(vec.x), y(vec.y), z(_z) {}
+inline ofVec3f::ofVec3f( const ofVec2f& vec, float _z ):x(vec.x), y(vec.y), z(_z) {}
 inline ofVec3f::ofVec3f( const ofVec4f& vec ):x(vec.x), y(vec.y), z(vec.z) {}
 inline ofVec3f::ofVec3f(): x(0), y(0), z(0) {};
 inline ofVec3f::ofVec3f( float _all ): x(_all), y(_all), z(_all) {};
