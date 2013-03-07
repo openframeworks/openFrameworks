9596e063d39927920b934a64f6a9a7b77c85b1e0
Philip Whitfield <public@underdoeg.com>
2013-03-07 15:39:37 +0100

    fixed linux 32 & 64
    maybe some dependencies have to be added to the install_dependencies
    script

--- libs/openFrameworksCompiled/project/linux/config.linux.default.mk
+++ libs/openFrameworksCompiled/project/linux/config.linux.default.mk
@@ -40,3 +40,27 @@ include $(OF_SHARED_MAKEFILES_PATH)/config.linux.common.mk
 
 PLATFORM_CFLAGS += -march=native
 PLATFORM_CFLAGS += -mtune=native
+
+
+################################################################################
+# PLATFORM LIBRARIES
+#   These are library names/paths that are platform specific and are specified 
+#   using names or paths.  The library flag (i.e. -l) is prefixed automatically.
+#
+#   PLATFORM_LIBRARIES are libraries that can be found in the library search 
+#       paths.
+#   PLATFORM_STATIC_LIBRARIES is a list of required static libraries.
+#   PLATFORM_SHARED_LIBRARIES is a list of required shared libraries.
+#   PLATFORM_PKG_CONFIG_LIBRARIES is a list of required libraries that are 
+#       under system control and are easily accesible via the package 
+#       configuration utility (i.e. pkg-config)
+#
+#   See the helpfile for the -l flag here for more information:
+#       http://gcc.gnu.org/onlinedocs/gcc/Link-Options.html
+#
+#   Note: Leave a leading space when adding list items with the += operator
+################################################################################
+
+PLATFORM_PKG_CONFIG_LIBRARIES += x11
+PLATFORM_PKG_CONFIG_LIBRARIES += xxf86vm
+PLATFORM_PKG_CONFIG_LIBRARIES += xrandr
