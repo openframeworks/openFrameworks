01c5090628a93925e62902bba7ffd528c919c753
arturo <arturo@openframeworks.cc>
2013-03-07 13:21:46 +0100

    linux templates: added linuxarm + removed APPSUFFIX

---
+++ scripts/linux/template/linuxarmv6l/src/main.cpp
@@ -0,0 +1,14 @@
+#include "ofMain.h"
+#include "testApp.h"
+
+//========================================================================
+int main( ){
+
+	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp( new testApp());
+
+}
