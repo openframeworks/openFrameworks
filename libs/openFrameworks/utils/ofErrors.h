ba593038a7e5f679bcd5bab6eed7a772ab25f5b0
Christopher Baker <me@christopherbaker.net>
2013-03-09 13:03:47 -0600

    Removing ofThreadErrorHandler because naming an error handler after a thread doesn't make sense for the Poco::Error handler global scheme.  If people want to have a name in their thread handler, they will need to subclass ofErrorHandler and ofPushErrorHandler(myCustomErrorHandler);

--- libs/openFrameworks/utils/ofErrors.h
+++ libs/openFrameworks/utils/ofErrors.h
@@ -38,36 +38,3 @@ public:
         ofLogError("ofErrorHandler::exception") << "Uncaught exception: Unknown exception.";
     }
 };
-
-class ofThreadErrorHandler : public ofErrorHandler {
-public:
-
-    ofThreadErrorHandler(const string& _threadName = "NONE") {
-        threadName = _threadName;
-    }
-
-    virtual ~ofThreadErrorHandler() { }
-
-    virtual void exception(const Poco::Exception& exc) {
-        ofLogError("ofThreadErrorHandler::exception") << "Thread: Uncaught thread exception: " << exc.displayText();
-    }
-
-    virtual void exception(const std::exception& exc) {
-        ofLogError("ofThreadErrorHandler::exception") << "Uncaught thread exception: " << exc.what();
-    }
-    virtual void exception() {
-        ofLogError("ofThreadErrorHandler::exception") << "Uncaught thread exception: Unknown exception.";
-    }
-
-    virtual void setName(const string& name) {
-        threadName = name;
-    }
-
-    string getName() const {
-        return threadName;
-    }
-
-private:
-    string threadName;
-};
-
