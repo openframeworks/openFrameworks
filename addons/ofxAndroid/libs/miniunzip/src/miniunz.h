22e39345e9ce7170746875e5782455d4ecae8c0c
arturo <arturo@openframeworks.cc>
2013-03-11 16:16:09 +0100

    android: several fixes
    EGLConfig: better configuration choosing, solves problems with some devices
    Data folder detection
    loadPercent callback
    ofConstants: android detection through __ANDROID__ define

--- addons/ofxAndroid/libs/miniunzip/src/miniunz.h
+++ addons/ofxAndroid/libs/miniunzip/src/miniunz.h
@@ -17,5 +17,12 @@ int do_extract(
     const char* password);
 
 int do_compress(int argc,char * argv[]);
+int do_extract_one_entry(
+    unzFile uf,
+    int opt_extract_without_path,
+    int opt_overwrite,
+    const char* password,
+    unz_global_info * gi,
+    uLong i);
 
 #endif /* MINIUNZ_H_ */
