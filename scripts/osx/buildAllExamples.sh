70f1112a25f4b539c481200eaccfee899d5c53c2
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-03-12 13:37:19 -0700

    make osx scripts more like the linux scripts

--- scripts/osx/buildAllExamples.sh
+++ scripts/osx/buildAllExamples.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
-cd ../../examples
-
-for category in $( ls . )
+for category in $( find ../../examples -maxdepth 1 -type d )
 do
-	if [ "$category" = "android" -o "$category" = "ios" ]; then
-		continue
-	fi
-	cd $category
-	for example in $( ls . )
-	do
-		cd $example
-		if [ ! -d Makefile ]; then
-		    cp ../../../libs/openFrameworksCompiled/project/makefileCommon/Makefile.examples ./Makefile
-		fi
-		echo "-----------------------------------------------------------------"
-		echo building $example Debug
-		make Debug
-		if [ "$?" != "0" ]; then
-			echo failed building $example Debug
-			exit
-		fi
-		echo "-----------------------------------------------------------------"
-		echo building $example Release
-		make Release
-		if [ "$?" != "0" ]; then
-			echo failed building $example Release
-			exit
-		fi
-		cd ../
-		echo "-----------------------------------------------------------------"
-		echo ""
-	done
-	cd ..
+    if [ "$category" = "../../examples/android" -o "$category" = "../../examples/ios" -o "$category" = "../../examples" ]; then
+       	continue
+    fi
+    echo "-----------------------------------------------------------------"
+    echo building ALL examples in $category
+
+    for example in $( find $category -maxdepth 1 -type d )
+    do
+        if [ "$example" = "$category" ]; then
+       	    continue
+       	fi
+
+       	if [ ! -e "$example"/Makefile ]; then
+       	    cp ../../libs/openFrameworksCompiled/project/makefileCommon/Makefile.examples "$example"/Makefile
+       	fi
+
+	echo "-----------------------------------------------------------------"
+	echo building $example Debug
+	make Debug -j2 -C $example
+	ret=$?
+	if [ $ret -ne 0 ]; then
+       	    echo failed building $example Debug
+       	    exit
+       	fi
+
+       	echo "-----------------------------------------------------------------"
+       	echo building $example Release
+       	make Release -j2 -C $example
+       	ret=$?
+       	if [ $ret -ne 0 ]; then
+       	    echo failed building $example Release
+       	    exit
+       	fi
+       	echo "-----------------------------------------------------------------"
+       	echo ""
+    done
 done
--- scripts/osx/cleanAllExamples.sh
+++ scripts/osx/cleanAllExamples.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-#careful...
-cd ../../examples
-
-for category in $( ls . )
+for category in $( find ../../examples -maxdepth 1 -type d )
 do
-	if [ "$category" = "android" -o "$category" = "ios" ]; then
-		continue
-	fi
-	cd $category
-	for example in $( ls . )
-	do
-		cd $example
-		if [ ! -d Makefile ]; then
-		    cp ../../../libs/openFrameworksCompiled/project/makefileCommon/Makefile.examples ./Makefile
-		fi
-		echo "-----------------------------------------------------------------"
-		echo cleaning $example
-		make clean
-		if [ "$?" != "0" ]; then
-			echo failed cleaning $example
-			exit
-		fi
-		cd ..
-		echo "-----------------------------------------------------------------"
-		echo ""
-	done
-	cd ..
+    if [ "$category" = "../../examples/android" -o "$category" = "../../examples/ios" -o "$category" = "../../examples" ]; then
+       	continue
+    fi
+    echo "-----------------------------------------------------------------"
+    echo cleaning ALL examples in $category
+
+    for example in $( find $category -maxdepth 1 -type d )
+    do
+        if [ "$example" = "$category" ]; then
+       	    continue
+       	fi
+
+       	if [ ! -e "$example"/Makefile ]; then
+       	    cp ../../libs/openFrameworksCompiled/project/makefileCommon/Makefile.examples "$example"/Makefile
+       	fi
+
+	echo "-----------------------------------------------------------------"
+	echo cleaning $example
+	make clean -C $example
+	ret=$?
+	if [ $ret -ne 0 ]; then
+       	    echo failed cleaning $example
+       	    exit
+       	fi
+	echo rm -rf $example/obj 2> /dev/null
+	rm -rf $example/obj 2> /dev/null
+
+       	echo "-----------------------------------------------------------------"
+       	echo ""
+    done
 done
