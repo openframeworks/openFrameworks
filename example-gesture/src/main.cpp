0cec5a42a3c9555c19db3fe89a50011b24b5913c
Kyle McDonald <kyle@kylemcdonald.net>
2013-03-14 14:23:49 +0900

    removed osx-specific line. closes #74.

--- example-gesture/src/main.cpp
+++ example-gesture/src/main.cpp
@@ -3,7 +3,6 @@
 
 int main() {
 	ofAppGlutWindow window;
-	window.setGlutDisplayString("rgba double samples>=8 depth");
 	ofSetupOpenGL(&window, 1280, 720, OF_FULLSCREEN);
 	ofRunApp(new testApp());
 }
