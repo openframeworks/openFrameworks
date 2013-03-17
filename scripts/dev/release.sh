8c978ea4fa2a97455e868f9cd98d567a41d2a011
LeoColomb <leocolombaro@yahoo.fr>
2013-03-17 01:01:44 +0100

    Scripts

--- scripts/dev/release.sh
+++ scripts/dev/release.sh
@@ -6,6 +6,7 @@ cd $(cat ~/.ofprojectgenerator/config)/scripts/dev
 ./create_package.sh linux64 $version
 ./create_package.sh win_cb $version
 ./create_package.sh vs2010 $version
+./create_package.sh vs2012 $version
 ./create_package.sh ios $version
 ./create_package.sh osx $version
 ./create_package.sh android $version
