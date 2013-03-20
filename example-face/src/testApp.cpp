920b5796a871f3f837bc87e7a0a268ed5b472520
Kyle McDonald <kyle@kylemcdonald.net>
2013-03-20 22:09:27 +0900

    working towards a generic object finder/tracker

--- example-face/src/testApp.cpp
+++ example-face/src/testApp.cpp
@@ -3,55 +3,22 @@
 using namespace ofxCv;
 using namespace cv;
 
-/*
-	A classifier helper should handle the initial resizing for you. It should
-	have two "preset" modes for very accurate face finding and very fast.
-	It should also do automatic drawing, and give you OF objects instead of CV
-	objects.
-*/
-
 void testApp::setup() {
-	classifier.load(ofToDataPath("haarcascade_frontalface_alt2.xml"));
-	
-	scaleFactor = .25;
+	finder.setup("haarcascade_frontalface_alt2.xml");
 	cam.initGrabber(640, 480);
-	
-	// shouldn't need to allocate, resize should do this for us
-	graySmall.allocate(cam.getWidth() * scaleFactor, cam.getHeight() * scaleFactor, OF_IMAGE_GRAYSCALE);
 }
 
 void testApp::update() {
 	cam.update();
 	if(cam.isFrameNew()) {
-		convertColor(cam, gray, CV_RGB2GRAY);
-		resize(gray, graySmall);
-		Mat graySmallMat = toCv(graySmall);
-		if(ofGetMousePressed()) {
-			equalizeHist(graySmallMat, graySmallMat);
-		}
-		graySmall.update();
-		
-		classifier.detectMultiScale(graySmallMat, objects, 1.06, 1,
-			//CascadeClassifier::DO_CANNY_PRUNING |
-			//CascadeClassifier::FIND_BIGGEST_OBJECT |
-			//CascadeClassifier::DO_ROUGH_SEARCH |
-			0);
+		finder.update(cam);
 	}
 }
 
 void testApp::draw() {
-	if(ofGetKeyPressed()) {
-		graySmall.draw(0, 0, 640, 480);
-	} else {
-		cam.draw(0, 0);
+	cam.draw(0, 0);
+	for(int i = 0; i < finder.size(); i++) {
+		ofRect(finder.getObject(i));
 	}
-	
-	ofNoFill();
-	
-	ofScale(1 / scaleFactor, 1 / scaleFactor);
-	for(int i = 0; i < objects.size(); i++) {
-		ofRect(toOf(objects[i]));
-	}
-	
-	ofDrawBitmapString(ofToString(objects.size()), 10, 20);
+	ofDrawBitmapString(ofToString(finder.size()), 10, 20);
 }
--- example-face/src/testApp.h
+++ example-face/src/testApp.h
@@ -3,6 +3,71 @@
 #include "ofMain.h"
 #include "ofxCv.h"
 
+/*
+ A classifier helper should handle the initial resizing for you. It should
+ have two "preset" modes for very accurate face finding and very fast.
+ It should also do automatic drawing, and give you OF objects instead of CV
+ objects. It should also allow rotations, and handle tracking.
+ */
+
+namespace ofxCv {
+	class ObjectFinder {
+	protected:
+		float rescale;
+		bool useHistogramEqualization;
+		cv::Mat gray, graySmall;
+		cv::CascadeClassifier classifier;
+		vector<cv::Rect> objects;
+		
+	public:
+		ObjectFinder()
+		:rescale(1)
+		,useHistogramEqualization(true)
+		{
+		}
+		void setup(string cascadeFilename) {
+			classifier.load(ofToDataPath(cascadeFilename));
+		}		
+		template <class T> 
+		void update(T& img) {
+			update(toCv(img));
+		}
+		void update(cv::Mat img) {
+			convertColor(img, gray, CV_RGB2GRAY);
+			resize(gray, graySmall);
+			cv::Mat graySmallMat = toCv(graySmall);
+			if(useHistogramEqualization) {
+				equalizeHist(graySmallMat, graySmallMat);
+			}
+			classifier.detectMultiScale(graySmallMat, objects, 1.06, 1,
+																	//CascadeClassifier::DO_CANNY_PRUNING |
+																	//CascadeClassifier::FIND_BIGGEST_OBJECT |
+																	//CascadeClassifier::DO_ROUGH_SEARCH |
+																	0);
+		}		
+		unsigned int size() const {
+			return objects.size();
+		}
+		ofRectangle getObject(int i) const {
+			ofRectangle rect = toOf(objects[i]);
+			rect.scale(1 / rescale);
+			return rect;
+		}
+		void setRescale() {
+			this->rescale = rescale;
+		}
+		float getRescale() const {
+			return rescale;
+		}
+		void setUseHistogramEqualization(bool useHistogramEqualization) {
+			this->useHistogramEqualization = useHistogramEqualization;
+		}
+		bool getUseHistogramEqualization() const {
+			return useHistogramEqualization;
+		}
+	};
+}
+
 class testApp : public ofBaseApp {
 public:
 	void setup();
@@ -10,9 +75,5 @@ public:
 	void draw();
 	
 	ofVideoGrabber cam;
-	ofImage gray, graySmall;
-	
-	cv::CascadeClassifier classifier;
-	vector<cv::Rect> objects;
-	float scaleFactor;
+	ofxCv::ObjectFinder finder;
 };
