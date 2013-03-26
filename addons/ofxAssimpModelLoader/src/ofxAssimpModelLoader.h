1f6ffe81cb1c40c95aef74b3fea50a463e437606
lukasz karluk <julapy@googlemail.com>
2013-03-27 10:12:28 +1100

    optimised texture loading for ofxAssimpModelLoader. previously a texture was loaded for every mesh without checking if the texture already existed. this approach saves on any unnecessary texture loading.

--- addons/ofxAssimpModelLoader/src/ofxAssimpModelLoader.h
+++ addons/ofxAssimpModelLoader/src/ofxAssimpModelLoader.h
@@ -17,6 +17,7 @@
 #include "ofMain.h"
 #include "ofxAssimpMeshHelper.h"
 #include "ofxAssimpAnimation.h"
+#include "ofxAssimpTexture.h"
 
 class aiScene;
 class aiNode;
@@ -147,6 +148,7 @@ class ofxAssimpModelLoader{
         ofMatrix4x4 modelMatrix;
 
         vector<ofLight> lights;
+        vector<ofxAssimpTexture> textures;
         vector<ofxAssimpMeshHelper> modelMeshes;
         vector<ofxAssimpAnimation> animations;
         int currentAnimation; // DEPRECATED - to be removed with deprecated animation functions.
