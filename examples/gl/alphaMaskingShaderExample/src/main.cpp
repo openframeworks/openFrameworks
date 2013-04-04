5a401f3987ea4e34b7f40341a673f10ea1bbe1ec
arturo <arturo@openframeworks.cc>
2013-04-04 12:55:35 +0200

    alphaMaskingShaderExample: fixes for programmable GL

--- examples/gl/alphaMaskingShaderExample/src/main.cpp
+++ examples/gl/alphaMaskingShaderExample/src/main.cpp
@@ -1,13 +1,8 @@
 #include "ofMain.h"
 #include "testApp.h"
 
-#include "ofGLES2Renderer.h"
-
 //========================================================================
 int main( ){
-
-    ofGLES2Renderer * renderer = new ofGLES2Renderer();
-	ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(renderer)); // switch to GLES2 renderer.
 
 	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
 
