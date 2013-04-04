99d97d395d9ec1e966e07b9d80e8d3ddbe9df8bd
arturo <arturo@openframeworks.cc>
2013-04-04 12:49:16 +0200

    3dprimitives example: fixes for gles / programmable gl

--- examples/3d/3DPrimitivesExample/src/main.cpp
+++ examples/3d/3DPrimitivesExample/src/main.cpp
@@ -3,8 +3,6 @@
 
 //--------------------------------------------------------------
 int main(){
-	ofAppGlutWindow window; // create a window
-	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(&window, 1280, 720, OF_WINDOW);
+	ofSetupOpenGL(1280, 720, OF_WINDOW);
 	ofRunApp(new testApp()); // start the app
 }
