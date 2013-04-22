f9688fce942fadcc88f3edfcf6cba7d9186b4d7e
danomatika <danomatika@gmail.com>
2013-04-22 16:18:32 -0400

    added missing space to ofQTKitGrabber logging

--- libs/openFrameworks/video/ofQTKitGrabber.mm
+++ libs/openFrameworks/video/ofQTKitGrabber.mm
@@ -301,7 +301,7 @@
 				ofLogError("ofQTKitGrabber") << "Failed to add the video device input. Error: " << [[error localizedDescription] UTF8String];
 			}
 			else{
-				ofLogVerbose("ofQTKitGrabber") << "Attached video device" << [_selectedVideoDevice.description UTF8String] << ".";
+				ofLogVerbose("ofQTKitGrabber") << "Attached video device: " << [_selectedVideoDevice.description UTF8String] << ".";
 			}
 		}
 		else {
