99262f546621ee7b0c197723fe5a5888a7fd60a9
Joshua Noble <joshuajnoble@gmail.com>
2013-05-16 13:38:40 -0700

    going for a simpler ofXml

--- apps/devApps/xmlTesting/src/testApp.cpp
+++ apps/devApps/xmlTesting/src/testApp.cpp
@@ -5,43 +5,47 @@
 void testApp::setup(){
 	ofBackground(255,255,255);
 
-		//load a monospaced font
+    //load a monospaced font
 	//which we will use to show part of the xml structure
-	TTF.loadFont("mono.ttf", 7);
+	//TTF.loadFont("mono.ttf", 7);
     
     ofFile file;
     file.open("mySettings1.xml");
     ofBuffer buffer = file.readToBuffer();
     XML.loadFromBuffer(buffer.getText());
     
-    ofXml child = XML.getChild("food[0]");
-    //cout << child.getNumChildren() << endl;
-    ofXml child2 = child.getChildAt(0);
-    cout << child2.getName() << " " << child2.getValue() << endl;
-    
-    ofXml child3 = child.getChildAt(1);
-    cout << child3.getName() << " " << child3.getValue() << endl;
-    
-    ofXml child4 = child.getChildAt(2);
-    cout << child4.getName() << " " << child4.getValue() << endl;
     
     
     ofXml newXML;
     // check here
-    newXML.addValue("bar", "fux");
-    newXML.getChildAt(0).addAttribute("Giroud", "France");
+    newXML.addChild("bar");
+    newXML.setCurrentElement("bar");
+    newXML.setAttribute("Giroud", "France");
     //cout << newXML.toString() << endl;
-    newXML.getChildAt(0).addValue("baz", "qux");
-    cout << newXML.toString() << endl;
-    newXML.getChildAt(0).addValue("flim", "flam");
-    cout << newXML.toString() << endl;
-    newXML.getChildAt(0).addValue("num", "2");
-    newXML.getChildAt(0).getChildAt(0).addValue("num", "3");
+    newXML.addChild("baz");
+    newXML.setAttribute("/test1/test2[@Attr]", "Value");
+    
+    map<string, string> attrMap = newXML.getAttributes();
+    
+    cout << attrMap.size() << endl;
+    
+    newXML.addValue("flim", "flam");
+    newXML.addValue("flim2", "flam2");
+    newXML.setCurrentElement("../");
+    
     cout << newXML.toString() << endl;
     
-    child.addChild(newXML);
-    //cout << " ------------------------------ " << endl;
-    //cout << child.toString() << endl;
+    XML.resetCurrentElement();
+    XML.addXml(newXML);
+    
+    //cout << XML.toString() << endl;
+    
+    ofXml tmp = XML;
+    
+    //tmp.setCurrentElement("food[0]/serving/@units");
+
+    cout << " TMP XML VALUE IS " << tmp.getValue("food[0]/serving[@units]") << endl;
+    
     
 }
 
@@ -49,13 +53,29 @@ void testApp::setup(){
 void testApp::update(){
 	//we change the background color here based on the values
 	//affected by the mouse position
-	ofBackground((int)red,(int)green,(int)blue);
+	/*ofBackground((int)red,(int)green,(int)blue);
+    
+    if( ofGetFrameNum() % 30 == 0 )
+    {
+    
+        ofXml aNewXML;
+        aNewXML.addChild("root");
+        aNewXML.setCurrentElement("root");
+        aNewXML.addChild("fooy");
+        aNewXML.setCurrentElement("fooy");
+        aNewXML.addValue("bar/eggs/spam", "quux");
+        aNewXML.resetCurrentElement();
+        aNewXML.addValue("foox/bar/eggs/ham", "qux");
+        
+        cout << aNewXML.toString() << endl;
+        
+    }*/
 
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
+    ofDrawBitmapString(ofToString(ofGetFrameNum()), ofPoint(50, 50));
 }
 
 //--------------------------------------------------------------
@@ -82,7 +102,7 @@ void testApp::mouseDragged(int x, int y, int button){
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
 
-    cout << XML.toString() << endl;
+    /*cout << XML.toString() << endl;
     
     cout << " number before remove " <<  XML.getNumChildren() << endl;
     XML.remove("food[0]");
@@ -101,7 +121,29 @@ void testApp::mousePressed(int x, int y, int button){
     
     cout << XML.toString() << endl;
     
-    //cout << " number after add " << XML.getNumChildren() << endl;
+    //cout << " number after add " << XML.getNumChildren() << endl;*/
+    
+    /*ofXml aNewXML;
+    aNewXML.addChild("root");
+    
+    aNewXML.getChild("root").addValue("foox/bar/eggs/ham", "qux");
+    
+    //cout << aNewXML.toString() << endl;
+    
+    aNewXML.getChildAt(0).addValue("fooy/bar/eggs/spam", "quux");
+    aNewXML.getChildAt(0).addValue("fooz/bar/green_eggs/butter/yoyo/spam/He/will/miss/the/second/leg", "quuux");
+    
+    //cout << aNewXML.toString() << endl;
+    
+    for( int i = 0; i < 100; i++) {
+        ofXml tmp;
+        tmp.addValue("number", ofToString(i));
+        aNewXML.getChild("root").addChild(tmp);
+    }*/
+    
+    //cout << aNewXML.toString() << endl;
+    
+    cout << XML.toString() << endl;
     
 }
 
--- libs/openFrameworks/types/ofXml.h
+++ libs/openFrameworks/types/ofXml.h
@@ -19,6 +19,7 @@
 #include <Poco/DOM/Document.h>
 #include <Poco/DOM/Attr.h>
 #include <Poco/DOM/Node.h>
+#include <Poco/DOM/Text.h>
 #include <Poco/DOM/NodeIterator.h>
 #include <Poco/DOM/NodeFilter.h>
 #include <Poco/DOM/NamedNodeMap.h>  
@@ -26,83 +27,9 @@
 
 using namespace Poco::XML;
 
-/*class ofXmlElement {
-public:
-    
-    ofXmlElement(const ofXmlElement &rhs);
-    
-    const ofXmlElement& operator =( const ofXmlElement& rhs );
-    
-    ofXmlElement(Element*& e) {
-        element = e;
-    }
-    
-    ~ofXmlElement() {
-        if(element) {
-            //element->release();
-        }
-    }
-    
-    Element* operator()() const {
-        return element;
-    }
-    
-private:
-    
-    Element *element;
-    
-};*/
-
-class ofXmlDocument {
-public:
-    
-    ofXmlDocument(Document*& d) {
-        document = d;
-    }
-    
-    // Q: what's this? why's it here?
-    // A: So we can use auto_ptr with our Element. Element has a protected destructor,
-    // which means auto_ptr can't call it. They want you to call release(), but auto_ptr
-    // won't do that, so we get a wrapper to do it for us. When we get C++11 we'll use
-    // unique_ptr and this won't be a problem.
-    ~ofXmlDocument() {
-        if(document) {
-            //element->release();
-        }
-    }
-    
-    Document *getDocument(){
-        return document;
-    }
-    
-private:
-    
-    Document *document;
-    
-};
-
-template <class T>
-class deleter 
-{ 
-public:
-    void operator()(T *p) 
-    { 
-        //p->release();
-    }
-    
-}; 
-
 class ofXml {
     
 public:
-    
-    void deleteFunc() {
-        //p->release();
-    }
-    
-    /*static void deleteFunc(Node* p) {
-        p->release();
-    }*/
     
     ofXml();
     ~ofXml();
@@ -111,14 +38,14 @@ public:
     const ofXml& operator =( const ofXml& rhs );
     
     bool            addValue(const string& path, const string& value, bool createEntirePath = false);
-    string          getValue();
+    bool            addChild( const string& path );
+    
     string          getValue(const string& path);
+    string          getValue();
     bool            setValue(const string& path, const string& value);
     
-    string          getName();
-    
     string          getAttribute(const string& path);
-    bool            addAttribute(const string& path, const string& value);
+    //bool            addAttribute(const string& path, const string& value);
     bool            setAttribute(const string& path, const string& value);
     
     bool            clearAttributes(const string& path);
@@ -130,48 +57,40 @@ public:
     bool            removeAll(const string& path); // works for both attributes and tags
     bool            exists(const string& path); // works for both attributes and tags
     
-    void copyXmlToString(string & str);
-    bool loadFromBuffer( string buffer );
+    bool            setCurrentElement(const string& path);
+    string          getCurrentElementName();
+    bool            resetCurrentElement();
     
-    ofXml getParent();
-    ofXml getChild(const string &path);
-    ofXml getChildAt(int i);
+    bool            setCurrentElementToParent();
+    bool            setCurrentElementToParent(int numLevelsUp);
     
-    int getNumChildren();
-    bool hasParent();
-    vector<ofXml> getChildren();
+    bool            setCurrentElementToSibling();
+    bool            setCurrentElementToPrevSibling();
+    
+    void            copyXmlToString(string & str);
+    bool            loadFromBuffer( string buffer );
+    int             getNumChildren();
+    
+    void            addXml( ofXml& xml );
+    
     map<string, string> getAttributes();
-    
     string toString();
     
-    //Element* getElement();
-    //Element* getElement(const string& path);
+    Element*        getCurrentElement();
+    Element*        getElement(const string& path);
     
-    ofPtr<Element> getElement();
-    ofPtr<Element> getElement(const string& path);
-    
-    Document* getDocument();
-    
-    bool addChild( ofXml& child );
-    void setName( string name );
-    
-    bool isEmpty() { return _empty; } 
-    
+    Document*       getDocument();
+    Document*       getDocument() const;
+
        
 protected:
     
-    // this is how we get parent and child.
-    // TODO should currentElement be unique_ptr?
-    ofXml(const ofPtr<ofXmlDocument>& pdocument, ofPtr<Element> pcurrentElement);
-    
-    void addList( Element *addTo, Element *addFrom, bool topLevel = false);
+    //void addList( Element *addTo, Element *addFrom, vector<Element*>& toBeReleased);
+    //void addList( Element *addTo, Element *addFrom);
     
     string DOMErrorMessage(short msg);
     
-    Poco::XML::Document *documentRawPtr;
-    ofPtr<ofXmlDocument> document;
-    ofPtr<Element> element;
-    
-    bool _empty;
+    Poco::XML::Document *document;
+    Element *element;
     
 };
