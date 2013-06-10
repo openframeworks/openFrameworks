133c44e7c5f8cbad36c6dcf2cac171521e0b727a
Christopher Baker <me@christopherbaker.net>
2013-06-09 19:32:07 -0500

    Add ofFloatPixelsRef and ofShortPixelsRef typedefs.

--- libs/openFrameworks/graphics/ofPixels.h
+++ libs/openFrameworks/graphics/ofPixels.h
@@ -120,7 +120,8 @@ typedef ofPixels_<unsigned short> ofShortPixels;
 
 
 typedef ofPixels& ofPixelsRef;
-
+typedef ofFloatPixels& ofFloatPixelsRef;
+typedef ofShortPixels& ofShortPixelsRef;
 
 // sorry for these ones, being templated functions inside a template i needed to do it in the .h
 // they allow to do things like:
