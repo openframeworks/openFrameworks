aadf87ae8eb10d49b400f6ee9c3aa0644e9ea333
Christopher Baker <me@christopherbaker.net>
2013-06-12 10:05:22 -0500

    Update all example main.cpp to use default window, rather than ofAppGlutWindow.

--- example-coherent-lines/src/main.cpp
+++ example-coherent-lines/src/main.cpp
@@ -1,8 +1,6 @@
 #include "testApp.h"
-#include "ofAppGlutWindow.h"
 
 int main() {
-	ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1280, 720, OF_WINDOW);
+	ofSetupOpenGL(1280, 720, OF_WINDOW);
 	ofRunApp(new testApp());
 }
