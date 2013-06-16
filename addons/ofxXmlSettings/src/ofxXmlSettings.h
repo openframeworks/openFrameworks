d64b0a6471e244b4add79a7a7cef143a4b52fc67
LeoColomb <leocolombaro@yahoo.fr>
2013-06-16 21:27:39 +0200

    Fix oxXml include for VS

--- addons/ofxXmlSettings/src/ofxXmlSettings.h
+++ addons/ofxXmlSettings/src/ofxXmlSettings.h
@@ -3,7 +3,11 @@
 
 #include "ofMain.h"
 #include <string.h>
-#include "../libs/tinyxml.h"
+#if (_MSC_VER)
+#include "../libs/tinyxml/src/tinyxml.h"
+#else
+#include "tinyxml.h"
+#endif
 
 using namespace std;
 
