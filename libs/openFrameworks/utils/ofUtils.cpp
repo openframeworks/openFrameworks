1442b023f507e3eba93b68dd8ac90f9bddf78565
Bruno Dias <contact@diasbruno.com.br>
2013-06-29 03:22:04 -0300

    [fixed] use the new ofSystem() in ofGetTargetPlatform().

--- libs/openFrameworks/utils/ofUtils.cpp
+++ libs/openFrameworks/utils/ofUtils.cpp
@@ -741,7 +741,10 @@ void ofSaveFrame(bool bUseViewport){
 //--------------------------------------------------
 ofTargetPlatform ofGetTargetPlatform(){
 #ifdef TARGET_LINUX
-    string arch = ofSystem("uname -m");
+	ofSystemArgs args;
+	args.push_back( "-m" );
+	ofSystemResult r = ofSystem("uname", args);
+    string arch = r.getResult();
     if(Poco::icompare(arch,"x86_64")==0) {
         return OF_TARGET_LINUX64;
     } else if(Poco::icompare(arch,"armv6l")==0) {
