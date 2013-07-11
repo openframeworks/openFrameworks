1e97dd1c0492c12a7b6d834b4513da14edcd3f37
LeoColomb <leocolombaro@yahoo.fr>
2013-07-11 22:55:28 +0200

    Merge branch 'develop' of github.com:openframeworks/openFrameworks into Feature-Win32-Path

---
+++ addons/ofxiPhone/src/sound/SoundInputStream.h
@@ -0,0 +1,13 @@
+//
+//  SoundInputStream.h
+//  Created by Lukasz Karluk on 13/06/13.
+//  http://julapy.com/blog
+//
+
+#import "SoundStream.h"
+
+@interface SoundInputStream : SoundStream {
+
+}
+
+@end
