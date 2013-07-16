93cb075eef996e5e37801b87037f5663450a3787
arturo <arturo@openframeworks.cc>
2013-07-16 11:51:04 +0200

    ofFbo: fix vector allocation using reserve instead of resize. Cloese #2261

--- libs/openFrameworks/gl/ofFbo.cpp
+++ libs/openFrameworks/gl/ofFbo.cpp
@@ -522,7 +522,7 @@ void ofFbo::createAndAttachTexture(GLenum internalFormat, GLenum attachmentPoint
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + attachmentPoint, tex.texData.textureTarget, tex.texData.textureID, 0);
 	textures.push_back(tex);
 	
-	settings.colorFormats.reserve(attachmentPoint + 1);
+	settings.colorFormats.resize(attachmentPoint + 1);
 	settings.colorFormats[attachmentPoint] = internalFormat;
 	settings.numColorbuffers = settings.colorFormats.size();
 
