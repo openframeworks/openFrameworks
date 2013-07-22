55e181e23ff5b312053513fe725413ebf021d86a
Kyle McDonald <kyle@kylemcdonald.net>
2013-07-22 09:56:25 -0400

    switched osx sans font from index 7 to index 4 to match other OSes

--- libs/openFrameworks/graphics/ofTrueTypeFont.cpp
+++ libs/openFrameworks/graphics/ofTrueTypeFont.cpp
@@ -421,7 +421,7 @@ static bool loadFontFace(string fontname, int _fontSize, FT_Face & face, string 
 #elif defined(TARGET_OSX)
 		if(fontname==OF_TTF_SANS){
 			fontname = "Helvetica Neue";
-			fontID = 7;
+			fontID = 4;
 		}else if(fontname==OF_TTF_SERIF){
 			fontname = "Times New Roman";
 		}else if(fontname==OF_TTF_MONO){
