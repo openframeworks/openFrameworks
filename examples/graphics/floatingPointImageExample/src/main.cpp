511c5b34c1333fa497d736790fac6aa0af35a3e6
arturo <arturo@openframeworks.cc>
2013-07-24 13:14:27 +0200

    examples: fixes for rpi + remove remaining calls to glBegin/glEnd

--- examples/graphics/floatingPointImageExample/src/main.cpp
+++ examples/graphics/floatingPointImageExample/src/main.cpp
@@ -1,22 +1,9 @@
 #include "ofMain.h"
 #include "testApp.h"
-#ifndef TARGET_OPENGLES
-#include "ofAppGlutWindow.h"
-#endif
 
 //========================================================================
 int main( ){
-	#ifndef TARGET_OPENGLES
-	ofAppGlutWindow window;
-
-	// antialiasing hacks
-	//window.setGlutDisplayString("rgba double samples>=4 depth"); //uncomment for mac fsaa
-	//window.setGlutDisplayString("rgb double depth alpha samples>=4") //uncomment for pc fsaa
-
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
-	#else
-	ofSetupOpenGL(1024,768,OF_WINDOW);
-	#endif
+    ofSetupOpenGL(1024,768,OF_WINDOW);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
