e916cb23687066605e18b9d5701f6654c5709494
arturo <arturo@openframeworks.cc>
2013-07-24 17:57:48 +0200

    ofVbo: hack to fix vbo usage in rpi

--- libs/openFrameworks/gl/ofVbo.cpp
+++ libs/openFrameworks/gl/ofVbo.cpp
@@ -315,6 +315,15 @@ void ofVbo::setVertexData(const float * vert0x, int numCoords, int total, int us
 	}
 #endif
 
+
+	//FIXME: hack to make vbos work under raspberry
+	// vbos with usage!=GL_STATIC_DRAW don't seem to work without gles2
+#ifdef TARGET_RASPBERRY_PI
+	if(!ofIsGLProgrammableRenderer()){
+		usage = GL_STATIC_DRAW;
+	}
+#endif
+
 	if(vert0x == NULL) {
 		ofLogWarning("ofVbo") << "setVertexData(): bad data, ignoring NULL vertex float *";
 		return;	
@@ -332,7 +341,7 @@ void ofVbo::setVertexData(const float * vert0x, int numCoords, int total, int us
 
 	vertUsage = usage;
 	vertSize = numCoords;
-	vertStride = stride;
+	vertStride = stride==0?3*sizeof(float):stride;
 	totalVerts = total;
 	
 	glBindBuffer(GL_ARRAY_BUFFER, vertId);
@@ -352,13 +361,20 @@ void ofVbo::setColorData(const float * color0r, int total, int usage, int stride
 		ofLogWarning("ofVbo") << "setColorData(): bad data, ignoring NULL color float *";
 		return;	
 	}
+	//FIXME: hack to make vbos work under raspberry
+	// vbos with usage!=GL_STATIC_DRAW don't seem to work without gles2
+#ifdef TARGET_RASPBERRY_PI
+	if(!ofIsGLProgrammableRenderer()){
+		usage = GL_STATIC_DRAW;
+	}
+#endif
 	if(colorId==0) {
 		glGenBuffers(1, &(colorId));
 		retain(colorId);
 		enableColors();
 	}
 	colorUsage = usage;
-	colorStride = stride;
+	colorStride = stride==0?4*sizeof(float):stride;
 	
 	glBindBuffer(GL_ARRAY_BUFFER, colorId);
 	glBufferData(GL_ARRAY_BUFFER, total * stride, color0r, usage);
@@ -376,13 +392,20 @@ void ofVbo::setNormalData(const float * normal0x, int total, int usage, int stri
 		ofLogWarning("ofVbo") << "setNormalData(): bad data, ignoring NULL normal float *";
 		return;	
 	}
+	//FIXME: hack to make vbos work under raspberry
+	// vbos with usage!=GL_STATIC_DRAW don't seem to work without gles2
+#ifdef TARGET_RASPBERRY_PI
+	if(!ofIsGLProgrammableRenderer()){
+		usage = GL_STATIC_DRAW;
+	}
+#endif
 	if(normalId==0) {
 		glGenBuffers(1, &(normalId));
 		retain(normalId);
 		enableNormals();
 	}
 	normUsage = usage;
-	normalStride = stride;
+	normalStride = stride==0?3*sizeof(float):stride;
 	
 	glBindBuffer(GL_ARRAY_BUFFER, normalId);
 	glBufferData(GL_ARRAY_BUFFER, total * stride, normal0x, usage);
@@ -400,13 +423,20 @@ void ofVbo::setTexCoordData(const float * texCoord0x, int total, int usage, int 
 		ofLogWarning("ofVbo") << "setTexCoordData(): bad data, ignoring NULL tex coord float *";
 		return;	
 	}
+	//FIXME: hack to make vbos work under raspberry
+	// vbos with usage!=GL_STATIC_DRAW don't seem to work without gles2
+#ifdef TARGET_RASPBERRY_PI
+	if(!ofIsGLProgrammableRenderer()){
+		usage = GL_STATIC_DRAW;
+	}
+#endif
 	if(texCoordId==0) {
 		glGenBuffers(1, &(texCoordId));
 		retain(texCoordId);
 		enableTexCoords();
 	}
 	texUsage = usage;
-	texCoordStride = stride;
+	texCoordStride = stride==0?2*sizeof(float):stride;
 	
 	glBindBuffer(GL_ARRAY_BUFFER, texCoordId);
 	glBufferData(GL_ARRAY_BUFFER, total * stride, texCoord0x, usage);
@@ -420,6 +450,13 @@ void ofVbo::setIndexData(const ofIndexType * indices, int total, int usage){
 		ofLogWarning("ofVbo") << "setIndexData(): bad data, ignoring NULL indices float *";
 		return;
 	}
+	//FIXME: hack to make vbos work under raspberry
+	// vbos with usage!=GL_STATIC_DRAW don't seem to work without gles2
+#ifdef TARGET_RASPBERRY_PI
+	if(!ofIsGLProgrammableRenderer()){
+		usage = GL_STATIC_DRAW;
+	}
+#endif
 	if(indexId==0){
 		glGenBuffers(1, &(indexId));
 		retain(indexId);
@@ -440,6 +477,13 @@ void ofVbo::setAttributeData(int location, const float * attrib0x, int numCoords
 		return;
 	}
 
+	//FIXME: hack to make vbos work under raspberry
+	// vbos with usage!=GL_STATIC_DRAW don't seem to work without gles2
+#ifdef TARGET_RASPBERRY_PI
+	if(!ofIsGLProgrammableRenderer()){
+		usage = GL_STATIC_DRAW;
+	}
+#endif
 	if(attributeIds.find(location)==attributeIds.end()){
 		glGenBuffers(1, &(attributeIds[location]));
 		retain(attributeIds[location]);
@@ -667,7 +711,6 @@ void ofVbo::bind(){
 
 		glBindVertexArray(vaoID);
 	}
-
 
 	if(vaoChanged || !supportVAOs){
 		bool programmable = ofIsGLProgrammableRenderer();
