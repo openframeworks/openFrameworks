9adf3363d33e4b605dc9717102136c4c19925ac7
arturo <arturo@openframeworks.cc>
2013-07-31 18:11:04 +0200

    makefile osx: fix for GST flag check

--- libs/openFrameworksCompiled/project/osx/config.osx.default.mk
+++ libs/openFrameworksCompiled/project/osx/config.osx.default.mk
@@ -194,7 +194,7 @@ PLATFORM_CORE_EXCLUSIONS =
 
 # core sources
 PLATFORM_CORE_EXCLUSIONS += $(OF_LIBS_PATH)/openFrameworks/video/ofDirectShowGrabber.cpp
-ifneq($(USE_GST),1)
+ifneq ($(USE_GST),1)
 	PLATFORM_CORE_EXCLUSIONS += $(OF_LIBS_PATH)/openFrameworks/video/ofGstUtils.cpp
 	PLATFORM_CORE_EXCLUSIONS += $(OF_LIBS_PATH)/openFrameworks/video/ofGstVideoGrabber.cpp
 	PLATFORM_CORE_EXCLUSIONS += $(OF_LIBS_PATH)/openFrameworks/video/ofGstVideoPlayer.cpp
