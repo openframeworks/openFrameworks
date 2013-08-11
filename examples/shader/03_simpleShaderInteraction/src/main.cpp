1efd2162815ef8590128f69957e9fc4b08604589
lukasz karluk <julapy@googlemail.com>
2013-08-11 14:20:35 +0900

    changed back to standard window size.

--- examples/shader/03_simpleShaderInteraction/src/main.cpp
+++ examples/shader/03_simpleShaderInteraction/src/main.cpp
@@ -16,8 +16,7 @@ int main( ){
     ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
 #endif
     
-//	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
-    ofSetupOpenGL(1024,512, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
     
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
