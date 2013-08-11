d560f4ee87615c967258521065d87751c39c8317
Roy Macdonald <macdonald.roy@gmail.com>
2013-08-11 16:19:01 +0900

    ofSoundMixer implementation

--- libs/openFrameworks/sound/ofSoundMixer.cpp
+++ libs/openFrameworks/sound/ofSoundMixer.cpp
@@ -8,3 +8,170 @@
 //
 
 #include "ofSoundMixer.h"
+ofSoundMixer::ofSoundMixer(){}
+ofSoundMixer::~ofSoundMixer(){}
+//void connectTo(ofBaseSoundOutput &output);
+//void disconnect();
+ofSoundMixerChannelStrip& ofSoundMixer::add(ofBaseSoundOutput &newSoundSource, string label){
+    newChannel(label);
+    channels.back().source = &newSoundSource;
+    return channels.back();
+}
+ofSoundMixerChannelStrip& ofSoundMixer::newChannel(string label){
+    string newLabel = label;
+    if (channelExists(label)) {
+        int n = 1;
+        while (channelExists(label + ofToString(n))) {
+            n++;
+        }
+        newLabel = label + ofToString(n);
+    }
+    ofLogNotice() << "ofMixer::newChannel " << newLabel;    
+    channels.push_back(ofSoundMixerChannelStrip(newLabel));
+    return channels.back();
+}
+ofBaseSoundOutput& ofSoundMixer::getChannelStripSource(int channelNumber, bool createNewUnfound){
+    if (channelExists(channelNumber)) {
+        return *channels[channelNumber].source;
+    }else{
+        return NULL;
+    }
+}
+
+ofBaseSoundOutput& ofSoundMixer::getChannelStripSource(string label, bool createNewUnfound){
+    int n = getChannelNumByLabel(label);
+    if (n >- 1) {
+        return *channels[n].source;
+    }else{
+        if (createNewUnfound) {
+            return *newChannel(label).source;
+        }else{
+            return NULL;
+        }
+    }
+}
+
+ofSoundMixerChannelStrip& ofSoundMixer::getChannelStrip(int channelNumber){
+    if (channelExists(channelNumber)) {
+        return channels[channelNumber];
+    }else{
+        return NULL;
+    }
+}
+ofSoundMixerChannelStrip& ofSoundMixer::getChannelStrip(string label){
+    int n = getChannelNumByLabel(label);
+    if (n >- 1) {
+        return channels[n];
+    }else{
+        return NULL;
+    }
+}
+
+int ofSoundMixer::getNumChannels(){
+    return channels.size();
+}
+//volume range 0.0 to 1.0
+float ofSoundMixer::getChannelVolume(int channelNumber){
+    if (channelExists(channelNumber)) {
+        return channels[channelNumber].vol;
+    }else{
+        return -1;
+    }    
+}
+float ofSoundMixer::getChannelVolume(string label){
+    int n = getChannelNumByLabel(label);
+    if (n >- 1) {
+        return channels[n].vol;
+    }else{
+        return -1;
+    }
+}
+
+void ofSoundMixer::setChannelVolume(int channelNumber, float volume){
+    if (channelExists(channelNumber)) {
+        channels[channelNumber].vol =volume;
+    }
+}
+void ofSoundMixer::setChannelVolume(string label, float volume){
+    int n = getChannelNumByLabel(label);
+    if (n >- 1) {
+        channels[n].vol = volume;
+    }
+}
+
+float ofSoundMixer::getChannelPan(int channelNumber){
+    if (channelExists(channelNumber)) {
+       return channels[channelNumber].pan;
+    }else{
+        return -1;
+    }
+}
+void  ofSoundMixer::setChannelPan(int channelNumber, float pan){
+    if (channelExists(channelNumber)) {
+        channels[channelNumber].pan =pan;
+    }
+}
+
+float ofSoundMixer::getChannelPan(string label){
+    int n = getChannelNumByLabel(label);
+    if (n >- 1) {
+        return channels[n].pan;
+    }else{
+        return -1;
+    }
+}
+void  ofSoundMixer::setChannelPan(string label, float pan){
+    int n = getChannelNumByLabel(label);
+    if (n >- 1) {
+        channels[n].pan = pan;
+    }
+}
+
+void ofSoundMixer::setMasterVolume(float vol){
+    masterVolume = vol;
+}
+float ofSoundMixer::getMasterVolume(){
+    return masterVolume;
+}
+
+float ofSoundMixer::getMasterPan(){
+    return masterPan;
+}
+void ofSoundMixer::setMasterPan(float pan){
+    masterPan = pan;
+}
+int ofSoundMixer::getChannelNumByLabel(string label){
+    for (int i = 0; i < channels.size(); i++){
+        if (channels[i].label == label) {
+            return i;
+        }
+    }
+    ofLogWarning() << "ofSoundMixer: The requested channel " + label + " doesn't exist!!";
+    return -1;
+}
+string ofSoundMixer::getChannelLabel(int channelNumber){
+    if (channelExists(channelNumber)) {
+        return channels[channelNumber].label;
+    }else{
+        return "";
+    }
+}
+bool ofSoundMixer::channelExists(int channelNumber){
+    if (channelNumber < channels.size()) {
+        return true;
+    }else{
+         ofLogWarning() << "ofSoundMixer: The requested channel " + ofToString(channelNumber) + " doesn't exist!!"; 
+        return false;
+    }
+}
+bool ofSoundMixer::channelExists(string label){
+    for (int i = 0; i < channels.size(); i++){
+        if (channels[i].label == label) {
+            return true;
+        }
+    }
+    ofLogWarning() << "ofSoundMixer: The requested channel " + label + " doesn't exist!!";    
+    return false;
+}
+
+
--- libs/openFrameworks/sound/ofSoundMixer.h
+++ libs/openFrameworks/sound/ofSoundMixer.h
@@ -11,14 +11,64 @@
 
 #include "ofMain.h"
 
-class ofSoundMixer{
+class ofSoundMixerChannelStrip{//if you want to have a fancier mixer you can extend this class
+public:
+    ofSoundMixerChannelStrip();
+    ofSoundMixerChannelStrip(string lbl ="",float vl =1.0f, float pn = 0.5f):vol(vl), pan(pn), label(lbl){}
+    ~ofSoundMixerChannelStrip(){}
+    
+    float vol, pan;
+    ofBaseSoundOutput *source;
+    string label;
+};
+
+class ofSoundMixer: public ofSoundObject{
     ofSoundMixer();
     ~ofSoundMixer();
-    void connectTo();
-    void disconnect();
-    void add();
-
+    //void connectTo(ofBaseSoundOutput &output);
+    //void disconnect();
+    ofSoundMixerChannelStrip& add(ofBaseSoundOutput &newSoundSource, string label ="");
+    ofSoundMixerChannelStrip& newChannel(string label);
+    //volume range 0.0 to 1.0
+    float getChannelVolume(int channelNumber);
+    float getChannelVolume(string label);
     
+    void setChannelVolume(int channelNumber, float volume);
+    void setChannelVolume(string label, float volume);
+
+    ofBaseSoundOutput& getChannelStripSource(int channelNumber, bool createNewUnfound = true);
+    ofBaseSoundOutput& getChannelStripSource(string label, bool createNewUnfound = true);
+    
+    ofSoundMixerChannelStrip& getChannelStrip(int channelNumber);
+    ofSoundMixerChannelStrip& getChannelStrip(string label); 
+    
+    int getNumChannels();
+    
+    float getChannelPan(int channelNumber);
+    void  setChannelPan(int channelNumber, float pan);
+    
+    float getChannelPan(string label);
+    void  setChannelPan(string label, float pan);
+    
+    void setMasterVolume(float vol);
+    float getMasterVolume();
+    
+    float getMasterPan();
+    void setMasterPan(float pan);
+    
+    //void setChannelStripClass(ofSoundMixerChannelStrip *chnlStrip);
+    int getChannelNumByLabel(string label);
+    string getChannelLabel(int channelNumber);
+    bool channelExists(int channelNumber);
+    bool channelExists(string label);
+protected:
+    vector<ofSoundMixerChannelStrip>channels;
+    float masterPan;
+    float masterVolume;
+    //void process();
+
 };
+
+
 
 #endif
