55ee7255ab5592c6646792a8f11e9aa1c288a20d
yty <yutianyu@gmail.com>
2013-10-25 19:50:52 +0800

    add ofSerial Parameter: parity, dataBits, stopBits.

--- libs/openFrameworks/communication/ofSerial.h
+++ libs/openFrameworks/communication/ofSerial.h
@@ -38,9 +38,9 @@ class ofSerial {
 			vector <ofSerialDeviceInfo> getDeviceList();
 
 			void 			close();
-			bool			setup();	// use default port, baud (0,9600)
-			bool			setup(string portName, int baudrate);
-			bool			setup(int deviceNumber, int baudrate);
+			bool			setup();	// use default port, baud (0,9600,'N',8,1)
+			bool			setup(string portName, int baudrate, char parity = 'N',int dataBits = 8, int stopBits = 1);
+			bool			setup(int deviceNumber, int baudrate, char parity = 'N',int dataBits = 8, int stopBits = 1);
 
 
 			int 			readBytes(unsigned char * buffer, int length);
