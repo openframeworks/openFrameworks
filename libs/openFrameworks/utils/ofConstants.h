1297c3cb7199dbfe32828c9e042ccc452a326fb9
Andreas Bergmeier <abergmeier@gmx.net>
2013-12-08 12:25:54 +0100

    Flatten compile time video player detection.

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -281,19 +281,17 @@ typedef TESSindex ofIndexType;
 		#define OF_VIDEO_PLAYER_GSTREAMER
 	#elif defined(TARGET_ANDROID)
 		#define OF_VIDEO_PLAYER_ANDROID
-	#else
-		#ifdef TARGET_OF_IOS
-			#define OF_VIDEO_PLAYER_IOS
-		#elif defined(TARGET_OSX)
-			//for 10.7 and 10.8 users we use QTKit for 10.6 users we use QuickTime
-			#ifndef MAC_OS_X_VERSION_10_7
-				#define OF_VIDEO_PLAYER_QUICKTIME
-			#else
-				#define OF_VIDEO_PLAYER_QTKIT
-			#endif
-		#elif !defined(TARGET_ANDROID)
+	#elif defined(TARGET_OF_IOS)
+		#define OF_VIDEO_PLAYER_IOS
+	#elif defined(TARGET_OSX)
+		//for 10.7 and 10.8 users we use QTKit for 10.6 users we use QuickTime
+		#ifndef MAC_OS_X_VERSION_10_7
 			#define OF_VIDEO_PLAYER_QUICKTIME
-	#endif
+		#else
+			#define OF_VIDEO_PLAYER_QTKIT
+		#endif
+	#else
+		#define OF_VIDEO_PLAYER_QUICKTIME
 	#endif
 #endif
 
