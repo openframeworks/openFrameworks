3fd132d02ce664c867b7f65bddb4dd1fa8e61be4
Christopher Baker <me@christopherbaker.net>
2014-01-23 15:38:34 -0600

    ofThread overhaul, documentation. Example fixes.

--- examples/utils/threadExample/src/testApp.cpp
+++ examples/utils/threadExample/src/testApp.cpp
@@ -1,79 +1,42 @@
 #include "testApp.h"
 
-bool locked = false;
 
-//--------------------------------------------------------------
 void testApp::setup(){
 	mainAppsCount = 0;
-	TO.start();
+	threadedObject.start();
 }
 
-//--------------------------------------------------------------
 void testApp::update(){
-	ofBackground(0,0,0);   // black because threads are EVIL ;)
 	mainAppsCount++;
 }
 
-//--------------------------------------------------------------
 void testApp::draw(){
-	ofSetHexColor(0xffffff);
-	TO.draw();
+    ofBackground(ofColor::black);
 
-    string str = "I am a the main opengl thread.\nmy current count is: ";
-	str += ofToString(mainAppsCount);
-    ofDrawBitmapString(str, 350, 56);
+	ofSetColor(ofColor::white);
 
+    // We can call this from our main thread because we know that
+    //  ofApp::draw() is being called by the main thread.
+    threadedObject.draw();
+
+    std::stringstream ss;
+    ss << "I am a the main opengl thread." << std::endl;
+    ss << "my current count is: " << mainAppsCount << std::endl;
+    ofDrawBitmapString(ss.str(), 350, 56);
 
     ofSetHexColor(0xff0033);
 
-    ofDrawBitmapString("press 's' to stop the thread and 'a' to start it", 50, 160);
+    ofDrawBitmapString("Press 's' to stop the thread and 'a' to start it", 50, 160);
 }
 
-//--------------------------------------------------------------
-void testApp::keyPressed  (int key){
+void testApp::keyPressed(int key){
 
-    if (key == 'a'){
-        TO.start();
-    } else if (key == 's'){
-        TO.stop();
+    if (key == 'a')
+    {
+        threadedObject.start();
     }
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+    else if (key == 's')
+    {
+        threadedObject.stop();
+    }
 }
