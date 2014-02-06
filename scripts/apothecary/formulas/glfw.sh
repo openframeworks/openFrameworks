8a69d86d32bccd6233f2caf3fd8c38f8d2b98fb3
Christopher Baker <me@christopherbaker.net>
2014-02-06 12:51:54 -0600

    *nix formula for GLFW.

--- scripts/apothecary/formulas/glfw.sh
+++ scripts/apothecary/formulas/glfw.sh
@@ -30,31 +30,44 @@ function prepare() {
 
 # executed inside the lib src dir
 function build() {
-	
+	rm -f CMakeCache.txt
+
 	if [ "$TYPE" == "vs" ] ; then
 		cmake -G "Visual Studio $VS_VER"
 		vs-build "GLFW.sln"
 
 	else
-		echoWarning "TODO: $TYPE build"
+		# *nix build system
+		cmake . -DCMAKE_INSTALL_PREFIX=$BUILD_ROOT_DIR \
+				-DGLFW_BUILD_UNIVERSAL=ON \
+				-DGLFW_BUILD_DOCS=OFF \
+				-DGLFW_BUILD_TESTS=OFF \
+				-DGLFW_BUILD_EXAMPLES=OFF \
+				-DBUILD_SHARED_LIBS=OFF
 
+ 		make clean
+ 		make
+ 		make install
 	fi
 }
 
 # executed inside the lib src dir, first arg $1 is the dest libs dir root
 function copy() {
-	# headers
-	mkdir -p $1/include
-	cp -Rv include/* $1/include
+	# prepare headers directory if needed
+	mkdir -p $1/include/GLFW
 
-	# libs
+	# prepare libs directory if needed
+	mkdir -p $1/lib/$TYPE
+
 	if [ "$TYPE" == "vs" ] ; then
-		mkdir -p $1/lib/$TYPE
+	    cp -Rv include/* $1/include
 		cp -v src/Release/glfw3.lib $1/lib/$TYPE/glfw3.lib
-
 	else
-		echoWarning "TODO: $TYPE copy"
-
+		# Standard *nix style copy.
+		# copy headers
+		cp -Rv $BUILD_ROOT_DIR/include/GLFW/* $1/include/GLFW/
+		# copy lib
+		cp -Rv $BUILD_ROOT_DIR/lib/libglfw3.a $1/lib/$TYPE/
 	fi
 }
 
@@ -63,6 +76,6 @@ function clean() {
 	if [ "$TYPE" == "vs" ] ; then
 		rm -f *.lib
 	else
-		echoWarning "TODO: $TYPE clean"
+		make clean;
 	fi
 }
