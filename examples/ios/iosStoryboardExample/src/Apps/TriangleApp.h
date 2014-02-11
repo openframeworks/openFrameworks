41c65a425afe93506ec571a02993c2352ed2c64f
lukasz karluk <julapy@googlemail.com>
2014-02-11 16:51:22 +1100

    added iosStoryboardExample
    the new iosStoryboardExample demonstrates how ios storyboards can be
    used with openframeworks.

---
+++ examples/ios/iosStoryboardExample/src/Apps/TriangleApp.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiOS.h"
+#include "ofxiOSExtras.h"
+
+class TriangleApp : public ofxiOSApp {
+	
+public:
+    
+     TriangleApp ();
+    ~TriangleApp ();
+    
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void touchDown(ofTouchEventArgs &touch);
+	void touchMoved(ofTouchEventArgs &touch);
+	void touchUp(ofTouchEventArgs &touch);
+	void touchDoubleTap(ofTouchEventArgs &touch);
+	void touchCancelled(ofTouchEventArgs &touch);
+
+	void lostFocus();
+	void gotFocus();
+	void gotMemoryWarning();
+	void deviceOrientationChanged(int newOrientation);
+
+    ofTrueTypeFont font;
+};
+
+
