278a2f728ad0f4cee85f74a62aeafb496b8455d8
Golan Levin <golan@flong.com>
2014-03-15 17:01:35 -0400

    moe functions

--- libs/openFrameworks/3d/ofCamera.h
+++ libs/openFrameworks/3d/ofCamera.h
@@ -3,6 +3,7 @@
  *  openFrameworksLib
  *
  *  Created by Memo Akten on 10/01/2011.
+ *  Copyright 2011 MSA Visuals Ltd. All rights reserved.
  *
  */
 
@@ -38,6 +39,17 @@ public:
 	
 	void setNearClip(float f);
 	void setFarClip(float f);
+	
+	/// \brief Set the "lens offset" applied to this camera.
+	/// 
+	/// Ordinarily, the camera is pointed straight down the center of its view frustum. 
+	/// However, it is possible to orient the camera towards a location offset from the center of its frustum. 
+	/// This is called an "assymetric frustum" and is used (for example) in stereo views. 
+	/// It is acheived by applying an offset to the center of projection. 
+	/// This function sets this offset from an ofVec2f argument. 
+	/// For more information see http://www.orthostereo.com/geometryopengl.html.
+	///
+	/// \param lensOffset The "lens offset" to apply to this camera, encoded in an ofVec2f.
 	void setLensOffset(const ofVec2f & lensOffset);
 	
 	/// \brief Set the recommended aspect ratio for a perspective camera.
@@ -48,14 +60,53 @@ public:
 	/// 
 	/// \param aspectRatio The desired aspect ratio, e.g. 1.3333, 1.6, 1.7777, etc. 
 	void setAspectRatio(float aspectRatio);
+	
+	/// \brief Set whether or not the aspect ratio of this camera is forced to a non-default setting.
+	/// 
+	/// The camera's aspect ratio, by default, is the aspect ratio of your viewport. 
+	/// If you have set a non-default value (with setAspectRatio()), 
+	/// you can toggle whether or not this value is applied. 
+	/// 
+	/// \param forceAspectRatio Whether or not this camera should use an aspect ratio you have set yourself.  
 	void setForceAspectRatio(bool forceAspectRatio);
 
+        /// \brief Get the camera's field of view, in degrees.
+	///
+	/// Get the horizontal camera's field of view, in degrees.
+	/// This is only meaningful for perspective cameras.
+	///
+	/// \returns The camera's field of view, in degrees.
 	float getFov() const { return fov; };
+	
+	
 	float getNearClip() const { return nearClip; };
 	float getFarClip() const { return farClip; };
+	
+	/// \brief Get the "lens offset" applied to this camera, encoded as an ofVec2f.
+	/// 
+	/// Ordinarily, the camera is pointed straight down the center of its view frustum. 
+	/// However, it is possible to orient the camera towards a location offset from the center of its frustum. 
+	/// This is called an "assymetric frustum" and is used (for example) in stereo views. 
+	/// It is acheived by applying an offset to the center of projection. 
+	/// This function returns the offset that has been applied, as an ofVec2f. 
+	/// For more information see http://www.orthostereo.com/geometryopengl.html.
+	///
+	/// \returns The "lens offset" applied to this camera, encoded in an ofVec2f.
 	ofVec2f getLensOffset() const { return lensOffset; };
+	
+	/// \brief Get the boolean state which indicates whether the aspect ratio of this camera is forced to a non-default setting.
+	/// 
+	/// \returns A boolean: whether or not this camera's aspect ratio is set to a non-default value. 
 	bool getForceAspectRatio() const {return forceAspectRatio;};
-    float getAspectRatio() const {return aspectRatio; };
+    
+    	/// \brief Get the aspect ratio of this camera's viewport.
+	///
+	/// Returns the aspect ratio of this camera's viewport. Usually this will be the ratio
+	/// of the width to height of your display. Intended for perspective cameras. 
+	/// 
+	/// \returns The aspect ratio of this camera's viewport.
+	float getAspectRatio() const {return aspectRatio; };
+    
 	void setupPerspective(bool vFlip = true, float fov = 60, float nearDist = 0, float farDist = 0, const ofVec2f & lensOffset = ofVec2f(0.0f, 0.0f));
 	void setupOffAxisViewPortal(const ofVec3f & topLeft, const ofVec3f & bottomLeft, const ofVec3f & bottomRight);
 	
