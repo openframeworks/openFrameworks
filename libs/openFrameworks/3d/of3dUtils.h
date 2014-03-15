e82cdb2948f3b386da5c3789648dcd09bf6c3c55
Golan Levin <golan@flong.com>
2014-03-15 11:15:28 -0400

    Added documentation for ofDrawAxis()

--- libs/openFrameworks/3d/of3dUtils.h
+++ libs/openFrameworks/3d/of3dUtils.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "ofVectorMath.h"
 
+
+/// \brief Draws x,y,z axes representing the current reference frame
+///
+/// This function draws a set of x,y,z axes, which can be helpful for understanding which way is up. 
+/// There are no unit markings nor arrowheads. Axes are not drawn in the negative directions.  
+/// Axes are drawn in red (+x), green (+y) and blue (+z), starting from the origin. 
+//
+/// \param size The size at which to draw the axes.
 void ofDrawAxis(float size);
 void ofDrawGrid(float scale = 10.0f, float ticks = 8.0f, bool labels = false, bool x = true, bool y = true, bool z = true);
 void ofDrawGridPlane(float scale, float ticks = 8.0f, bool labels = false);
