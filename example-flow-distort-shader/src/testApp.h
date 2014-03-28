c47214540d49393cf12ed7216ef8f977a02faef5
Kyle McDonald <kyle@kylemcdonald.net>
2014-03-29 00:11:17 +0100

    accidentally a RateTimer

--- example-flow-distort-shader/src/testApp.h
+++ example-flow-distort-shader/src/testApp.h
@@ -4,40 +4,6 @@
 #include "ofxCv.h"
 #include "MotionAmplifier.h"
 
-class RateTimer {
-protected:
-    float lastTick, averagePeriod, smoothing;
-    bool secondTick;
-public:
-    RateTimer(float smoothing = .9) :
-    smoothing(smoothing) {
-        reset();
-    }
-    void reset() {
-        lastTick = 0;
-        averagePeriod = 0;
-        secondTick = false;
-    }
-    float getFrameRate() {
-        return averagePeriod == 0 ? 0 : 1 / averagePeriod;
-    }
-    void tick() {
-        float curTick = ofGetElapsedTimef();
-        if(lastTick == 0) {
-            secondTick = true;
-        } else {
-            float curDiff = curTick - lastTick;;
-            if(secondTick) {
-                averagePeriod = curDiff;
-                secondTick = false;
-            } else {
-                averagePeriod = ofLerp(curDiff, averagePeriod, smoothing);
-            }
-        }
-        lastTick = curTick;
-    }
-};
-
 class testApp : public ofBaseApp {
 public:
 	void setup();
