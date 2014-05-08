308f5d4852da982914da7607455ec098c3f09762
Christopher Baker <me@christopherbaker.net>
2014-05-07 19:37:47 -0500

    Clean up documentation of ofImage member variables.

--- libs/openFrameworks/graphics/ofImage.h
+++ libs/openFrameworks/graphics/ofImage.h
@@ -473,8 +473,13 @@ public:
     /// \brief Loads ofPixels data into ofTexture so that draw() calls reflect changes to the pixels.
     void reloadTexture();
 
-    int width, height, bpp; ///< w,h, bits per pixel
-    int type; ///< OF_IMAGE_GRAYSCALE, OF_IMAGE_COLOR, OF_IMAGE_COLOR_ALPHA
+    int width;  ///< \brief Image width.
+    int height; ///< \brief Image Height.
+    int bpp;    ///< \brief Image bits per pixel.
+    int type;   ///< \brief Image type
+                ///< \sa OF_IMAGE_GRAYSCALE
+                ///< \sa OF_IMAGE_COLOR
+                ///< \sa OF_IMAGE_COLOR_ALPHA
 
     ofImage_<PixelType> & operator=(ofPixels_<PixelType> & pixels);
 
