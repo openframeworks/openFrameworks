efb42df4428aed03127f72164c744cba426d121c
trentbrooks <trentbrooks@gmail.com>
2014-07-23 23:12:09 +1000

    added back load from buffer method

--- addons/ofxAssimpModelLoader/src/ofxAssimpModelLoader.h
+++ addons/ofxAssimpModelLoader/src/ofxAssimpModelLoader.h
@@ -29,6 +29,7 @@ class ofxAssimpModelLoader{
         ofxAssimpModelLoader();
 
         bool loadModel(string modelName, bool optimize=false);
+        bool loadModel(ofBuffer & buffer, bool optimize=false, const char * extension="");
         void createEmptyModel();
         void createLightsFromAiModel();
         void optimizeScene();
