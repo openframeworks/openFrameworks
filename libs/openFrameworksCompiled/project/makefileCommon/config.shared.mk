8158ab99eed6346e5b5c462d5e7d6344c52a83df
Michael Hadley <michaelwesthadley@gmail.com>
2014-08-12 09:45:13 -0500

    Oops, forgot to add _NT to the platform detection

--- libs/openFrameworksCompiled/project/makefileCommon/config.shared.mk
+++ libs/openFrameworksCompiled/project/makefileCommon/config.shared.mk
@@ -79,7 +79,7 @@ ifndef PLATFORM_LIB_SUBPATH
         else
             $(error This makefile does not support your architecture $(PLATFORM_ARCH))
         endif
-    else ifneq (,$(findstring MINGW32,$(PLATFORM_OS)))
+    else ifneq (,$(findstring MINGW32_NT,$(PLATFORM_OS)))
         PLATFORM_LIB_SUBPATH=win_cb
     else ifeq ($(PLATFORM_OS),Android)
         PLATFORM_LIB_SUBPATH=android
