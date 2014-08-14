794277d7e33c9e048e0eae12d6983c07846d46cb
Daniel Rosser <danoli3@gmail.com>
2014-08-15 01:46:21 +1000

    Apothecary: FreeType 2.5.3 Formula iOS OSX
    iOS - Build using cc and libc++
    —————
    - [x] armv7
    - [x] armv7s
    - [x] arm64
    - [x] i386
    - [x] x86_64
    
    OSX
    ———
    - [x] i386 (libstdc++)
    - [x] x86_64 (libc++)
    
    (cherry picked from commit 15318e439c52a500821178e4f3f961e0c17a6f7e)

--- scripts/apothecary/formulas/freetype.sh
+++ scripts/apothecary/formulas/freetype.sh
@@ -9,11 +9,11 @@
 FORMULA_TYPES=( "osx" "vs" "win_cb" "ios" "android" )
 
 # define the version
-VER=2.4.12
+VER=2.5.3
 
 # tools for git use
 GIT_URL=http://git.savannah.gnu.org/r/freetype/freetype2.git
-GIT_TAG=VER-2-4-12
+GIT_TAG=VER-2-5-3
 
 # download the source code and unpack it into LIB_NAME
 function download() {
@@ -26,23 +26,55 @@ function download() {
 # prepare the build environment, executed inside the lib src dir
 function prepare() {
 	: # noop
+	mkdir -p lib/$TYPE
 }
 
 # executed inside the lib src dir
 function build() {
 	
 	if [ "$TYPE" == "osx" ] ; then
+		local BUILD_TO_DIR=$BUILD_DIR/freetype/build/$TYPE/
 
-		export CFLAGS="-arch i386 -arch x86_64"
+		local STDLIB="libstdc++"
+		local OSX_ARCH="i386"
+		local TOOLCHAIN=$XCODE_DEV_ROOT/Toolchains/XcodeDefault.xctoolchain 
+		export CC=$TOOLCHAIN/usr/bin/cc
+		export CPP=$TOOLCHAIN/usr/bin/cpp
+		export CXX=$TOOLCHAIN/usr/bin/c++
 
-		# 32 + 64 bit
-		./configure --prefix=$BUILD_ROOT_DIR
+		./configure --prefix=$BUILD_TO_DIR --without-bzip2 --enable-static=yes --enable-shared=no \
+			CFLAGS="-arch $OSX_ARCH -pipe -stdlib=$STDLIB -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden" \
+			CPP=$CPP \
+			CXX=$CXX
+		make clean 
+		make
+		make install
+		cp $BUILD_TO_DIR/lib/libfreetype.a lib/$TYPE/libfreetype-$OSX_ARCH.a
 
-		make clean; 
-		make;
-		make install;
+		unset OSX_ARCH STDLIB
 
-		unset CFLAGS
+		# x86_64
+		local STDLIB="libc++"
+		local OSX_ARCH="x86_64"
+		./configure --prefix=$BUILD_TO_DIR --without-bzip2 --enable-static=yes --enable-shared=no \
+			CFLAGS="-arch $OSX_ARCH -pipe -stdlib=$STDLIB -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden" \
+			CPP=$CPP \
+			CXX=$CXX
+		make clean
+		make
+		make install
+		cp $BUILD_TO_DIR/lib/libfreetype.a lib/$TYPE/libfreetype-$OSX_ARCH.a
+
+		cd lib/$TYPE/
+		lipo -create libfreetype-i386.a \
+					libfreetype-x86_64.a \
+					-output libfreetype.a
+		cd ../../
+
+		unset OSX_ARCH STDLIB TOOLCHAIN CC CPP CXX
+
+		echo "-----------"
+		echo "$BUILD_DIR"
 	
 	elif [ "$TYPE" == "vs" ] ; then
    		make clean
@@ -62,34 +94,113 @@ function build() {
 		make
 
 	elif [ "$TYPE" == "ios" ] ; then
-		# http://shift.net.nz/2010/09/compiling-freetype-for-iphoneios/
-		# http://stackoverflow.com/questions/6425643/compiling-freetype-for-iphone
-	
+
+		unset IOS_DEVROOT IOS_SDKROOT
+		local TOOLCHAIN=$XCODE_DEV_ROOT/Toolchains/XcodeDefault.xctoolchain 
+		local IOS_DEVROOT=$XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer
+		local IOS_SDKROOT=$IOS_DEVROOT/SDKs/iPhoneOS$IOS_SDK_VER.sdk
+		local IOS_CC=$TOOLCHAIN/usr/bin/cc
+		local IOS_HOST="arm-apple-darwin"
+		local IOS_PREFIX="/usr/local/iphone"
+
+		export CPP=$TOOLCHAIN/usr/bin/cpp
+		export CXX=$TOOLCHAIN/usr/bin/c++
+		export CXXCPP=$TOOLCHAIN/usr/bin/cpp
+		export CC=$TOOLCHAIN/usr/bin/cc
+		export LD=$TOOLCHAIN/usr/bin/ld
+		export AR=$TOOLCHAIN/usr/bin/ar
+		export AS=$TOOLCHAIN/usr/bin/as
+		export NM=$TOOLCHAIN/usr/bin/nm
+		export RANLIB=$TOOLCHAIN/usr/bin/ranlib
+		export LDFLAGS="-L$IOS_SDKROOT/usr/lib/"
+
+		local STDLIB="libc++" 
+		#local STDLIB="libstdc++"
+
 		# armv7
-		./configure --without-bzip2 --prefix=/usr/local/iphone --host=arm-apple-darwin --enable-static=yes --enable-shared=no \
-			CC=$XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/usr/bin/gcc \
-			CFLAGS="-arch armv7 -pipe -std=c99 -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$IOS_MIN_SDK_VER -I$XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$IOS_SDK_VER.sdk/usr/include/libxml2 -isysroot $XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$IOS_SDK_VER.sdk" \
-			AR=$XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/usr/bin/ar \
-			LDFLAGS="-arch armv7 -isysroot $XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$IOS_SDK_VER.sdk -miphoneos-version-min=$IOS_MIN_SDK_VER"
+		local IOS_ARCH="armv7"
+		./configure --without-bzip2 --prefix=$IOS_PREFIX --host=$IOS_HOST --enable-static=yes --enable-shared=no \
+			CC=$IOS_CC \
+			CFLAGS="-arch $IOS_ARCH -pipe -stdlib=$STDLIB -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$IOS_MIN_SDK_VER -I$IOS_SDKROOT/usr/include/libxml2 -isysroot $IOS_SDKROOT" \
+			AR=$AR \
+			LDFLAGS="-arch $IOS_ARCH -isysroot $IOS_SDKROOT -miphoneos-version-min=$IOS_MIN_SDK_VER"
 		make clean; make
-		cp objs/.libs/libfreetype.a libfreetype-armv7.a
+		cp objs/.libs/libfreetype.a lib/$TYPE/libfreetype-$IOS_ARCH.a
+		unset IOS_ARCH
 
 		# armv7s
-		./configure --without-bzip2 --prefix=/usr/local/iphone --host=arm-apple-darwin --enable-static=yes --enable-shared=no \
-			CC=$XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/usr/bin/gcc \
-			CFLAGS="-arch armv7s -pipe -std=c99 -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$IOS_MIN_SDK_VER -I$XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$IOS_SDK_VER.sdk/usr/include/libxml2 -isysroot $XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$IOS_SDK_VER.sdk" \
-			AR=$XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/usr/bin/ar \
-			LDFLAGS="-arch armv7s -isysroot $XCODE_DEV_ROOT/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS$IOS_SDK_VER.sdk -miphoneos-version-min=$IOS_MIN_SDK_VER"
+		local IOS_ARCH="armv7s"
+		./configure --without-bzip2 --prefix=$IOS_PREFIX --host=$IOS_HOST --enable-static=yes --enable-shared=no \
+			CC=$IOS_CC \
+			CFLAGS="-arch $IOS_ARCH -pipe -stdlib=$STDLIB -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$IOS_MIN_SDK_VER -I$IOS_SDKROOT/usr/include/libxml2 -isysroot $IOS_SDKROOT" \
+			AR=$AR \
+			LDFLAGS="-arch $IOS_ARCH -isysroot $IOS_SDKROOT -miphoneos-version-min=$IOS_MIN_SDK_VER"
 		make clean; make
-		cp objs/.libs/libfreetype.a libfreetype-armv7s.a
+		cp objs/.libs/libfreetype.a lib/$TYPE/libfreetype-$IOS_ARCH.a
+		unset IOS_ARCH
+
+		# arm64
+
+		local IOS_MIN_ARM64_SDK_VER="7.0"
+		local IOS_ARCH="arm64"
+		#export STDLIB="libc++"
+		./configure --without-bzip2 --prefix=$IOS_PREFIX --host=$IOS_HOST --enable-static=yes --enable-shared=no \
+			CC=$IOS_CC \
+			CFLAGS="-arch $IOS_ARCH -pipe -stdlib=$STDLIB -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$IOS_MIN_SDK_VER -I$IOS_SDKROOT/usr/include/libxml2 -isysroot $IOS_SDKROOT" \
+ 			AS=$AS \
+ 			AR=$AR \
+	        NM=$IOS_DEVROOT/usr/bin/nm \
+			LDFLAGS="-arch $IOS_ARCH -isysroot $IOS_SDKROOT -miphoneos-version-min=$IOS_MIN_SDK_VER"
+		make clean; make
+		cp objs/.libs/libfreetype.a lib/$TYPE/libfreetype-$IOS_ARCH.a
+		unset IOS_ARCH
+		unset IOS_MIN_ARM64_SDK_VER
 
 		# simulator
-		./configure CFLAGS="-arch i386"
+
+		# i386
+		unset IOS_DEVROOT IOS_SDKROOT
+		#export STDLIB="libstdc++"
+		local IOS_DEVROOT=$XCODE_DEV_ROOT/Platforms/iPhoneSimulator.platform/Developer
+		local IOS_SDKROOT=$IOS_DEVROOT/SDKs/iPhoneSimulator$IOS_SDK_VER.sdk
+
+		unset LDFLAGS
+		export LDFLAGS="-L$IOS_SDKROOT/usr/lib/"
+		local IOS_ARCH="i386"
+		./configure --without-bzip2 --enable-static=yes --enable-shared=no \
+			CC=$IOS_CC \
+			CFLAGS="-arch $IOS_ARCH -pipe -stdlib=$STDLIB -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$IOS_MIN_SDK_VER -I$IOS_SDKROOT/usr/include/libxml2 -isysroot $IOS_SDKROOT" \
+			CPP=$CPP \
+			CXX=$CXX \
+			LDFLAGS="-arch $IOS_ARCH -isysroot $IOS_SDKROOT -miphoneos-version-min=$IOS_MIN_SDK_VER"
 		make clean; make
-		cp objs/.libs/libfreetype.a libfreetype-i386.a
+		cp objs/.libs/libfreetype.a lib/$TYPE/libfreetype-$IOS_ARCH.a
+		unset IOS_ARCH
+
+		# x86_64
+		local IOS_ARCH="x86_64"
+		#export STDLIB="libc++"
+		./configure --without-bzip2 --enable-static=yes --enable-shared=no \
+			CC=$IOS_CC \
+			CFLAGS="-arch $IOS_ARCH -pipe -stdlib=$STDLIB -Wno-trigraphs -fpascal-strings -O2 -Wreturn-type -Wunused-variable -fmessage-length=0 -fvisibility=hidden -miphoneos-version-min=$IOS_MIN_SDK_VER -I$IOS_SDKROOT/usr/include/libxml2 -isysroot $IOS_SDKROOT" \
+			CPP=$CPP \
+			CXX=$CXX \
+			LDFLAGS="-arch $IOS_ARCH -isysroot $IOS_SDKROOT -miphoneos-version-min=$IOS_MIN_SDK_VER"
+		make clean; make
+		cp objs/.libs/libfreetype.a lib/$TYPE/libfreetype-$IOS_ARCH.a
+		unset IOS_ARCH
 
 		# link into universal lib
-		lipo -create libfreetype-armv7.a libfreetype-armv7s.a libfreetype-i386.a -output libfreetype.a
+		cd lib/$TYPE/
+		lipo -create libfreetype-armv7.a \
+					libfreetype-armv7s.a  \
+					libfreetype-arm64.a \
+					libfreetype-i386.a \
+					libfreetype-x86_64.a \
+					-output libfreetype.a
+		cd ../../
+ 
+		unset IOS_DEVROOT IOS_SDKROOT IOS_AR IOS_HOST IOS_PREFIX  CPP CXX CXXCPP CXXCPP CC LD AS AR NM RANLIB LDFLAGS STDLIB
 
 	elif [ "$TYPE" == "android" ] ; then
 		echoWarning "TODO: build android"
@@ -100,24 +211,29 @@ function build() {
 function copy() {
 
 	# copy headers 
-	mkdir -p $1/include/freetype2/freetype/config
+	mkdir -p $1/include/freetype2/config
 
 	# copy files from the build root
-	cp -Rv $BUILD_ROOT_DIR/include/freetype2/* $1/include/freetype2/
-	cp -Rv $BUILD_ROOT_DIR/include/ft2build.h $1/include/freetype2/
+
+	cp -Rv include/* $1/include/freetype2/
+
+	# older versions before 2.5.x
+	#mkdir -p $1/include/freetype2/freetype/config
+	#cp -Rv include/* $1/include/freetype2/freetype
+	#cp -Rv include/ft2build.h $1/include/
 
 	# copy lib
 	mkdir -p $1/lib/$TYPE
 
-	if [ "$TYPE" == "osx" -o "$TYPE" == "ios" ] ; then
-		# TODO use the lib prefix.  
-		# Required CoreOF.xcconfig mod.
-		# Required ios CoreOF.xcconfig mod.
-		cp -v $BUILD_ROOT_DIR/lib/libfreetype.a $1/lib/$TYPE/freetype.a
+	if [ "$TYPE" == "osx" ] ; then
+		cp -v lib/$TYPE/libfreetype.a $1/lib/$TYPE/freetype.a
+	elif [ "$TYPE" == "ios" ] ; then
+		cp -v lib/$TYPE/libfreetype.a $1/lib/$TYPE/freetype.a
 	elif [ "$TYPE" == "vs" ] ; then
+		# cp -v lib/$TYPE/libfreetype.lib $1/lib/$TYPE/libfreetype.lib
 		echoWarning "TODO: copy vs lib"
 	elif [ "$TYPE" == "win_cb" ] ; then
-		#cp -v objs/.libs/libfreetype.a $1/lib/$TYPE/freetype.a
+		# cp -v lib/$TYPE/libfreetype.a $1/lib/$TYPE/libfreetype.a
 		echoWarning "TODO: copy win_cb lib"
 	elif [ "$TYPE" == "android" ] ; then
 		echoWarning "TODO: copy android lib"
