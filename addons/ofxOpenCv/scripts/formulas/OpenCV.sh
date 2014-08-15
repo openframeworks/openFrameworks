56982033bbca9a846f646b3641053c0a7d8ab45f
ofTheo <theo@openframeworks.cc>
2014-08-15 17:09:21 -0400

    loads of fixes for iOS and OSX - cleanup and reduncy fixes

--- addons/ofxOpenCv/scripts/formulas/OpenCV.sh
+++ addons/ofxOpenCv/scripts/formulas/OpenCV.sh
@@ -16,12 +16,8 @@ GIT_URL=https://github.com/Itseez/opencv.git
 GIT_TAG=$VER
 
 # these paths don't really matter - they are set correctly further down
-local LIB_FOLDER="$BUILD_ROOT_DIR/OpenCv"
-local LIB_FOLDER32="$LIB_FOLDER-32"
-local LIB_FOLDER64="$LIB_FOLDER-64"
-local LIB_FOLDER_IOS="$LIB_FOLDER-IOS"
-local LIB_FOLDER_IOS_SIM="$LIB_FOLDER-IOSIM"
-
+local LIB_FOLDER=""
+local LIB_FOLDER_OUT="" 
  
 # download the source code and unpack it into LIB_NAME
 function download() {
@@ -37,112 +33,83 @@ function prepare() {
 }
  
 function build_osx() {
-  
-  if [ "$1" == "64" ] ; then
-    cmake . -DCMAKE_INSTALL_PREFIX=$LIB_FOLDER64 \
-      -DGLFW_BUILD_UNIVERSAL=ON \
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 \
-      -DCMAKE_OSX_ARCHITECTURES="x86_64" \
-      -DENABLE_FAST_MATH=ON \
-      -DCMAKE_CXX_FLAGS="-fvisibility-inlines-hidden -stdlib=libc++ -O3" \
-      -DCMAKE_C_FLAGS="-fvisibility-inlines-hidden -stdlib=libc++ -O3" \
-      -DCMAKE_BUILD_TYPE="Release" \
-      -DBUILD_SHARED_LIBS=OFF \
-      -DBUILD_DOCS=OFF \
-      -DBUILD_EXAMPLES=OFF \
-      -DBUILD_FAT_JAVA_LIB=OFF \
-      -DBUILD_JASPER=OFF \
-      -DBUILD_PACKAGE=OFF \
-      -DBUILD_opencv_java=OFF \
-      -DBUILD_opencv_python=OFF \
-      -DBUILD_opencv_apps=OFF \
-      -DWITH_1394=OFF \
-      -DWITH_CARBON=OFF \
-      -DWITH_FFMPEG=OFF \
-      -DWITH_OPENCL=OFF \
-      -DWITH_OPENCLAMDBLAS=OFF \
-      -DWITH_OPENCLAMDFFT=OFF \
-      -DWITH_GIGEAPI=OFF \
-      -DWITH_CUDA=OFF \
-      -DWITH_CUFFT=OFF \
-      -DWITH_JASPER=OFF \
-      -DWITH_LIBV4L=OFF \
-      -DWITH_IMAGEIO=OFF \
-      -DWITH_IPP=OFF \
-      -DWITH_OPENNI=OFF \
-      -DWITH_QT=OFF \
-      -DWITH_QUICKTIME=OFF \
-      -DWITH_V4L=OFF \
-      -DWITH_PVAPI=OFF \
-      -DBUILD_TESTS=OFF \
-      -DBUILD_PERF_TESTS=OFF
-  elif [ "$1" == "32" ] ; then
-    # NB - using a special BUILD_ROOT_DIR
-    cmake . -DCMAKE_INSTALL_PREFIX=$LIB_FOLDER32 \
-      -DGLFW_BUILD_UNIVERSAL=ON \
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=10.6 \
-      -DCMAKE_OSX_ARCHITECTURES="i386" \
-      -DENABLE_FAST_MATH=ON \
-      -DCMAKE_CXX_FLAGS="-fvisibility-inlines-hidden -stdlib=libstdc++ -O3" \
-      -DCMAKE_C_FLAGS="-fvisibility-inlines-hidden -stdlib=libstdc++ -O3" \
-      -DCMAKE_BUILD_TYPE="Release" \
-      -DBUILD_SHARED_LIBS=OFF \
-      -DBUILD_DOCS=OFF \
-      -DBUILD_EXAMPLES=OFF \
-      -DBUILD_FAT_JAVA_LIB=OFF \
-      -DBUILD_JASPER=OFF \
-      -DBUILD_PACKAGE=OFF \
-      -DBUILD_opencv_java=OFF \
-      -DBUILD_opencv_python=OFF \
-      -DBUILD_opencv_apps=OFF \
-      -DWITH_1394=OFF \
-      -DWITH_CARBON=OFF \
-      -DWITH_FFMPEG=OFF \
-      -DWITH_OPENCL=OFF \
-      -DWITH_OPENCLAMDBLAS=OFF \
-      -DWITH_OPENCLAMDFFT=OFF \
-      -DWITH_GIGEAPI=OFF \
-      -DWITH_CUDA=OFF \
-      -DWITH_CUFFT=OFF \
-      -DWITH_JASPER=OFF \
-      -DWITH_LIBV4L=OFF \
-      -DWITH_IMAGEIO=OFF \
-      -DWITH_IPP=OFF \
-      -DWITH_OPENNI=OFF \
-      -DWITH_QT=OFF \
-      -DWITH_QUICKTIME=OFF \
-      -DWITH_V4L=OFF \
-      -DWITH_PVAPI=OFF \
-      -DBUILD_TESTS=OFF \
-      -DBUILD_PERF_TESTS=OFF
+
+  arch="$1"
+  targetFolder="$LIB_FOLDER/$arch/"
+  libType="libc++" 
+  deployTarget="10.7"
+
+  if [ "$arch" == "i386" ] ; then
+    libType="libstdc++" 
+    deployTarget="10.6" 
   fi
+  
+    cmake . -DCMAKE_INSTALL_PREFIX="$targetFolder" \
+      -DGLFW_BUILD_UNIVERSAL=ON \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET="$deployTarget" \
+      -DCMAKE_OSX_ARCHITECTURES="$arch" \
+      -DENABLE_FAST_MATH=ON \
+      -DCMAKE_CXX_FLAGS="-fvisibility-inlines-hidden -stdlib=$libType -O3" \
+      -DCMAKE_C_FLAGS="-fvisibility-inlines-hidden -stdlib=$libType -O3" \
+      -DCMAKE_BUILD_TYPE="Release" \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DBUILD_DOCS=OFF \
+      -DBUILD_EXAMPLES=OFF \
+      -DBUILD_FAT_JAVA_LIB=OFF \
+      -DBUILD_JASPER=OFF \
+      -DBUILD_PACKAGE=OFF \
+      -DBUILD_opencv_java=OFF \
+      -DBUILD_opencv_python=OFF \
+      -DBUILD_opencv_apps=OFF \
+      -DWITH_1394=OFF \
+      -DWITH_CARBON=OFF \
+      -DWITH_FFMPEG=OFF \
+      -DWITH_OPENCL=OFF \
+      -DWITH_OPENCLAMDBLAS=OFF \
+      -DWITH_OPENCLAMDFFT=OFF \
+      -DWITH_GIGEAPI=OFF \
+      -DWITH_CUDA=OFF \
+      -DWITH_CUFFT=OFF \
+      -DWITH_JASPER=OFF \
+      -DWITH_LIBV4L=OFF \
+      -DWITH_IMAGEIO=OFF \
+      -DWITH_IPP=OFF \
+      -DWITH_OPENNI=OFF \
+      -DWITH_QT=OFF \
+      -DWITH_QUICKTIME=OFF \
+      -DWITH_V4L=OFF \
+      -DWITH_PVAPI=OFF \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_PERF_TESTS=OFF
  
   make clean
   make
   make install
+
+  # copy headers
+  cp -R "$targetFolder/include/opencv" $LIB_FOLDER_OUT/include/opencv
+  cp -R "$targetFolder/include/opencv2" $LIB_FOLDER_OUT/include/opencv2
 }
 
 function build_ios() {
 
   iosSDK="7.1"
   iosPlatform="iPhoneOS"
-  arch="armv7"
+  arch="$1"
   isSimulator="FALSE";
   effectivePlatforms="-iphoneos;"
 
-  targetFolder=$LIB_FOLDER_IOS
+  targetFolder="$LIB_FOLDER/$arch/"
 
-  if [ "$1" == "i386" ] ; then
-    arch="i386"
+  if [ "$arch" == "i386" ] || [ "$arch" == "x86_64" ] ; then
     iosPlatform="iPhoneSimulator"
-    targetFolder=$LIB_FOLDER_IOS_SIM
     isSimulator="TRUE"
     effectivePlatforms="-iphonesimulator;"
   fi
 
   iosPlatSDK="$iosPlatform$iosSDK.sdk"
 
-  #-miphoneos-version-min is REALLY needed to cmake allows you to build for simulator
+  #-miphoneos-version-min in CXX AND C FLAGS REALLY needed to cmake allows you to build for simulator
 
     cmake . -DCMAKE_INSTALL_PREFIX=$targetFolder \
       -DIOS=1 \
@@ -196,88 +163,103 @@ function build_ios() {
   make clean
   make
   make install
+
+  # copy headers
+  cp -R "$targetFolder/include/opencv" $LIB_FOLDER_OUT/include/opencv
+  cp -R "$targetFolder/include/opencv2" $LIB_FOLDER_OUT/include/opencv2
 }
 
-
+#first argument is the destination
+#second argument is a list of src architectures
 function make_universal_binary() {
   shopt -s nullglob
 
-  src1="$1"
-  src2="$2"
-  dst="$3"
+  dst="$1"
+  shift
+  src_archs=("${@}")
+  src_list="$LIB_FOLDER/$src_archs"
 
-  libs="$src1/lib/libopencv*.a"
+  libs="$src_list/lib/lib*.a"
+  allArchs=""
+
+  echo "src_list $libs"
 
   for lib in $libs
   do
-    fname=$(basename "$lib"); 
-    otherLib="$src2/lib/$fname"
+    fname=$(basename "$lib")
+    allArchs=""
+
+    for arch in "${src_archs[@]}"
+    do
+        allArchs="$allArchs$LIB_FOLDER/$arch/lib/$fname "
+    done
+
     echo "lipoing $fname"
-    lipo -create $lib $otherLib -o "$dst/lib/$fname" || true
+    output="${fname/lib/}"
+
+    command="lipo -create $allArchs -o $dst/lib/$output"
+    $command || true
   done
 
-  thirdparty="$src1/share/OpenCV/3rdparty/lib/*.a"
+  thirdparty="$src_list/share/OpenCV/3rdparty/lib/*.a"
 
   for lib in $thirdparty
   do
-    fname=$(basename "$lib"); 
-    otherLib="$src2/share/OpenCV/3rdparty/lib/$fname"
+    fname=$(basename "$lib")
+    allArchs=""
+
+    for arch in "${src_archs[@]}"
+    do
+        allArchs="$allArchs$LIB_FOLDER/$arch/share/OpenCV/3rdparty/lib/$fname "
+    done
+
     echo "lipoing $fname"
-    lipo -create $lib $otherLib -o "$dst/lib/$fname" || true
+
+    #remove the lib prefix
+    output="${fname/lib/}"
+
+    command="lipo -create $allArchs -o $dst/lib/$output"
+    $command || true
   done
 
-  outputlist="$dst/lib/lib*.a"
-
-  libtool -static $outputlist -o "$dst/lib/opencv.a"
 }
 
 # executed inside the lib src dir
 function build() {
-  rm -f CMakeCache.txt
- 
-  LIB_FOLDER="$BUILD_ROOT_DIR/$TYPE/FAT/OpenCv"
-  LIB_FOLDER32="$BUILD_ROOT_DIR/$TYPE/32bit/OpenCv"
-  LIB_FOLDER64="$BUILD_ROOT_DIR/$TYPE/64bit/OpenCv"
+
+  LIB_FOLDER="$BUILD_ROOT_DIR/$TYPE/OpenCv"
+  LIB_FOLDER_OUT="$LIB_FOLDER/FAT"
 
   if [ "$TYPE" == "osx" ] ; then
-    # 64-bit OF transition, build x86-64 and i386 separately
-    rm -f CMakeCache.txt
-    build_osx "64";
-    rm -f CMakeCache.txt
-    build_osx "32";
+	mkdir -p $LIB_FOLDER_OUT/include
+	mkdir -p $LIB_FOLDER_OUT/lib
 
-	mkdir -p $LIB_FOLDER/include
-	mkdir -p $LIB_FOLDER/lib
- 
-    # lipo shit together
-    make_universal_binary "$LIB_FOLDER64" "$LIB_FOLDER32" "$LIB_FOLDER" 
+    archs=("i386" "x86_64")
 
-    # copy headers
-    cp -R $LIB_FOLDER64/include/opencv $LIB_FOLDER/include/opencv
-    cp -R $LIB_FOLDER64/include/opencv2 $LIB_FOLDER/include/opencv2
+    for curArch in "${archs[@]}"
+    do
+        rm -f CMakeCache.txt
+        build_osx "$curArch"
+    done
+
+    make_universal_binary "$LIB_FOLDER_OUT" "${archs[@]}"
+
   fi
 
   if [ "$TYPE" == "ios" ] ; then
 
-    local LIB_FOLDER_IOS="$BUILD_ROOT_DIR/$TYPE/iOS/OpenCv"
-    local LIB_FOLDER_IOS_SIM="$BUILD_ROOT_DIR/$TYPE/iOS_SIMULATOR/OpenCv"
+    mkdir -p $LIB_FOLDER_OUT/include
+    mkdir -p $LIB_FOLDER_OUT/lib
 
-    # 64-bit OF transition, build x86-64 and i386 separately
-    rm -f CMakeCache.txt
-    build_ios "i386";
+    archs=("armv7" "armv7s" "arm64" "i386" "x86_64")
 
-    rm -f CMakeCache.txt
-    build_ios "armv7";
+    for curArch in "${archs[@]}"
+    do
+        rm -f CMakeCache.txt
+        build_ios "$curArch"
+    done
 
-	mkdir -p $LIB_FOLDER/include
-	mkdir -p $LIB_FOLDER/lib
-
-    make_universal_binary "$LIB_FOLDER_IOS" "$LIB_FOLDER_IOS_SIM" "$LIB_FOLDER"
-
-    # copy headers
-    cp -R $LIB_FOLDER_IOS/include/opencv $LIB_FOLDER/include/opencv
-    cp -R $LIB_FOLDER_IOS/include/opencv2 $LIB_FOLDER/include/opencv2
-
+    make_universal_binary "$LIB_FOLDER_OUT" "${archs[@]}"
   fi
 
 }
@@ -285,7 +267,8 @@ function build() {
 # executed inside the lib src dir, first arg $1 is the dest libs dir root
 function copy() {
 
-  LIB_FOLDER="$BUILD_ROOT_DIR/$TYPE/FAT/OpenCv"
+  LIB_FOLDER="$BUILD_ROOT_DIR/$TYPE/OpenCv"
+  LIB_FOLDER_OUT="$LIB_FOLDER/FAT"
 
   # prepare headers directory if needed
   mkdir -p $1/include
@@ -293,23 +276,15 @@ function copy() {
   # prepare libs directory if needed
   mkdir -p $1/lib/$TYPE
  
-  if [ "$TYPE" == "osx" ] ; then
+  if [ "$TYPE" == "osx" ] || [ "$TYPE" == "ios" ] ; then
     # Standard *nix style copy.
     # copy headers
-    cp -R $LIB_FOLDER/include/ $1/include/
+    cp -R $LIB_FOLDER_OUT/include/ $1/include/
  
     # copy lib
-    cp -R $LIB_FOLDER/lib/opencv.a $1/lib/$TYPE/
+    cp -R $LIB_FOLDER_OUT/lib/ $1/lib/$TYPE/
   fi
 
-  if [ "$TYPE" == "ios" ] ; then
-    # Standard *nix style copy.
-    # copy headers
-    cp -R $LIB_FOLDER/include/ $1/include/
- 
-    # copy lib
-    cp -R $LIB_FOLDER/lib/opencv.a $1/lib/$TYPE/
-  fi
 
 }
  
