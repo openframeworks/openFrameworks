ed1b7b8e492bd33ead337f05ddcd2d6b9a7ac71e
Christopher Baker <me@christopherbaker.net>
2014-08-19 22:41:35 -0500

    Fix poco header installation.

--- libs/poco/include/Poco/Data/SessionImpl.h
+++ libs/poco/include/Poco/Data/SessionImpl.h
@@ -1,10 +1,10 @@
 //
 // SessionImpl.h
 //
-// $Id: //poco/Main/Data/SQLite/include/Poco/Data/SQLite/SessionImpl.h#2 $
+// $Id: //poco/Main/Data/include/Poco/Data/SessionImpl.h#5 $
 //
-// Library: SQLite
-// Package: SQLite
+// Library: Data
+// Package: DataCore
 // Module:  SessionImpl
 //
 // Definition of the SessionImpl class.
@@ -16,148 +16,208 @@
 //
 
 
-#ifndef Data_SQLite_SessionImpl_INCLUDED
-#define Data_SQLite_SessionImpl_INCLUDED
+#ifndef Data_SessionImpl_INCLUDED
+#define Data_SessionImpl_INCLUDED
 
 
-#include "Poco/Data/SQLite/SQLite.h"
-#include "Poco/Data/SQLite/Connector.h"
-#include "Poco/Data/SQLite/Binder.h"
-#include "Poco/Data/AbstractSessionImpl.h"
-#include "Poco/SharedPtr.h"
-
-
-struct sqlite3;
-struct sqlite3_stmt;
+#include "Poco/Data/Data.h"
+#include "Poco/RefCountedObject.h"
+#include "Poco/String.h"
+#include "Poco/Format.h"
+#include "Poco/Any.h"
 
 
 namespace Poco {
-
-class Mutex;
-
 namespace Data {
-namespace SQLite {
 
 
-class SQLite_API SessionImpl: public Poco::Data::AbstractSessionImpl<SessionImpl>
-	/// Implements SessionImpl interface.
+class StatementImpl;
+
+
+class Data_API SessionImpl: public Poco::RefCountedObject
+	/// Interface for Session functionality that subclasses must extend. 
+	/// SessionImpl objects are noncopyable.
 {
 public:
-	SessionImpl(const std::string& fileName,
-		std::size_t loginTimeout = LOGIN_TIMEOUT_DEFAULT);
-		/// Creates the SessionImpl. Opens a connection to the database.
+	static const std::size_t LOGIN_TIMEOUT_INFINITE = 0;
+		/// Infinite connection/login timeout.
 
-	~SessionImpl();
+	static const std::size_t LOGIN_TIMEOUT_DEFAULT = 60;
+		/// Default connection/login timeout in seconds.
+
+	static const std::size_t CONNECTION_TIMEOUT_INFINITE = 0;
+		/// Infinite connection/login timeout.
+
+	static const std::size_t CONNECTION_TIMEOUT_DEFAULT = CONNECTION_TIMEOUT_INFINITE;
+		/// Default connection/login timeout in seconds.
+
+	SessionImpl(const std::string& connectionString,
+		std::size_t timeout = LOGIN_TIMEOUT_DEFAULT);
+		/// Creates the SessionImpl.
+
+	virtual ~SessionImpl();
 		/// Destroys the SessionImpl.
 
-	Poco::Data::StatementImpl* createStatementImpl();
-		/// Returns an SQLite StatementImpl.
+	virtual StatementImpl* createStatementImpl() = 0;
+		/// Creates a StatementImpl.
 
-	void open(const std::string& connect = "");
-		/// Opens a connection to the Database.
-		/// 
-		/// An in-memory system database (sys), with a single table (dual) 
-		/// containing single field (dummy) is attached to the database.
-		/// The in-memory system database is used to force change count
-		/// to be reset to zero on every new query (or batch of queries) 
-		/// execution. Without this functionality, select statements
-		/// executions that do not return any rows return the count of
-		/// changes effected by the most recent insert, update or delete.
-		/// In-memory system database can be queried and updated but can not
-		/// be dropped. It may be used for other purposes 
-		/// in the future.
+	virtual void open(const std::string& connectionString = "") = 0;
+		/// Opens the session using the supplied string.
+		/// Can also be used with default empty string to reconnect 
+		/// a disconnected session.
+		/// If the connection is not established within requested timeout 
+		/// (specified in seconds), a ConnectionFailedException is thrown. 
+		/// Zero timout means indefinite
 
-	void close();
-		/// Closes the session.
+	virtual void close() = 0;
+		/// Closes the connection.
 
-	bool isConnected();
-		/// Returns true if connected, false otherwise.
+	virtual bool isConnected() = 0;
+		/// Returns true if session is connected, false otherwise.
 
-	void setConnectionTimeout(std::size_t timeout);
+	void setLoginTimeout(std::size_t timeout);
+		/// Sets the session login timeout value.
+
+	std::size_t getLoginTimeout() const;
+		/// Returns the session login timeout value.
+
+	virtual void setConnectionTimeout(std::size_t timeout) = 0;
 		/// Sets the session connection timeout value.
 
-	std::size_t getConnectionTimeout();
+	virtual std::size_t getConnectionTimeout() = 0;
 		/// Returns the session connection timeout value.
 
-	void begin();
+	void reconnect();
+		/// Closes the connection and opens it again.
+
+	virtual void begin() = 0;
 		/// Starts a transaction.
 
-	void commit();
+	virtual void commit() = 0;
 		/// Commits and ends a transaction.
 
-	void rollback();
+	virtual void rollback() = 0;
 		/// Aborts a transaction.
 
-	bool canTransact();
+	virtual bool canTransact() = 0;
 		/// Returns true if session has transaction capabilities.
 
-	bool isTransaction();
+	virtual bool isTransaction() = 0;
 		/// Returns true iff a transaction is a transaction is in progress, false otherwise.
 
-	void setTransactionIsolation(Poco::UInt32 ti);
+	virtual void setTransactionIsolation(Poco::UInt32) = 0;
 		/// Sets the transaction isolation level.
 
-	Poco::UInt32 getTransactionIsolation();
+	virtual Poco::UInt32 getTransactionIsolation() = 0;
 		/// Returns the transaction isolation level.
 
-	bool hasTransactionIsolation(Poco::UInt32 ti);
+	virtual bool hasTransactionIsolation(Poco::UInt32) = 0;
 		/// Returns true iff the transaction isolation level corresponding
 		/// to the supplied bitmask is supported.
 
-	bool isTransactionIsolation(Poco::UInt32 ti);
+	virtual bool isTransactionIsolation(Poco::UInt32) = 0;
 		/// Returns true iff the transaction isolation level corresponds
 		/// to the supplied bitmask.
 
-	void autoCommit(const std::string&, bool val);
-		/// Sets autocommit property for the session.
-
-	bool isAutoCommit(const std::string& name="");
-		/// Returns autocommit property value.
-
-	const std::string& connectorName() const;
+	virtual const std::string& connectorName() const = 0;
 		/// Returns the name of the connector.
 
+	const std::string& connectionString() const;
+		/// Returns the connection string.
+
+	static std::string uri(const std::string& connector, const std::string& connectionString);
+		/// Returns formatted URI.
+
+	std::string uri() const;
+		/// Returns the URI for this session.
+
+	virtual void setFeature(const std::string& name, bool state) = 0;
+		/// Set the state of a feature.
+		///
+		/// Features are a generic extension mechanism for session implementations.
+		/// and are defined by the underlying SessionImpl instance.
+		///
+		/// Throws a NotSupportedException if the requested feature is
+		/// not supported by the underlying implementation.
+	
+	virtual bool getFeature(const std::string& name) = 0;
+		/// Look up the state of a feature.
+		///
+		/// Features are a generic extension mechanism for session implementations.
+		/// and are defined by the underlying SessionImpl instance.
+		///
+		/// Throws a NotSupportedException if the requested feature is
+		/// not supported by the underlying implementation.
+
+	virtual void setProperty(const std::string& name, const Poco::Any& value) = 0;
+		/// Set the value of a property.
+		///
+		/// Properties are a generic extension mechanism for session implementations.
+		/// and are defined by the underlying SessionImpl instance.
+		///
+		/// Throws a NotSupportedException if the requested property is
+		/// not supported by the underlying implementation.
+
+	virtual Poco::Any getProperty(const std::string& name) = 0;
+		/// Look up the value of a property.
+		///
+		/// Properties are a generic extension mechanism for session implementations.
+		/// and are defined by the underlying SessionImpl instance.
+		///
+		/// Throws a NotSupportedException if the requested property is
+		/// not supported by the underlying implementation.
+
+protected:
+	void setConnectionString(const std::string& connectionString);
+		/// Sets the connection string. Should only be called on
+		/// disconnetced sessions. Throws InvalidAccessException when called on
+		/// a connected session.
+
 private:
-	std::string _connector;
-	sqlite3*    _pDB;
-	bool        _connected;
-	bool        _isTransaction;
-	int         _timeout;
-	Mutex       _mutex;
-	static const std::string DEFERRED_BEGIN_TRANSACTION;
-	static const std::string COMMIT_TRANSACTION;
-	static const std::string ABORT_TRANSACTION;
+	SessionImpl();
+	SessionImpl(const SessionImpl&);
+	SessionImpl& operator = (const SessionImpl&);
+
+	std::string _connectionString;
+	std::size_t _loginTimeout;
 };
 
 
 //
 // inlines
 //
-inline bool SessionImpl::canTransact()
+inline const std::string& SessionImpl::connectionString() const
 {
-	return true;
+	return _connectionString;
 }
 
 
-inline 	bool SessionImpl::isTransaction()
+inline void SessionImpl::setLoginTimeout(std::size_t timeout)
 {
-	return _isTransaction;
+	_loginTimeout = timeout;
 }
 
 
-inline const std::string& SessionImpl::connectorName() const
+inline std::size_t SessionImpl::getLoginTimeout() const
 {
-	return _connector;
+	return _loginTimeout;
 }
 
 
-inline std::size_t SessionImpl::getConnectionTimeout()
+inline std::string SessionImpl::uri(const std::string& connector,
+	const std::string& connectionString)
 {
-	return static_cast<std::size_t>(_timeout);
+	return format("%s:///%s", connector, connectionString);
 }
 
 
-} } } // namespace Poco::Data::SQLite
+inline std::string SessionImpl::uri() const
+{
+	return uri(connectorName(), connectionString());
+}
 
 
-#endif // Data_SQLite_SessionImpl_INCLUDED
+} } // namespace Poco::Data
+
+
+#endif // Data_SessionImpl_INCLUDED
