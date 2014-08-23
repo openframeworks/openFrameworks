db0b53b5f1b13d27750cd9b6e1ce3af028077144
arturo <arturo@openframeworks.cc>
2014-08-23 19:48:37 +0200

    fix tr1::unordered_map

--- libs/openFrameworks/gl/ofShader.h
+++ libs/openFrameworks/gl/ofShader.h
@@ -14,9 +14,10 @@
 #include "Poco/RegularExpression.h"
 #include <map>
 #if __cplusplus>=200103L
-#include <unordered_map>
+	#include <unordered_map>
 #else
-#include <tr1/unordered_map>
+	#include <tr1/unordered_map>
+	using std::tr1::unordered_map;
 #endif
 #include "ofAppBaseWindow.h"
 
