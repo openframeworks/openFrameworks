d919d642787bc0f9cdfc07e821fd6e0079d53f6f
Elie Zananiri <prisonerjohn@gmail.com>
2014-09-07 14:58:05 -0400

    Updated video player example to ensure video is loaded before drawing it.

--- examples/video/videoPlayerExample/src/ofApp.cpp
+++ examples/video/videoPlayerExample/src/ofApp.cpp
@@ -22,20 +22,24 @@ void ofApp::update(){
 void ofApp::draw(){
 
 	ofSetHexColor(0xFFFFFF);
-
-    fingerMovie.draw(20,20);
-    ofSetHexColor(0x000000);
-    unsigned char * pixels = fingerMovie.getPixels();
     
-    int nChannels = fingerMovie.getPixelsRef().getNumChannels();
-    
-    // let's move through the "RGB(A)" char array
-    // using the red pixel to control the size of a circle.
-    for (int i = 4; i < 320; i+=8){
-        for (int j = 4; j < 240; j+=8){
-            unsigned char r = pixels[(j * 320 + i)*nChannels];
-            float val = 1 - ((float)r / 255.0f);
-            ofCircle(400 + i,20+j,10*val);
+    // movie loads asynchronously, so make sure it's ready
+    // before drawing it
+    if (fingerMovie.isLoaded()) {
+        fingerMovie.draw(20,20);
+        ofSetHexColor(0x000000);
+        unsigned char * pixels = fingerMovie.getPixels();
+        
+        int nChannels = fingerMovie.getPixelsRef().getNumChannels();
+        
+        // let's move through the "RGB(A)" char array
+        // using the red pixel to control the size of a circle.
+        for (int i = 4; i < 320; i+=8){
+            for (int j = 4; j < 240; j+=8){
+                unsigned char r = pixels[(j * 320 + i)*nChannels];
+                float val = 1 - ((float)r / 255.0f);
+                ofCircle(400 + i,20+j,10*val);
+            }
         }
     }
 
