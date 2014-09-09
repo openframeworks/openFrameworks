0b42a71a31184d7e554d97b8af77251605a697fe
ofTheo <theo@openframeworks.cc>
2014-09-09 13:29:11 -0400

    changes to reenable broadcast and reuse.

--- addons/ofxOsc/src/ofxOscSender.cpp
+++ addons/ofxOsc/src/ofxOscSender.cpp
@@ -47,12 +47,12 @@ ofxOscSender::~ofxOscSender()
 		shutdown();
 }
 
-void ofxOscSender::setup( std::string hostname, int port )
+void ofxOscSender::setup( std::string hostname, int port, bool enableBroadcast )
 {
 	if ( socket )
 		shutdown();
 	
-	socket = new UdpTransmitSocket( IpEndpointName( hostname.c_str(), port ) );
+    socket = new UdpTransmitSocket(IpEndpointName( hostname.c_str(), port), enableBroadcast);
 }
 
 void ofxOscSender::shutdown()
