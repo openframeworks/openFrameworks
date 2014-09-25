aa1ff1ea4e10978874fd1d11bb10f723ebb93239
lukasz karluk <julapy@googlemail.com>
2014-09-25 18:18:50 +1000

    ofAppiOSWindow::Settings
    - ofAppiOSWindow can now be initiated with a Settings object (same as
    in ofFbo::Settings)
    - updated all examples to show how settings can be used.

--- examples/ios/audioOutputExample/src/main.mm
+++ examples/ios/audioOutputExample/src/main.mm
@@ -1,8 +1,23 @@
 #include "ofMain.h"
+#include "ofAppiOSWindow.h"
 #include "ofApp.h"
 
-int main(){
-	ofSetupOpenGL(1024,768, OF_FULLSCREEN);			// <-------- setup the GL context
-
-	ofRunApp(new ofApp());
+int main() {
+    
+    //  here are the most commonly used iOS window settings.
+    //------------------------------------------------------
+    ofAppiOSWindow::Settings settings;
+    settings.enableRetina = false; // enables retina resolution if the device supports it.
+    settings.enableDepth = false; // enables depth buffer for 3d drawing.
+    settings.enableAntiAliasing = false; // enables anti-aliasing which smooths out graphics on the screen.
+    settings.numOfAntiAliasingSamples = 0; // number of samples used for anti-aliasing.
+    settings.enableHardwareOrientation = false; // enables native view orientation.
+    settings.enableHardwareOrientationAnimation = false; // enables native orientation changes to be animated.
+    settings.rendererType = OFXIOS_RENDERER_ES1; // type of renderer to use, ES1, ES2, etc.
+    
+    ofAppiOSWindow window(settings);
+    
+	ofSetupOpenGL(&window, 0, 0, OF_FULLSCREEN);
+    
+	ofRunApp(new ofApp);
 }
