0888087d3efbf0e54a8ba219581b19fb7a65144c
Daniel Rosser <danoli3@gmail.com>
2014-09-29 15:38:49 +1000

    Apothecary Formula - Tess2 iOS (arm64, armv7, armv7s, i386, x86_64)
    - Updated with -fPIC
    - Removed external build.sh.
    - Looping thru architectures
    - Building against libc++, std=c++11
    - Building with Clang, Clang++

--- scripts/apothecary/formulas/tess2/tess2.sh
+++ scripts/apothecary/formulas/tess2/tess2.sh
@@ -15,6 +15,12 @@ VER=1.0
 GIT_URL=https://github.com/memononen/libtess2.git
 GIT_TAG=master
 
+CSTANDARD=c11 # c89 | c99 | c11 | gnu11
+CPPSTANDARD=c++11 # c89 | c99 | c11 | gnu11
+COMPILER_CTYPE=clang # clang, gcc
+COMPILER_CPPTYPE=clang++ # clang, gcc
+STDLIB=libc++
+
 # download the source code and unpack it into LIB_NAME
 function download() {
 	curl -LO https://libtess2.googlecode.com/files/libtess2-$VER.zip
@@ -34,8 +40,6 @@ function prepare() {
 	# copy in build script and CMake toolchains adapted from Assimp
 	if [ "$OS" == "osx" ] ; then
 		mkdir -p build
-		cp -Rv $FORMULA_DIR/ios build
-		chmod +x build/ios/build.sh
 	fi
 }
 
@@ -65,8 +69,165 @@ function build() {
 		vs-build "tess2.sln"
 
 	elif [ "$TYPE" == "ios" ] ; then
-		cd build/ios
-		./build.sh # build fat lib
+	
+		IOS_ARCHS="i386 x86_64 armv7 armv7s arm64"
+
+		SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`	
+		set -e
+		CURRENTPATH=`pwd`
+		
+		DEVELOPER=$XCODE_DEV_ROOT
+		TOOLCHAIN=${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain
+		VERSION=$VER
+
+		mkdir -p "builddir/$TYPE"
+	
+		# Validate environment
+		case $XCODE_DEV_ROOT in  
+		     *\ * )
+		           echo "Your Xcode path contains whitespaces, which is not supported."
+		           exit 1
+		          ;;
+		esac
+		case $CURRENTPATH in  
+		     *\ * )
+		           echo "Your path contains whitespaces, which is not supported by 'make install'."
+		           exit 1
+		          ;;
+		esac 
+
+		
+		export CC=$TOOLCHAIN/usr/bin/$COMPILER_CTYPE
+		export CPP=$TOOLCHAIN/usr/bin/$COMPILER_CPPTYPE
+		export CXX=$TOOLCHAIN/usr/bin/$COMPILER_CTYPE
+		export CXXCPP=$TOOLCHAIN/usr/bin/$COMPILER_CPPTYPE
+
+		export LD=$TOOLCHAIN/usr/bin/ld
+		export AR=$TOOLCHAIN/usr/bin/ar
+		export AS=$TOOLCHAIN/usr/bin/as
+		export NM=$$TOOLCHAIN/usr/bin/nm
+		export RANLIB=$TOOLCHAIN/usr/bin/ranlib
+	
+		EXTRA_LINK_FLAGS="-stdlib=libc++ -Os -fPIC"
+		EXTRA_FLAGS="$EXTRA_LINK_FLAGS -fvisibility-inlines-hidden"
+		
+
+		# loop through architectures! yay for loops!
+		for IOS_ARCH in ${IOS_ARCHS}
+		do
+
+			unset CFLAGS CPPFLAGS LINKFLAGS CXXFLAGS LDFLAGS
+			rm -f CMakeCache.txt
+			set +e
+
+			#export ALL_IOS_ARCH="-arch armv7 -arch armv7s -arch arm64"
+			if [[ "${IOS_ARCH}" == "i386" || "${IOS_ARCH}" == "x86_64" ]];
+			then
+				PLATFORM="iPhoneSimulator"
+			
+			else
+				PLATFORM="iPhoneOS"
+			fi
+
+			export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
+			export CROSS_SDK="${PLATFORM}${SDKVERSION}.sdk"
+			export BUILD_TOOLS="${DEVELOPER}"
+
+			MIN_IOS_VERSION=$IOS_MIN_SDK_VER
+		    # min iOS version for arm64 is iOS 7
+		
+		    if [[ "${IOS_ARCH}" == "arm64" || "${IOS_ARCH}" == "x86_64" ]]; then
+		    	MIN_IOS_VERSION=7.0 # 7.0 as this is the minimum for these architectures
+		    elif [ "${IOS_ARCH}" == "i386" ]; then
+		    	MIN_IOS_VERSION=5.1 # 6.0 to prevent start linking errors
+		    fi
+
+		    MIN_TYPE=-miphoneos-version-min=
+		    if [[ "${IOS_ARCH}" == "i386" || "${IOS_ARCH}" == "x86_64" ]]; then
+		    	MIN_TYPE=-mios-simulator-version-min=
+		    fi
+
+
+			export CFLAGS="-arch $IOS_ARCH -pipe -no-cpp-precomp -isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} $MIN_TYPE$MIN_IOS_VERSION -I${CROSS_TOP}/SDKs/${CROSS_SDK}/usr/include/" 
+	
+			export CPPFLAGS=$CFLAGS
+			export LINKFLAGS="$CFLAGS $EXTRA_LINK_FLAGS"
+			export LDFLAGS="-L${CROSS_TOP}/SDKs/${CROSS_SDK}/usr/lib/ $LINKFLAGS"
+			export CXXFLAGS="$CFLAGS $EXTRA_FLAGS"
+
+			mkdir -p "$CURRENTPATH/builddir/$TYPE/$IOS_ARCH"
+			LOG="$CURRENTPATH/builddir/$TYPE/$IOS_ARCH/build-tess2-${VER}-$IOS_ARCH.log"
+			echo "-----------------"
+			echo "Building tess2-${VER} for ${PLATFORM} ${SDKVERSION} ${IOS_ARCH} : iOS Minimum=$MIN_IOS_VERSION"
+			set +e
+
+			echo "Running make for ${IOS_ARCH}"
+			echo "Please stand by..."
+
+			cmake -G 'Unix Makefiles' 
+			make clean >> "${LOG}" 2>&1
+			make >> "${LOG}" 2>&1
+
+			if [ $? != 0 ];
+		    then 
+		    	echo "Problem while make - Please check ${LOG}"
+		    	exit 1
+		    else
+		    	echo "Make Successful for ${IOS_ARCH}"
+		    fi
+
+			mv libtess2.a builddir/$TYPE/libtess2-$IOS_ARCH.a
+
+		done
+
+		echo "-----------------"
+		echo `pwd`
+		echo "Finished for all architectures."
+		mkdir -p "$CURRENTPATH/builddir/$TYPE/$IOS_ARCH"
+		LOG="$CURRENTPATH/builddir/$TYPE/build-tess2-${VER}-lipo.log"
+
+		mkdir -p "lib/$TYPE"
+
+		# link into universal lib
+		echo "Running lipo to create fat lib"
+		echo "Please stand by..."
+		lipo -create builddir/$TYPE/libtess2-armv7.a \
+			 	builddir/$TYPE/libtess2-armv7s.a \
+			 	builddir/$TYPE/libtess2-arm64.a \
+			 	builddir/$TYPE/libtess2-i386.a \
+			 	builddir/$TYPE/libtess2-x86_64.a \
+			 	-output builddir/$TYPE/libtess2.a \
+			 	>> "${LOG}" 2>&1
+
+		if [ $? != 0 ];
+		then 
+		    echo "Problem while creating fat lib with lipo - Please check ${LOG}"
+		    exit 1
+		else
+		   	echo "Lipo Successful."
+		fi
+
+		mv builddir/$TYPE/libtess2.a lib/$TYPE/libtess2.a
+		lipo -info lib/$TYPE/libtess2.a
+
+		echo "--------------------"
+		echo "Stripping any lingering symbols"
+
+		SLOG="$CURRENTPATH/lib/$TYPE/tess2-stripping.log"
+
+		strip -x lib/$TYPE/libtess2.a >> "${SLOG}" 2>&1
+		if [ $? != 0 ];
+		then 
+		    echo "Problem while stripping lib - Please check ${SLOG}"
+		    exit 1
+		else
+		    echo "Strip Successful for ${SLOG}"
+		fi
+
+		echo "--------------------"
+		echo "Build Successful for Tess2 $TYPE"
+		unset SDKROOT CFLAGS CC LD CPP CXX AR AS NM CXXCPP RANLIB LDFLAGS CPPFLAGS CXXFLAGS LINKFLAGS
+		unset CROSS_TOP CROSS_SDK BUILD_TOOLS
 
 	elif [ "$TYPE" == "android" ] ; then
 		echoWarning "TODO: android build"
@@ -86,7 +247,7 @@ function copy() {
 
 	# source
 	mkdir -p $1/Source
-	cp -v Source/* $1/Source
+	cp -v Source/* $1/Sources
 
 	# lib
 	mkdir -p $1/lib/$TYPE
@@ -94,7 +255,7 @@ function copy() {
 		cp -v Release/tess2.lib $1/lib/$TYPE/tess2.lib
 
 	elif [ "$TYPE" == "ios" ] ; then 
-		cp -v libtess2.a $1/lib/$TYPE/tess2.a
+		cp -v lib/$TYPE/libtess2.a $1/lib/$TYPE/tess2.a
 
 	elif [ "$TYPE" == "android" ] ; then
 		echoWarning "TODO: copy android lib"
@@ -112,7 +273,12 @@ function clean() {
 	
 	elif [ "$TYPE" == "android" ] ; then
 		echoWarning "TODO: clean android"
-
+	elif [ "$TYPE" == "ios" ] ; then
+		make clean
+		rm -f CMakeCache.txt *.a *.lib
+		rm -f builddir/$TYPE
+		rm -f builddir
+		rm -f lib
 	else
 		make clean
 		rm -f CMakeCache.txt *.a *.lib
