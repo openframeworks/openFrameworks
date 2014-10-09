8d6312cebc3cda0b329fb1e103722d94f5eb6cad
arturo <arturo@openframeworks.cc>
2014-10-09 11:06:42 +0200

    ofThreadChannel: inter thread communication
    this implements a way of synchronizing threads that allows to avoid
    locks in most cases making it way easier to work with multithreaded
    applications.
    
    instead of having some shared memory area + mutexes to lock, 2 threads
    send messages from one to another through a channel. the channel
    automatically does the locking internally.
    
    the receiving end of the channel can wait for a message to arrive
    which effectively blocks the thread until there's new data to
    process, avoiding consuming any CPU cycles at all if there's nothing
    to do (ie: no need to call to ofSleep to throtle the thread CPU usage)
    
    both ofURLFileLoader and ofxThreadedImageLoader are now implemented
    using channels which simplifies their logic a lot. there's also an
    example in utils/threadedChannelExample showing how it's used

--- addons/ofxThreadedImageLoader/src/ofxThreadedImageLoader.h
+++ addons/ofxThreadedImageLoader/src/ofxThreadedImageLoader.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include <deque>
 #include "ofThread.h"
 #include "ofImage.h"
 #include "ofURLFileLoader.h"
 #include "ofTypes.h" 
+#include "ofThreadChannel.h"
 
-// must use poco condition not lock for this
-#include "Poco/Condition.h"
 
 using namespace std;
 
@@ -25,48 +23,31 @@ private:
 	void update(ofEventArgs & a);
     virtual void threadedFunction();
 	void urlResponse(ofHttpResponse & response);
-
-    // Where to load form?
-    enum ofLoaderType {
-        OF_LOAD_FROM_DISK
-        ,OF_LOAD_FROM_URL
-    };
-    
     
     // Entry to load.
     struct ofImageLoaderEntry {
         ofImageLoaderEntry() {
             image = NULL;
-            type = OF_LOAD_FROM_DISK;
-            id=0;
         }
         
-        ofImageLoaderEntry(ofImage & pImage, ofLoaderType nType) {
+        ofImageLoaderEntry(ofImage & pImage) {
             image = &pImage;
-            type = nType;
-            id=0;
         }
         ofImage* image;
-        ofLoaderType type;
         string filename;
         string url;
         string name;
-        int id;
     };
 
 
-    typedef deque<ofImageLoaderEntry>::iterator entry_iterator;
-	entry_iterator      getEntryFromAsyncQueue(string name);
+    typedef map<string, ofImageLoaderEntry>::iterator entry_iterator;
 
 	int                 nextID;
-
-    Poco::Condition     condition;
-
     int                 lastUpdate;
 
-	deque<ofImageLoaderEntry> images_async_loading; // keeps track of images which are loading async
-	deque<ofImageLoaderEntry> images_to_load_buffer;
-    deque<ofImageLoaderEntry> images_to_update;
+	map<string,ofImageLoaderEntry> images_async_loading; // keeps track of images which are loading async
+	ofThreadChannel<ofImageLoaderEntry> images_to_load_from_disk;
+	ofThreadChannel<ofImageLoaderEntry> images_to_update;
 };
 
 
