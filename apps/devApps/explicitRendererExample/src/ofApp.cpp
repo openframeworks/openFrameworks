09ef5dadb224647f6f2cb440f81a85a25e69c77a
arturo <arturo@openframeworks.cc>
2014-10-28 20:32:41 +0100

    explicit renderer dev app

---
+++ apps/devApps/explicitRendererExample/src/ofApp.cpp
@@ -0,0 +1,74 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	gl->background(255,255,255);
+	camera.setEvents(window->events());
+	camera.setRenderer(gl);
+	gui->radius.addListener(this,&ofApp::radiusChanged);
+}
+
+void ofApp::radiusChanged(float & radius){
+	sphere.set(radius,radius*0.2);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	gl->bind(camera,gl->getCurrentViewport());
+	gl->setColor(gui->color);
+	gl->draw(sphere,OF_MESH_WIREFRAME);
+	gl->unbind(camera);
+
+	gl->setColor(0);
+	gl->drawString(ofToString(window->events().getFrameRate()),20,20,0);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
