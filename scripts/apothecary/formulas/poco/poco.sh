728e2638624f08d9ff814a0d060deb71f4bc750f
Daniel Rosser <danoli3@gmail.com>
2014-11-25 02:47:10 +1100

    Apothecary Formula - iOS Update for Poco - Removed armv7s
    (cherry picked from commit ae730a006e0068a197030c73c913d388a69315d3)

--- scripts/apothecary/formulas/poco/poco.sh
+++ scripts/apothecary/formulas/poco/poco.sh
@@ -165,7 +165,7 @@ function build() {
 		TOOLCHAIN=${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain
 		VERSION=$VER
 
-		local IOS_ARCHS="i386 x86_64 armv7 armv7s arm64"
+		local IOS_ARCHS="i386 x86_64 armv7 arm64"
 		echo "--------------------"
 		echo $CURRENTPATH
 
@@ -283,7 +283,7 @@ function build() {
 		for lib in $( ls -1 i386) ; do
 			local renamedLib=$(echo $lib | sed 's|lib||')
 			if [ ! -e $renamedLib ] ; then
-				lipo -c armv7/$lib armv7s/$lib arm64/$lib i386/$lib x86_64/$lib -o ../ios/$renamedLib
+				lipo -c armv7/$lib arm64/$lib i386/$lib x86_64/$lib -o ../ios/$renamedLib
 			fi
 		done
 
