ee935951976bc168cf5acc6df58e81c5e5560314
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2015-01-10 13:48:56 -0800

    remove video player handling for 10.6 and 10.7
    With OS X 10.8 as the minimum, the AV Foundation player can be used for all supported versions of OS X, 10.8-10.10.

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -299,23 +299,16 @@ typedef TESSindex ofIndexType;
 //------------------------------------------------  video player
 // check if any video player system is already defined from the compiler
 #if !defined(OF_VIDEO_PLAYER_GSTREAMER) && !defined(OF_VIDEO_PLAYER_IOS) && !defined(OF_VIDEO_PLAYER_DIRECTSHOW) && !defined(OF_VIDEO_PLAYER_QUICKTIME) && !defined(OF_VIDEO_PLAYER_AVFOUNDATION) && !defined(OF_VIDEO_PLAYER_EMSCRIPTEN)
-    #ifdef TARGET_LINUX
+    #if defined(TARGET_LINUX)
         #define OF_VIDEO_PLAYER_GSTREAMER
     #elif defined(TARGET_ANDROID)
         #define OF_VIDEO_PLAYER_ANDROID
+    #elif defined(TARGET_OSX)
+        #define OF_VIDEO_PLAYER_AVFOUNDATION
     #elif defined(TARGET_OF_IOS)
         #define OF_VIDEO_PLAYER_IOS
-	#elif defined(TARGET_WIN32) && !defined(__MINGW32__)
+    #elif defined(TARGET_WIN32) && !defined(__MINGW32__)
         #define OF_VIDEO_PLAYER_DIRECTSHOW
-    #elif defined(TARGET_OSX)
-        //for 10.8 and 10.9 users we use AVFoundation, for 10.7 we use QTKit, for 10.6 users we use QuickTime
-        #ifndef MAC_OS_X_VERSION_10_7
-            #define OF_VIDEO_PLAYER_QUICKTIME
-        #elif !defined(MAC_OS_X_VERSION_10_8)
-            #define OF_VIDEO_PLAYER_QTKIT
-        #else
-            #define OF_VIDEO_PLAYER_AVFOUNDATION
-        #endif
     #elif defined(TARGET_EMSCRIPTEN)
         #define OF_VIDEO_PLAYER_EMSCRIPTEN
     #else
