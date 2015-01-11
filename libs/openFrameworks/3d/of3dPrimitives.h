e34204550643aa2c3e4d10ffc2e30ce924219244
Johan Bichel Lindegaard <johan@johan.cc>
2015-01-11 16:41:57 +0100

    sphere

--- libs/openFrameworks/3d/of3dPrimitives.h
+++ libs/openFrameworks/3d/of3dPrimitives.h
@@ -155,6 +155,57 @@ protected:
     ofVec2f resolution;
 };
 
+
+/// \brief The ofSpherePrimitive allows you to create a UV sphere.
+/// A UV spherewhich is a sphere based on UV slices rather than faces like 
+/// the ofIcoSpherePrimitive. Note this:
+/// 
+/// [IMG](3d/spheretypes.png)
+/// 
+/// On the left is a UV sphere and on the right is an ICO sphere.
+/// 
+/// Like all primitives it allows you to set the size (radius), draw it, 
+/// set positions, etc, as a simple example:
+/// 
+/// ~~~~{.cpp}
+/// 
+/// void setup()
+/// {
+/// 
+///     sphere.setRadius( width );
+/// }
+/// 
+/// void draw()
+/// {
+/// 
+///     sphere.setPosition(ofGetWidth()*.2, ofGetHeight()*.75, 0);
+///     sphere.rotate(spinX, 1.0, 0.0, 0.0);
+///     sphere.rotate(spinY, 0, 1.0, 0.0);
+/// 
+///     // get all the faces from the icoSphere, handy when you want to copy
+///     // individual vertices or tweak them a little ;)
+///     vector<ofMeshFace> triangles = sphere.getMesh().getUniqueFaces();
+/// 
+///     // now draw
+///     sphere.draw();
+/// }
+/// ~~~~
+/// 
+/// To draw a texture over any primitive, simply bind the ofTexture instance 
+/// and then draw your primitive:
+/// 
+/// ~~~~{.cpp}
+/// 
+/// texture.getTextureReference().bind();
+/// // now draw
+/// sphere.draw();
+/// 
+/// ~~~~
+/// 
+/// The sphere can look a little weird if you don't do ofEnabledDepthTesting() 
+/// and ofDisableAlphaBlending().
+/// 
+
 class ofSpherePrimitive : public of3dPrimitive {
 public:
     ofSpherePrimitive();
@@ -266,7 +317,6 @@ protected:
 /// cylinder.draw();
 /// 
 /// ~~~~
-
 
 class ofCylinderPrimitive : public of3dPrimitive {
 public:
