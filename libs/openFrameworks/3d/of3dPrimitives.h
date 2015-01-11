f787ea2d31d2d32465a0c6d27775776afbf8e7ee
Johan Bichel Lindegaard <johan@johan.cc>
2015-01-11 16:34:38 +0100

    icoSphere

--- libs/openFrameworks/3d/of3dPrimitives.h
+++ libs/openFrameworks/3d/of3dPrimitives.h
@@ -129,6 +129,37 @@ protected:
     int resolution;
 };
 
+/// \brief The ofIcoSpherePrimitive allows you to create an icosphere. 
+/// An icosphere is a sphere not based on UV slices like the ofSpherePrimitive,
+/// but rather faces. Like this:
+/// 
+/// Like all primitives it allows you to set the radius, 
+/// draw it, set positions, etc.
+/// 
+/// ~~~~{.cpp}
+/// 
+/// void setup()
+/// {
+/// 
+///     icoSphere.setRadius( width );
+/// }
+/// 
+/// void draw()
+/// {
+/// 
+///     icoSphere.setPosition(ofGetWidth()*.2, ofGetHeight()*.75, 0);
+///     icoSphere.rotate(spinX, 1.0, 0.0, 0.0);
+///     icoSphere.rotate(spinY, 0, 1.0, 0.0);
+/// 
+///     // get all the faces from the icoSphere, handy when you want to copy
+///     // individual vertices or tweak them a little ;)
+///     vector<ofMeshFace> triangles = icoSphere.getMesh().getUniqueFaces();
+/// 
+///     // now draw
+///     icoSphere.draw();
+/// }
+/// ~~~~
+
 class ofIcoSpherePrimitive : public of3dPrimitive {
 public:
     ofIcoSpherePrimitive();
