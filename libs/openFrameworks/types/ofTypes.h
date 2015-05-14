b83b78d08f4bcc39d2ced9e71deebe23ef0bfb75
tgfrerer <tim@poniesandlight.co.uk>
2015-05-14 11:37:00 +0200

    update ofTypes
    + include <memory> for emscripten, osx, ios, linux if cpp11 is available, otherwise include <tr1/memory>
    
    i'm making this a multiline #ifdef so that future additions should be easier to add / merge.

--- libs/openFrameworks/types/ofTypes.h
+++ libs/openFrameworks/types/ofTypes.h
@@ -5,10 +5,10 @@
 
 #if (_MSC_VER) \
 	|| ((defined(TARGET_EMSCRIPTEN) \
-		|| defined(TARGET_LINUX) \
-		|| defined(TARGET_ANDROID) \
-		|| defined(TARGET_OSX) \
-	) && __cplusplus>=201103L)
+	|| defined(TARGET_OF_IOS) \
+	|| defined(TARGET_LINUX) \
+	|| defined(TARGET_OSX) \
+) && __cplusplus>=201103L)
 #include <memory>
 #else
 #include <tr1/memory>
