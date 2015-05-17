52edfab3f028e328de40e9d644e5b8664b20a695
arturo castro <arturo@openframeworks.cc>
2015-05-17 20:59:29 +0200

    ofTimer: osx fix

--- libs/openFrameworks/utils/ofTimer.cpp
+++ libs/openFrameworks/utils/ofTimer.cpp
@@ -74,8 +74,8 @@ void ofTimer::calculateNextPeriod(){
         nextWakeTime.tv_nsec-=NANOS_PER_SEC*secs;
         nextWakeTime.tv_sec+=secs;
     }
-#if (defined(TARGET_LINUX) && !defined(TARGET_RASPBERRY_PI))
     timespec now;
+#if (defined(TARGET_LINUX) && !defined(TARGET_RASPBERRY_PI))
     clock_gettime(CLOCK_MONOTONIC,&now);
 #else
     ofGetMonotonicTime(now.tv_sec,now.tv_ns);
