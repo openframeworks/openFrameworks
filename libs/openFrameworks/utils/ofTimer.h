e2273af7ce25fe324952043d6a9d0072a898bc50
arturo castro <arturo@openframeworks.cc>
2015-05-17 15:53:49 +0200

    ofTimer: check that next wake up time is in the future. Closes #3822

--- libs/openFrameworks/utils/ofTimer.h
+++ libs/openFrameworks/utils/ofTimer.h
@@ -5,19 +5,16 @@ class ofTimer {
 public:
 	ofTimer();
 	void reset();
-	void setPeriodicEvent(unsigned long long nanoseconds);
+	void setPeriodicEvent(uint64_t nanoseconds);
 	void waitNext();
 private:
 	void calculateNextPeriod();
-#if (defined(TARGET_LINUX) && !defined(TARGET_RASPBERRY_PI))
-	timespec nextWakeTime;
-#elif defined(TARGET_WIN32)
+#if defined(TARGET_WIN32)
 	LARGE_INTEGER nextWakeTime;
 	HANDLE hTimer;
 #else
-	unsigned long long   nextWakeTimeSecs;
-	unsigned long long   nextWakeTimeNanos;
+	timespec nextWakeTime;
 #endif
-	unsigned long long nanosPerPeriod;
+	uint64_t nanosPerPeriod;
 };
 
