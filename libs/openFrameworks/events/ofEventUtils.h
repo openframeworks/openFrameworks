e2440b379ce03b4d6d687015f0feafe199832957
arturo castro <arturo@openframeworks.cc>
2015-06-05 12:32:21 +0200

    events: c++11 version ifdef'd to use poco in older compilers

--- libs/openFrameworks/events/ofEventUtils.h
+++ libs/openFrameworks/events/ofEventUtils.h
@@ -1,41 +1,11 @@
 #pragma once
 
 #include "ofConstants.h"
-
-#include "Poco/PriorityEvent.h"
-#include "Poco/PriorityDelegate.h"
-#include "ofDelegate.h"
+#include "ofEvent.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 
-//-----------------------------------------
-// define ofEvent as a poco FIFOEvent
-// to create your own events use:
-// ofEvent<argType> myEvent
-
-template <typename ArgumentsType>
-class ofEvent: public Poco::PriorityEvent<ArgumentsType> {
-public:
-
-	ofEvent():Poco::PriorityEvent<ArgumentsType>(){
-
-	}
-
-	// allow copy of events, by copying everything except the mutex
-	ofEvent(const ofEvent<ArgumentsType> & mom)
-	:Poco::PriorityEvent<ArgumentsType>()
-	{
-		this->_enabled = mom._enabled;
-	}
-
-	ofEvent<ArgumentsType> & operator=(const ofEvent<ArgumentsType> & mom){
-		if(&mom == this) return *this;
-		this->_enabled = mom._enabled;
-		return *this;
-	}
-
-};
 
 
 enum ofEventOrder{
@@ -55,50 +25,50 @@ enum ofEventOrder{
 
 template <class EventType,typename ArgumentsType, class ListenerClass>
 void ofAddListener(EventType & event, ListenerClass  * listener, void (ListenerClass::*listenerMethod)(const void*, ArgumentsType&), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= Poco::priorityDelegate(listener, listenerMethod, prio);
-    event += Poco::priorityDelegate(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
+    event.add(listener, listenerMethod, prio);
 }
 
 template <class EventType,typename ArgumentsType, class ListenerClass>
 void ofAddListener(EventType & event, ListenerClass  * listener, void (ListenerClass::*listenerMethod)(ArgumentsType&), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= Poco::priorityDelegate(listener, listenerMethod, prio);
-    event += Poco::priorityDelegate(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
+    event.add(listener, listenerMethod, prio);
 }
 
 template <class ListenerClass>
 void ofAddListener(ofEvent<void> & event, ListenerClass  * listener, void (ListenerClass::*listenerMethod)(const void*), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= Poco::priorityDelegate(listener, listenerMethod, prio);
-    event += Poco::priorityDelegate(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
+    event.add(listener, listenerMethod, prio);
 }
 
 template <class ListenerClass>
 void ofAddListener(ofEvent<void> & event, ListenerClass  * listener, void (ListenerClass::*listenerMethod)(), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= Poco::priorityDelegate(listener, listenerMethod, prio);
-    event += Poco::priorityDelegate(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
+    event.add(listener, listenerMethod, prio);
 }
 
 template <class EventType,typename ArgumentsType, class ListenerClass>
 void ofAddListener(EventType & event, ListenerClass  * listener, bool (ListenerClass::*listenerMethod)(const void*, ArgumentsType&), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= ofDelegate<ListenerClass,ArgumentsType,true>(listener, listenerMethod, prio);
-    event += ofDelegate<ListenerClass,ArgumentsType,true>(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
+    event.add(listener, listenerMethod, prio);
 }
 
 template <class EventType,typename ArgumentsType, class ListenerClass>
 void ofAddListener(EventType & event, ListenerClass  * listener, bool (ListenerClass::*listenerMethod)(ArgumentsType&), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= ofDelegate<ListenerClass,ArgumentsType,false>(listener, listenerMethod, prio);
-    event += ofDelegate<ListenerClass,ArgumentsType,false>(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
+    event.add(listener, listenerMethod, prio);
 }
 
 template <class ListenerClass>
 void ofAddListener(ofEvent<void> & event, ListenerClass  * listener, bool (ListenerClass::*listenerMethod)(const void*), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= ofDelegate<ListenerClass,void,true>(listener, listenerMethod, prio);
-    event += ofDelegate<ListenerClass,void,true>(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
+    event.add(listener, listenerMethod, prio);
 }
 
 template <class ListenerClass>
 void ofAddListener(ofEvent<void> & event, ListenerClass  * listener, bool (ListenerClass::*listenerMethod)(), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= ofDelegate<ListenerClass,void,false>(listener, listenerMethod, prio);
-    event += ofDelegate<ListenerClass,void,false>(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
+    event.add(listener, listenerMethod, prio);
 }
 //----------------------------------------------------
 // unregister any method of any class to an event.
@@ -111,42 +81,42 @@ void ofAddListener(ofEvent<void> & event, ListenerClass  * listener, bool (Liste
 
 template <class EventType,typename ArgumentsType, class ListenerClass>
 void ofRemoveListener(EventType & event, ListenerClass  * listener, void (ListenerClass::*listenerMethod)(const void*, ArgumentsType&), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= Poco::priorityDelegate(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
 }
 
 template <class EventType,typename ArgumentsType, class ListenerClass>
 void ofRemoveListener(EventType & event, ListenerClass  * listener, void (ListenerClass::*listenerMethod)(ArgumentsType&), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= Poco::priorityDelegate(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
 }
 
 template <class ListenerClass>
 void ofRemoveListener(ofEvent<void> & event, ListenerClass  * listener, void (ListenerClass::*listenerMethod)(const void*), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= Poco::priorityDelegate(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
 }
 
 template <class ListenerClass>
 void ofRemoveListener(ofEvent<void> & event, ListenerClass  * listener, void (ListenerClass::*listenerMethod)(), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= Poco::priorityDelegate(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
 }
 
 template <class EventType,typename ArgumentsType, class ListenerClass>
 void ofRemoveListener(EventType & event, ListenerClass  * listener, bool (ListenerClass::*listenerMethod)(const void*, ArgumentsType&), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= ofDelegate<ListenerClass,ArgumentsType,true>(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
 }
 
 template <class EventType,typename ArgumentsType, class ListenerClass>
 void ofRemoveListener(EventType & event, ListenerClass  * listener, bool (ListenerClass::*listenerMethod)(ArgumentsType&), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= ofDelegate<ListenerClass,ArgumentsType,false>(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
 }
 
 template <class ListenerClass>
 void ofRemoveListener(ofEvent<void> & event, ListenerClass  * listener, bool (ListenerClass::*listenerMethod)(const void*), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= ofDelegate<ListenerClass,void,true>(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
 }
 
 template <class ListenerClass>
 void ofRemoveListener(ofEvent<void> & event, ListenerClass  * listener, bool (ListenerClass::*listenerMethod)(), int prio=OF_EVENT_ORDER_AFTER_APP){
-    event -= ofDelegate<ListenerClass,void,false>(listener, listenerMethod, prio);
+    event.remove(listener, listenerMethod, prio);
 }
 //----------------------------------------------------
 // notifies an event so all the registered listeners
