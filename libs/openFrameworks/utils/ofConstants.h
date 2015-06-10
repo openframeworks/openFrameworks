7af488eded14e362d6034f932aee23239e71fd7b
arturo castro <arturo@openframeworks.cc>
2015-06-10 21:20:32 +0200

    ofConstants: avoid errors on HAS_TLS
    the clang feature detection gives errors on other compilers, this
    hides the ifdef from compilers other than clang
    
    Conflicts:
    	libs/openFrameworks/utils/ofConstants.h

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -353,9 +353,18 @@ typedef TESSindex ofIndexType;
   #endif
 #endif
 
-// comment out this line to disable all poco related code
-#define OF_USING_POCO
-
+//------------------------------------------------ thread local storage
+// xcode has a bug where it won't support tls on some versions even
+// on c++11, this is a workaround that bug
+#if __cplusplus>=201103
+	#if !defined(TARGET_OSX) && !defined(TARGET_OF_IOS)
+		#define HAS_TLS 1
+	#elif __clang__
+		#if __has_feature(cxx_thread_local)
+			#define HAS_TLS 1
+		#endif
+	#endif
+#endif
 
 //we don't want to break old code that uses ofSimpleApp
 //so we forward declare ofBaseApp and make ofSimpleApp mean the same thing
