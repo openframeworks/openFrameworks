75a86e7de3f2bdfcbb2b5caa84cddb91ddb748d0
arturo <arturo@openframeworks.cc>
2015-06-12 16:55:32 +0200

    fix cairo. Closes #3911
    Removes unnecesary dependencies includes
    adds libpng to osx
    modifies apothecary scripts to do the right thinkg

--- libs/cairo/include/cairo/cairo-version.h
+++ libs/cairo/include/cairo/cairo-version.h
@@ -1,14 +1,8 @@
-/* This is a dummy file.
- * The actual version info is in toplevel cairo-version.h.
- * The purpose of this file is to make most of the source files NOT depend
- * on the real cairo-version.h, and as a result, changing library version
- * would not cause a complete rebuild of all object files (just a relink).
- * This is useful when bisecting. */
 #ifndef CAIRO_VERSION_H
 #define CAIRO_VERSION_H
 
-#define CAIRO_VERSION_MAJOR USE_cairo_version_OR_cairo_version_string_INSTEAD
-#define CAIRO_VERSION_MINOR USE_cairo_version_OR_cairo_version_string_INSTEAD
-#define CAIRO_VERSION_MICRO USE_cairo_version_OR_cairo_version_string_INSTEAD
+#define CAIRO_VERSION_MAJOR 1
+#define CAIRO_VERSION_MINOR 12
+#define CAIRO_VERSION_MICRO 14
 
 #endif
