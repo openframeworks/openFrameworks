db7f019fdf413a2373c5b187f0448926e89849a2
tpltnt <tpltnt.github@dropcut.net>
2015-06-20 21:49:10 +0200

    suggesting sudo instead of su for elevated privileges

--- scripts/linux/debian/install_dependencies.sh
+++ scripts/linux/debian/install_dependencies.sh
@@ -4,8 +4,7 @@ if [ $EUID != 0 ]; then
 	echo "this script must be run as root"
 	echo ""
 	echo "usage:"
-	echo "su -"
-	echo "./install_dependencies.sh"
+	echo "sudo "$0
 	exit $exit_code
    exit 1
 fi
