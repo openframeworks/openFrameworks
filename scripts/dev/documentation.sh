7fabb9fa9cd2c2bd49a653683157de4bc6399b83
Daniel Rosser <danoli3@gmail.com>
2015-07-03 03:45:52 +1000

    Travis Update

---
+++ scripts/dev/documentation.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## This script is to build the OpenFrameworks documentation automatically
+CURRENT_BRANCH=$(git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3);
+echo "Current Branch: $CURRENT_BRANCH"
+### Only build documentation on the master branch.
+if [ "$CURRENT_BRANCH" == "master" ]; then
+    if [ "$1" == "before_install" ]; then
+        if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then 
+            echo "Documentation Before Install"
+            brew update 
+            brew install libreoffice;
+            brew install doxygen;
+            brew install ncftp;
+            gem install sass --version "=3.2.12";
+            gem install compass --version "=0.12.2";
+        fi
+    elif [ "$1" == "script" ]; then
+        if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+            echo "Documentation Building"
+            cd scripts/; 
+            git clone https://github.com/halfdanJ/ofDocGenerator; 
+            cd ofDocGenerator; 
+            npm install; 
+            npm run start; 
+        fi
+    elif [ "$1" == "after_success" ]; then
+        if [[ $TRAVIS_PULL_REQUEST == "false" && $FTP_USER ]]; then
+            ncftpput -R -v -u $FTP_USER -p $FTP_PASSWORD 104.130.212.175 / output/*;
+        fi
+    fi
+    return 0;
+fi
