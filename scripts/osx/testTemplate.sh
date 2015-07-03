27610bf304ddf9b9180f63c7d5b505bb49113a6d
Daniel Rosser <danoli3@gmail.com>
2015-07-03 21:11:46 +1000

    Travis Updates from feedback

--- scripts/osx/testTemplate.sh
+++ scripts/osx/testTemplate.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-OF_DIR=`pwd`;
-echo $OF_DIR;
-TEST_DIR="$OF_DIR/scripts/osx"
-cd $TEST_DIR
-  echo "Looking for OSX Template Project"
-  if [ ! -d template/bin/emptyExample.app ]; then
-      echo "-----------------------------------------------------------------"
- 	    echo building emptyExample
-      if [ ! -e template/emptyExample.xcodeproj ]; then
-          echo "-----------------------------------------------------------------"
-          echo no xcode project for emptyExample
-      fi
-      echo "Building openFrameworks - OSX Template Project"
-      xcodebuild -configuration Release -target emptyExample -project template/emptyExample.xcodeproj
-      ret=$?
-      if [ $ret -ne 0 ]; then
- 	        echo failed building emptyExample
- 	        exit 1
-      fi
-      echo "-----------------------------------------------------------------"
-  fi
-cd $OF_DIR
+set -e
+if [ "$SCRIPT_PATH" == "" ]; then SCRIPT_PATH="."; fi
+echo "Looking for OSX Template Project"
+if [ ! -d $SCRIPT_PATH/template/bin/emptyExample.app ]; then
+    echo "---------------------------------"
+    if [ ! -e $SCRIPT_PATH/template/emptyExample.xcodeproj ]; then
+        echo "-----------------------------"
+        echo "No Xcode project found for emptyExample"
+    fi
+    echo "Building openFrameworks - OSX Template Project"
+    xcodebuild -configuration Release -target emptyExample -project $SCRIPT_PATH/template/emptyExample.xcodeproj
+    ret=$?
+    if [ $ret -ne 0 ]; then
+          echo "Failed building emptyExample"
+          exit 1
+    fi
+    echo "---------------------------------"
+fi
