9f976701565d82c23e856b5912934fd9e8e17202
Daniel Rosser <danoli3@gmail.com>
2015-07-03 21:11:46 +1000

    Travis Updates from feedback

--- scripts/dev/documentation.sh
+++ scripts/dev/documentation.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
+set -e
 ## This script is to build the OpenFrameworks documentation automatically
-
-
 echo "Current Branch: $TRAVIS_BRANCH"
 ### Only build documentation on the master branch.
 if [ "$TRAVIS_BRANCH" == "master" ]; then
@@ -10,7 +8,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
         if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then 
             echo "Documentation Before Install"
             brew update 
-            brew install libreoffice;
+            brew install Caskroom/cask/libreoffice;
             brew install doxygen;
             brew install ncftp;
             gem install sass --version "=3.2.12";
@@ -26,9 +24,9 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
             npm run start; 
         fi
     elif [ "$1" == "after_success" ]; then
+        echo "Uploading to FTP"
         if [[ $TRAVIS_PULL_REQUEST == "false" && $FTP_USER ]]; then
             ncftpput -R -v -u $FTP_USER -p $FTP_PASSWORD 104.130.212.175 / output/*;
         fi
     fi
-    return 0;
 fi
