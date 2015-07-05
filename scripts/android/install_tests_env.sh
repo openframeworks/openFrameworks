cbb1ce44146303c33e724c8605c1dbdb6888462c
arturo castro <arturo@openframeworks.cc>
2015-07-05 16:07:12 +0200

    android: fixing tests

--- scripts/android/install_tests_env.sh
+++ scripts/android/install_tests_env.sh
@@ -1,4 +1,5 @@
 curl -Lk http://dl.google.com/android/ndk/android-ndk-r10e-darwin-x86_64.bin -o ndk.bin
 chmod a+x ndk.bin
 ./ndk.bin -y | grep -v Extracting
-cat libs/openFrameworksCompiled/project/android/paths.default.make | sed s/path_to/${PWD}/ > libs/openFrameworksCompiled/project/android/paths.make
+NDK_ROOT=$(echo ${PWD} | sed s/\//\\\//)
+cat libs/openFrameworksCompiled/project/android/paths.default.make | sed s/path_to/${NDK_ROOT}/ > libs/openFrameworksCompiled/project/android/paths.make
