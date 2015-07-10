b98f7f3c7d4f305c054fea991be315703a67af59
Constantine Tarasenkov <constantine@protonmail.com>
2015-07-10 14:04:07 +0300

    Fixing header names for case-sensitive MinGW

--- libs/openFrameworks/video/ofDirectShowPlayer.cpp
+++ libs/openFrameworks/video/ofDirectShowPlayer.cpp
@@ -14,15 +14,14 @@
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-#include <DShow.h>
+#include <dshow.h>
 #pragma include_alias( "dxtrans.h", "qedit.h" )
 #define __IDxtCompositor_INTERFACE_DEFINED__
 #define __IDxtAlphaSetter_INTERFACE_DEFINED__
 #define __IDxtJpeg_INTERFACE_DEFINED__
 #define __IDxtKey_INTERFACE_DEFINED__
-#include <uuids.h>
-#include <Aviriff.h>
-#include <Windows.h>
+#include <aviriff.h>
+#include <windows.h>
 
 //for threading
 #include <process.h>
