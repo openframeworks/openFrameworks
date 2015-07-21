d28a4b1af544b8e50e8724afd2bd11868e7b9aaa
Daniel Rosser <danoli3@gmail.com>
2015-07-21 17:06:24 +1000

    Fix for OpenGLES for ofGLRenderer and ofGLProgrammableRenderer - Fix for type issues with ```glGetIntegerv(GL_FRAMEBUFFER_BINDING``` - Fix bindForBlitting - defined out for OpenGLES. I checked the commands, they do exist for OpenGLES 3.0 however throws an error of undefined for anything else.

--- libs/openFrameworks/gl/ofGLProgrammableRenderer.cpp
+++ libs/openFrameworks/gl/ofGLProgrammableRenderer.cpp
@@ -1350,12 +1350,15 @@ void ofGLProgrammableRenderer::bindForBlitting(const ofFbo & fboSrc, ofFbo & fbo
 	// I'm keeping it here, so that if we want to do more fancyful
 	// named framebuffers with GL 4.5+, we can have
 	// different implementations.
+#ifndef TARGET_OPENGLES
 	framebufferIdStack.push_back(currentFramebufferId);
 	currentFramebufferId = fboSrc.getId();
 	glBindFramebuffer(GL_READ_FRAMEBUFFER, currentFramebufferId);
 	glReadBuffer(GL_COLOR_ATTACHMENT0 + attachmentPoint);
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fboDst.getIdDrawBuffer());
 	glDrawBuffer(GL_COLOR_ATTACHMENT0 + attachmentPoint);
+#endif
+
 }
 
 //----------------------------------------------------------
