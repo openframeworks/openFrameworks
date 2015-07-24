92a6132f6b803662eb3698261ec907c165622676
Christoph Buchner <bilderbuchi@users.noreply.github.com>
2015-07-24 11:15:58 +0200

    Optimize linux compilation, build separately
    0 worked, but made the build much slower (9->14min).
    Let's see if 1 is sufficient to make the builds still pass

--- scripts/ci/android/install.sh
+++ scripts/ci/android/install.sh
@@ -8,6 +8,6 @@ curl -Lk http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin -o n
 chmod a+x ndk.bin
 # ./ndk.bin -y | grep -v Extracting
 # extract with 7zip, -mmt=1 makes it use only 1 thread to reduce memory consumption
-7zr x -mmt=1 ndk.bin $(sed 's/^/ -xr!/g' $ROOT/scripts/ci/android/NDK_excludes.txt) | grep -v -e "Extracting" -e "Skipping"
+7z x -mmt=1 ndk.bin $(sed 's/^/ -xr!/g' $ROOT/scripts/ci/android/NDK_excludes.txt) | grep -v -e "Extracting" -e "Skipping"
 NDK_ROOT=$(echo ${PWD} | sed "s/\//\\\\\//g")
 cat $ROOT/libs/openFrameworksCompiled/project/android/paths.default.make | sed s/path_to/${NDK_ROOT}/ > $ROOT/libs/openFrameworksCompiled/project/android/paths.make
