73ef279ca57cb0f8532a7fe6d20b79301f89993b
Christoph Buchner <bilderbuchi@users.noreply.github.com>
2015-07-24 11:04:11 +0200

    Switch Android builds to docker infrastructure

--- scripts/ci/android/install.sh
+++ scripts/ci/android/install.sh
@@ -3,7 +3,7 @@ set -ev
 # capture failing exits in commands obscured behind a pipe
 set -o pipefail
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
-sudo apt-get install -y p7zip
+# sudo apt-get install -y p7zip
 curl -Lk http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin -o ndk.bin
 chmod a+x ndk.bin
 # ./ndk.bin -y | grep -v Extracting
