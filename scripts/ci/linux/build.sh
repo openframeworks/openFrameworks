569130340c3a7181b613fb29a35094f96ca6bc1f
Christoph Buchner <bilderbuchi@users.noreply.github.com>
2015-07-24 11:15:58 +0200

    Optimize linux compilation, build separately
    0 worked, but made the build much slower (9->14min).
    Let's see if 1 is sufficient to make the builds still pass

--- scripts/ci/linux/build.sh
+++ scripts/ci/linux/build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -ev
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
-cd $ROOT
-cp scripts/linux/template/linux/Makefile examples/empty/emptyExample/
-cp scripts/linux/template/linux/config.make examples/empty/emptyExample/
-cd examples/empty/emptyExample
 # Add compiler flag to reduce memory usage to enable builds to complete
 # see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56746#c7
 # the "proper" way does not work currently:
 #export CXXFLAGS="$(CXXFLAGS) --param ftrack-macro-expansion=0"
-echo "PROJECT_CFLAGS = -ftrack-macro-expansion=0" >> config.make
+CUSTOMFLAGS="-ftrack-macro-expansion=1"
+
+echo "Building OF core"
+cd $ROOT
+echo "PLATFORM_CFLAGS += $CUSTOMFLAGS" >> libs/openFrameworksCompiled/project/linux64/config.linux64.default.mk
+cd libs/openFrameworksCompiled/project
+make Debug
+
+echo "Building emptyExample"
+cd $ROOT
+cp scripts/linux/template/linux64/Makefile examples/empty/emptyExample/
+cp scripts/linux/template/linux64/config.make examples/empty/emptyExample/
+cd examples/empty/emptyExample
+# this is not even necessary if we include that in the default.mk above
+# echo "PROJECT_CFLAGS = $CUSTOMFLAGS" >> config.make
 make Debug
