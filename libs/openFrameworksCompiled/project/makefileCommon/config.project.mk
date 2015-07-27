7992e8507168d65dc3fbf217bdb0763fab5fdeef
arturo castro <arturo@openframeworks.cc>
2015-07-27 15:40:49 +0200

    makefiles: use absolute path on output to make parsing more accurate

--- libs/openFrameworksCompiled/project/makefileCommon/config.project.mk
+++ libs/openFrameworksCompiled/project/makefileCommon/config.project.mk
@@ -3,11 +3,11 @@
 # lists of source files, search paths, libraries, etc.
 #
 ifndef OF_ROOT
-    OF_ROOT= ../../..
+    OF_ROOT= $(realpath ../../..)
 endif
 
 ifndef PROJECT_ROOT
-    PROJECT_ROOT= .
+    PROJECT_ROOT= $(realpath .)
 endif
 
 
