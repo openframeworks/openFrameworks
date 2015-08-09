0f98d3920e440d20795e1ab04448cd29fe22eef7
Ingo Randolf <ingo.randolf@servus.at>
2015-08-09 16:15:08 +0200

    add ofFmodSetBuffersize to set the size prior to initialization

--- libs/openFrameworks/sound/ofFmodSoundPlayer.h
+++ libs/openFrameworks/sound/ofFmodSoundPlayer.h
@@ -29,6 +29,7 @@ void ofFmodSoundStopAll();
 void ofFmodSoundSetVolume(float vol);
 void ofFmodSoundUpdate();						// calls FMOD update.
 float * ofFmodSoundGetSpectrum(int nBands);		// max 512...
+void ofFmodSetBuffersize(unsigned int bs);
 
 
 // --------------------- player functions:
