44f08b929dc057539514bf9588ec4e3236735338
arturo castro <arturo@openframeworks.cc>
2015-08-10 12:21:03 +0200

    create_package: get PG from local folder

--- scripts/dev/create_package.sh
+++ scripts/dev/create_package.sh
@@ -100,30 +100,30 @@ function deleteEclipse {
 
 
 function createProjectFiles {
-if [ "$pkg_platform" == "win_cb" ]; then
+    if [ "$pkg_platform" == "win_cb" ]; then
 
-	# copy all examples to pkg_ofroot
-	cp -Rf $packageroot/examples $pkg_ofroot
+	    # copy all examples to pkg_ofroot
+	    cp -Rf $packageroot/examples $pkg_ofroot
 	
-	# copy config.make and Makefile into every subfolder
-	for d in `find ${pkg_ofroot}/examples -maxdepth 2 -type d`; do
-	  cp $packageroot/scripts/win_cb/template/config.make ${d}
-	  cp $packageroot/scripts/win_cb/template/Makefile ${d}
-	done
+	    # copy config.make and Makefile into every subfolder
+	    for d in `find ${pkg_ofroot}/examples -maxdepth 2 -type d`; do
+	      cp $packageroot/scripts/win_cb/template/config.make ${d}
+	      cp $packageroot/scripts/win_cb/template/Makefile ${d}
+	    done
 	
-	# remove config.make and Makefile from level 1
-  for d in `find ${pkg_ofroot}/examples -maxdepth 1 -type d`; do
-	  rm "${d}/config.make"
-	  rm "${d}/Makefile"
-	done
+	    # remove config.make and Makefile from level 1
+      for d in `find ${pkg_ofroot}/examples -maxdepth 1 -type d`; do
+	      rm "${d}/config.make"
+	      rm "${d}/Makefile"
+	    done
 	
-	# remove config.make and Makefile from level 0
-	rm "$pkg_ofroot/examples/config.make"
-	rm "$pkg_ofroot/examples/Makefile"
-  
-else
-  projectGenerator --allexamples --${pkg_platform}
-fi
+	    # remove config.make and Makefile from level 0
+	    rm "$pkg_ofroot/examples/config.make"
+	    rm "$pkg_ofroot/examples/Makefile"
+      
+    else
+      projectGenerator --allexamples --${pkg_platform}
+    fi
 }
 
 function createPackage {
@@ -290,10 +290,7 @@ function createPackage {
 		rm -Rf __MACOSX
 	fi
     if [ "$pkg_platform" = "osx" ]; then
-		rm projectGenerator_osx.zip
-		wget http://www.openframeworks.cc/pgSimple/projectGenerator_osx.zip
-		unzip projectGenerator_osx.zip
-		rm projectGenerator_osx.zip
+		cp -r /home/tests/projectGeneratorSimple_osx projectGenerator
 		rm -Rf __MACOSX
 	fi
     if [ "$pkg_platform" = "ios" ]; then
