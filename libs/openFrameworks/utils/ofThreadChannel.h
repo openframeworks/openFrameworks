0d49950769f38db96fba261b7b7d9d4ce25d355a
Christopher Baker <me@christopherbaker.net>
2015-08-10 23:06:33 -0500

    Use tabs.  Some of us still waste bytes on spaces.

--- libs/openFrameworks/utils/ofThreadChannel.h
+++ libs/openFrameworks/utils/ofThreadChannel.h
@@ -56,40 +56,40 @@
 template<typename T>
 class ofThreadChannel{
 public:
-    /// \brief Create a default ofThreadChannel.
-    ///
-    /// ofThreadChannel must be instantiated with a template parameter such as:
-    /// ~~~~{.cpp}
-    /// ofThreadChannel<ofPixels> myThreadChannel;
-    /// ~~~~
+	/// \brief Create a default ofThreadChannel.
+	///
+	/// ofThreadChannel must be instantiated with a template parameter such as:
+	/// ~~~~{.cpp}
+	/// ofThreadChannel<ofPixels> myThreadChannel;
+	/// ~~~~
 	ofThreadChannel()
 	:closed(false){}
 
-    /// \brief Block the receiving thread until a new sent value is available.
-    ///
-    /// The receiving thread will pause (or "block") until a new sent value
-    /// is available. In order to receive data, the user must create an instance
-    /// of the data type, and pass it to the receive method to be filled.
-    ///
-    /// ofThreadChannel::receive will not make a copy or reallocate data.
-    ///
-    /// ~~~~{.cpp}
-    /// ofThreadChannel<ofPixels> myThreadChannel;
-    ///
-    /// // ofThreadChannel<ofPixels> initialized elsewhere.
-    ///
-    /// ofPixels myPixelsToSet;
-    ///
-    /// if (myThreadChannel.receive(myPixelsToSet)) {
-    ///    // If true, `myPixelsToSet` can be used.
-    /// } else {
-    ///    // If false, thread channel was closed and the value of
-    ///    // `myPixelsToSet` may be invalid depending on the scope of
-    ///    // `myPixelsToSet`.
-    /// }
-    /// ~~~~
-    ///
-    /// \param sentValue A reference to a sent value.
+	/// \brief Block the receiving thread until a new sent value is available.
+	///
+	/// The receiving thread will pause (or "block") until a new sent value
+	/// is available. In order to receive data, the user must create an instance
+	/// of the data type, and pass it to the receive method to be filled.
+	///
+	/// ofThreadChannel::receive will not make a copy or reallocate data.
+	///
+	/// ~~~~{.cpp}
+	/// ofThreadChannel<ofPixels> myThreadChannel;
+	///
+	/// // ofThreadChannel<ofPixels> initialized elsewhere.
+	///
+	/// ofPixels myPixelsToSet;
+	///
+	/// if (myThreadChannel.receive(myPixelsToSet)) {
+	///	// If true, `myPixelsToSet` can be used.
+	/// } else {
+	///	// If false, thread channel was closed and the value of
+	///	// `myPixelsToSet` may be invalid depending on the scope of
+	///	// `myPixelsToSet`.
+	/// }
+	/// ~~~~
+	///
+	/// \param sentValue A reference to a sent value.
 	/// \returns True if a new value was received or false if the ofThreadChannel was closed.
 	bool receive(T & sentValue){
 		std::unique_lock<std::mutex> lock(mutex);
@@ -108,37 +108,37 @@ public:
 		}
 	}
 
-    /// \brief If available, receive a new sent value without blocking.
-    ///
-    /// ofThreadChannel::tryReceive is similar to ofThreadChannel::receive,
-    /// except that it will not pause (or "block") the receiving thread as it
-    /// waits for sent data.  If no data is available, it will return false and
-    /// continue immediately.
-    ///
-    /// ofThreadChannel::tryReceive will not make a copy or reallocate data.
-    ///
-    /// Like ofThreadChannel::receive, in order to receive data, the user must
-    /// create an instance of the data type, and pass it to the receive method
-    /// to be filled.
-    ///
-    /// ~~~~{.cpp}
-    /// ofThreadChannel<ofPixels> myThreadChannel;
-    ///
-    /// // ofThreadChannel<ofPixels> initialized elsewhere.
-    ///
-    /// ofPixels myPixelsToSet;
-    ///
-    /// if (myThreadChannel.tryReceive(myPixelsToSet)) {
-    ///    // If true, `myPixelsToSet` can be used.
-    /// } else {
-    ///    // If false, there was no new data OR the thread channel was closed.
-    ///    // Either way, the value of `myPixelsToSet` may be invalid depending
-    ///    // on the scope of `myPixelsToSet`.
-    /// }
-    /// ~~~~
-    ///
-    /// \param sentValue A reference to a sent value.
-    /// \returns True if a new value was received or false if the ofThreadChannel was closed.
+	/// \brief If available, receive a new sent value without blocking.
+	///
+	/// ofThreadChannel::tryReceive is similar to ofThreadChannel::receive,
+	/// except that it will not pause (or "block") the receiving thread as it
+	/// waits for sent data.  If no data is available, it will return false and
+	/// continue immediately.
+	///
+	/// ofThreadChannel::tryReceive will not make a copy or reallocate data.
+	///
+	/// Like ofThreadChannel::receive, in order to receive data, the user must
+	/// create an instance of the data type, and pass it to the receive method
+	/// to be filled.
+	///
+	/// ~~~~{.cpp}
+	/// ofThreadChannel<ofPixels> myThreadChannel;
+	///
+	/// // ofThreadChannel<ofPixels> initialized elsewhere.
+	///
+	/// ofPixels myPixelsToSet;
+	///
+	/// if (myThreadChannel.tryReceive(myPixelsToSet)) {
+	///	// If true, `myPixelsToSet` can be used.
+	/// } else {
+	///	// If false, there was no new data OR the thread channel was closed.
+	///	// Either way, the value of `myPixelsToSet` may be invalid depending
+	///	// on the scope of `myPixelsToSet`.
+	/// }
+	/// ~~~~
+	///
+	/// \param sentValue A reference to a sent value.
+	/// \returns True if a new value was received or false if the ofThreadChannel was closed.
 	bool tryReceive(T & sentValue){
 		std::unique_lock<std::mutex> lock(mutex);
 		if(closed){
@@ -153,38 +153,38 @@ public:
 		}
 	}
 
-    /// \brief If available, receive a new sent value or wait for a user-specified duration.
-    ///
-    /// ofThreadChannel::tryReceive is similar to ofThreadChannel::receive,
-    /// except that it will pause (or "block") the receiving thread for a
-    /// maximum of `timeoutMs` while it waits for sent data.  If no data is
-    /// available during that time, it will return false and continue.
-    ///
-    /// ofThreadChannel::tryReceive will not make a copy or reallocate data.
-    ///
-    /// Like ofThreadChannel::receive, in order to receive data, the user must
-    /// create an instance of the data type, and pass it to the receive method
-    /// to be filled.
-    ///
-    /// ~~~~{.cpp}
-    /// ofThreadChannel<ofPixels> myThreadChannel;
-    ///
-    /// // ofThreadChannel<ofPixels> initialized elsewhere.
-    ///
-    /// ofPixels myPixelsToSet;
-    ///
-    /// if (myThreadChannel.tryReceive(myPixelsToSet)) {
-    ///    // If true, `myPixelsToSet` can be used.
-    /// } else {
-    ///    // If false, there was no new data OR the thread channel was closed.
-    ///    // Either way, the value of `myPixelsToSet` may be invalid depending
-    ///    // on the scope of `myPixelsToSet`.
-    /// }
-    /// ~~~~
-    ///
-    /// \param sentValue A reference to a sent value.
-    /// \param timeoutMs The number of milliseconds to wait for new data before continuing.
-    /// \returns True if a new value was received or false if the ofThreadChannel was closed.
+	/// \brief If available, receive a new sent value or wait for a user-specified duration.
+	///
+	/// ofThreadChannel::tryReceive is similar to ofThreadChannel::receive,
+	/// except that it will pause (or "block") the receiving thread for a
+	/// maximum of `timeoutMs` while it waits for sent data.  If no data is
+	/// available during that time, it will return false and continue.
+	///
+	/// ofThreadChannel::tryReceive will not make a copy or reallocate data.
+	///
+	/// Like ofThreadChannel::receive, in order to receive data, the user must
+	/// create an instance of the data type, and pass it to the receive method
+	/// to be filled.
+	///
+	/// ~~~~{.cpp}
+	/// ofThreadChannel<ofPixels> myThreadChannel;
+	///
+	/// // ofThreadChannel<ofPixels> initialized elsewhere.
+	///
+	/// ofPixels myPixelsToSet;
+	///
+	/// if (myThreadChannel.tryReceive(myPixelsToSet)) {
+	///	// If true, `myPixelsToSet` can be used.
+	/// } else {
+	///	// If false, there was no new data OR the thread channel was closed.
+	///	// Either way, the value of `myPixelsToSet` may be invalid depending
+	///	// on the scope of `myPixelsToSet`.
+	/// }
+	/// ~~~~
+	///
+	/// \param sentValue A reference to a sent value.
+	/// \param timeoutMs The number of milliseconds to wait for new data before continuing.
+	/// \returns True if a new value was received or false if the ofThreadChannel was closed.
 	bool tryReceive(T & sentValue, int64_t timeoutMs){
 		std::unique_lock<std::mutex> lock(mutex);
 		if(closed){
@@ -205,32 +205,32 @@ public:
 		}
 	}
 
-    /// \brief Send a value to the receiver by making a copy.
-    ///
-    /// This method copies the contents of the sent value, leaving the original
-    /// data unchanged.
-    ///
-    /// ~~~~{.cpp}
-    /// ofThreadChannel<ofPixels> myThreadChannel;
-    ///
-    /// // ofThreadChannel<ofPixels> initialized elsewhere.
-    ///
-    /// ofPixels myPixelsToSend;
-    ///
-    /// // Fill the pixels with valid data, an image for example.
-    ///
-    /// ofLoadImage(myPixelsToSend, "myImage.jpg");
-    ///
-    /// // Send `myPixelsToSend` by copying it. `myPixelsToSend` is still valid
-    /// // after sending.
-    /// if (myThreadChannel.send(myPixelsToSend)) {
-    ///    // If true, `myPixelsToSend` was sent successfully.
-    /// } else {
-    ///    // If false, the thread channel was closed.
-    /// }
-    /// ~~~~
-    ///
-    /// \returns true if the value was sent successfully or false if the channel was closed.
+	/// \brief Send a value to the receiver by making a copy.
+	///
+	/// This method copies the contents of the sent value, leaving the original
+	/// data unchanged.
+	///
+	/// ~~~~{.cpp}
+	/// ofThreadChannel<ofPixels> myThreadChannel;
+	///
+	/// // ofThreadChannel<ofPixels> initialized elsewhere.
+	///
+	/// ofPixels myPixelsToSend;
+	///
+	/// // Fill the pixels with valid data, an image for example.
+	///
+	/// ofLoadImage(myPixelsToSend, "myImage.jpg");
+	///
+	/// // Send `myPixelsToSend` by copying it. `myPixelsToSend` is still valid
+	/// // after sending.
+	/// if (myThreadChannel.send(myPixelsToSend)) {
+	///	// If true, `myPixelsToSend` was sent successfully.
+	/// } else {
+	///	// If false, the thread channel was closed.
+	/// }
+	/// ~~~~
+	///
+	/// \returns true if the value was sent successfully or false if the channel was closed.
 	bool send(const T & value){
 		std::unique_lock<std::mutex> lock(mutex);
 		if(closed){
@@ -241,37 +241,37 @@ public:
 		return true;
 	}
 
-    /// \brief Send a value to the receiver without making a copy.
-    ///
-    /// This method moves the contents of the sent value using modern `std::move`
-    /// semantics. This avoids copying the data, but the original data data will
-    /// be invalidated. Note that the original data will be invalideated even if
-    /// the send fails because the channel is already closed.
-    ///
-    /// ~~~~{.cpp}
-    /// ofThreadChannel<ofPixels> myThreadChannel;
-    ///
-    /// // ofThreadChannel<ofPixels> initialized elsewhere.
-    ///
-    /// ofPixels myPixelsToSend;
-    ///
-    /// // Fill the pixels with valid data, an image for example.
-    ///
-    /// ofLoadImage(myPixelsToSend, "myImage.jpg");
-    ///
-    /// // Send `myPixelsToSend` by moving it. `myPixelsToSend` will no longer
-    /// // be valid, even if the send fails because the channel is closed.
-    /// if (myThreadChannel.send(std::move(myPixelsToSend))) {
-    ///    // If true, `myPixelsToSend` was sent successfully.
-    ///    // `myPixelsToSend` is no longer valid because it was moved.
-    /// } else {
-    ///    // If false, the thread channel was closed.
-    ///    // `myPixelsToSend` is no longer valid because it was moved.
-    /// }
-    ///
-    /// ~~~~
-    ///
-    /// \returns true if the value was sent successfully or false if the channel was closed.
+	/// \brief Send a value to the receiver without making a copy.
+	///
+	/// This method moves the contents of the sent value using modern `std::move`
+	/// semantics. This avoids copying the data, but the original data data will
+	/// be invalidated. Note that the original data will be invalideated even if
+	/// the send fails because the channel is already closed.
+	///
+	/// ~~~~{.cpp}
+	/// ofThreadChannel<ofPixels> myThreadChannel;
+	///
+	/// // ofThreadChannel<ofPixels> initialized elsewhere.
+	///
+	/// ofPixels myPixelsToSend;
+	///
+	/// // Fill the pixels with valid data, an image for example.
+	///
+	/// ofLoadImage(myPixelsToSend, "myImage.jpg");
+	///
+	/// // Send `myPixelsToSend` by moving it. `myPixelsToSend` will no longer
+	/// // be valid, even if the send fails because the channel is closed.
+	/// if (myThreadChannel.send(std::move(myPixelsToSend))) {
+	///	// If true, `myPixelsToSend` was sent successfully.
+	///	// `myPixelsToSend` is no longer valid because it was moved.
+	/// } else {
+	///	// If false, the thread channel was closed.
+	///	// `myPixelsToSend` is no longer valid because it was moved.
+	/// }
+	///
+	/// ~~~~
+	///
+	/// \returns true if the value was sent successfully or false if the channel was closed.
 	bool send(T && value){
 		std::unique_lock<std::mutex> lock(mutex);
 		if(closed){
@@ -282,29 +282,29 @@ public:
 		return true;
 	}
 
-    /// \brief Close the ofThreadChannel.
-    ///
-    /// Closing the ofThreadChannel means that no new messages can be sent or
-    /// received. All threads waiting to receive new values will be notified and
-    /// all ofThreadChannel::receive and ofThreadChannel::tryReceive methods
-    /// will return false.
-    void close(){
+	/// \brief Close the ofThreadChannel.
+	///
+	/// Closing the ofThreadChannel means that no new messages can be sent or
+	/// received. All threads waiting to receive new values will be notified and
+	/// all ofThreadChannel::receive and ofThreadChannel::tryReceive methods
+	/// will return false.
+	void close(){
 		std::unique_lock<std::mutex> lock(mutex);
 		closed = true;
 		condition.notify_all();
 	}
 
 private:
-    /// \brief The FIFO data queue.
-    std::queue<T> queue;
+	/// \brief The FIFO data queue.
+	std::queue<T> queue;
 
-    /// \brief The mutext to protect the data.
+	/// \brief The mutext to protect the data.
 	std::mutex mutex;
 
-    /// \brief The condition even to notify receivers.
+	/// \brief The condition even to notify receivers.
 	std::condition_variable condition;
 
-    /// \brief True if the channel is closed.
+	/// \brief True if the channel is closed.
 	bool closed;
 
 };
