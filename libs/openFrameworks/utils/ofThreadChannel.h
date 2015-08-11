e934b0caa27011de0e182e4d24f336f5361a17ab
Christopher Baker <me@christopherbaker.net>
2015-08-11 09:44:34 -0500

    Slimmed documentation.

--- libs/openFrameworks/utils/ofThreadChannel.h
+++ libs/openFrameworks/utils/ofThreadChannel.h
@@ -9,16 +9,14 @@
 
 /// \brief Safely send data between threads without additional synchronization.
 ///
-/// ofThreadChannel is a powerful class that makes it easy to safely pass data
-/// between threads. When data is sent form one thread to another via the
-/// ofThreadChannel, the user does not need to use additonal mutexes, condition
-/// variables, critical sections, semaphores, monitors or other synchronization
-/// primitives or techniques. Data sent with ofThreadChannel is synchronized
-/// by ofThreadChannel's internal `std::mutex` and notifies receiving threads when
-/// data is available from the sending thread.
+/// ofThreadChannel makes it easy to safely and efficiently share data between
+/// threads without the need for shared memory, mutexes, or other
+/// synchronization techniques. Additionally, ofThreadChannel employs a
+/// signalling system that allows receiving threads to sleep until new data
+/// arrives or the ofThreadChannel is closed.
 ///
-/// A single ofThreadChannel class implements uni-directional communication. In
-/// most cases an additional ofThreadChannel can be used for bi-directional
+/// A single ofThreadChannel class is desgined for one-way communication. In
+/// most cases an additional ofThreadChannel can be used for two-way
 /// communication.
 ///
 /// Data is sent through the ofThreadChannel in a FIFO (first in, first out)
@@ -26,32 +24,10 @@
 /// it was sent.
 ///
 /// If multiple threads attempt to send data using the same ofThreadChannel, the
-/// send method will block the calling thread until it is free. ofThreadChannel
-/// does not use recursive mutexes and is therefore not designed to send and
-/// receive data from the same thread. Doing will result in deadlocks.
+/// send method will block the calling thread until it is free. Sending and
+/// receiving data from the same thread may result in deadlocks.
 ///
-/// ofThreadChannel is a templated class, meaning that the value type must be
-/// set as a template argument during declaration.
-///
-/// ofThreadChannel is ideal for sharing most complex data structures between
-/// threads. For example, if you want to share `ofPixels` between threads, one
-/// would declare `ofThreadChannel<ofPixels> myChannel`. When used
-/// with traditional C++ syntax, the data type must be copyable and one copy of
-/// the data will be made when it is sent. This copy (and any associated
-/// performance penalties) can be avoided using modern C++ `std::move`
-/// semantics, but remember that source data will be invalidated.
-///
-/// While ofThreadChannel will certainly work for "plain old datatypes" (e.g.
-/// `int`, `char`, `float`, etc), users are encouraged to use the new
-/// `std::atomic` data types available with C++11 and later. These "lock-free"
-/// data types require no special data protection for shared use between threads
-/// and are guaranteed to be "thread-safe".
-///
-/// \sa http://en.cppreference.com/w/cpp/thread/mutex
-/// \sa http://en.cppreference.com/w/cpp/utility/move
-/// \sa http://en.cppreference.com/w/cpp/atomic/atomic
 /// \sa https://github.com/openframeworks/ofBook/blob/master/chapters/threads/chapter.md
-/// \sa https://en.wikipedia.org/wiki/Thread_(computing)
 /// \tparam T The data type sent by the ofThreadChannel.
 template<typename T>
 class ofThreadChannel{
@@ -60,32 +36,32 @@ public:
 	///
 	/// ofThreadChannel must be instantiated with a template parameter such as:
 	/// ~~~~{.cpp}
-	/// ofThreadChannel<ofPixels> myThreadChannel;
+	/// 	ofThreadChannel<ofPixels> myThreadChannel;
 	/// ~~~~
 	ofThreadChannel()
 	:closed(false){}
 
 	/// \brief Block the receiving thread until a new sent value is available.
 	///
-	/// The receiving thread will pause (or "block") until a new sent value
-	/// is available. In order to receive data, the user must create an instance
-	/// of the data type, and pass it to the receive method to be filled.
+	/// The receiving thread will block until a new sent value is available. In
+    /// order to receive data, the user must create an instance of the data
+    /// type, and pass it to the receive method to be set.
 	///
 	/// ofThreadChannel::receive will not make a copy or reallocate data.
 	///
 	/// ~~~~{.cpp}
 	/// ofThreadChannel<ofPixels> myThreadChannel;
 	///
-	/// // ofThreadChannel<ofPixels> initialized elsewhere.
+	/// // ofThreadChannel<ofPixels> declared elsewhere.
 	///
 	/// ofPixels myPixelsToSet;
 	///
 	/// if (myThreadChannel.receive(myPixelsToSet)) {
-	///	// If true, `myPixelsToSet` can be used.
+	///		// If true, `myPixelsToSet` can be used.
 	/// } else {
-	///	// If false, thread channel was closed and the value of
-	///	// `myPixelsToSet` may be invalid depending on the scope of
-	///	// `myPixelsToSet`.
+	///		// If false, thread channel was closed and the value of
+	///		// `myPixelsToSet` may be invalid depending on the scope of
+	///		// `myPixelsToSet`.
 	/// }
 	/// ~~~~
 	///
@@ -111,15 +87,15 @@ public:
 	/// \brief If available, receive a new sent value without blocking.
 	///
 	/// ofThreadChannel::tryReceive is similar to ofThreadChannel::receive,
-	/// except that it will not pause (or "block") the receiving thread as it
-	/// waits for sent data.  If no data is available, it will return false and
-	/// continue immediately.
+	/// except that it will block the receiving thread as it waits for sent
+    /// data.  If no data is available, it will return false and continue
+    /// immediately.
 	///
 	/// ofThreadChannel::tryReceive will not make a copy or reallocate data.
 	///
 	/// Like ofThreadChannel::receive, in order to receive data, the user must
 	/// create an instance of the data type, and pass it to the receive method
-	/// to be filled.
+	/// to be set.
 	///
 	/// ~~~~{.cpp}
 	/// ofThreadChannel<ofPixels> myThreadChannel;
@@ -129,11 +105,11 @@ public:
 	/// ofPixels myPixelsToSet;
 	///
 	/// if (myThreadChannel.tryReceive(myPixelsToSet)) {
-	///	// If true, `myPixelsToSet` can be used.
+	///		// If true, `myPixelsToSet` can be used.
 	/// } else {
-	///	// If false, there was no new data OR the thread channel was closed.
-	///	// Either way, the value of `myPixelsToSet` may be invalid depending
-	///	// on the scope of `myPixelsToSet`.
+	///		// If false, there was no new data OR the thread channel was closed.
+	///		// Either way, the value of `myPixelsToSet` may be invalid depending
+	///		// on the scope of `myPixelsToSet`.
 	/// }
 	/// ~~~~
 	///
@@ -156,15 +132,15 @@ public:
 	/// \brief If available, receive a new sent value or wait for a user-specified duration.
 	///
 	/// ofThreadChannel::tryReceive is similar to ofThreadChannel::receive,
-	/// except that it will pause (or "block") the receiving thread for a
-	/// maximum of `timeoutMs` while it waits for sent data.  If no data is
-	/// available during that time, it will return false and continue.
+	/// except that it will block the receiving thread for a maximum of
+    /// `timeoutMs` while it waits for sent data.  If no data is available
+    /// during that time, it will return false and continue.
 	///
 	/// ofThreadChannel::tryReceive will not make a copy or reallocate data.
 	///
 	/// Like ofThreadChannel::receive, in order to receive data, the user must
 	/// create an instance of the data type, and pass it to the receive method
-	/// to be filled.
+	/// to be set.
 	///
 	/// ~~~~{.cpp}
 	/// ofThreadChannel<ofPixels> myThreadChannel;
@@ -174,11 +150,11 @@ public:
 	/// ofPixels myPixelsToSet;
 	///
 	/// if (myThreadChannel.tryReceive(myPixelsToSet)) {
-	///	// If true, `myPixelsToSet` can be used.
+	///		// If true, `myPixelsToSet` can be used.
 	/// } else {
-	///	// If false, there was no new data OR the thread channel was closed.
-	///	// Either way, the value of `myPixelsToSet` may be invalid depending
-	///	// on the scope of `myPixelsToSet`.
+	///		// If false, there was no new data OR the thread channel was closed.
+	///		// Either way, the value of `myPixelsToSet` may be invalid depending
+	///		// on the scope of `myPixelsToSet`.
 	/// }
 	/// ~~~~
 	///
@@ -224,9 +200,9 @@ public:
 	/// // Send `myPixelsToSend` by copying it. `myPixelsToSend` is still valid
 	/// // after sending.
 	/// if (myThreadChannel.send(myPixelsToSend)) {
-	///	// If true, `myPixelsToSend` was sent successfully.
+	///		// If true, `myPixelsToSend` was sent successfully.
 	/// } else {
-	///	// If false, the thread channel was closed.
+	///		// If false, the thread channel was closed.
 	/// }
 	/// ~~~~
 	///
@@ -243,7 +219,7 @@ public:
 
 	/// \brief Send a value to the receiver without making a copy.
 	///
-	/// This method moves the contents of the sent value using modern `std::move`
+	/// This method moves the contents of the sent value using `std::move`
 	/// semantics. This avoids copying the data, but the original data data will
 	/// be invalidated. Note that the original data will be invalideated even if
 	/// the send fails because the channel is already closed.
@@ -262,11 +238,11 @@ public:
 	/// // Send `myPixelsToSend` by moving it. `myPixelsToSend` will no longer
 	/// // be valid, even if the send fails because the channel is closed.
 	/// if (myThreadChannel.send(std::move(myPixelsToSend))) {
-	///	// If true, `myPixelsToSend` was sent successfully.
-	///	// `myPixelsToSend` is no longer valid because it was moved.
+	///		// If true, `myPixelsToSend` was sent successfully.
+	///		// `myPixelsToSend` is no longer valid because it was moved.
 	/// } else {
-	///	// If false, the thread channel was closed.
-	///	// `myPixelsToSend` is no longer valid because it was moved.
+	///		// If false, the thread channel was closed.
+	///		// `myPixelsToSend` is no longer valid because it was moved.
 	/// }
 	///
 	/// ~~~~
