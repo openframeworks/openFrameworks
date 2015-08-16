2d8bdc288c8f7a94904b6f373ee595847defceac
arturoc <arturo@openframeworks.cc>
2015-08-17 00:26:33 +0200

    ofxProjectGenerator: LibraryBinary represents a library file

---
+++ addons/ofxProjectGenerator/src/utils/LibraryBinary.cpp
@@ -0,0 +1,4 @@
+#include "LibraryBinary.h"
+
+const std::vector<std::string> LibraryBinary::archs{ "x86","Win32","x64","armv7" };
+const std::vector<std::string> LibraryBinary::targets{ "Debug","Release" };
