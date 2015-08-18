c7a44f9b45722b88140626decbdb28f4829edf14
Christoph Buchner <bilderbuchi@phononoia.at>
2015-08-18 12:19:17 +0200

    Separate building and test running steps.

--- scripts/ci/osx/build.sh
+++ scripts/ci/osx/build.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 set -ev
-echo "Building openFrameworks - OSX Template Project"
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
+echo "**** Building oF + emptyExample - OSX Template Project ****"
 xcodebuild -configuration Release -target emptyExample -project "$ROOT/scripts/osx/template/emptyExample.xcodeproj"
-
-echo "Unit tests"
-cd $ROOT/tests
-for group in *; do
-	if [ -d $group ]; then
-		for test in $group/*; do
-			if [ -d $test ]; then
-				cd $test
-				cp ../../../scripts/osx/template/Makefile .
-				cp ../../../scripts/osx/template/config.make . 
-				make Debug
-				make RunDebug 
-			fi
-		done
-	fi
-done
