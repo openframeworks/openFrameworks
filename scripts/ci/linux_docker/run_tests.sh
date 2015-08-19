4152c3fc13449a93a8238dd6f2c102cb1a5a06df
Christoph Buchner <bilderbuchi@phononoia.at>
2015-08-19 16:31:48 +0200

    Revert "Fix linux_docker script invocation"
    This reverts commit a7cba47dcd1d89724d3871f5e5ca8841cf4a8340.

--- scripts/ci/linux_docker/run_tests.sh
+++ scripts/ci/linux_docker/run_tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -ev
-ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 # run linux script so DRY is followed
-$ROOT/scripts/ci/linux/run_tests.sh
+../linux/run_tests.sh
