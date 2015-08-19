a7cba47dcd1d89724d3871f5e5ca8841cf4a8340
Christoph Buchner <bilderbuchi@phononoia.at>
2015-08-19 15:13:21 +0200

    Fix linux_docker script invocation

--- scripts/ci/linux_docker/run_tests.sh
+++ scripts/ci/linux_docker/run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ev
+ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 # run linux script so DRY is followed
-../linux/run_tests.sh
+$ROOT/scripts/ci/linux/run_tests.sh
