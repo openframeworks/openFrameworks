58e85715c96002da0a9f6084bf487abe640d8d5e
Christoph Buchner <bilderbuchi@phononoia.at>
2015-08-20 11:05:53 +0200

    Remove superfluous semicolons in docs CI scripts

--- scripts/ci/docs/install.sh
+++ scripts/ci/docs/install.sh
@@ -2,9 +2,9 @@
 set -ev
 echo "Installing documentation dependencies"
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
-gem install sass --version "=3.2.12";
-gem install compass --version "=0.12.2";
-cd $ROOT/scripts;
-git clone https://github.com/halfdanJ/ofDocGenerator;
-cd ofDocGenerator;
-npm install;
+gem install sass --version "=3.2.12"
+gem install compass --version "=0.12.2"
+cd $ROOT/scripts
+git clone https://github.com/halfdanJ/ofDocGenerator
+cd ofDocGenerator
+npm install
