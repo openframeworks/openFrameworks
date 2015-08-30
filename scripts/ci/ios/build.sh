c6f6f3a099b5060e585d28c5cb463ffa20907b7c
arturo castro <arturo@openframeworks.cc>
2015-08-30 22:23:45 +0200

    ci ios: fix script

--- scripts/ci/ios/build.sh
+++ scripts/ci/ios/build.sh
@@ -4,4 +4,5 @@ echo "Building openFrameworks - iOS Template Project"
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 mkdir -p $ROOT/apps/myApps
 cp -r "$ROOT/scripts/ios/template/standard" "$ROOT/apps/myApps/emptyExample"
-xcodebuild -configuration Release -target emptyExample -project "$ROOT/apps/myApps/emptyExample/emptyExample.xcodeproj"
+xcodebuild -project "$ROOT/apps/myApps/emptyExample/emptyExample.xcodeproj"  -target emptyExample -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+
