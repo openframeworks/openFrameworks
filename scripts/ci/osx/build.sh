f466a1f1840e050083bde1785417d61deabb2d00
arturo castro <arturo@openframeworks.cc>
2015-08-30 17:56:31 +0200

    fix ci scripts to use new template fs

--- scripts/ci/osx/build.sh
+++ scripts/ci/osx/build.sh
@@ -2,11 +2,11 @@
 set -ev
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 echo "**** Building oF + emptyExample - OSX Template Project ****"
-xcodebuild -configuration Release -target emptyExample -project "$ROOT/scripts/osx/template/emptyExample.xcodeproj"
+xcodebuild -configuration Release -target emptyExample -project "$ROOT/scripts/osx/template/standard/emptyExample.xcodeproj"
 
 echo "**** Building allAddonsExample ****"
 cd $ROOT
-cp scripts/osx/template/Makefile examples/addons/allAddonsExample/
-cp scripts/osx/template/config.make examples/addons/allAddonsExample/
+cp scripts/osx/template/standard/Makefile examples/addons/allAddonsExample/
+cp scripts/osx/template/standard/config.make examples/addons/allAddonsExample/
 cd examples/addons/allAddonsExample/
 make Debug
