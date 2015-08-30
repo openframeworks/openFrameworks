f96b46199be2414081a9cc955baede664854d689
arturo castro <arturo@openframeworks.cc>
2015-08-30 18:15:55 +0200

    more fixes for ci scripts with new templates fs

--- scripts/ci/ios/build.sh
+++ scripts/ci/ios/build.sh
@@ -2,4 +2,6 @@
 set -ev
 echo "Building openFrameworks - iOS Template Project"
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
-xcodebuild -project "$ROOT/scripts/ios/template/standard/emptyExample.xcodeproj" -target emptyExample -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
+mkdir -p $ROOT/apps/myApps
+cp -r "$ROOT/scripts/ios/template/standard" "$ROOT/apps/myApps/emptyExample"
+xcodebuild -configuration Release -target emptyExample -project "$ROOT/apps/myApps/emptyExample.xcodeproj"
--- scripts/ci/linux/run_tests.sh
+++ scripts/ci/linux/run_tests.sh
@@ -9,8 +9,8 @@ for group in *; do
 		for test in $group/*; do
 			if [ -d $test ]; then
 				cd $test
-				cp ../../../scripts/linux/template/linux/Makefile .
-				cp ../../../scripts/linux/template/linux/config.make .
+				cp ../../../scripts/linux/template/linux/standard/Makefile .
+				cp ../../../scripts/linux/template/linux/standard/config.make .
 				make Debug
 				make RunDebug
 			fi
