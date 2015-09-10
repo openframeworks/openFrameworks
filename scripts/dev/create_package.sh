9f0977d3920e20ebd4144829471209a0c96b47fa
arturo castro <arturo@openframeworks.cc>
2015-09-10 14:59:32 +0200

    create_pkg: fix typo

--- scripts/dev/create_package.sh
+++ scripts/dev/create_package.sh
@@ -136,7 +136,7 @@ function createProjectFiles {
 	        cp $pkg_ofroot/scripts/templates/win_cb/config.make ${example}
 	        cp $pkg_ofroot/scripts/templates/win_cb/Makefile ${example}
 	    done
-    elif ["$pkg_platform" != "android" ]; then
+    elif [ "$pkg_platform" != "android" ]; then
         cd ${main_ofroot}/apps/projectGenerator
         git pull origin master
         cd commandLine
