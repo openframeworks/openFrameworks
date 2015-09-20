3d74abc72bc3960cc4a4f4c2ec354f5b898be224
Andreas Borg <communic@tion.to>
2015-09-20 19:20:06 -0400

    +ofxiOSViewController: made race condition bReadyToRotate iOS7 specific +ofxiOSViewController: rotation rotateToInterfaceOrientation fixed offset center

--- examples/ios/iosNativeARCExample/src/main.mm
+++ examples/ios/iosNativeARCExample/src/main.mm
@@ -49,7 +49,7 @@ int main(){
     settings.enableDepth = false; // enables depth buffer for 3d drawing.
     settings.enableAntiAliasing = true; // enables anti-aliasing which smooths out graphics on the screen.
     settings.numOfAntiAliasingSamples = 4; // number of samples used for anti-aliasing.
-    settings.enableHardwareOrientation = true; // enables native view orientation. if true ofSetOrientation doesn't do anything
+    settings.enableHardwareOrientation = false; // enables native view orientation.
     settings.enableHardwareOrientationAnimation = true; // enables native orientation changes to be animated.
     settings.glesVersion = OFXIOS_RENDERER_ES2; // type of renderer to use, ES1, ES2, etc.
     
