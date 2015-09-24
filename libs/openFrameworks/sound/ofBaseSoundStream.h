5d94119f54ebc0afbf557e986c4f8e191b56aa58
DomAmato <dominic.a.amato@gmail.com>
2015-09-24 00:25:58 -0500

    ofSoundStream error...

--- libs/openFrameworks/sound/ofBaseSoundStream.h
+++ libs/openFrameworks/sound/ofBaseSoundStream.h
@@ -38,27 +38,34 @@ public:
 	std::vector<unsigned int> sampleRates;
 };
 
-class ofBaseSoundStream{
+class ofBaseSoundStream {
+public:
+	class ofBaseStreamSettings {
 	public:
-		virtual ~ofBaseSoundStream(){}
-	
-		virtual void setDeviceID(int deviceID) = 0;
-		virtual bool setup(int outChannels, int inChannels, int sampleRate, int bufferSize, int nBuffers)=0;
-		virtual bool setup(ofBaseApp * app, int outChannels, int inChannels, int sampleRate, int bufferSize, int nBuffers)=0;
-		virtual void setInput(ofBaseSoundInput * soundInput) = 0;
-		virtual void setOutput(ofBaseSoundOutput * soundOutput) = 0;
-	
-		virtual std::vector<ofSoundDevice> getDeviceList() const = 0;
-		virtual void printDeviceList() const;
-	
-		virtual void start() = 0;
-		virtual void stop() = 0;
-		virtual void close() = 0;
+		virtual ~ofBaseStreamSettings() {}
+	};
 
-		virtual long unsigned long getTickCount() const = 0;
-		virtual int getNumInputChannels() const = 0;
-		virtual int getNumOutputChannels() const = 0;
-		virtual int getSampleRate() const = 0;
-		virtual int getBufferSize() const = 0;
-		virtual int getDeviceID() const = 0;
+
+	virtual ~ofBaseSoundStream() {}
+
+	virtual void setDeviceID(int deviceID) = 0;
+	virtual bool setup(const ofBaseStreamSettings & settings) = 0;
+	//virtual bool setup(int outChannels, int inChannels, int sampleRate, int bufferSize, int nBuffers) = 0;
+	//virtual bool setup(ofBaseApp * app, int outChannels, int inChannels, int sampleRate, int bufferSize, int nBuffers) = 0;
+	virtual void setInput(ofBaseSoundInput * soundInput) = 0;
+	virtual void setOutput(ofBaseSoundOutput * soundOutput) = 0;
+
+	virtual std::vector<ofSoundDevice> getDeviceList() const = 0;
+	virtual void printDeviceList() const;
+
+	virtual void start() = 0;
+	virtual void stop() = 0;
+	virtual void close() = 0;
+
+	virtual long unsigned long getTickCount() const = 0;
+	virtual int getNumInputChannels() const = 0;
+	virtual int getNumOutputChannels() const = 0;
+	virtual int getSampleRate() const = 0;
+	virtual int getBufferSize() const = 0;
+	virtual int getDeviceID() const = 0;
 };
