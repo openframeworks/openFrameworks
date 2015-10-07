953ab9f4eafa8786dcc4762b1529c3568a8a6f25
arturo castro <arturo@openframeworks.cc>
2015-10-07 11:04:09 +0200

    ofEvent: fix listener remove

--- libs/openFrameworks/events/ofEvent.h
+++ libs/openFrameworks/events/ofEvent.h
@@ -87,15 +87,15 @@ protected:
 	template<typename TFunction>
 	void remove(const TFunction & function){
 		std::unique_lock<Mutex> lck(mtx);
-		functions.erase(std::remove_if(functions.begin(), functions.end(),
-			[&](std::shared_ptr<Function> & f){
-				if(f == function){
-					f->disable();
-					return true;
-				}else{
-					return false;
-				}
-			}), functions.end());
+		auto it = functions.begin();
+		for(; it!=functions.end(); ++it){
+			auto f = *it;
+			if(*f == *function){
+				f->disable();
+				functions.erase(it);
+				break;
+			}
+		}
 	}
 
 	Mutex mtx;
@@ -435,7 +435,7 @@ template<typename T>
 class ofFastEvent: public ofEvent<T,of::priv::NoopMutex>{
 public:
 	inline void notify(const void* sender, T & param){
-		if(ofFastEvent::enabled && !ofFastEvent::functions.empty()){
+		if(ofFastEvent::enabled){
 			for(auto & f: ofFastEvent::functions){
                 if(f->notify(sender,param)){
                     throw ofEventAttendedException();
