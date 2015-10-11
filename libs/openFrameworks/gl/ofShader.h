17682b80e59317d40eb0530c336d361ebccb06b5
arturoc <arturo@openframeworks.cc>
2015-10-11 16:25:30 +0200

    ofTexture/ofShader: add bufferid to texture
    In case they are backed by a buffer we need to call glTexBuffer to
    properly bind the texture as a uniform

--- libs/openFrameworks/gl/ofShader.h
+++ libs/openFrameworks/gl/ofShader.h
@@ -54,7 +54,6 @@ public:
 	void setUniformTexture(const string & name, const ofBaseHasTexture& img, int textureLocation) const;
 	void setUniformTexture(const string & name, const ofTexture& img, int textureLocation) const;
 	void setUniformTexture(const string & name, int textureTarget, GLint textureID, int textureLocation) const;
-	void setUniformTextureBuffer(const string & name, const ofTexture& tex, const ofBufferObject & buffer, int textureLocation)  const;
 	
 	// set a single uniform value
 	void setUniform1i(const string & name, int v1) const;
