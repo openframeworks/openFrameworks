ebf3c28ddefd45c5576065be6fdab98d945390d0
arturo castro <arturo@openframeworks.cc>
2015-10-15 21:54:10 +0200

    nightly builds: set OF PATH env variable

--- scripts/dev/nightlybuilds.sh
+++ scripts/dev/nightlybuilds.sh
@@ -26,7 +26,7 @@ error() {
 }
 trap 'error ${LINENO}' ERR
 
-
+export PG_OF_PATH=$(cat ~/.ofprojectgenerator/config)
 cd $(cat ~/.ofprojectgenerator/config)
 git fetch upstreamhttps
 git reset --hard upstreamhttps/master
