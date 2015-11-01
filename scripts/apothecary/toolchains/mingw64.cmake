c21adfce4ee472b08fbf3f045b75198f553b77cb
oxillo <oxillo@hotmail.com>
2015-11-01 21:58:44 +0100

    Cmake toolchains for MSYS2
    MSYS2 cmake doesn't recognize MINGW64-NT_10 and MINGW32_NT_10. Define
    toolchains to "cross-compile"

---
+++ scripts/apothecary/toolchains/mingw64.cmake
@@ -0,0 +1,17 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER gcc)
+SET(CMAKE_CXX_COMPILER g++)
+SET(CMAKE_RC_COMPILER windres)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH /mingw64)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
