4120c3b1ccf81fbaa3d265a2d230f2ff0b6a2912
arturo castro <arturo@openframeworks.cc>
2015-11-08 20:01:34 +0100

    ofConstants: move master to next version

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -3,9 +3,9 @@
 
 //-------------------------------
 #define OF_VERSION_MAJOR 0
-#define OF_VERSION_MINOR 9
+#define OF_VERSION_MINOR 10
 #define OF_VERSION_PATCH 0
-#define OF_VERSION_PRE_RELEASE "stable"
+#define OF_VERSION_PRE_RELEASE "master"
 
 //-------------------------------
 
