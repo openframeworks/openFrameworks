1b7ccc0d3c1c45aa67f2b1d570efe5fa2502217b
Daniel Rosser <danoli3@gmail.com>
2015-11-13 02:47:36 +1100

    tvOS Template changes

--- scripts/templates/tvOS/src/ofApp.mm
+++ scripts/templates/tvOS/src/ofApp.mm
@@ -16,6 +16,31 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
+void ofApp::touchDown(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchMoved(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchUp(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchDoubleTap(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchCancelled(ofTouchEventArgs & touch){
+    
+}
+
+//--------------------------------------------------------------
 void ofApp::controllerPressed(ofControllerEventArgs & event) {
     
 }
