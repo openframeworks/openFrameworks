b76dd172df278bb362080731f68da219eeac533f
Daniel Rosser <danoli3@gmail.com>
2015-11-16 20:45:30 +1100

    Apothecary - OpenSSL Formula Fixes

--- scripts/apothecary/formulas/openssl/opensslconf.h
+++ scripts/apothecary/formulas/openssl/opensslconf.h
@@ -1,8 +1,9 @@
+#include <TargetConditionals.h>
 #if defined(_MSC_VER)
 # include <openssl/opensslconf_vs.h>
 #elif defined( __WIN32__ ) || defined( _WIN32 )
 #   include <openssl/opensslconf_win32.h>
-#elif TARGET_OS_IPHONE_SIMULATOR || TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE || TARGET_IPHONE
+#elif TARGET_OS_IOS || (TARGET_OS_IPHONE && !TARGET_OS_TV)
 #   include <openssl/opensslconf_ios.h>
 #elif TARGET_OS_TV
 #   include <openssl/opensslconf_tvos.h>
