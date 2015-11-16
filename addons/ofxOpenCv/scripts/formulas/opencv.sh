4d42f46d6acbaac05dfb671738114717bab06dbd
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-11-16 17:30:58 +0900

    Fix opencv build on osx: set `-DWITH_OPENEXR=OFF`
    fixes #4576

--- addons/ofxOpenCv/scripts/formulas/opencv.sh
+++ addons/ofxOpenCv/scripts/formulas/opencv.sh
@@ -95,6 +95,7 @@ function build() {
       -DWITH_QUICKTIME=OFF \
       -DWITH_V4L=OFF \
       -DWITH_PVAPI=OFF \
+      -DWITH_OPENEXR=OFF \
       -DBUILD_TESTS=OFF \
       -DBUILD_PERF_TESTS=OFF 2>&1 | tee -a ${LOG}
     echo "CMAKE Successful"
