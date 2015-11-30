3daa49e85a2bd355ad27522c004f84bbdf775d34
arturo castro <arturo@openframeworks.cc>
2015-11-30 12:32:26 +0100

    network examples: better comments

--- examples/addons/networkTcpClientExample/src/ofApp.cpp
+++ examples/addons/networkTcpClientExample/src/ofApp.cpp
@@ -6,34 +6,31 @@
 void ofApp::setup(){
     ofSetBackgroundColor(230, 230, 230);
 
-	//our send and recieve strings
+    // our send and recieve strings
 	msgTx	= "";
 	msgRx	= "";
 
-    //connect to the server - if this fails or disconnects
-    //we'll check every few seconds to see if the server exists
-    tcpClient.setup("192.168.2.42", 11999);
+    // connect to the server - if this fails or disconnects
+    // we'll check every few seconds to see if the server exists
+    tcpClient.setup("127.0.0.1", 11999);
 
-    //optionally set the delimiter to something else.  The delimiter in the client and the server have to be the same
+    // optionally set the delimiter to something else.  The delimiter in the client and the server have to be the same
 	tcpClient.setMessageDelimiter("\n");
 	
 	connectTime = 0;
-	deltaTime = 0;
-
-	tcpClient.setVerbose(true);
-
+    deltaTime = 0;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
 	if(tcpClient.isConnected()){
-        //we are connected - lets try to receive from the server
+        // we are connected - lets try to receive from the server
         string str = tcpClient.receive();
         if( str.length() > 0 ){
 			msgRx = str;
         }
     }else{
-		//if we are not connected lets try and reconnect every 5 seconds
+        // if we are not connected lets try and reconnect every 5 seconds
 		deltaTime = ofGetElapsedTimeMillis() - connectTime;
 
 		if( deltaTime > 5000 ){
@@ -68,8 +65,9 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
-	//you can only type if you're connected
+    // you can only type if you're connected
+    // we accumulate 1 line of text and send every typed character
+    // on the next character after a breakline we clear the buffer
 	if(tcpClient.isConnected()){
 		if(key == OF_KEY_RETURN) key = '\n';
 		if(key == OF_KEY_BACKSPACE || key == OF_KEY_DEL){
--- examples/addons/networkTcpServerExample/src/ofApp.cpp
+++ examples/addons/networkTcpServerExample/src/ofApp.cpp
@@ -3,9 +3,9 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	//setup the server to listen on 11999
+	// setup the server to listen on 11999
 	TCP.setup(11999);
-	//optionally set the delimiter to something else.  The delimiter in the client and the server have to be the same, default being [/TCP]
+	// optionally set the delimiter to something else.  The delimiter in the client and the server have to be the same, default being [/TCP]
 	TCP.setMessageDelimiter("\n");
 	lastSent = 0;
 }
@@ -14,8 +14,8 @@ void ofApp::setup(){
 void ofApp::update(){
 	ofBackground(20, 20, 20);
 
-	//for each client lets send them a message letting them know what port they are connected on
-	//we throttle the message sending frequency to once every 100ms
+	// for each client lets send them a message letting them know what port they are connected on
+	// we throttle the message sending frequency to once every 100ms
 	uint64_t now = ofGetElapsedTimeMillis();
 	if(now - lastSent >= 100){
 		for(int i = 0; i < TCP.getLastID(); i++){
@@ -39,32 +39,32 @@ void ofApp::draw(){
 
 	ofSetHexColor(0xDDDDDD);
 
-	//for each connected client lets get the data being sent and lets print it to the screen
+	// for each connected client lets get the data being sent and lets print it to the screen
 	for(unsigned int i = 0; i < (unsigned int)TCP.getLastID(); i++){
 
 		if( !TCP.isClientConnected(i) )continue;
 
-		//give each client its own color
+		// give each client its own color
 		ofSetColor(255 - i*30, 255 - i * 20, 100 + i*40);
 
-		//calculate where to draw the text
+		// calculate where to draw the text
 		int xPos = 15;
 		int yPos = 80 + (12 * i * 4);
 
-		//get the ip and port of the client
+		// get the ip and port of the client
 		string port = ofToString( TCP.getClientPort(i) );
 		string ip   = TCP.getClientIP(i);
 		string info = "client "+ofToString(i)+" -connected from "+ip+" on port: "+port;
 
 
-		//if we don't have a string allocated yet
-		//lets create one
+		// if we don't have a string allocated yet
+		// lets create one
 		if(i >= storeText.size() ){
 			storeText.push_back( string() );
 		}
 
-		//receive all the available messages, separated by \n
-		//and keep only the last one
+		// receive all the available messages, separated by \n
+		// and keep only the last one
         string str;
         string tmp;
         do{
@@ -77,7 +77,7 @@ void ofApp::draw(){
 			storeText[i] = str;
 		}
 
-		//draw the info text and the received text bellow it
+		// draw the info text and the received text bellow it
 		ofDrawBitmapString(info, xPos, yPos);
 		ofDrawBitmapString(storeText[i], 25, yPos + 20);
 
