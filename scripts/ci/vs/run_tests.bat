d6f70126426f2f4c5f18181b723851ed465060c7
arturo castro <arturo@openframeworks.cc>
2015-12-02 11:19:11 +0100

    appveyor: move tests script to .ba in scripts/ci

---
+++ scripts/ci/vs/run_tests.bat
@@ -0,0 +1,16 @@
+cd %APPVEYOR_BUILD_FOLDER%\tests
+set TESTS_PLATFORM=%PLATFORM%
+if "%PLATFORM%" equ "x86" set TESTS_PLATFORM=Win32
+FOR /D %%G IN (*) DO ( 
+    echo %APPVEYOR_BUILD_FOLDER%\tests\%%G
+    cd %APPVEYOR_BUILD_FOLDER%\tests\%%G
+    FOR /D %%E IN (*) DO ( 
+        echo %APPVEYOR_BUILD_FOLDER%\tests\%%G\%%E
+        cd %APPVEYOR_BUILD_FOLDER%\tests\%%G\%%E
+        msbuild %%E.sln /p:Configuration=Debug /p:Platform=%TESTS_PLATFORM%
+        cd bin
+        %%E_debug.exe
+        if "%errorlevel%" neq "0" EXIT /B 1
+    )
+)
+cd ..
