7537e19f25adcc7bd5c7990d3d8a8e3cee2d0385
arturo castro <arturo@openframeworks.cc>
2015-12-06 14:28:53 +0100

    udp test: test that ports stay bound after send and receive

--- tests/addons/networkUdp/src/main.cpp
+++ tests/addons/networkUdp/src/main.cpp
@@ -127,10 +127,76 @@ class ofApp: public ofxUnitTestsApp{
         test_gt(now-then, 4500, "socket receive timeouts after 5s");
 	}
 
+    void testPortsStayBound(){
+        ofLogNotice() << "----------------------";
+        ofLogNotice() << "testPortsStayBound";
+        ofLogNotice() << "tests #3656";
+
+        int serverport = ofRandom(15000, 65535);
+        int clientport = serverport-1;
+        ofxUDPManager server;
+        test(server.Create(),"create udp socket");
+        test(server.SetNonBlocking(false), "set blocking");
+        test(server.Bind(serverport), "bind udp socket");
+        test(server.Connect("127.0.0.1", clientport), "set ip and port to send for udp socket");
+
+        std::string messageSent="message";
+        std::vector<char> bufferReceive(messageSent.size(),0);
+        std::string messageReceived;
+
+        ofxUDPManager client;
+        test(client.Create(), "create udp socket");
+        test(client.SetNonBlocking(false), "set udp socket blocking");
+        test(client.Bind(clientport), "bind udp socket");
+        test(client.Connect("127.0.0.1", serverport), "set ip and port to send for udp socket");
+
+
+        std::string receivedAddress;
+        int receivedPort;
+
+        test(client.Send(messageSent.c_str(), messageSent.size()), "client send blocking");
+        auto ret = server.Receive(bufferReceive.data(), messageSent.size());
+        test(ret>=0, "server receiving block");
+        messageReceived.assign(bufferReceive.begin(), bufferReceive.end());
+        test_eq(messageSent, messageReceived, "client messageSent == server messageReceived");
+        test(server.GetRemoteAddr(receivedAddress, receivedPort), "Could get remote address and port");
+        test_eq(receivedPort, clientport, "server received from clients bound port");
+
+
+        test(server.Send(messageSent.c_str(), messageSent.size()), "server send blocking");
+        bufferReceive.assign(messageSent.size(),0);
+        ret = client.Receive(bufferReceive.data(), messageSent.size());
+        test(ret>=0, "client receiving block");
+        messageReceived.assign(bufferReceive.begin(), bufferReceive.end());
+        test_eq(messageSent, messageReceived, "server messageSent == client messageReceived");
+        test(client.GetRemoteAddr(receivedAddress, receivedPort), "Could get remote address and port");
+        test_eq(receivedPort, serverport, "client received from servers bound port");
+
+
+        test(client.Send(messageSent.c_str(), messageSent.size()), "client send blocking 2nd time");
+        ret = server.Receive(bufferReceive.data(), messageSent.size());
+        test(ret>=0, "server receiving block 2nd time");
+        messageReceived.assign(bufferReceive.begin(), bufferReceive.end());
+        test_eq(messageSent, messageReceived, "2nd client messageSent == server messageReceived");
+        test(server.GetRemoteAddr(receivedAddress, receivedPort), "Could get remote address and port");
+        test_eq(receivedPort, clientport, "server received from clients bound port");
+
+
+        test(server.Send(messageSent.c_str(), messageSent.size()), "server send blocking 2nd time");
+        bufferReceive.assign(messageSent.size(),0);
+        ret = client.Receive(bufferReceive.data(), messageSent.size());
+        test(ret>=0, "client receiving block 2nd time");
+        messageReceived.assign(bufferReceive.begin(), bufferReceive.end());
+        test_eq(messageSent, messageReceived, "2nd server messageSent == client messageReceived");
+        test(client.GetRemoteAddr(receivedAddress, receivedPort), "Could get remote address and port");
+        test_eq(receivedPort, serverport, "client received from servers bound port");
+    }
+
 	void run(){
 		testNonBlocking();
 		testBlocking();
 		testTimeOutRecv();
+        testPortsStayBound();
 	}
 };
 
