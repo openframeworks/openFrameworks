da179c05fa0172160817a98838a93e37c11e3687
Christopher Baker <me@christopherbaker.net>
2015-12-16 23:11:57 -0600

    Spaces to tabs.

--- scripts/templates/ios/src/main.mm
+++ scripts/templates/ios/src/main.mm
@@ -2,22 +2,22 @@
 #include "ofApp.h"
 
 int main(){
-    //  Here are the most commonly used iOS window settings.
-    //------------------------------------------------------
-    ofiOSWindowSettings settings;
-    settings.enableRetina = false; // Enables retina resolution if the device supports it.
-    settings.enableDepth = false; // Enables depth buffer for 3d drawing.
-    settings.enableAntiAliasing = false; // Enables anti-aliasing which smooths out graphics on the screen.
-    settings.numOfAntiAliasingSamples = 0; // The number of samples used for anti-aliasing.
-    settings.enableHardwareOrientation = false; // Enables native view orientation.
-    settings.enableHardwareOrientationAnimation = false; // Enables native orientation changes to be animated.
-    settings.glesVersion = OFXIOS_RENDERER_ES1; // The type of renderer to use, ES1, ES2, ES3.
-    settings.windowMode = OF_FULLSCREEN;
+	//  Here are the most commonly used iOS window settings.
+	//------------------------------------------------------
+	ofiOSWindowSettings settings;
+	settings.enableRetina = false; // Enables retina resolution if the device supports it.
+	settings.enableDepth = false; // Enables depth buffer for 3d drawing.
+	settings.enableAntiAliasing = false; // Enables anti-aliasing which smooths out graphics on the screen.
+	settings.numOfAntiAliasingSamples = 0; // The number of samples used for anti-aliasing.
+	settings.enableHardwareOrientation = false; // Enables native view orientation.
+	settings.enableHardwareOrientationAnimation = false; // Enables native orientation changes to be animated.
+	settings.glesVersion = OFXIOS_RENDERER_ES1; // The type of renderer to use, ES1, ES2, ES3.
+	settings.windowMode = OF_FULLSCREEN;
 
-    auto window = ofCreateWindow(settings);
+	auto window = ofCreateWindow(settings);
 	auto app = std::make_shared<ofApp>();
 
-    ofRunApp(window,app);
+	ofRunApp(window,app);
 
 	return ofRunMainLoop();
 }
