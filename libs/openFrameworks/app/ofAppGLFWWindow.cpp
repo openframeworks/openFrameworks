91c1f66bbdaf4dfb40a1ef89a97586b364602ac8
ofTheo <theo@openframeworks.cc>
2015-12-28 15:56:12 -0500

    bugfix for shrinking window on ofToggleFullscreen closes #4696

--- libs/openFrameworks/app/ofAppGLFWWindow.cpp
+++ libs/openFrameworks/app/ofAppGLFWWindow.cpp
@@ -1120,8 +1120,8 @@ void ofAppGLFWWindow::keyboard_cb(GLFWwindow* windowP_, int keycode, int scancod
 //------------------------------------------------------------
 void ofAppGLFWWindow::resize_cb(GLFWwindow* windowP_,int w, int h) {
 	ofAppGLFWWindow * instance = setCurrent(windowP_);
-	instance->windowW = w;
-	instance->windowH = h;
+	instance->windowW = w * instance->pixelScreenCoordScale;
+	instance->windowH = h * instance->pixelScreenCoordScale;
 	instance->events().notifyWindowResized(w*instance->pixelScreenCoordScale, h*instance->pixelScreenCoordScale);
 
 	instance->nFramesSinceWindowResized = 0;
