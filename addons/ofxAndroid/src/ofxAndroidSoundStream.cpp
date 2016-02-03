5dbe254daf7c49e8d20b0eeb1f46e6064a61e9d6
arturo castro <arturo@openframeworks.cc>
2016-02-03 15:28:05 +0100

    ofxAndroidSoundStream: fixes for new api

--- addons/ofxAndroid/src/ofxAndroidSoundStream.cpp
+++ addons/ofxAndroid/src/ofxAndroidSoundStream.cpp
@@ -45,7 +45,7 @@ ofxAndroidSoundStream::~ofxAndroidSoundStream(){
 	ofRemoveListener(ofxAndroidEvents().resume,this,&ofxAndroidSoundStream::resume);
 }
 
-vector<ofSoundDevice> ofxAndroidSoundStream::getDeviceList() const{
+std::vector<ofSoundDevice> getDeviceList(ofSoundDevice::Api api) const;{
     ofLogWarning("ofxAndroidSoundStream") << "getDeviceList() isn't implemented on android";
     return vector<ofSoundDevice>();
 }
@@ -62,28 +62,28 @@ void ofxAndroidSoundStream::setOutput(ofBaseSoundOutput * _soundOutput){
 	soundOutputPtr = _soundOutput;
 }
 
-bool ofxAndroidSoundStream::setup(int outChannels, int inChannels, int sampleRate, int bufferSize, int nBuffers){
+bool ofxAndroidSoundStream::setup(const ofSoundStreamSettings & settings);
 	if(instance!=NULL && instance!=this){
 		ofLogError("ofxAndroidSoundStream") << "setup(): multiple instances detected, only one instance allowed";
 		return false;
 	}
 
 	// Find the minimum input buffer size allowed by the Android device
-	int input_buffer_size = inChannels*getMinInBufferSize(sampleRate,inChannels) * 2;
+	int input_buffer_size = inChannels*getMinInBufferSize(settings.sampleRate,settings.numInputChannels) * 2;
 	// setup size of input circular-buffer
 	input_buffer.setup(input_buffer_size,0);
 	
 	// deallocate and reallocate if setup() is called more than once
-	in_float_buffer.allocate(bufferSize,inChannels);
-	in_float_buffer.setSampleRate(sampleRate);
-	out_float_buffer.allocate(bufferSize,outChannels);
-	out_float_buffer.setSampleRate(sampleRate);
+	in_float_buffer.allocate(settings.bufferSize,settings.numInputChannels);
+	in_float_buffer.setSampleRate(settings.sampleRate);
+	out_float_buffer.allocate(settings.bufferSize,settings.numOutputChannels);
+	out_float_buffer.setSampleRate(settings.sampleRate);
 
 	tickCount = 0;
 
-	requestedBufferSize = bufferSize;
-	totalOutRequestedBufferSize = bufferSize*outChannels;
-	totalInRequestedBufferSize = bufferSize*inChannels;
+	requestedBufferSize = settings.bufferSize;
+	totalOutRequestedBufferSize = settings.bufferSize*settings.outChannels;
+	totalInRequestedBufferSize = settings.bufferSize*numInputChannels;
 
 	// JNI: Try to find and call OFAndroidSoundStream.getInstance().setup(outChannels,inChannels,sampleRate,bufferSize,nBuffers)
 	if(!ofGetJavaVMPtr()){
@@ -111,7 +111,7 @@ bool ofxAndroidSoundStream::setup(int outChannels, int inChannels, int sampleRat
 	jmethodID javaSetup = env->GetMethodID(javaClass,"setup","(IIIII)V");
 	// call setup()
 	if(javaObject && javaSetup)
-		env->CallVoidMethod(javaObject,javaSetup,outChannels,inChannels,sampleRate,bufferSize,nBuffers);
+		env->CallVoidMethod(javaObject,javaSetup,settings.numOutputChannels,settings.numInputChannels,settings.sampleRate,settings.bufferSize,settings.numBuffers);
 	else
 		ofLogError("ofxAndroidSoundStream") << "setup(): couldn't get OFAndroidSoundStream instance or setup method";
 
