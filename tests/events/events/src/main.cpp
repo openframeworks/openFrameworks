32eef55d846db4b6a48b353b199913235be3ed77
arturo castro <arturo@openframeworks.cc>
2016-02-04 12:19:41 +0100

    ofEvent now supports adding std::function as a listener, that allows among other things:
    - pass a lambda function as a listener
    - pass a function with a different signature as a listener.
    
    For example to pass a lambda as a listener now you can do:
    
    ```cpp
    ofEvents().mouseMoved.newListener([](ofMouseEventArgs & mouse){
    	cout << mouse << endl;
    });
    ```
    
    Since std::function doesn't have an equality operator, there's no way to
    unregister an event the old way calling `ofRemoveListener(function)` since
    that required to compare the passed function with every other registered in
    the event and remove it if it was the same.
    
    `ofRemoveListener`also has some other problems, for example, the typical pattern:
    
    ```cpp
    class MyClass{
    public:
        MyClass(){
             ofAddListener(ofEvents().mouseMoved,this,&MyClass::mouseMoved);
        }
    
        ~MyClass(){
             ofRemoveListener(ofEvents().mouseMoved,this,&MyClass::mouseMoved);
        }
    
        void mouseMoved(ofMouseMoved & mouse){
        ....
        }
    }
    ```
    
    Has a problem with copy construction:
    
    ```
    MyClass obj;
    vector<MyClass> objs;
    objs.push_back(obj);
    ```
    
    Won't be registered as listener and there won't be any compiler error.
    Because of the [rule of three](https://en.wikipedia.org/wiki/Rule_of_three_%28C%2B%2B_programming%29)
    Any class that adds a listener in it's constructor and unregisters
    in it's destructor. Needs also a copy constructor, operator=, and in
    c++11 to be completely correct also a move constructor and an operator=
    with move parameter.
    
    With the new way of registering listeners one does:
    
    ```cpp
    class MyClass{
        ofEventListener mouseMovedListener;
    public:
        MyClass(){
             mouseMovedListener = ofEvents().mouseMoved.newListener(this,&MyClass::mouseMoved);
        }
    
        void mouseMoved(ofMouseMoved & mouse){
        ....
        }
    }
    ```
    
    The ofEventListener class unregisters the event automatically when it goes out
    of scope avoiding the need for a destructor and because of that avoiding the
    need for copy constructor...
    
    Also since the class is not copyable if someone tries to copy an instance
    of a class that contains an ofEventListener the compiler will give an error.
    
    In any case the old method still works.

---
+++ tests/events/events/src/main.cpp
@@ -0,0 +1,173 @@
+#include "ofMain.h"
+#include "ofAppNoWindow.h"
+#include "ofxUnitTests.h"
+
+namespace {
+	int lastIntFromCFunc = 0;
+	int lastIntFromCFuncWithToken = 0;
+	int selfUnregisterValue = 0;
+	bool toggleVoidFunc = false;
+	ofEvent<const int> selfUnregisterEvent;
+
+	void intFunctListener(const int & i){
+		lastIntFromCFunc = i;
+	}
+
+	void intFunctListenerWithToken(const int & i){
+		lastIntFromCFuncWithToken = i;
+	}
+
+	void selfUnregister(const int & i){
+		selfUnregisterValue = i;
+		ofRemoveListener(selfUnregisterEvent, selfUnregister);
+	}
+
+	void voidFunc(){
+		toggleVoidFunc = !toggleVoidFunc;
+	}
+}
+
+class ofApp: public ofxUnitTestsApp{
+	int lastInt = 0;
+	int lastIntFromLambda = 0;
+	int lastIntWithToken = 0;
+	bool toggleForVoidListener = false;
+	bool toggleForVoidLambdaListener = false;
+	bool toggleForVoidListenerWithToken = false;
+
+	void intListener(const int & i){
+		lastInt = i;
+	}
+
+	void intListenerWithToken(const int & i){
+		lastIntWithToken = i;
+	}
+
+	void voidListener(){
+		toggleForVoidListener = !toggleForVoidListener;
+	}
+
+	void voidListenerWithToken(){
+		toggleForVoidListenerWithToken = !toggleForVoidListenerWithToken;
+	}
+
+	void run(){
+		{
+			ofEvent<const int> intEvent;
+			ofAddListener(intEvent, this, &ofApp::intListener);
+			ofAddListener(intEvent, intFunctListener);
+			auto listenerLambda = intEvent.newListener([&](const int & i){
+				lastIntFromLambda = i;
+			}, 0);
+			auto listenerMember = intEvent.newListener(this, &ofApp::intListenerWithToken);
+			auto listenerFunc = intEvent.newListener(intFunctListenerWithToken);
+
+			ofNotifyEvent(intEvent, 5);
+			test_eq(lastInt, 5, "Testing int event to member function");
+			test_eq(lastIntWithToken, 5, "Testing int event to member function with release token");
+			test_eq(lastIntFromLambda, 5, "Testing int event to lambda function");
+			test_eq(lastIntFromCFunc, 5, "Testing int event to c function");
+			test_eq(lastIntFromCFuncWithToken, 5, "Testing int event to c function with release token");
+
+			intEvent.disable();
+			ofNotifyEvent(intEvent, 6);
+			test_eq(lastInt, 5, "Testing disabled int event to member function");
+			test_eq(lastIntWithToken, 5, "Testing disabled int event to member function with release token");
+			test_eq(lastIntFromLambda, 5, "Testing disabled int event to lambda function");
+			test_eq(lastIntFromCFunc, 5, "Testing disabled int event to c function");
+			test_eq(lastIntFromCFuncWithToken, 5, "Testing disabled int event to c function with release token");
+
+			intEvent.enable();
+			ofNotifyEvent(intEvent, 6);
+			test_eq(lastInt, 6, "Testing re-enabled int event to member function");
+			test_eq(lastIntWithToken, 6, "Testing re-enabled int event to member function with release token");
+			test_eq(lastIntFromLambda, 6, "Testing re-enabled int event to lambda function");
+			test_eq(lastIntFromCFunc, 6, "Testing re-enabled int event to c function");
+			test_eq(lastIntFromCFuncWithToken, 6, "Testing re-enabled int event to c function with release token");
+
+			ofRemoveListener(intEvent, this, &ofApp::intListener);
+			ofRemoveListener(intEvent, &intFunctListener);
+			listenerLambda.unsubscribe();
+			listenerMember.unsubscribe();
+			listenerFunc.unsubscribe();
+			ofNotifyEvent(intEvent, 5);
+			test_eq(lastInt, 6, "Testing remove listener on int event to member function");
+			test_eq(lastIntWithToken, 6, "Testing remove listener on int event to member function with release token");
+			test_eq(lastIntFromLambda, 6, "Testing remove listener on int event to lambda function");
+			test_eq(lastIntFromCFunc, 6, "Testing remove listener on int event to c function");
+			test_eq(lastIntFromCFuncWithToken, 6, "Testing remove listener on int event to c function with release token");
+		}
+
+		{
+			ofEventListener listener;
+			{
+				ofEvent<const int> intEvent;
+				listener = intEvent.newListener([&](const int & i){
+					lastIntFromLambda = i;
+				}, 0);
+			}
+			listener.unsubscribe();
+		}
+
+		{
+			lastIntFromCFunc = 0;
+			auto listener = selfUnregisterEvent.newListener(selfUnregister);
+			selfUnregisterEvent.notify(5);
+			test_eq(selfUnregisterValue, 5, "Testing remove listener on event callback, first call");
+			selfUnregisterEvent.notify(6);
+			test_eq(selfUnregisterValue, 5, "Testing remove listener on event callback, second call");
+		}
+
+		{
+			ofEvent<void> voidEvent;
+			ofAddListener(voidEvent, this, &ofApp::voidListener);
+			auto listenerVoidLambda = voidEvent.newListener([&]{
+				toggleForVoidLambdaListener = !toggleForVoidLambdaListener;
+			});
+			auto listenerVoidMember = voidEvent.newListener(this, &ofApp::voidListenerWithToken);
+			auto listenerVoidFunc = voidEvent.newListener(voidFunc);
+
+			voidEvent.notify();
+			test_eq(toggleForVoidListener, true, "Void event with member function");
+			test_eq(toggleForVoidLambdaListener, true, "Void event with lambda function");
+			test_eq(toggleForVoidListenerWithToken, true, "Void event with member function with release token");
+			test_eq(toggleVoidFunc, true, "Void event with c function");
+
+			voidEvent.disable();
+			voidEvent.notify();
+			test_eq(toggleForVoidListener, true, "Disabled void event with member function");
+			test_eq(toggleForVoidLambdaListener, true, "Disabled void event with lambda function");
+			test_eq(toggleForVoidListenerWithToken, true, "Disabled void event with member function with release token");
+			test_eq(toggleVoidFunc, true, "Disabled void event with c function");
+
+			voidEvent.enable();
+			voidEvent.notify();
+			test_eq(toggleForVoidListener, false, "Re-enabled void event with member function");
+			test_eq(toggleForVoidLambdaListener, false, "Re-enabled void event with lambda function");
+			test_eq(toggleForVoidListenerWithToken, false, "Re-enabled void event with member function with release token");
+			test_eq(toggleVoidFunc, false, "Re-enabled void event with c function");
+
+			ofRemoveListener(voidEvent, this, &ofApp::voidListener);
+			listenerVoidLambda.unsubscribe();
+			listenerVoidMember.unsubscribe();
+			listenerVoidFunc.unsubscribe();
+			test_eq(toggleForVoidListener, false, "Unregistered void event with member function");
+			test_eq(toggleForVoidLambdaListener, false, "Unregistered void event with lambda function");
+			test_eq(toggleForVoidListenerWithToken, false, "Unregistered void event with member function with release token");
+			test_eq(toggleVoidFunc, false, "Unregistered void event with c function");
+		}
+	}
+};
+
+//========================================================================
+int main( ){
+    ofInit();
+    auto window = make_shared<ofAppNoWindow>();
+    auto app = make_shared<ofApp>();
+    // this kicks off the running of my app
+    // can be OF_WINDOW or OF_FULLSCREEN
+    // pass in width and height too:
+    ofRunApp(window, app);
+    return ofRunMainLoop();
+
+}
