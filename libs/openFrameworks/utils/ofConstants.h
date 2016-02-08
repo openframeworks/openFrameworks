d2f4b764c2f70108b2522db0d4b8fdc684906550
arturo castro <arturo@openframeworks.cc>
2016-02-08 15:30:09 +0100

    bump version to 0.9.2

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -4,7 +4,7 @@
 //-------------------------------
 #define OF_VERSION_MAJOR 0
 #define OF_VERSION_MINOR 9
-#define OF_VERSION_PATCH 1
+#define OF_VERSION_PATCH 2
 #define OF_VERSION_PRE_RELEASE "master"
 
 //-------------------------------
