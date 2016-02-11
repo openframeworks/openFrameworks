c3b11d6cec01b178bbfb13ca5c8a0b2423c2f3a6
arturo castro <arturo@openframeworks.cc>
2016-02-11 15:45:02 +0100

    add -y to apt-get

--- scripts/ci/linuxarmv6/install.sh
+++ scripts/ci/linuxarmv6/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
-sudo apt-get update
-sudo apt-get install multistrap unzip
+sudo apt-get -y update
+sudo apt-get -y install multistrap unzip
 cd $(dirname $0)
 multistrap -a armhf -d raspbian -f multistrap.conf
 wget https://github.com/raspberrypi/tools/archive/master.zip -O raspberrytools.zip
