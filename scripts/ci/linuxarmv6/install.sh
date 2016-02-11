2d5111cdba7c6d2e25dba9e7f048c3a4b1b377a8
arturo castro <arturo@openframeworks.cc>
2016-02-11 15:08:45 +0100

    fix rPI tests paths

--- scripts/ci/linuxarmv6/install.sh
+++ scripts/ci/linuxarmv6/install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -ev
-SCRIPTS_DIR=$(dirname "$0")
-cd ~
 sudo apt-get update
 sudo apt-get install multistrap unzip
-multistrap -a armhf -d raspbian -f $SCRIPTS_DIR/multistrap.conf
+multistrap -a armhf -d raspbian -f multistrap.conf
 wget https://github.com/raspberrypi/tools/archive/master.zip -O raspberrytools.zip
 unzip raspberrytools.zip
 rm raspberrytools.zip
