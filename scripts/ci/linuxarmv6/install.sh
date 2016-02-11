42503a22a5a536f82387eeb2f02af954861e887d
arturo castro <arturo@openframeworks.cc>
2016-02-11 16:05:43 +0100

    comment on workaround

--- scripts/ci/linuxarmv6/install.sh
+++ scripts/ci/linuxarmv6/install.sh
@@ -2,6 +2,7 @@
 set -ev
 sudo apt-get -y update
 sudo apt-get -y install multistrap unzip
+#workaround for https://bugs.launchpad.net/ubuntu/+source/multistrap/+bug/1313787
 sudo sed -i s/\$forceyes//g /usr/sbin/multistrap
 cd $(dirname $0)
 multistrap -a armhf -d raspbian -f multistrap.conf
