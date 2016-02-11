a69703cd2eb6e7974f837c09d4f6f32573df742f
arturo castro <arturo@openframeworks.cc>
2016-02-11 16:03:12 +0100

    armv6 install: workaround for multistrap bug in ubuntu 14.04

--- scripts/ci/linuxarmv6/install.sh
+++ scripts/ci/linuxarmv6/install.sh
@@ -2,6 +2,7 @@
 set -ev
 sudo apt-get -y update
 sudo apt-get -y install multistrap unzip
+sudo sed -i s/\$forceyes//g /usr/sbin/multistrap
 cd $(dirname $0)
 multistrap -a armhf -d raspbian -f multistrap.conf
 wget https://github.com/raspberrypi/tools/archive/master.zip -O raspberrytools.zip
