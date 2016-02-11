8c73d27aa651962ff2e6f981997fc9aed54afab7
arturo castro <arturo@openframeworks.cc>
2016-02-11 14:08:19 +0100

    use sudo for travis on linux armv6

--- scripts/ci/linuxarmv6/install.sh
+++ scripts/ci/linuxarmv6/install.sh
@@ -2,6 +2,8 @@
 set -ev
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 cd ~
+sudo apt-get update
+sudo apt-get install multistrap unzip
 multistrap -a armhf -d raspbian -f multistrap.conf
 wget https://github.com/raspberrypi/tools/archive/master.zip -O raspberrytools.zip
 unzip raspberrytools.zip
