23f916b8e07248789d7716dc1cc54f90642cf268
arturo castro <arturo@openframeworks.cc>
2016-02-11 15:18:01 +0100

    linux arm ci: cd to script fodler

--- scripts/ci/linuxarmv6/install.sh
+++ scripts/ci/linuxarmv6/install.sh
@@ -2,6 +2,7 @@
 set -ev
 sudo apt-get update
 sudo apt-get install multistrap unzip
+cd $(dirname $0)
 multistrap -a armhf -d raspbian -f multistrap.conf
 wget https://github.com/raspberrypi/tools/archive/master.zip -O raspberrytools.zip
 unzip raspberrytools.zip
