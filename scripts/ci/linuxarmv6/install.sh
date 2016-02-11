8a40d8da12015c4c9892b97935b3d32f186204f9
arturo castro <arturo@openframeworks.cc>
2016-02-11 14:59:53 +0100

    ci linuxarmv6: fix path to mulstistrap config

--- scripts/ci/linuxarmv6/install.sh
+++ scripts/ci/linuxarmv6/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -ev
+SCRIPTS_DIR=$(dirname "$0")
 cd ~
 sudo apt-get update
 sudo apt-get install multistrap unzip
-multistrap -a armhf -d raspbian -f multistrap.conf
+multistrap -a armhf -d raspbian -f $SCRIPTS_DIR/multistrap.conf
 wget https://github.com/raspberrypi/tools/archive/master.zip -O raspberrytools.zip
 unzip raspberrytools.zip
 rm raspberrytools.zip
