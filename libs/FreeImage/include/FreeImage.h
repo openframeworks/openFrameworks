64ad6fe49b303acba1e30fc6a16f4a616152b254
Daniel Rosser <danoli3@gmail.com>
2016-02-17 23:01:38 +1100

    Fix for defines

--- libs/FreeImage/include/FreeImage.h
+++ libs/FreeImage/include/FreeImage.h
@@ -150,22 +150,22 @@ FI_STRUCT (FIMULTIBITMAP) { void *data; };
 #ifndef _MSC_VER
 // define portable types for 32-bit / 64-bit OS
 #include <inttypes.h>
-#define BOOL int32_t;
-#define BYTE uint8_t;
-#define WORD uint16_t;
-#define DWORD uint32_t;
-#define LONG int32_t;
-#define INT64 int64_t;
-#define UINT64 uint64_t;
+#define BOOL int32_t
+#define BYTE uint8_t
+#define WORD uint16_t
+#define DWORD uint32_t
+#define LONG int32_t
+#define INT64 int64_t
+#define UINT64 uint64_t
 #else
 // MS is not C99 ISO compliant
-#define BOOL long;
-#define BYTE unsigned char;
-#define WORD unsigned short;
-#define DWORD unsigned long;
-#define LONG long;
-#define INT64 signed __int64;
-#define UINT64 unsigned __int64;
+#define BOOL long
+#define BYTE unsigned char
+#define WORD unsigned short
+#define DWORD unsigned long
+#define LONG long
+#define INT64 signed __int64
+#define UINT64 unsigned __int64
 #endif // _MSC_VER
 
 #if (defined(_WIN32) || defined(__WIN32__))
