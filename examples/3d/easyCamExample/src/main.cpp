65ee74aa3a5c07885efb0ee7eb56cc0df31bbff6
Caroline Record <recordcaroline@gmail.com>
2016-02-18 17:54:20 -0700

    easycam overlay text

--- examples/3d/easyCamExample/src/main.cpp
+++ examples/3d/easyCamExample/src/main.cpp
@@ -4,7 +4,7 @@
 //========================================================================
 int main( ){
 
-	ofSetupOpenGL(640,480, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(750,480, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
