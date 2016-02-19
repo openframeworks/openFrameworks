86055b4daaa38330d15a66a338a7147bca8bb115
Christopher Baker <me@christopherbaker.net>
2016-02-19 12:19:28 -0700

    ofVideoPlayer documentation updates.

--- libs/openFrameworks/video/ofVideoPlayer.h
+++ libs/openFrameworks/video/ofVideoPlayer.h
@@ -61,28 +61,27 @@ class ofVideoPlayer : public ofBaseVideoPlayer,public ofBaseVideoDraws{
 		/// \brief Get the path to the loaded video file.
 		///
 		/// If no video file is loaded this returns an empty string.
-		/// \returns A path to the loaded video or an empty string if the video
-		/// is not loaded.
+		///
+		/// \returns A path to the loaded video or an empty string if not loaded.
 		string				getMoviePath() const;
 
 		bool				setPixelFormat(ofPixelFormat pixelFormat);
 		ofPixelFormat		getPixelFormat() const;
-		
-		/// \brief Closes the movie file and de-allocates its resources.
+
+		/// \brief Closes the movie file and releases its resources.
 		///
-		/// This is an alias of close()
+		/// This is an alias for close().
 		///
 		/// \sa close()
 		void 				closeMovie();
-		/// \brief Closes the movie file and de-allocates its resources.
+		/// \brief Closes the movie file releases its resources.
 		///
-		/// This is an alias of closeMovie()
+		/// This is an alias for closeMovie().
 		///
 		/// \sa closeMovie()
 		void 				close();
 
-		/// \brief Update the video player's internal state to continue
-		/// playback.
+		/// \brief Update the video player's internal state to continue playback.
 		///
 		/// If normal video playback is desired, this method is usually called
 		/// once per animation frame inside of ofApp::update().
@@ -101,11 +100,11 @@ class ofVideoPlayer : public ofBaseVideoPlayer,public ofBaseVideoDraws{
 		bool				getIsMovieDone() const;
 
 		void 				setPosition(float pct);
-		void 				setVolume(float volume); // 0..1
+		void 				setVolume(float volume);
 		void 				setLoopState(ofLoopType state);
 		ofLoopType			getLoopState() const;
 		void   				setSpeed(float speed);
-		void				setFrame(int frame);  // frame 0 = first frame...
+		void				setFrame(int frame);
 
 		void 				setUseTexture(bool bUse);
 		bool 				isUsingTexture() const;
@@ -134,11 +133,9 @@ class ofVideoPlayer : public ofBaseVideoPlayer,public ofBaseVideoDraws{
 		/// \sa ofTexture::unbind()
 		void 				unbind() const;
 
-		//the anchor is the point the image is drawn around.
-		//this can be useful if you want to rotate an image around a particular point.
-        void				setAnchorPercent(float xPct, float yPct);	//set the anchor as a percentage of the image width/height ( 0.0-1.0 range )
-        void				setAnchorPoint(float x, float y);				//set the anchor point in pixels
-        void				resetAnchor();								//resets the anchor to (0, 0)
+        void				setAnchorPercent(float xPct, float yPct);
+        void				setAnchorPoint(float x, float y);
+        void				resetAnchor();
 
 		void 				setPaused(bool bPause);
 
@@ -157,66 +154,76 @@ class ofVideoPlayer : public ofBaseVideoPlayer,public ofBaseVideoDraws{
 		bool				isPlaying() const;
 		bool 				isInitialized() const;
 
-		/// \brief Explicitly set the video player to use for playback.
+		/// \brief Set the internal video player implementation.
 		///
-		/// newPlayer must extend from ofBaseVideoPlayer.
+		/// Advanced users may find it useful to set a custom internal video
+		/// player implementation. The custom video player must implment the
+		/// ofBaseVideoPlayer interface.
 		///
 		/// \param newPlayer Shared pointer to the new video player that extends
 		/// from ofBaseVideoPlayer.
 		void				setPlayer(shared_ptr<ofBaseVideoPlayer> newPlayer);
-		/// \brief Get a shared pointer to the video player currently being used
-		/// for playback.
-		/// \returns A shared pointer to the video player currently being used
-		/// for playback.
+		/// \brief Get a pointer to the internal video player implementation.
+		///
+		/// This returns a pointer to the ofBaseVideoPlayer interface. For
+		/// implementation-specfic features, this can be cast to the subtype
+		/// using dynamic_cast<MyVideoPlayerImplementation>(getPlayer()) or the
+		/// templated getPlayer<MyVideoPlayerImplementation>() method.
+		///
+		/// \returns A pointer to the internal video player implementation.
 		shared_ptr<ofBaseVideoPlayer>	getPlayer();
-		/// \brief Get a const shared pointer to the video player currently
-		/// being used.
-		/// \returns A const shared pointer to the video player currently
-		/// being used for playback.
+		/// \brief Get a const pointer to the internal video player implementation.
+		///
+		/// This returns a pointer to the ofBaseVideoPlayer interface. For
+		/// implementation-specfic features, this can be cast to the subtype
+		/// using dynamic_pointer_cast<MyVideoPlayerImplementation>(getPlayer())
+		/// or the templated getPlayer<MyVideoPlayerImplementation>() method.
+		///
+		/// \returns A const pointer to the internal video player implementation.
 		const shared_ptr<ofBaseVideoPlayer>	getPlayer() const;
 
-		/// \brief Get a shared pointer to the video player currently being used
-		/// for playback.
+		/// \brief Get a pointer to the internal video player implementation.
 		///
-		/// This is a templated version of ofVideoPlayer::getPlayer()
-		/// that dynamically casts the shared player pointer to the PlayerType
-		/// template type.
+		/// Calling getPlayer<MyVideoPlayerImplementation>() is equivalent to
+		/// dynamic_pointer_cast<MyVideoPlayerImplementation>(getPlayer()).
 		///
-		/// \returns A shared pointer to the video player currently being used
-		/// for playback that is dynamically casted to the PlayerType template
-		/// type.
+		/// \returns A pointer to the internal video player implementation or
+		///			 nullptr if the cast fails.
 		template<typename PlayerType>
 		shared_ptr<PlayerType> getPlayer(){
 			return dynamic_pointer_cast<PlayerType>(getPlayer());
 		}
 
-		/// \brief Get a const shared pointer to the video player currently
-		/// being used for playback.
+		/// \brief Get a const pointer to the internal video player implementation.
 		///
-		/// This is a templated version of ofVideoPlayer::getPlayer()
-		/// that dynamically casts the shared player pointer to the PlayerType
-		/// template type.
+		/// Calling getPlayer<MyVideoPlayerImplementation>() is equivalent to
+		/// dynamic_pointer_cast<MyVideoPlayerImplementation>(getPlayer()).
 		///
-		/// \returns A const shared pointer to the video player currently being
-		/// used for playback that is dynamically casted to the PlayerType
-		/// template type.
+		/// \returns A const pointer to the internal video player implementation
+		///			 or nullptr if the cast fails.
 		template<typename PlayerType>
 		const shared_ptr<PlayerType> getPlayer() const{
 			return dynamic_pointer_cast<PlayerType>(getPlayer());
 		}
 
 	private:
+		/// \brief Initialize the default player implementations.
 		void initDefaultPlayer();
+		/// \brief A pointer to the internal video player implementation.
 		shared_ptr<ofBaseVideoPlayer>		player;
-		
+		/// \brief A collection of texture planes used by the video player.
 		vector<ofTexture> tex;
-		ofTexture * playerTex; // a seperate texture that may be optionally implemented by the player to avoid excessive pixel copying.
+		/// \brief A pointer to the internal player's texture if available.
+		///
+		/// Video players that implement ofBaseVideoPlayer::getTexturePtr()
+		/// can provide a pointer to an internal texture. When possible,
+		/// ofVideoPlayer will use the internal texture to avoid extra pixel
+		/// copies.
+		ofTexture * playerTex;
+		/// \brief True if the video player is using a texture.
 		bool bUseTexture;
+		/// \brief The internal pixel format.
 		mutable ofPixelFormat internalPixelFormat;
+		/// \brief The stored path to the video's path.
 	    string moviePath;
 };
-
-
-
-
-
