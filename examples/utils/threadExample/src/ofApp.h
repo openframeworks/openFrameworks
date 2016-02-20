4a990532ab41aa6ddbd99590b61fad0aa7a4e075
random <random@frauzufall.de>
2016-02-20 01:12:00 +0100

    threadExample: adds keyboard shortcuts to set or remove lock, adds README

--- examples/utils/threadExample/src/ofApp.h
+++ examples/utils/threadExample/src/ofApp.h
@@ -1,3 +1,5 @@
+/* Refer to the README.md in the example's root folder for more information on usage */
+
 #pragma once
 
 
@@ -9,24 +11,24 @@ class ofApp: public ofBaseApp
 {
 public:
 
-    void setup();
-    void update();
-    void draw();
+	void setup();
+	void update();
+	void draw();
 
-    void keyPressed  (int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void mouseEntered(int x, int y);
-    void mouseExited(int x, int y);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
+	void keyPressed  (int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
 
-    // Our threaded object.
-    ThreadedObject threadedObject;
+	// Our threaded object.
+	ThreadedObject threadedObject;
 	bool doLock;
 
 };
