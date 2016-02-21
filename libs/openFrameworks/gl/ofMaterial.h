68545c0da35a40d6baa3008070acd856571d86f7
danomatika <danomatika@gmail.com>
2016-02-21 16:12:31 -0700

    updated doxy commenting to match style guide

--- libs/openFrameworks/gl/ofMaterial.h
+++ libs/openFrameworks/gl/ofMaterial.h
@@ -12,27 +12,39 @@
 //   * Ogre3D: http://www.ogre3d.org/docs/manual/manual_11.html#SEC14
 //   * assim material: http://assimp.sourceforge.net/lib_html/ai_material_8h.html#7dd415ff703a2cc53d1c22ddbbd7dde0
 
+/// \class ofMaterial
+/// \brief material parameter properties that can be applied to vertices in the OpenGL lighting model
+/// used in determining both the intensity and color of reflected light based on the lighting model in use
+/// and if the vertices are on a front or back sided face
 class ofMaterial: public ofBaseMaterial {
 public:
 	ofMaterial();
 	virtual ~ofMaterial(){};
 	
-	/// set all material colors: reflectance type & light intensity
+	/// \brief set all material colors: reflectance type & light intensity
+	/// \param oDiffuse the diffuse reflectance
+	/// \param oAmbient the ambient reflectance
+	/// \param oSpecular the specular reflectance
+	/// \param oEmmissive the emitted light intensity
 	void setColors(ofFloatColor oDiffuse, ofFloatColor oAmbient, ofFloatColor oSpecular, ofFloatColor emissive);
 	
-	/// set the diffuse reflectance
+	/// \brief set the diffuse reflectance
+	/// \param oDiffuse the diffuse reflectance
 	void setDiffuseColor(ofFloatColor oDiffuse);
 	
-	/// set the ambient reflectance
+	/// \brief set the ambient reflectance
+	/// \param oAmbient the ambient reflectance
 	void setAmbientColor(ofFloatColor oAmbient);
 	
-	/// set the specular reflectance
+	/// \brief set the specular reflectance
+	/// \param oSpecular the specular reflectance
 	void setSpecularColor(ofFloatColor oSpecular);
 	
-	/// set the emitted light intensity
+	/// \brief set the emitted light intensity
+	/// \param oEmmissive the emitted light intensity
 	void setEmissiveColor(ofFloatColor oEmmisive);
 	
-	/// set the specular exponent
+	/// \brief set the specular exponent
 	void setShininess(float nShininess);
 
 	// documented in ofBaseMaterial
@@ -42,7 +54,8 @@ public:
 	ofFloatColor getEmissiveColor() const;
 	float getShininess() const;
 	
-	/// data wrapper for material color properties
+	/// \class Data
+	/// \brief wrapper for material color properties
     struct Data{
 		Data();
         ofFloatColor diffuse; ///< diffuse reflectance
@@ -52,10 +65,10 @@ public:
         float shininess; //< specular exponent
     };
 	
-	/// \returns material color properties data struct
+	/// \return material color properties data struct
     Data getData() const;
 	
-	/// sets the material color properties data struct
+	/// \brief set the material color properties data struct
 	void setData(const ofMaterial::Data& data);
 	
 	// documented in ofBaseMaterial
