98ce010f751d3fec4b99c9e424fcc527f3f91213
Reed Tothong <litchirhythm@gmail.com>
2016-02-23 10:37:05 -0500

    handfix indentation on github

--- examples/communication/oscSenderExample/src/ofApp.cpp
+++ examples/communication/oscSenderExample/src/ofApp.cpp
@@ -4,38 +4,37 @@
 void ofApp::setup(){
 	// open an outgoing connection to HOST:PORT
 	sender.setup(HOST, PORT);
-    imgAsBuffer = ofBufferFromFile("of-logo.png", true);
+    	imgAsBuffer = ofBufferFromFile("of-logo.png", true);
     
-    ofSetWindowTitle("osc sender");
+    	ofSetWindowTitle("osc sender");
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    
-    ofxOscMessage m;
-    m.setAddress("/mouse/position");
-    m.addFloatArg(ofMap(ofGetMouseX(), 0, ofGetWidth(), 0.f, 1.f, true));
-    m.addFloatArg(ofMap(ofGetMouseY(), 0, ofGetHeight(), 0.f, 1.f, true));
-    sender.sendMessage(m, false);
+    	ofxOscMessage m;
+    	m.setAddress("/mouse/position");
+    	m.addFloatArg(ofMap(ofGetMouseX(), 0, ofGetWidth(), 0.f, 1.f, true));
+    	m.addFloatArg(ofMap(ofGetMouseY(), 0, ofGetHeight(), 0.f, 1.f, true));
+    	sender.sendMessage(m, false);
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofBackgroundGradient(255, 100);
+    	ofBackgroundGradient(255, 100);
     
-    if(img.getWidth() > 0){
-        ofSetColor(255);
-        img.draw(ofGetWidth()/2-img.getWidth()/2,
-                 ofGetHeight()/2-img.getHeight()/2);
-    }
+    	if(img.getWidth() > 0){
+        	ofSetColor(255);
+        	img.draw(ofGetWidth()/2-img.getWidth()/2,
+                 	ofGetHeight()/2-img.getHeight()/2);
+    	}
     
-    // display instructions
-    string buf = "sending osc messages to : " + string(HOST);
-    buf += " : " + ofToString(PORT);
-    buf += "\npress A to send osc message [/test 1 3.5 hello <time>]\n";
-    buf += "press I to send a (small) image as a osc blob to [/image]";
-    ofDrawBitmapStringHighlight(buf, 10, 20);
+    	// display instructions
+    	string buf = "sending osc messages to : " + string(HOST);
+    	buf += " : " + ofToString(PORT);
+    	buf += "\npress A to send osc message [/test 1 3.5 hello <time>]\n";
+    	buf += "press I to send a (small) image as a osc blob to [/image]";
+    	ofDrawBitmapStringHighlight(buf, 10, 20);
 }
 
 //--------------------------------------------------------------
@@ -50,17 +49,17 @@ void ofApp::keyPressed(int key){
 		sender.sendMessage(m, false);
 	}
     
-    //send an image. (Note: the size of the image depends greatly on your network buffer sizes - if an image is too big the message won't come through )
+    	//send an image. (Note: the size of the image depends greatly on your network buffer sizes - if an image is too big the message won't come through )
 
-    if( key == 'i' || key == 'I'){
-        img.load(imgAsBuffer);
+    	if( key == 'i' || key == 'I'){
+        	img.load(imgAsBuffer);
 
-        ofxOscMessage m;
-        m.setAddress("/image");
-        m.addBlobArg(imgAsBuffer);
-        sender.sendMessage(m);
-        cout << "ofApp:: sending image with size: " << imgAsBuffer.size() << endl;
-    }
+        	ofxOscMessage m;
+        	m.setAddress("/image");
+        	m.addBlobArg(imgAsBuffer);
+        	sender.sendMessage(m);
+        	cout << "ofApp:: sending image with size: " << imgAsBuffer.size() << endl;
+    	}
 }
 
 //--------------------------------------------------------------
@@ -80,20 +79,20 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    ofxOscMessage m;
-    m.setAddress("/mouse/button");
-    m.addIntArg(1);
-    m.addStringArg("down");
-    sender.sendMessage(m, false);
+    	ofxOscMessage m;
+    	m.setAddress("/mouse/button");
+    	m.addIntArg(1);
+    	m.addStringArg("down");
+    	sender.sendMessage(m, false);
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-    ofxOscMessage m;
-    m.setAddress("/mouse/button");
-    m.addIntArg(0);
-    m.addStringArg("up");
-    sender.sendMessage(m, false);
+    	ofxOscMessage m;
+    	m.setAddress("/mouse/button");
+    	m.addIntArg(0);
+    	m.addStringArg("up");
+    	sender.sendMessage(m, false);
 }
 
 //--------------------------------------------------------------
