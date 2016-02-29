2ac0c6b8ab99129d21bcd8ecdea4de973ad51fb5
Christopher Baker <me@christopherbaker.net>
2016-02-29 14:56:23 -0600

    Broke templated ofIsFloatEqual into two functions for float and double.

--- libs/openFrameworks/math/ofMath.h
+++ libs/openFrameworks/math/ofMath.h
@@ -534,20 +534,40 @@ int ofSign(float n);
 /// Never use `==` to compare two floating point numbers.
 ///
 /// This solution is not completely symmetric, meaning it
-/// is possible for `ofIsEqual(x,y) != ofIsEqual(y,x)`. From a practical
+/// is possible for `ofIsEqual(x, y) != ofIsEqual(y, x)`. From a practical
 /// standpoint, this does not usually occur when the magnitudes of x and y are
 /// significantly larger than epsilon, but your mileage may vary.
 ///
 /// \sa https://isocpp.org/wiki/faq/newbie#floating-point-arith
 /// \sa https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
-/// \tparam The floating point data type.
 /// \param a The first floating point type variable to compare.
 /// \param b The second floating point type variable to compare.
 /// \returns True if `std::abs(x - y) <= std::numeric_limits<Type>::epsilon() * std::abs(x)`.
-template<typename Type>
-typename std::enable_if<std::is_floating_point<Type>::value, bool>::type ofIsFloatEqual(const Type& a, const Type& b)
+double ofIsFloatEqual(double a, double b)
 {
-	return std::abs(a - b) <= std::numeric_limits<Type>::epsilon() * std::abs(a);
+	return std::abs(a - b) <= std::numeric_limits<double>::epsilon() * std::abs(a);
+}
+
+/// \brief Compare two floating point types for equality.
+///
+/// From C++ FAQ:
+///
+/// Floating point arithmetic is different from real number arithmetic.
+/// Never use `==` to compare two floating point numbers.
+///
+/// This solution is not completely symmetric, meaning it
+/// is possible for `ofIsEqual(x, y) != ofIsEqual(y, x)`. From a practical
+/// standpoint, this does not usually occur when the magnitudes of x and y are
+/// significantly larger than epsilon, but your mileage may vary.
+///
+/// \sa https://isocpp.org/wiki/faq/newbie#floating-point-arith
+/// \sa https://docs.oracle.com/cd/E19957-01/806-3568/ncg_goldberg.html
+/// \param a The first floating point type variable to compare.
+/// \param b The second floating point type variable to compare.
+/// \returns True if `std::abs(x - y) <= std::numeric_limits<Type>::epsilon() * std::abs(x)`.
+double ofIsFloatEqual(float a, float b)
+{
+	return std::abs(a - b) <= std::numeric_limits<float>::epsilon() * std::abs(a);
 }
 
 
