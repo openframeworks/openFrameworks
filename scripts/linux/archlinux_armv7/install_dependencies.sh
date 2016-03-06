cc41dbdb3501436543cda5130f69bdba616df425
arturo castro <arturo@openframeworks.cc>
2016-03-06 19:15:05 +0100

    linux armv7 fixes for archlinux

---
+++ scripts/linux/archlinux_armv7/install_dependencies.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ $EUID != 0 ]; then
+	echo "this script must be run as root"
+	echo ""
+	echo "usage:"
+	echo "su -"
+	echo "./install_dependencies.sh"
+	exit $exit_code
+   exit 1
+fi
+
+pacman -Sy --needed make pkg-config gcc openal glew freeglut freeimage gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-libav raspberrypi-firmware gst-omx-rpi assimp boost libxcursor opencv assimp
+
+wget http://ci.openframeworks.cc/rtaudio-armv7hf.tar.bz2
+tar xjf rtaudio-armv7hf.tar.bz2 -C /
+
+exit_code=$?
+if [ $exit_code != 0 ]; then
+	echo "error installing packages, there could be an error with your internet connection"
+	exit $exit_code
+fi
+
