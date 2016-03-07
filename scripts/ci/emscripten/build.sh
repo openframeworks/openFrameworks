95251ef19eb9c331be55f1cac0fcd8ee3cb7434c
arturo castro <arturo@openframeworks.cc>
2016-03-07 19:53:49 +0100

    emscripten tests

---
+++ scripts/ci/emscripten/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ev
+ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
+# Add compiler flag to reduce memory usage to enable builds to complete
+# see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56746#c7
+# the "proper" way does not work currently:
+#export CXXFLAGS="$(CXXFLAGS) --param ftrack-macro-expansion=0"
+CUSTOMFLAGS="-ftrack-macro-expansion=0"
+source ~/emscripten-sdk/emsdk_env.sh
+
+echo "**** Building OF core ****"
+cd $ROOT
+# this carries over to subsequent compilations of examples
+echo "PLATFORM_CFLAGS += $CUSTOMFLAGS" >> libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
+sed -i "s/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = .*/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = -g0/" libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
+cd libs/openFrameworksCompiled/project
+emmake make Debug
+
+echo "**** Building emptyExample ****"
+cd $ROOT/scripts/templates/linux64
+emmake make Debug
+
+echo "**** Building allAddonsExample ****"
+cd $ROOT
+cp scripts/templates/linux64/Makefile examples/templates/allAddonsExample/
+cp scripts/templates/linux64/config.make examples/templates/allAddonsExample/
+cd examples/templates/allAddonsExample/
+emmake make Debug
---
+++ scripts/ci/emscripten/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ev
+
+cd ~/
+git clone https://github.com/urho3d/emscripten-sdk
