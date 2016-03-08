9b2bb875650aa94acdb6d09b3e69cf4028d44b6e
arturo castro <arturo@openframeworks.cc>
2016-03-08 16:03:33 +0100

    emscripten tests: trying to make emscripten sdk work

--- scripts/ci/emscripten/install.sh
+++ scripts/ci/emscripten/install.sh
@@ -5,3 +5,5 @@ cp scripts/ci/emscripten/.emscripten ~/
 sed -i "s|%HOME%|${HOME}|g" ~/.emscripten
 cd ~/
 git clone https://github.com/urho3d/emscripten-sdk
+cd emscripten-sdk
+./emsdk activate latest
