7a7d52279df77bc0a63021b3a1dff9ea14821007
ofTheo <theo@openframeworks.cc>
2016-03-16 17:22:17 -0400

    Bugfix for 32bit FreeImage lib for OS X missing symbols. Closes #4993

--- scripts/apothecary/formulas/FreeImage/FreeImage.sh
+++ scripts/apothecary/formulas/FreeImage/FreeImage.sh
@@ -26,7 +26,7 @@ function download() {
 		rm FreeImage"$VER"Win32Win64.zip
 	elif [[ "${TYPE}" == "osx" || "${TYPE}" == "ios" || "${TYPE}" == "tvos" ]]; then
         # Fixed issues for OSX / iOS for FreeImage compiling in git repo.
-        echo "Downloading from $GIT_URL for OSX/iOS"
+        echo "Downloading from $GIT_URL $GIT_TAG for OSX/iOS"
 		echo $GIT_URL
 		curl -Lk $GIT_URL/archive/$GIT_TAG.tar.gz -o FreeImage-$GIT_TAG.tar.gz
 		tar -xf FreeImage-$GIT_TAG.tar.gz
