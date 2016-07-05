3d555a519ad7a2603fd5142a7b403e6c077b6cd7
arturo castro <arturo@openframeworks.cc>
2016-07-05 14:57:56 +0200

    ofPoint: back to alias for ofVec3f and include ofVec3f.h

--- libs/openFrameworks/types/ofPoint.h
+++ libs/openFrameworks/types/ofPoint.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "ofConstants.h"
+#include "ofVec3f.h"
 
 /// \file 
 /// ofPoint is a typedef (alias) of ofVec3f
 
 /// \brief Look at ofVec3f for documentation
-typedef glm::vec3 ofPoint;
+typedef ofVec3f ofPoint;
