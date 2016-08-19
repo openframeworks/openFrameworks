62fe32e37fe2d42c6cfe6e107ef465f726f86f64
arturo castro <arturo@openframeworks.cc>
2016-08-19 17:42:42 +0200

    ios: fix glm port

--- addons/ofxiOS/src/core/ofxiOSEAGLView.mm
+++ addons/ofxiOS/src/core/ofxiOSEAGLView.mm
@@ -149,14 +149,14 @@ static ofxiOSEAGLView * _instanceRef = nil;
 }
 
 - (void)updateDimensions {
-    windowPos->set(self.frame.origin.x * scaleFactor, self.frame.origin.y * scaleFactor, 0);
-    windowSize->set(self.bounds.size.width * scaleFactor, self.bounds.size.height * scaleFactor, 0);
+    *windowPos = glm::vec2(self.frame.origin.x * scaleFactor, self.frame.origin.y * scaleFactor, 0);
+    *windowSize = glm::vec2(self.bounds.size.width * scaleFactor, self.bounds.size.height * scaleFactor, 0);
     
     UIScreen * currentScreen = self.window.screen;  // current screen is the screen that GLView is attached to.
     if(!currentScreen) {                            // if GLView is not attached, assume to be main device screen.
         currentScreen = [UIScreen mainScreen];
     }
-    screenSize->set(currentScreen.bounds.size.width * scaleFactor, currentScreen.bounds.size.height * scaleFactor, 0);
+    *screenSize = glm::vec2(currentScreen.bounds.size.width * scaleFactor, currentScreen.bounds.size.height * scaleFactor, 0);
 }
 
 - (void)notifyResized {
