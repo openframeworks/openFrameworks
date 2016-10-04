3d753773174c88a910a89e4812610f39395bf2be
arturo castro <arturo@openframeworks.cc>
2016-10-04 13:24:50 +0200

    msys2 install_dependencies fixes

--- scripts/msys2/install_dependencies.sh
+++ scripts/msys2/install_dependencies.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function usage {
-	echo usage: 
+	echo usage:
     echo ./install_dependencies.sh [--help] [--noconfirm]
     echo --help:
     echo display this message
-    echo 
+    echo
     echo --noconfirm:
     echo install packages without user confirmation
 }
@@ -35,27 +35,51 @@ if [ "$NOT_HAS_PATH" -ne "0" ]; then
 fi
 
 arch=i686
-pacman -S $confirm --needed mingw-w64-$arch-harfbuzz
-pacman -S $confirm --needed ca-certificates 
-if [ ! -z ${APPVEYOR+x} ]; then
-    pacman -S $confirm --needed make 
-    pacman -S $confirm --needed mingw-w64-$arch-gcc 
+if [ -z ${confirm+x} ]; then
+	pacman -S $confirm --needed ca-certificates
+	if [ ! -z ${APPVEYOR+x} ]; then
+		pacman -S $confirm --needed make mingw-w64-$arch-gcc
+	fi
+	pacman -S $confirm --needed mingw-w64-$arch-glew \
+		mingw-w64-$arch-freeglut \
+		mingw-w64-$arch-FreeImage \
+		mingw-w64-$arch-opencv \
+		mingw-w64-$arch-assimp \
+		mingw-w64-$arch-boost \
+		mingw-w64-$arch-cairo \
+		mingw-w64-$arch-clang \
+		mingw-w64-$arch-gdb \
+		mingw-w64-$arch-zlib \
+		mingw-w64-$arch-tools \
+		mingw-w64-$arch-pkg-config \
+		mingw-w64-$arch-poco \
+		mingw-w64-$arch-glfw \
+		mingw-w64-$arch-libusb \
+		mingw-w64-$arch-harfbuzz
+else
+	pacman -S $confirm --needed mingw-w64-$arch-harfbuzz
+	pacman -S $confirm --needed ca-certificates
+	if [ ! -z ${APPVEYOR+x} ]; then
+		pacman -S $confirm --needed make 
+		pacman -S $confirm --needed mingw-w64-$arch-gcc
+	fi
+	pacman -S $confirm --needed mingw-w64-$arch-glew
+	pacman -S $confirm --needed mingw-w64-$arch-freeglut
+	pacman -S $confirm --needed mingw-w64-$arch-FreeImage
+	pacman -S $confirm --needed mingw-w64-$arch-opencv
+	pacman -S $confirm --needed mingw-w64-$arch-assimp
+	pacman -S $confirm --needed mingw-w64-$arch-boost
+	pacman -S $confirm --needed mingw-w64-$arch-cairo
+	pacman -S $confirm --needed mingw-w64-$arch-clang
+	pacman -S $confirm --needed mingw-w64-$arch-gdb
+	pacman -S $confirm --needed mingw-w64-$arch-zlib
+	pacman -S $confirm --needed mingw-w64-$arch-tools
+	pacman -S $confirm --needed mingw-w64-$arch-pkg-config
+	pacman -S $confirm --needed mingw-w64-$arch-poco
+	pacman -S $confirm --needed mingw-w64-$arch-glfw
+	pacman -S $confirm --needed mingw-w64-$arch-libusb
 fi
-pacman -S $confirm --needed mingw-w64-$arch-glew 
-pacman -S $confirm --needed mingw-w64-$arch-freeglut 
-pacman -S $confirm --needed mingw-w64-$arch-FreeImage 
-pacman -S $confirm --needed mingw-w64-$arch-opencv 
-pacman -S $confirm --needed mingw-w64-$arch-assimp 
-pacman -S $confirm --needed mingw-w64-$arch-boost 
-pacman -S $confirm --needed mingw-w64-$arch-cairo 
-pacman -S $confirm --needed mingw-w64-$arch-clang 
-pacman -S $confirm --needed mingw-w64-$arch-gdb 
-pacman -S $confirm --needed mingw-w64-$arch-zlib  
-pacman -S $confirm --needed mingw-w64-$arch-tools 
-pacman -S $confirm --needed mingw-w64-$arch-pkg-config 
-pacman -S $confirm --needed mingw-w64-$arch-poco 
-pacman -S $confirm --needed mingw-w64-$arch-glfw
-pacman -S $confirm --needed mingw-w64-$arch-libusb
+
 
 # this would install gstreamer which can be used in mingw too
 #pacman -Sy mingw-w64-$arch-gst-libav mingw-w64-$arch-gst-plugins-bad mingw-w64-$arch-gst-plugins-base mingw-w64-$arch-gst-plugins-good mingw-w64-$arch-gst-plugins-ugly mingw-w64-$arch-gstreamer
