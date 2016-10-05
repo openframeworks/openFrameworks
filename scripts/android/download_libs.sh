9c5400ec612172a5ad675f5f697a3449a505e207
arturo castro <arturo@openframeworks.cc>
2016-10-05 21:21:49 +0200

    fix download_libs scripts

--- scripts/android/download_libs.sh
+++ scripts/android/download_libs.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-../dev/download_libs.sh $1 android armv7
-../dev/download_libs.sh $1 android x86
+if [ $# -gt 1 ]; then
+    version=$1
+else
+    version=master
+fi
+../dev/download_libs.sh $version android armv7
+../dev/download_libs.sh $version android x86
