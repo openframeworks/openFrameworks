6619f5afca095519c77cafbb094ec3a31201e387
arturo castro <arturo@openframeworks.cc>
2016-10-06 10:48:46 +0200

    download_libs: allow to not overwrite for multi downloads

--- scripts/android/download_libs.sh
+++ scripts/android/download_libs.sh
@@ -4,5 +4,7 @@ if [ $# -gt 1 ]; then
 else
     version=master
 fi
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
 ../dev/download_libs.sh $version android armv7
-../dev/download_libs.sh $version android x86
+../dev/download_libs.sh $version android x86 0
