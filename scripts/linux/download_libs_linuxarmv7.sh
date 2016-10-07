e215ca0d8ca7b5e6a0eff75b635a97064c6b5b60
arturo castro <arturo@openframeworks.cc>
2016-10-07 16:30:01 +0200

    merge linux download libs in one script that detects platform

--- scripts/linux/download_libs_linuxarmv7.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ $# -gt 1 ]; then
-    version=$1
-else
-    version=master
-fi
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR
-../dev/download_libs.sh $version linux armv7l
