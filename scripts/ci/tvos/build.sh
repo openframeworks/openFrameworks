c5a6e815189dde537ee066e902aa90dfa53624e4
arturo castro <arturo@openframeworks.cc>
2016-10-08 15:38:39 +0200

    ci: build ios and tvos for device target

--- scripts/ci/tvos/build.sh
+++ scripts/ci/tvos/build.sh
@@ -2,5 +2,4 @@
 set -ev
 echo "Building openFrameworks - tvOS Template Project"
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
-xcodebuild -project "$ROOT/scripts/templates/tvos/emptyExample.xcodeproj"  -target emptyExample -sdk appletvsimulator ONLY_ACTIVE_ARCH=NO
-
+xcodebuild -project "$ROOT/scripts/templates/tvos/emptyExample.xcodeproj"  -target emptyExample -sdk appletvos ONLY_ACTIVE_ARCH=NO
