0672392a5ab3076da4dc3523ca1d385bfffc90c7
arturo castro <arturo@openframeworks.cc>
2016-10-12 17:32:44 +0200

    osx makefiles: fix copy dylib rule

--- libs/openFrameworksCompiled/project/osx/config.osx.default.mk
+++ libs/openFrameworksCompiled/project/osx/config.osx.default.mk
@@ -388,7 +388,7 @@ afterplatform: $(TARGET_NAME)
 	@mv $(TARGET) bin/$(BIN_NAME).app/Contents/MacOS
 
 ifneq ($(USE_FMOD),0)
-	@cp $(OF_LIBS_PATH)/*/lib/$(PLATFORM_LIB_SUBPATH)/*.$(SHARED_LIB_EXTENSION) bin/$(BIN_NAME).app/Contents/MacOS; fi
+	@cp $(OF_LIBS_PATH)/*/lib/$(PLATFORM_LIB_SUBPATH)/*.$(SHARED_LIB_EXTENSION) bin/$(BIN_NAME).app/Contents/MacOS;
 endif
 
 	@echo
