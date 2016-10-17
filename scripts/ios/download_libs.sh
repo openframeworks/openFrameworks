92b7d3de53c5062f79fc8483117b84d7f7f4d6bd
arturo castro <arturo@openframeworks.cc>
2016-10-17 22:26:27 +0200

    ios download libs: remove ls openssl

--- scripts/ios/download_libs.sh
+++ scripts/ios/download_libs.sh
@@ -3,4 +3,3 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 ../dev/download_libs.sh -p tvos $@
 ../dev/download_libs.sh -p ios -n $@
-ls ../../libs/openssl/include/openssl
