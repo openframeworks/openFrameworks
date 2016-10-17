4d9696af57c7344e1394f04a388a34137f10bf32
arturo castro <arturo@openframeworks.cc>
2016-10-17 15:08:00 +0200

    vs install script: call PG using env var for OF path

--- scripts/ci/vs/install.sh
+++ scripts/ci/vs/install.sh
@@ -5,5 +5,5 @@ wget http://ci.openframeworks.cc/projectGenerator/projectGenerator-vs.zip 2> /de
 unzip projectGenerator-vs.zip 2> /dev/null
 rm projectGenerator-vs.zip
 cd $OF_ROOT
-~/projectGenerator/projectGenerator.exe -o . examples/templates/emptyExample
-~/projectGenerator/projectGenerator.exe -o . examples/templates/allAddonsExample
+PG_OF_PATH=$OF_ROOT ~/projectGenerator/projectGenerator.exe examples/templates/emptyExample
+PG_OF_PATH=$OF_ROOT ~/projectGenerator/projectGenerator.exe examples/templates/allAddonsExample
