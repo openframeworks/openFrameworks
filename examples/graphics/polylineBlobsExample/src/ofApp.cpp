b6e52212d5c175d3b3ad6cf0a2fe830efa1396a2
Andrew Goodney <agoodney@yahoo.com>
2016-12-10 11:44:33 -0800

    fix call to .addVerticies()

--- examples/graphics/polylineBlobsExample/src/ofApp.cpp
+++ examples/graphics/polylineBlobsExample/src/ofApp.cpp
@@ -39,7 +39,8 @@ void ofApp::update() {
 		for(unsigned int i = 0; i < contourFinder.blobs.size(); i++) {
 			ofPolyline cur;
 			// add all the current vertices to cur polyline
-			cur.addVertices(contourFinder.blobs[i].pts);
+            
+			cur.addVertices({contourFinder.blobs[i].pts[0],contourFinder.blobs[i].pts[1],contourFinder.blobs[i].pts[2]});
 			cur.setClosed(true);
 
 			// add the cur polyline to all these vector<ofPolyline>
