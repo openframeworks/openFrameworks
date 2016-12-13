0f0c62ad2a7898b05daddc64da19be69f20f9e26
arturo castro <arturo@openframeworks.cc>
2016-12-13 20:36:19 +0100

    travis: fix ssh config copy

--- scripts/ci/upload_of_lib.sh
+++ scripts/ci/upload_of_lib.sh
@@ -3,7 +3,7 @@ OF_ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 CI_ROOT=$OF_ROOT/scripts/ci
 if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/openFrameworks/master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     openssl aes-256-cbc -K $encrypted_cd38768cbb9d_key -iv $encrypted_cd38768cbb9d_iv -in $CI_ROOT/id_rsa.enc -out $CI_ROOT/id_rsa -d
-    cp ssh_config ~/.ssh/config
+    cp $CI_ROOT/ssh_config ~/.ssh/config
     chmod 600 $CI_ROOT/id_rsa
     if [ "$TARGET"=="android" ]; then
         scp -i $CI_ROOT/id_rsa $OF_ROOT/libs/openFrameworksCompiled/lib/$TARGET/armv7/libopenFrameworksDebug.a tests@ci.openframeworks.cc:openFrameworks_libs/armv7/$TARGET
@@ -14,4 +14,4 @@ if [ "${TRAVIS_REPO_SLUG}/${TRAVIS_BRANCH}" = "openframeworks/openFrameworks/mas
         scp -i $CI_ROOT/id_rsa $OF_ROOT/libs/openFrameworksCompiled/lib/$TARGET/*/libopenFrameworksDebug.a tests@ci.openframeworks.cc:openFrameworks_libs/$TARGET/
     fi
 fi
-rm -rf scripts/id_rsa
+rm -rf $CI_ROOT/id_rsa
