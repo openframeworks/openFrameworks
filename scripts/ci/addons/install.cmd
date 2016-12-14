e13c58b48d3fae66b0d222a58401dc0f4f455e23
arturo castro <arturo@openframeworks.cc>
2016-12-14 18:07:53 +0100

    fix addons ci install scripts

--- scripts/ci/addons/install.cmd
+++ scripts/ci/addons/install.cmd
@@ -4,7 +4,7 @@ move %APPVEYOR_PROJECT_NAME% openFrameworks\addons\%APPVEYOR_PROJECT_NAME%
 cd openFrameworks
 
 if "%BUILDER%"=="VS" (
-    set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
+    set PATH="C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%"
     set PG_OF_PATH=%APPVEYOR_BUILD_FOLDER%\..\openFrameworks
     %MSYS2_PATH%\usr\bin\bash -lc "scripts/vs/download_libs.sh --silent"
     echo "Downloading projectGenerator from ci server"
