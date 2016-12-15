c104655371e6d0ceb1733221519ee517d6a0b7dd
arturo castro <arturo@openframeworks.cc>
2016-12-15 16:09:33 +0100

    fix addons ci scripts

--- scripts/ci/addons/build.cmd
+++ scripts/ci/addons/build.cmd
@@ -1,5 +1,4 @@
 if "%BUILDER%"=="VS" (
-    set PATH="C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%"
     msbuild libs/openFrameworksCompiled/project/vs/openframeworksLib.vcxproj  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
     for /D %%e in (addons\%APPVEYOR_PROJECT_NAME%\example*) do (
         msbuild %%e/%%~ne.vcxproj  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
