451a5575999ec3d239d76f84d148dbb0036caea7
arturo castro <arturo@openframeworks.cc>
2016-12-15 02:03:43 +0100

    fixing appveyor addon scripts

--- scripts/ci/addons/build.cmd
+++ scripts/ci/addons/build.cmd
@@ -1,12 +1,12 @@
-if "%BUILDER%"=="VS" (
-    msbuild libs/openFrameworksCompiled/project/vs/openframeworksLib.vcxproj  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" &&
-    for /D %%e in (addons\%APPVEYOR_PROJECT_NAME%\example*) do (
+rem if "%BUILDER%"=="VS" (
+rem     msbuild libs/openFrameworksCompiled/project/vs/openframeworksLib.vcxproj  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" &&
+rem     for /D %%e in (addons\%APPVEYOR_PROJECT_NAME%\example*) do (
         msbuild %%e/%%~ne.vcxproj  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-    )
-)
+rem     )
+rem )
   
-if "%BUILDER%"=="MSYS2" (    
-    for /D %%e in (addons\%APPVEYOR_PROJECT_NAME%\example*) do (
-        %MSYS2_PATH%\usr\bin\bash -lc "make -C addons/%APPVEYOR_PROJECT_NAME%/%%~ne Debug"
-    )
-)
+rem if "%BUILDER%"=="MSYS2" (    
+rem     for /D %%e in (addons\%APPVEYOR_PROJECT_NAME%\example*) do (
+rem         %MSYS2_PATH%\usr\bin\bash -lc "make -C addons/%APPVEYOR_PROJECT_NAME%/%%~ne Debug"
+rem     )
+rem )
