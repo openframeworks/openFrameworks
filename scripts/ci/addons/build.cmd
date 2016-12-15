7527e8e3da1c19b856607a15e095e43f3ede5efd
arturo castro <arturo@openframeworks.cc>
2016-12-15 15:33:32 +0100

    addons ci: fix vs path

--- scripts/ci/addons/build.cmd
+++ scripts/ci/addons/build.cmd
@@ -1,8 +1,9 @@
 if "%BUILDER%"=="VS" (
-     msbuild libs/openFrameworksCompiled/project/vs/openframeworksLib.vcxproj  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-     for /D %%e in (addons\%APPVEYOR_PROJECT_NAME%\example*) do (
+    set PATH="C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%"
+    msbuild libs/openFrameworksCompiled/project/vs/openframeworksLib.vcxproj  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+    for /D %%e in (addons\%APPVEYOR_PROJECT_NAME%\example*) do (
         msbuild %%e/%%~ne.vcxproj  /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-     )
+    )
 )
   
 if "%BUILDER%"=="MSYS2" (    
