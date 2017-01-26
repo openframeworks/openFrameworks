openframeworks/openFrameworks

commit ec057361d170bd6b3ca64e54a32e89caaacd4fd5

Substitute remaining spaces in Makefiles with tabs

libs/openFrameworksCompiled/project/makefileCommon/compile.core.mk
@@ -2,31 +2,31 @@
 
 ################################################################################
 ifdef MAKEFILE_DEBUG
-    $(info ===================compile.core.make================================)
+	$(info ===================compile.core.make================================)
 endif
 
 ifdef PLATFORM_CXX
-    CXX = $(PLATFORM_CXX)
+	CXX = $(PLATFORM_CXX)
 endif
 
 ifdef PROJECT_CXX
-    CXX = $(PROJECT_CXX)
+	CXX = $(PROJECT_CXX)
 endif
 
 ifdef PLATFORM_CC
-    CC = $(PLATFORM_CC)
+	CC = $(PLATFORM_CC)
 endif
 
 ifdef PROJECT_CC
-    CC = $(PROJECT_CC)
+	CC = $(PROJECT_CC)
 endif
 
 ifdef PLATFORM_AR
-    AR = $(PLATFORM_AR)
+	AR = $(PLATFORM_AR)
 endif
 
 ifdef PROJECT_AR
-    AR = $(PROJECT_AR)
+	AR = $(PROJECT_AR)
 endif
 
 ifdef PLATFORM_ARFLAGS
@@ -56,13 +56,13 @@ CXXFLAGS += $(OF_CORE_BASE_CXXFLAGS)
 # COMPILER OPTIMIZATIONS and TARGET GENERATION
 ################################################################################
 #
-# $(TARGET_NAME) is the name of the target defined for the use of the 
+# $(TARGET_NAME) is the name of the target defined for the use of the
 # CLEANTARGET target. $(TARGET) is the name of the library that will be created.
 #
-# If TARGET has not already been defined in a platform-based make file, we use 
+# If TARGET has not already been defined in a platform-based make file, we use
 # this selection system to define our TARGET.  Platform specific targets should
 # define the following and respect the "CLEANTARGET system below."
-#  
+#
 #  $(TARGET)
 #  $(TARGET_NAME)
 #  #(OPTIMIZATION_CFLAGS)
@@ -70,55 +70,55 @@ CXXFLAGS += $(OF_CORE_BASE_CXXFLAGS)
 
 # check to see if this is a "pure" clean target
 ifeq ($(findstring clean,$(MAKECMDGOALS)),clean)
-    TARGET =
-    ifdef PLATFORM_CORELIB_DEBUG_TARGET
-    	TARGET += $(PLATFORM_CORELIB_DEBUG_TARGET)
-    else
-    	TARGET += $(OF_CORE_LIB_PATH)/libopenFrameworksDebug.a
-    endif
-    
-    ifdef PLATFORM_CORELIB_RELEASE_TARGET
-    	TARGET += $(PLATFORM_CORELIB_RELEASE_TARGET)
-    else
-    	TARGET += $(OF_CORE_LIB_PATH)/libopenFrameworks.a
-    endif
-    
-    TARGET_NAME = 
+	TARGET =
+	ifdef PLATFORM_CORELIB_DEBUG_TARGET
+		TARGET += $(PLATFORM_CORELIB_DEBUG_TARGET)
+	else
+		TARGET += $(OF_CORE_LIB_PATH)/libopenFrameworksDebug.a
+	endif
+
+	ifdef PLATFORM_CORELIB_RELEASE_TARGET
+		TARGET += $(PLATFORM_CORELIB_RELEASE_TARGET)
+	else
+		TARGET += $(OF_CORE_LIB_PATH)/libopenFrameworks.a
+	endif
+
+	TARGET_NAME =
 
 # check to see if any part of our target includes the String "Debug"
 # this will happen if we call Debug OR CleanDebug
 else ifeq ($(findstring Debug,$(MAKECMDGOALS)),Debug)
-    OPTIMIZATION_CFLAGS = $(PLATFORM_OPTIMIZATION_CFLAGS_DEBUG) -DDEBUG
-    TARGET_NAME = Debug
-    ifdef PLATFORM_CORELIB_DEBUG_TARGET
-    	TARGET = $(PLATFORM_CORELIB_DEBUG_TARGET)
-    else
-    	TARGET = $(OF_CORE_LIB_PATH)/libopenFrameworksDebug.a
-    endif
+	OPTIMIZATION_CFLAGS = $(PLATFORM_OPTIMIZATION_CFLAGS_DEBUG) -DDEBUG
+	TARGET_NAME = Debug
+	ifdef PLATFORM_CORELIB_DEBUG_TARGET
+		TARGET = $(PLATFORM_CORELIB_DEBUG_TARGET)
+	else
+		TARGET = $(OF_CORE_LIB_PATH)/libopenFrameworksDebug.a
+	endif
 
 # check to see if any part of our target includes the String "Release"
 # this will happen if we call Release OR CleanRelease
 else ifeq ($(findstring Release,$(MAKECMDGOALS)),Release)
-    OPTIMIZATION_CFLAGS = $(PLATFORM_OPTIMIZATION_CFLAGS_RELEASE) -DNDEBUG
-    TARGET_NAME = Release
-    ifdef PLATFORM_CORELIB_RELEASE_TARGET
-    	TARGET = $(PLATFORM_CORELIB_RELEASE_TARGET)
-    else
-    	TARGET = $(OF_CORE_LIB_PATH)/libopenFrameworks.a
-    endif
-    
+	OPTIMIZATION_CFLAGS = $(PLATFORM_OPTIMIZATION_CFLAGS_RELEASE) -DNDEBUG
+	TARGET_NAME = Release
+	ifdef PLATFORM_CORELIB_RELEASE_TARGET
+		TARGET = $(PLATFORM_CORELIB_RELEASE_TARGET)
+	else
+		TARGET = $(OF_CORE_LIB_PATH)/libopenFrameworks.a
+	endif
+
 else ifeq ($(MAKECMDGOALS),after)
-    TARGET =
-    TARGET_NAME = 
-    
+	TARGET =
+	TARGET_NAME =
+
 else ## why doesn't make allow for easy logical operators?
-    OPTIMIZATION_CFLAGS = $(PLATFORM_OPTIMIZATION_CFLAGS_RELEASE) -DNDEBUG
-    TARGET_NAME = Release
-    ifdef PLATFORM_CORELIB_RELEASE_TARGET
-    	TARGET += $(PLATFORM_CORELIB_RELEASE_TARGET)
-    else
-    	TARGET += $(OF_CORE_LIB_PATH)/libopenFrameworks.a
-    endif
+	OPTIMIZATION_CFLAGS = $(PLATFORM_OPTIMIZATION_CFLAGS_RELEASE) -DNDEBUG
+	TARGET_NAME = Release
+	ifdef PLATFORM_CORELIB_RELEASE_TARGET
+		TARGET += $(PLATFORM_CORELIB_RELEASE_TARGET)
+	else
+		TARGET += $(OF_CORE_LIB_PATH)/libopenFrameworks.a
+	endif
  endif
 
 # we only get a CLEAN_TARGET if a TARGET_NAME has been defined
@@ -148,10 +148,10 @@ endif
 
 ################################################################################
 # CORE OBJECT AND DEPENDENCY FILES DEFINITIONS
-#	Object file paths are generated here (as opposed to with the rest of the 
+#	Object file paths are generated here (as opposed to with the rest of the
 #   flags) because we want to place them in target-specific folders. We
 #   determine targets above. We –could– determine the target info earlier if we
-#   wanted to.  It's here because that's approximately where it was in the 
+#   wanted to.  It's here because that's approximately where it was in the
 #   legacy makefiles.
 ################################################################################
 
@@ -163,75 +163,75 @@ else
 endif
 
 # create a named list of dependency files
-# 1. create a list of .d dependency files based on the current list of 
+# 1. create a list of .d dependency files based on the current list of
 #  OF_CORE_SOURCE_FILES $(patsubst $(OF_ROOT)/%.cpp,%.d,$(OF_CORE_SOURCE_FILES))
-# 2. Add the OF_CORE_OBJ_OUTPUT_PATH as a prefix 
+# 2. Add the OF_CORE_OBJ_OUTPUT_PATH as a prefix
 #  $(addprefix $(OF_CORE_OBJ_OUTPUT_PATH), ...)
 OF_CORE_DEPENDENCY_FILES = $(addprefix $(OF_CORE_OBJ_OUTPUT_PATH),$(patsubst $(OF_ROOT)/%.cpp,%.d,$(patsubst $(OF_ROOT)/%.mm,%.d,$(patsubst $(OF_ROOT)/%.m,%.d,$(OF_CORE_SOURCE_FILES)))))
 
 # create a named list of object files
 # 1. create a list of object files based on the current list of
 #   OF_CORE_SOURCE_FILES $(patsubst $(OF_ROOT)/%.cpp,%.o,$(OF_CORE_SOURCE_FILES)
-# 2. Add the OF_CORE_OBJ_OUTPUT_PATH as a prefix 
+# 2. Add the OF_CORE_OBJ_OUTPUT_PATH as a prefix
 #	$(addprefix $(OF_CORE_OBJ_OUTPUT_PATH), ...)
 OF_CORE_OBJ_FILES = $(addprefix $(OF_CORE_OBJ_OUTPUT_PATH),$(patsubst $(OF_ROOT)/%.cpp,%.o,$(patsubst $(OF_ROOT)/%.mm,%.o,$(patsubst $(OF_ROOT)/%.m,%.o,$(OF_CORE_SOURCE_FILES)))))
 
-    
+
 ################################################################################
 # DEBUG INFO
 ################################################################################
 ifdef MAKEFILE_DEBUG
-    $(info ========================= compile.core.make flags ========================)
-    $(info OF_CORE_OBJ_OUTPUT_PATH=$(OF_CORE_OBJ_OUTPUT_PATH))
-    
-    $(info ---OF_CORE_DEPENDENCY_FILES---)
-    $(foreach v, $(OF_CORE_DEPENDENCY_FILES),$(info $(v)))
-    
-    $(info ---OF_CORE_OBJ_FILES---)
-    $(foreach v, $(OF_CORE_OBJ_FILES),$(info $(v)))
+	$(info ========================= compile.core.make flags ========================)
+	$(info OF_CORE_OBJ_OUTPUT_PATH=$(OF_CORE_OBJ_OUTPUT_PATH))
+
+	$(info ---OF_CORE_DEPENDENCY_FILES---)
+	$(foreach v, $(OF_CORE_DEPENDENCY_FILES),$(info $(v)))
+
+	$(info ---OF_CORE_OBJ_FILES---)
+	$(foreach v, $(OF_CORE_OBJ_FILES),$(info $(v)))
 endif
 
 
 ################################################################################
-# While most MAKE targets respond to lists of filenames, .PHONY targets are 
+# While most MAKE targets respond to lists of filenames, .PHONY targets are
 # targets that are "recipe" only -- that is recipes that respond to specific
-# requests, not filenames or lists of filenames.  .PNONY targets are used to 
+# requests, not filenames or lists of filenames.  .PNONY targets are used to
 # avoid conflict with files of the same name and to improve performance.
 .PHONY: all Debug Release after clean CleanDebug CleanRelease help force
 
-Release: 
+Release:
 ifndef ABIS_TO_COMPILE_RELEASE
 	@$(MAKE) --no-print-directory ReleaseABI
 else
-	@$(foreach abi,$(ABIS_TO_COMPILE_RELEASE),$(MAKE) --no-print-directory ReleaseABI ABI=$(abi) &&) echo 
+	@$(foreach abi,$(ABIS_TO_COMPILE_RELEASE),$(MAKE) --no-print-directory ReleaseABI ABI=$(abi) &&) echo
 endif
-	@$(MAKE) --no-print-directory after 
-	
-Debug: 
+	@$(MAKE) --no-print-directory after
+
+Debug:
 ifndef ABIS_TO_COMPILE_DEBUG
 	@$(MAKE) --no-print-directory DebugABI
 else
-	@$(foreach abi,$(ABIS_TO_COMPILE_DEBUG),$(MAKE) --no-print-directory DebugABI ABI=$(abi) &&) echo 
+	@$(foreach abi,$(ABIS_TO_COMPILE_DEBUG),$(MAKE) --no-print-directory DebugABI ABI=$(abi) &&) echo
 endif
-	@$(MAKE) --no-print-directory after 
-	
-# Release will pass the library name (i.e. ... libopenFrameworks.a) 
+	@$(MAKE) --no-print-directory after
+
+# Release will pass the library name (i.e. ... libopenFrameworks.a)
 # down the the @(TARGET) target
 ReleaseABI: $(TARGET)
 
 # Debug will pass the library name (i.e. ... libopenFrameworksDebug.a)
 # down the the @(TARGET) target
 DebugABI: $(TARGET)
-	
+
 # all will first run the debug target, then the release target
-all: 
+all:
 	@$(MAKE) --no-print-directory Debug
 	@$(MAKE) --no-print-directory Release
-	
+
 $(OF_CORE_OBJ_OUTPUT_PATH).compiler_flags: force
 	@mkdir -p $(OF_CORE_OBJ_OUTPUT_PATH)
 	@if [ "$(strip $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS))" != "$(strip $$(cat $@))" ]; then echo "Compiler flags have changed, recompiling"; echo "Old: $(strip $$(cat $@))"; echo "New $(strip $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS))"; echo $(strip $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS))> $@; fi
-	
+
 
 #This rule does the compilation
 $(OF_CORE_OBJ_OUTPUT_PATH)%.o: $(OF_ROOT)/%.cpp $(OF_CORE_OBJ_OUTPUT_PATH).compiler_flags
@@ -250,18 +250,18 @@ $(OF_CORE_OBJ_OUTPUT_PATH)%.o: $(OF_ROOT)/%.m $(OF_CORE_OBJ_OUTPUT_PATH).compile
 	$(CC) $(OPTIMIZATION_CFLAGS) $(CFLAGS) $(CXXFLAGS) -MMD -MP -MF $(OF_CORE_OBJ_OUTPUT_PATH)$*.d -MT$(OF_CORE_OBJ_OUTPUT_PATH)$*.o -o $@ -c $<
 
 # this target does the linking of the library
-# $(TARGET) : $(OF_CORE_OBJ_FILES) means that each of the items in the 
-# $(OF_CORE_OBJ_FILES) must be processed first  
+# $(TARGET) : $(OF_CORE_OBJ_FILES) means that each of the items in the
+# $(OF_CORE_OBJ_FILES) must be processed first
 ifeq ($(SHAREDCORE),1)
 $(TARGET) : $(OF_CORE_OBJ_FILES) $(OF_CORE_OBJ_OUTPUT_PATH).compiler_flags
 	@echo "Creating library " $(TARGET)
 	@mkdir -p $(@D)
-	$(CC) -shared $(OF_CORE_OBJ_FILES) -o $@  
+	$(CC) -shared $(OF_CORE_OBJ_FILES) -o $@
 else ifeq ($(BYTECODECORE),1)
 $(TARGET) : $(OF_CORE_OBJ_FILES) $(OF_CORE_OBJ_OUTPUT_PATH).compiler_flags
 	@echo "Creating library " $(TARGET)
 	@mkdir -p $(@D)
-	$(CC) $(OF_CORE_OBJ_FILES) -o $@  
+	$(CC) $(OF_CORE_OBJ_FILES) -o $@
 else
 $(TARGET) : $(OF_CORE_OBJ_FILES) $(OF_CORE_OBJ_OUTPUT_PATH).compiler_flags
 	@echo "Creating library " $(TARGET)
@@ -272,7 +272,7 @@ endif
 
 #.PHONY: clean CleanDebug CleanRelease
 
-	
+
 clean:
 	$(MAKE) CleanRelease
 	$(MAKE) CleanDebug
@@ -282,7 +282,7 @@ $(CLEANTARGET)ABI:
 	rm -Rf $(OF_CORE_OBJ_OUTPUT_PATH)
 	@echo "Removing " $(TARGET)
 	rm -f $(TARGET)
-	
+
 $(CLEANTARGET):
 ifndef ABIS_TO_COMPILE
 	$(MAKE) --no-print-directory $(CLEANTARGET)ABI
@@ -296,10 +296,10 @@ endif
 
 after: $(TARGET)
 	@echo "Done!"
-	
-	
+
+
 help:
-	@echo 
+	@echo
 	@echo openFrameworks compiled library makefile
 	@echo
 	@echo "Targets:"
@@ -312,13 +312,13 @@ help:
 	@echo "make CleanRelease:	cleans the Release target"
 	@echo "make clean:		cleans everything"
 	@echo "make help:		this help message"
-	@echo 
+	@echo
 	@echo "Platform OS"
-	@echo 
+	@echo
 	@echo "Currently the only platform that needs to specify the OS is"
 	@echo "android. This is already setup in the eclipse porjects but"
 	@echo "the correct parameter is:"
-	@echo 
+	@echo
 	@echo "e.g."
 	@echo
 	@echo "make Release PLATFORM_OS=Android"
@@ -329,9 +329,9 @@ help:
 	@echo "(i.e. Linux or Darwin) and can be further customized with a"
 	@echo "platform variant make files. To use a platform variant makefile"
 	@echo "set the PLATFORM_VARIANT variable to a valid variant."
-	@echo 
+	@echo
 	@echo "e.g."
-	@echo 
+	@echo
 	@echo "make Release PLATFORM_VARIANT=raspberrypi"
 	@echo
 	@echo "In this case, the makefile will automatically determine architecture"

libs/openFrameworksCompiled/project/makefileCommon/config.addons.mk
@@ -17,7 +17,7 @@ define parse_addons_includes
 	$(eval PARSED_ADDONS_FILTERED_LIBS_INCLUDE_PATHS = $(filter-out $(ADDONS_INCLUDES_FILTER),$(PARSED_ADDONS_LIBS_INCLUDES))) \
 	$(eval PARSED_ADDONS_INCLUDES = $(PARSED_ADDONS_FILTERED_INCLUDE_PATHS)) \
 	$(eval PARSED_ADDONS_INCLUDES += $(PARSED_ADDONS_FILTERED_LIBS_SOURCE_INCLUDE_PATHS)) \
-	$(eval PARSED_ADDONS_INCLUDES += $(PARSED_ADDONS_FILTERED_LIBS_INCLUDE_PATHS)) 
+	$(eval PARSED_ADDONS_INCLUDES += $(PARSED_ADDONS_FILTERED_LIBS_INCLUDE_PATHS))
 endef
 
 # parses addons sources, in PARSED_ADDON_SOURCES receives full PATHS to addons
@@ -30,7 +30,7 @@ define parse_addons_sources
 	$(eval PARSED_ADDONS_LIBS_SOURCES = $(shell $(FIND) $(PARSED_ADDONS_LIBS_SOURCE_PATHS) -type f \( -name "*.cpp" -or -name "*.c" -or -name "*.cc" -or -name "*.cxx" \) 2> /dev/null | grep -v "/\.[^\.]"  )) \
 	$(eval PARSED_ADDONS_FILTERED_LIBS_SOURCE_PATHS = $(filter-out $(ADDONS_SOURCES_FILTER),$(PARSED_ADDONS_LIBS_SOURCES))) \
 	$(eval PARSED_ADDONS_SOURCE_FILES = $(PARSED_ADDONS_FILTERED_SOURCE_PATHS)) \
-	$(eval PARSED_ADDONS_SOURCE_FILES += $(PARSED_ADDONS_FILTERED_LIBS_SOURCE_PATHS)) 
+	$(eval PARSED_ADDONS_SOURCE_FILES += $(PARSED_ADDONS_FILTERED_LIBS_SOURCE_PATHS))
 endef
 
 # parses addons libraries, in PARSED_ADDON_LIBS receives full PATHS to addons and libs_exclude
@@ -49,7 +49,7 @@ endef
 
 
 space :=
-space += 
+space +=
 
 define src_to_obj
 	$(addsuffix .o,$(basename $(filter %.c %.cpp %.cc %.cxx %.cc %.s %.S, $(addprefix $3,$(addprefix $2,$1)))))
@@ -158,7 +158,7 @@ define parse_addon
 			$(eval PROJECT_ADDONS += $(addon_dep)) \
 			$(call parse_addon,$(addon_dep)) \
 		) \
-	) 
+	)
 endef
 
 
@@ -194,24 +194,24 @@ OF_PROJECT_ADDONS_DEPS = $(patsubst %.o,%.d,$(PROJECT_ADDONS_OBJ_FILES))
 ########################################################################
 # print debug information if so instructed
 ifdef MAKEFILE_DEBUG
-    $(info ---PROJECT_ADDONS_PATHS---)
-    $(foreach v, $(PROJECT_ADDONS_PATHS),$(info $(v)))
-    $(info ---PROJECT_ADDONS_WITH_CONFIG---)
-    $(foreach v, $(PROJECT_ADDONS_WITH_CONFIG),$(info $(v)))
-    $(info ---PROJECT_ADDONS_INCLUDES---)
-    $(foreach v, $(PROJECT_ADDONS_INCLUDES),$(info $(v)))
-    $(info ---PROJECT_ADDONS_SOURCE_FILES---)
-    $(foreach v, $(PROJECT_ADDONS_SOURCE_FILES),$(info $(v)))
-    $(info ---PROJECT_ADDONS_LIBS---)
-    $(foreach v, $(PROJECT_ADDONS_LIBS),$(info $(v)))
-    $(info ---PROJECT_ADDONS_OBJFILES---)
-    $(foreach v, $(PROJECT_ADDONS_OBJFILES),$(info $(v)))
-    $(info ---PROJECT_ADDONS_BASE_CFLAGS---)
-    $(foreach v, $(PROJECT_ADDONS_BASE_CFLAGS),$(info $(v)))
-    $(info ---PROJECT_ADDONS_DEFINES_CFLAGS---)
-    $(foreach v, $(PROJECT_ADDONS_DEFINES_CFLAGS),$(info $(v)))
-    $(info ---PROJECT_ADDONS_INCLUDES_CFLAGS---)
-    $(foreach v, $(PROJECT_ADDONS_INCLUDES_CFLAGS),$(info $(v)))
-    $(info ---PROJECT_ADDONS_LDFLAGS---)
-    $(foreach v, $(PROJECT_ADDONS_LDFLAGS),$(info $(v)))
+	$(info ---PROJECT_ADDONS_PATHS---)
+	$(foreach v, $(PROJECT_ADDONS_PATHS),$(info $(v)))
+	$(info ---PROJECT_ADDONS_WITH_CONFIG---)
+	$(foreach v, $(PROJECT_ADDONS_WITH_CONFIG),$(info $(v)))
+	$(info ---PROJECT_ADDONS_INCLUDES---)
+	$(foreach v, $(PROJECT_ADDONS_INCLUDES),$(info $(v)))
+	$(info ---PROJECT_ADDONS_SOURCE_FILES---)
+	$(foreach v, $(PROJECT_ADDONS_SOURCE_FILES),$(info $(v)))
+	$(info ---PROJECT_ADDONS_LIBS---)
+	$(foreach v, $(PROJECT_ADDONS_LIBS),$(info $(v)))
+	$(info ---PROJECT_ADDONS_OBJFILES---)
+	$(foreach v, $(PROJECT_ADDONS_OBJFILES),$(info $(v)))
+	$(info ---PROJECT_ADDONS_BASE_CFLAGS---)
+	$(foreach v, $(PROJECT_ADDONS_BASE_CFLAGS),$(info $(v)))
+	$(info ---PROJECT_ADDONS_DEFINES_CFLAGS---)
+	$(foreach v, $(PROJECT_ADDONS_DEFINES_CFLAGS),$(info $(v)))
+	$(info ---PROJECT_ADDONS_INCLUDES_CFLAGS---)
+	$(foreach v, $(PROJECT_ADDONS_INCLUDES_CFLAGS),$(info $(v)))
+	$(info ---PROJECT_ADDONS_LDFLAGS---)
+	$(foreach v, $(PROJECT_ADDONS_LDFLAGS),$(info $(v)))
 endif

commit cfb07ac367894de488951725baae3992fe700f56

Fixes MAKEFILEDEBUG functionality broken by ec057361d170bd6b3ca64e54a32e89caaacd4fd5.

libs/openFrameworksCompiled/project/makefileCommon/compile.core.mk
@@ -2,7 +2,7 @@
 
 ################################################################################
 ifdef MAKEFILE_DEBUG
-	$(info ===================compile.core.make================================)
+$(info ===================compile.core.make================================)
 endif
 
 ifdef PLATFORM_CXX
@@ -181,14 +181,14 @@ OF_CORE_OBJ_FILES = $(addprefix $(OF_CORE_OBJ_OUTPUT_PATH),$(patsubst $(OF_ROOT)
 # DEBUG INFO
 ################################################################################
 ifdef MAKEFILE_DEBUG
-	$(info ========================= compile.core.make flags ========================)
-	$(info OF_CORE_OBJ_OUTPUT_PATH=$(OF_CORE_OBJ_OUTPUT_PATH))
+$(info ========================= compile.core.make flags ========================)
+$(info OF_CORE_OBJ_OUTPUT_PATH=$(OF_CORE_OBJ_OUTPUT_PATH))
 
-	$(info ---OF_CORE_DEPENDENCY_FILES---)
-	$(foreach v, $(OF_CORE_DEPENDENCY_FILES),$(info $(v)))
+$(info ---OF_CORE_DEPENDENCY_FILES---)
+$(foreach v, $(OF_CORE_DEPENDENCY_FILES),$(info $(v)))
 
-	$(info ---OF_CORE_OBJ_FILES---)
-	$(foreach v, $(OF_CORE_OBJ_FILES),$(info $(v)))
+$(info ---OF_CORE_OBJ_FILES---)
+$(foreach v, $(OF_CORE_OBJ_FILES),$(info $(v)))
 endif
 
 

libs/openFrameworksCompiled/project/makefileCommon/config.addons.mk
@@ -194,24 +194,24 @@ OF_PROJECT_ADDONS_DEPS = $(patsubst %.o,%.d,$(PROJECT_ADDONS_OBJ_FILES))
 ########################################################################
 # print debug information if so instructed
 ifdef MAKEFILE_DEBUG
-	$(info ---PROJECT_ADDONS_PATHS---)
-	$(foreach v, $(PROJECT_ADDONS_PATHS),$(info $(v)))
-	$(info ---PROJECT_ADDONS_WITH_CONFIG---)
-	$(foreach v, $(PROJECT_ADDONS_WITH_CONFIG),$(info $(v)))
-	$(info ---PROJECT_ADDONS_INCLUDES---)
-	$(foreach v, $(PROJECT_ADDONS_INCLUDES),$(info $(v)))
-	$(info ---PROJECT_ADDONS_SOURCE_FILES---)
-	$(foreach v, $(PROJECT_ADDONS_SOURCE_FILES),$(info $(v)))
-	$(info ---PROJECT_ADDONS_LIBS---)
-	$(foreach v, $(PROJECT_ADDONS_LIBS),$(info $(v)))
-	$(info ---PROJECT_ADDONS_OBJFILES---)
-	$(foreach v, $(PROJECT_ADDONS_OBJFILES),$(info $(v)))
-	$(info ---PROJECT_ADDONS_BASE_CFLAGS---)
-	$(foreach v, $(PROJECT_ADDONS_BASE_CFLAGS),$(info $(v)))
-	$(info ---PROJECT_ADDONS_DEFINES_CFLAGS---)
-	$(foreach v, $(PROJECT_ADDONS_DEFINES_CFLAGS),$(info $(v)))
-	$(info ---PROJECT_ADDONS_INCLUDES_CFLAGS---)
-	$(foreach v, $(PROJECT_ADDONS_INCLUDES_CFLAGS),$(info $(v)))
-	$(info ---PROJECT_ADDONS_LDFLAGS---)
-	$(foreach v, $(PROJECT_ADDONS_LDFLAGS),$(info $(v)))
+$(info ---PROJECT_ADDONS_PATHS---)
+$(foreach v, $(PROJECT_ADDONS_PATHS),$(info $(v)))
+$(info ---PROJECT_ADDONS_WITH_CONFIG---)
+$(foreach v, $(PROJECT_ADDONS_WITH_CONFIG),$(info $(v)))
+$(info ---PROJECT_ADDONS_INCLUDES---)
+$(foreach v, $(PROJECT_ADDONS_INCLUDES),$(info $(v)))
+$(info ---PROJECT_ADDONS_SOURCE_FILES---)
+$(foreach v, $(PROJECT_ADDONS_SOURCE_FILES),$(info $(v)))
+$(info ---PROJECT_ADDONS_LIBS---)
+$(foreach v, $(PROJECT_ADDONS_LIBS),$(info $(v)))
+$(info ---PROJECT_ADDONS_OBJFILES---)
+$(foreach v, $(PROJECT_ADDONS_OBJFILES),$(info $(v)))
+$(info ---PROJECT_ADDONS_BASE_CFLAGS---)
+$(foreach v, $(PROJECT_ADDONS_BASE_CFLAGS),$(info $(v)))
+$(info ---PROJECT_ADDONS_DEFINES_CFLAGS---)
+$(foreach v, $(PROJECT_ADDONS_DEFINES_CFLAGS),$(info $(v)))
+$(info ---PROJECT_ADDONS_INCLUDES_CFLAGS---)
+$(foreach v, $(PROJECT_ADDONS_INCLUDES_CFLAGS),$(info $(v)))
+$(info ---PROJECT_ADDONS_LDFLAGS---)
+$(foreach v, $(PROJECT_ADDONS_LDFLAGS),$(info $(v)))
 endif

libs/openFrameworksCompiled/project/makefileCommon/config.project.mk
@@ -92,18 +92,18 @@ OF_CORE_LIBRARY_LDFLAGS += $(addprefix -L,$(PLATFORM_LIBRARY_SEARCH_PATHS))
 # DEBUG INFO
 ################################################################################
 ifdef MAKEFILE_DEBUG
-	$(info =============================configure.core.flags.make========================)
-	$(info ---OF_CORE_LIBS_LDFLAGS---)
-	$(foreach v, $(OF_CORE_LIBS_LDFLAGS),$(info $(v)))
+$(info =============================configure.core.flags.make========================)
+$(info ---OF_CORE_LIBS_LDFLAGS---)
+$(foreach v, $(OF_CORE_LIBS_LDFLAGS),$(info $(v)))
 
-	$(info ---OF_CORE_LIBS---)
-	$(foreach v, $(OF_CORE_LIBS),$(info $(v)))
+$(info ---OF_CORE_LIBS---)
+$(foreach v, $(OF_CORE_LIBS),$(info $(v)))
 endif
 
 ################################# ADDONS ######################################
 
 ifdef MAKEFILE_DEBUG
-	$(info ===================ADDONS================)
+$(info ===================ADDONS================)
 endif
 
 # check to make sure OF_ROOT is defined
@@ -194,9 +194,9 @@ ifdef B_PROCESS_ADDONS
 	PROJECT_ADDONS = $(filter-out $(INVALID_PROJECT_ADDONS),$(REQUESTED_PROJECT_ADDONS))
 
 	ifdef MAKEFILE_DEBUG
-		$(info ---PROJECT_ADDONS---)
-		$(foreach v, $(PROJECT_ADDONS),$(info $(v)))
-		$(info --------------------)
+$(info ---PROJECT_ADDONS---)
+$(foreach v, $(PROJECT_ADDONS),$(info $(v)))
+$(info --------------------)
 	endif
 
 	############################################################################
@@ -288,11 +288,11 @@ endif
 OF_PROJECT_SOURCE_PATHS = $(filter-out $(OF_PROJECT_EXCLUSIONS),$(ALL_OF_PROJECT_SOURCE_PATHS))
 
 ifdef MAKEFILE_DEBUG
-	$(info ---OF_PROJECT_SOURCE_PATHS---)
-	$(foreach v, $(OF_PROJECT_SOURCE_PATHS),$(info $(v)))
+$(info ---OF_PROJECT_SOURCE_PATHS---)
+$(foreach v, $(OF_PROJECT_SOURCE_PATHS),$(info $(v)))
 
-	$(info ---OF_PROJECT_EXCLUSIONS---)
-	$(foreach v, $(OF_PROJECT_EXCLUSIONS),$(info $(v)))
+$(info ---OF_PROJECT_EXCLUSIONS---)
+$(foreach v, $(OF_PROJECT_EXCLUSIONS),$(info $(v)))
 endif
 
 # find all sources inside the project's source directory (recursively)
@@ -307,8 +307,8 @@ OF_PROJECT_INCLUDES_CFLAGS := $(addprefix -I,$(filter-out $(PROJECT_INCLUDE_EXCL
 OF_ADDON_INCLUDES_CFLAGS += $(addprefix -I,$(filter-out $(PROJECT_INCLUDE_EXCLUSIONS),$(PROJECT_ADDONS_INCLUDES)))
 
 ifdef MAKEFILE_DEBUG
-	$(info ---OF_PROJECT_INCLUDES_CFLAGS---)
-	$(foreach v, $(OF_PROJECT_INCLUDES_CFLAGS),$(info $(v)))
+$(info ---OF_PROJECT_INCLUDES_CFLAGS---)
+$(foreach v, $(OF_PROJECT_INCLUDES_CFLAGS),$(info $(v)))
 endif
 
 ################################################################################
@@ -370,7 +370,7 @@ OF_PROJECT_LDFLAGS += $(addprefix -framework ,$(PROJECT_ADDONS_FRAMEWORKS))
 
 ################################################################################
 ifdef MAKEFILE_DEBUG
-	$(info ===================compile.project.make=============================)
+$(info ===================compile.project.make=============================)
 endif
 
 ifdef PLATFORM_CXX
@@ -487,12 +487,12 @@ endif
 # define the subdirectory for our target name
 
 ifdef MAKEFILE_DEBUG
-	$(info ---OF_PROJECT_SOURCE_FILES---)
-	$(foreach v, $(OF_PROJECT_SOURCE_FILES),$(info $(v)))
+$(info ---OF_PROJECT_SOURCE_FILES---)
+$(foreach v, $(OF_PROJECT_SOURCE_FILES),$(info $(v)))
 endif
 ifdef MAKEFILE_DEBUG
-	$(info ---OF_PROJECT_DEPENDENCY_FILES---)
-	$(foreach v, $(OF_PROJECT_DEPENDENCY_FILES),$(info $(v)))
+$(info ---OF_PROJECT_DEPENDENCY_FILES---)
+$(foreach v, $(OF_PROJECT_DEPENDENCY_FILES),$(info $(v)))
 endif
 
 
@@ -509,6 +509,6 @@ OF_PROJECT_DEPENDENCY_FILES = $(OF_PROJECT_DEPS) $(OF_PROJECT_ADDONS_DEPS)
 
 
 ifdef MAKEFILE_DEBUG
-	$(info ---OF_PROJECT_DEPENDENCY_FILES---)
-	$(foreach v, $(OF_PROJECT_DEPENDENCY_FILES),$(info $(v)))
+$(info ---OF_PROJECT_DEPENDENCY_FILES---)
+$(foreach v, $(OF_PROJECT_DEPENDENCY_FILES),$(info $(v)))
 endif

libs/openFrameworksCompiled/project/makefileCommon/config.shared.mk
@@ -121,11 +121,11 @@ endif
 
 # if desired, print the variables
 ifdef MAKEFILE_DEBUG
-	$(info =================== config.mk platform detection ================)
-	$(info PLATFORM_ARCH=$(PLATFORM_ARCH))
-	$(info PLATFORM_OS=$(PLATFORM_OS))
-	$(info PLATFORM_VARIANT=$(PLATFORM_VARIANT))
-	$(info PLATFORM_LIB_SUBPATH=$(PLATFORM_LIB_SUBPATH))
+$(info =================== config.mk platform detection ================)
+$(info PLATFORM_ARCH=$(PLATFORM_ARCH))
+$(info PLATFORM_OS=$(PLATFORM_OS))
+$(info PLATFORM_VARIANT=$(PLATFORM_VARIANT))
+$(info PLATFORM_LIB_SUBPATH=$(PLATFORM_LIB_SUBPATH))
 endif
 
 
@@ -188,17 +188,17 @@ endif
 ################################################################################
 # print debug information if needed
 ifdef MAKEFILE_DEBUG
-	$(info =================== config.mk paths =============================)
-	$(info OF_ADDONS_PATH=$(OF_ADDONS_PATH))
-	$(info OF_EXAMPLES_PATH=$(OF_EXAMPLES_PATH))
-	$(info OF_APPS_PATH=$(OF_APPS_PATH))
-	$(info OF_LIBS_PATH=$(OF_LIBS_PATH))
-	$(info OF_LIBS_OPENFRAMEWORKS_PATH=$(OF_LIBS_OPENFRAMEWORKS_PATH))
-	$(info OF_LIBS_OF_COMPILED_PATH=$(OF_LIBS_OF_COMPILED_PATH))
-	$(info OF_LIBS_OF_COMPILED_PROJECT_PATH=$(OF_LIBS_OF_COMPILED_PROJECT_PATH))
-	$(info OF_SHARED_MAKEFILES_PATH=$(OF_SHARED_MAKEFILES_PATH))
-	$(info OF_PLATFORM_MAKEFILES=$(OF_PLATFORM_MAKEFILES))
-	$(info OF_CORE_LIB_PATH=$(OF_CORE_LIB_PATH))
+$(info =================== config.mk paths =============================)
+$(info OF_ADDONS_PATH=$(OF_ADDONS_PATH))
+$(info OF_EXAMPLES_PATH=$(OF_EXAMPLES_PATH))
+$(info OF_APPS_PATH=$(OF_APPS_PATH))
+$(info OF_LIBS_PATH=$(OF_LIBS_PATH))
+$(info OF_LIBS_OPENFRAMEWORKS_PATH=$(OF_LIBS_OPENFRAMEWORKS_PATH))
+$(info OF_LIBS_OF_COMPILED_PATH=$(OF_LIBS_OF_COMPILED_PATH))
+$(info OF_LIBS_OF_COMPILED_PROJECT_PATH=$(OF_LIBS_OF_COMPILED_PROJECT_PATH))
+$(info OF_SHARED_MAKEFILES_PATH=$(OF_SHARED_MAKEFILES_PATH))
+$(info OF_PLATFORM_MAKEFILES=$(OF_PLATFORM_MAKEFILES))
+$(info OF_CORE_LIB_PATH=$(OF_CORE_LIB_PATH))
 endif
 
 
@@ -334,24 +334,24 @@ OF_CORE_HEADER_FILES=$(filter-out $(CORE_EXCLUSIONS),$(shell $(FIND) $(OF_CORE_S
 # DEBUG INFO
 ################################################################################
 ifdef MAKEFILE_DEBUG
-	$(info ========================= config.mk flags ========================)
-	$(info ---OF_CORE_DEFINES_CFLAGS---)
-	$(foreach v, $(OF_CORE_DEFINES_CFLAGS),$(info $(v)))
+$(info ========================= config.mk flags ========================)
+$(info ---OF_CORE_DEFINES_CFLAGS---)
+$(foreach v, $(OF_CORE_DEFINES_CFLAGS),$(info $(v)))
 
-	$(info ---OF_CORE_INCLUDES_CFLAGS---)
-	$(foreach v, $(OF_CORE_INCLUDES_CFLAGS),$(info $(v)))
+$(info ---OF_CORE_INCLUDES_CFLAGS---)
+$(foreach v, $(OF_CORE_INCLUDES_CFLAGS),$(info $(v)))
 
-	$(info ---OF_CORE_FRAMEWORKS_CFLAGS---)
-	$(foreach v, $(OF_CORE_FRAMEWORKS_CFLAGS),$(info $(v)))
+$(info ---OF_CORE_FRAMEWORKS_CFLAGS---)
+$(foreach v, $(OF_CORE_FRAMEWORKS_CFLAGS),$(info $(v)))
 
-	$(info ---OF_CORE_SOURCE_FILES---)
-	$(foreach v, $(OF_CORE_SOURCE_FILES),$(info $(v)))
+$(info ---OF_CORE_SOURCE_FILES---)
+$(foreach v, $(OF_CORE_SOURCE_FILES),$(info $(v)))
 
-	$(info ---OF_CORE_HEADER_FILES---)
-	$(foreach v, $(OF_CORE_HEADER_FILES),$(info $(v)))
+$(info ---OF_CORE_HEADER_FILES---)
+$(foreach v, $(OF_CORE_HEADER_FILES),$(info $(v)))
 
-	$(info ---PLATFORM_CORE_EXCLUSIONS---)
-	$(foreach v, $(PLATFORM_CORE_EXCLUSIONS),$(info $(v)))
+$(info ---PLATFORM_CORE_EXCLUSIONS---)
+$(foreach v, $(PLATFORM_CORE_EXCLUSIONS),$(info $(v)))
 endif
 
 