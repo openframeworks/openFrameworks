ff59d6b38ebaf8157fcec765fc70e4104a46c2ed
Matt Felsen <mattfelsen@gmail.com>
2017-02-22 18:07:18 -0500

    ofImage: encode path to URI before parsing

--- libs/openFrameworks/graphics/ofImage.cpp
+++ libs/openFrameworks/graphics/ofImage.cpp
@@ -184,17 +184,29 @@ template<typename PixelType>
 static bool loadImage(ofPixels_<PixelType> & pix, const std::filesystem::path& _fileName, const ofImageLoadSettings& settings){
 	ofInitFreeImage();
 
-	UriParserStateA state;
-	UriUriA uri;
 	auto uriStr = _fileName.string();
+	const int bytesNeeded = 8 + 3 * strlen(uriStr.c_str()) + 1;
+	char* absUri = (char*) malloc(bytesNeeded * sizeof(char));
+
+#ifdef TARGET_WIN32
+	uriWindowsFilenameToUriStringA(uriStr.c_str(), absUri);
+#else
+	uriUnixFilenameToUriStringA(uriStr.c_str(), absUri);
+#endif
+
+	UriUriA uri;
+	UriParserStateA state;
 	state.uri = &uri;
-	if(uriParseUriA(&state, uriStr.c_str())!=URI_SUCCESS){
+	if(uriParseUriA(&state, absUri)!=URI_SUCCESS){
 		ofLogError("ofImage") << "loadImage(): malformed uri when loading image from uri " << _fileName;
+		free(absUri);
 		uriFreeUriMembersA(&uri);
 		return false;
 	}
 	std::string scheme(uri.scheme.first, uri.scheme.afterLast);
+	free(absUri);
 	uriFreeUriMembersA(&uri);
+
 	if(scheme == "http" || scheme == "https"){
 		return ofLoadImage(pix, ofLoadURL(_fileName.string()).data);
 	}
