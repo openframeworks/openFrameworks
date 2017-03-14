openframeworks/openFrameworks

commit 6b593ee3ae272dbbd377d1100d1802994bd60c10

Fallback to /dev/input/event* for uinput mouse and keyboard

libs/openFrameworks/app/ofAppEGLWindow.cpp
@@ -100,6 +100,16 @@ static int string_ends_with(const char *str, const char *suffix) {
 	return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
 }
 
+static int string_begins_with(const char *str, const char *prefix) {
+	if (!str || !prefix)
+		return 0;
+	size_t lenstr = strlen(str);
+	size_t lenprefix = strlen(prefix);
+	if (lenprefix > lenstr)
+		return 0;
+	return strncmp(str, prefix, lenprefix) == 0;
+}
+
 static int dummy_sort(const struct dirent **a,const struct dirent **b) {
 	return 1; // dummy sort
 }
@@ -114,6 +124,14 @@ static int filter_kbd(const struct dirent *d) {
 
 static int filter_mouse(const struct dirent *d) {
 	if(d->d_type != DT_DIR && string_ends_with(d->d_name,"event-mouse")) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+static int filter_event(const struct dirent *d) {
+	if(d->d_type != DT_DIR && string_begins_with(d->d_name,"event")) {
 		return 1;
 	} else {
 		return 0;
@@ -1337,17 +1355,23 @@ void ofAppEGLWindow::destroyNativeUDev() {
 //------------------------------------------------------------
 void ofAppEGLWindow::setupNativeMouse() {
 	struct dirent **eps;
-	int n = scandir("/dev/input/by-path/", &eps, filter_mouse, dummy_sort);
+	// fallback to /dev/input/eventX since some vnc servers use uinput to handle mouse & keyboard
+	typedef int (*filter_ptr)(const struct dirent *d);
+	filter_ptr mouse_filters[2] = { filter_mouse, filter_event };
+	string devicePathBuffers[2] = { "/dev/input/by-path", "/dev/input/" };
 
-	// make sure that we found an appropriate entry
-	if(n >= 0 && eps != 0 && eps[0] != 0) {
-		string devicePathBuffer;
-		devicePathBuffer.append("/dev/input/by-path/");
-		devicePathBuffer.append(eps[0]->d_name);
-		mouse_fd = open(devicePathBuffer.c_str(), O_RDONLY | O_NONBLOCK);
-		ofLogNotice("ofAppEGLWindow") << "setupMouse(): mouse_fd= " <<  mouse_fd << " devicePath=" << devicePathBuffer;
-	} else {
-		ofLogNotice("ofAppEGLWindow") << "setupMouse(): unabled to find mouse";
+	for(int i=0; i<2; i++){
+		int n = scandir(devicePathBuffers[i].c_str(), &eps, mouse_filters[i], dummy_sort);
+
+		// make sure that we found an appropriate entry
+		if(n >= 0 && eps != 0 && eps[0] != 0) {
+			string devicePathBuffer;
+			devicePathBuffer.append(devicePathBuffers[i]);
+			devicePathBuffer.append(eps[0]->d_name);
+			mouse_fd = open(devicePathBuffer.c_str(), O_RDONLY | O_NONBLOCK);
+			ofLogNotice("ofAppEGLWindow") << "setupMouse(): mouse_fd=" <<  mouse_fd << " devicePath=" << devicePathBuffer;
+			break;
+		}
 	}
 
 	if (mouse_fd >= 0) {
@@ -1362,7 +1386,7 @@ void ofAppEGLWindow::setupNativeMouse() {
 
 	if(mouse_fd < 0) {
 		ofLogError("ofAppEGLWindow") << "setupMouse(): did not open mouse, mouse_fd < 0";
-	}else {
+	} else {
 		mouseDetected = true;
 	}
 
@@ -1372,18 +1396,22 @@ void ofAppEGLWindow::setupNativeMouse() {
 //------------------------------------------------------------
 void ofAppEGLWindow::setupNativeKeyboard() {
 	struct dirent **eps;
-	int n = scandir("/dev/input/by-path/", &eps, filter_kbd, dummy_sort);
+	typedef int (*filter_ptr)(const struct dirent *d);
+	filter_ptr kbd_filters[2] = { filter_kbd, filter_event };
+	string devicePathBuffers[2] = { "/dev/input/by-path", "/dev/input/" };
 
-	// make sure that we found an appropriate entry
-	if(n >= 0 && eps != 0 && eps[0] != 0) {
-		string devicePathBuffer;
-		devicePathBuffer.append("/dev/input/by-path/");
-		devicePathBuffer.append(eps[0]->d_name);
-		keyboard_fd = open(devicePathBuffer.c_str(), O_RDONLY | O_NONBLOCK);
-		ofLogNotice("ofAppEGLWindow") << "setupKeyboard(): keyboard_fd= " <<  keyboard_fd << " devicePath=" << devicePathBuffer;
+	for(int i=0; i<2; i++){
+		int n = scandir(devicePathBuffers[i].c_str(), &eps, kbd_filters[i], dummy_sort);
 
-	} else {
-		ofLogWarning("ofAppEGLWindow") << "setupKeyboard(): unabled to find keyboard";
+		// make sure that we found an appropriate entry
+		if(n >= 0 && eps != 0 && eps[0] != 0) {
+			string devicePathBuffer;
+			devicePathBuffer.append(devicePathBuffers[i]);
+			devicePathBuffer.append(eps[0]->d_name);
+			keyboard_fd = open(devicePathBuffer.c_str(), O_RDONLY | O_NONBLOCK);
+			ofLogNotice("ofAppEGLWindow") << "setupKeyboard(): keyboard_fd=" <<  keyboard_fd << " devicePath=" << devicePathBuffer;
+			break;
+		}
 	}
 
 	if (keyboard_fd >= 0) {
@@ -1409,7 +1437,7 @@ void ofAppEGLWindow::setupNativeKeyboard() {
 
 	if(keyboard_fd < 0) {
 		ofLogError("ofAppEGLWindow") << "setupKeyboard(): did not open keyboard, keyboard_fd < 0";
-	}else {
+	} else {
 		keyboardDetected = true;
 	}
 }

commit 34d91bf4cbadd0f40865965c12335047e7480311

Fixed opening of mouse and keyboard on RPi
Typo introduced in 6b593ee3ae272dbbd377d1100d1802994bd60c10

libs/openFrameworks/app/ofAppEGLWindow.cpp
@@ -1349,7 +1349,7 @@ void ofAppEGLWindow::setupNativeMouse() {
 	// fallback to /dev/input/eventX since some vnc servers use uinput to handle mouse & keyboard
 	typedef int (*filter_ptr)(const struct dirent *d);
 	filter_ptr mouse_filters[2] = { filter_mouse, filter_event };
-	string devicePathBuffers[2] = { "/dev/input/by-path", "/dev/input/" };
+	string devicePathBuffers[2] = { "/dev/input/by-path/", "/dev/input/" };
 
 	for(int i=0; i<2; i++){
 		int n = scandir(devicePathBuffers[i].c_str(), &eps, mouse_filters[i], dummy_sort);
@@ -1389,7 +1389,7 @@ void ofAppEGLWindow::setupNativeKeyboard() {
 	struct dirent **eps;
 	typedef int (*filter_ptr)(const struct dirent *d);
 	filter_ptr kbd_filters[2] = { filter_kbd, filter_event };
-	string devicePathBuffers[2] = { "/dev/input/by-path", "/dev/input/" };
+	string devicePathBuffers[2] = { "/dev/input/by-path/", "/dev/input/" };
 
 	for(int i=0; i<2; i++){
 		int n = scandir(devicePathBuffers[i].c_str(), &eps, kbd_filters[i], dummy_sort);