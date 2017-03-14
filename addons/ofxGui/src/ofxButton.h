openframeworks/openFrameworks

commit 3337737fc571f2ff86a4673b40f951201d893e3a

add ofParameter<void> -> ofxButton

addons/ofxGui/src/ofxButton.h
@@ -8,7 +8,9 @@ class ofxButton : public ofxToggle{
 	
 public:
 	ofxButton();
+	ofxButton(ofParameter<void> _bVal, float width = defaultWidth, float height = defaultHeight);
 	~ofxButton();
+	ofxButton* setup(ofParameter<void> _bVal, float width = defaultWidth, float height = defaultHeight);
     ofxButton* setup(const std::string& toggleName, float width = defaultWidth, float height = defaultHeight);
 
 	virtual bool mouseReleased(ofMouseEventArgs & args);
@@ -18,16 +20,17 @@ public:
 
 	template<class ListenerClass, typename ListenerMethod>
 	void addListener(ListenerClass * listener, ListenerMethod method){
-		ofAddListener(triggerEvent,listener,method);
+		value.addListener(listener,method);
 	}
 
 	template<class ListenerClass, typename ListenerMethod>
 	void removeListener(ListenerClass * listener, ListenerMethod method){
-		ofRemoveListener(triggerEvent,listener,method);
+		value.addListener(listener,method);
 	}
+	virtual ofAbstractParameter & getParameter();
 
 private:
-	ofEvent<void> triggerEvent;
+	ofParameter<void> parameter;
 	void valueChanged(bool & v);
 
 };

addons/ofxGui/src/ofxButton.cpp
@@ -5,18 +5,39 @@ ofxButton::ofxButton(){
 	value.setSerializable(false);
 }
 
+ofxButton::ofxButton(ofParameter<void> _bVal, float width, float height){
+	setup(_bVal, width, height);
+}
+
 ofxButton::~ofxButton(){
 	//
 }
 
-ofxButton* ofxButton::setup(const std::string& toggleName, float width, float height){
-	setName(toggleName);
+ofxButton* ofxButton::setup(ofParameter<void> _bVal, float width, float height){
+	parameter = _bVal;
+	value = false;
 	b.x = 0;
 	b.y = 0;
 	b.width = width;
 	b.height = height;
 	bGuiActive = false;
+	checkboxRect.set(1, 1, b.height - 2, b.height - 2);
+
+	registerMouseEvents();
+
+	value.addListener(this,&ofxButton::valueChanged);
+
+	return this;
+}
+
+ofxButton* ofxButton::setup(const std::string& toggleName, float width, float height){
+	setName(toggleName);
 	value = false;
+	b.x = 0;
+	b.y = 0;
+	b.width = width;
+	b.height = height;
+	bGuiActive = false;
 	checkboxRect.set(1, 1, b.height - 2, b.height - 2);
 
 	registerMouseEvents();
@@ -48,8 +69,12 @@ bool ofxButton::mouseDragged(ofMouseEventArgs & args){
 	return ofxToggle::mouseDragged(args);
 }
 
+ofAbstractParameter & ofxButton::getParameter(){
+	return parameter;
+}
+
 void ofxButton::valueChanged(bool & v){
 	if(!v){
-		ofNotifyEvent(triggerEvent, this);
+		parameter.trigger();
 	}
 }

addons/ofxGui/src/ofxGuiGroup.cpp
@@ -74,6 +74,9 @@ ofxGuiGroup * ofxGuiGroup::setup(const ofParameterGroup & _parameters, const std
 		}else if(type == typeid(ofParameter <bool> ).name()){
 			auto p = _parameters.getBool(i);
 			add(p);
+		}else if(type == typeid(ofParameter <void> ).name()){
+			auto p = _parameters.getVoid(i);
+			add(p);
 		}else if(type == typeid(ofParameter <ofDefaultVec2> ).name()){
 			auto p = _parameters.getVec2f(i);
 			add(p);
@@ -157,6 +160,10 @@ void ofxGuiGroup::add(const ofParameterGroup & parameters){
 	ofxGuiGroup * panel = new ofxGuiGroup(parameters);
 	panel->parent = this;
 	add(panel);
+}
+
+void ofxGuiGroup::add(ofParameter <void> & parameter){
+	add(new ofxButton(parameter, b.width));
 }
 
 void ofxGuiGroup::add(ofParameter <bool> & parameter){

addons/ofxGui/src/ofxGuiGroup.h
@@ -22,6 +22,7 @@ class ofxGuiGroup : public ofxBaseGui {
 		typename std::enable_if<std::is_arithmetic<T>::value, void>::type add(ofParameter<T> & p){
 			add(new ofxSlider<T>(p));
 		}
+		void add(ofParameter <void> & parameter);
 		void add(ofParameter <bool> & parameter);
 		void add(ofParameter <std::string> & parameter);
 		void add(ofParameter <ofVec2f> & parameter);

libs/openFrameworks/types/ofParameter.cpp
@@ -96,6 +96,10 @@ void ofParameter<void>::trigger(){
 	ofNotifyEvent(obj->changedE,this);
 }
 
+void ofParameter<void>::trigger(const void * sender){
+	ofNotifyEvent(obj->changedE,sender);
+}
+
 void ofParameter<void>::enableEvents(){
 	obj->changedE.enable();
 }

libs/openFrameworks/types/ofParameter.h
@@ -100,6 +100,7 @@ public:
 
 	void clear();
 
+	const ofParameter<void> & getVoid(const string& name) const;
 	const ofParameter<bool> & getBool(const string& name) const;
 	const ofParameter<int> & getInt(const string& name) const;
 	const ofParameter<float> & getFloat(const string& name) const;
@@ -115,6 +116,7 @@ public:
 	const ofParameterGroup & getGroup(const string& name) const;
 
 
+	const ofParameter<void> & getVoid(std::size_t pos) const;
 	const ofParameter<bool> & getBool(std::size_t pos) const;
 	const ofParameter<int> & getInt(std::size_t pos) const;
 	const ofParameter<float> & getFloat(std::size_t pos) const;
@@ -129,6 +131,7 @@ public:
 	const ofParameter<ofFloatColor> & getFloatColor(std::size_t pos) const;
 	const ofParameterGroup & getGroup(std::size_t pos) const;
 
+	ofParameter<void> & getVoid(const string& name);
 	ofParameter<bool> & getBool(const string& name);
 	ofParameter<int> & getInt(const string& name);
 	ofParameter<float> & getFloat(const string& name);
@@ -144,6 +147,7 @@ public:
 	ofParameterGroup & getGroup(const string& name);
 
 
+	ofParameter<void> & getVoid(std::size_t pos);
 	ofParameter<bool> & getBool(std::size_t pos);
 	ofParameter<int> & getInt(std::size_t pos);
 	ofParameter<float> & getFloat(std::size_t pos);
@@ -957,6 +961,7 @@ public:
 	}
 
 	void trigger();
+	void trigger(const void * sender);
 
 	void enableEvents();
 	void disableEvents();

libs/openFrameworks/types/ofParameterGroup.cpp
@@ -45,6 +45,10 @@ void ofParameterGroup::clear(){
 	obj.reset(new Value);
 }
 
+const ofParameter<void> & ofParameterGroup::getVoid(const string& name) const	{
+	return get<void>(name);
+}
+
 const ofParameter<bool> & ofParameterGroup::getBool(const string& name) const	{
 	return get<bool>(name);
 }
@@ -95,6 +99,10 @@ const ofParameter<ofFloatColor> & ofParameterGroup::getFloatColor(const string& 
 
 const ofParameterGroup & ofParameterGroup::getGroup(const string& name) const{
 	return static_cast<const ofParameterGroup&>(get(name));
+}
+
+const ofParameter<void> & ofParameterGroup::getVoid(std::size_t pos) const{
+	return get<void>(pos);
 }
 
 const ofParameter<bool> & ofParameterGroup::getBool(std::size_t pos) const{
@@ -158,6 +166,10 @@ const ofParameterGroup & ofParameterGroup::getGroup(std::size_t pos) const{
 	}
 }
 
+ofParameter<void> & ofParameterGroup::getVoid(const string& name){
+	return get<void>(name);
+}
+
 ofParameter<bool> & ofParameterGroup::getBool(const string& name){
 	return get<bool>(name);
 }
@@ -208,6 +220,10 @@ ofParameter<ofFloatColor> & ofParameterGroup::getFloatColor(const string& name){
 
 ofParameterGroup & ofParameterGroup::getGroup(const string& name){
 	return static_cast<ofParameterGroup& >(get(name));
+}
+
+ofParameter<void> & ofParameterGroup::getVoid(std::size_t pos){
+	return get<void>(pos);
 }
 
 ofParameter<bool> & ofParameterGroup::getBool(std::size_t pos){

commit 4ae50b370b9a45d2203ecf7e48465bd426644aa7

Fixed typo introduced in 3337737fc571f2ff86a4673b40f951201d893e3a

addons/ofxGui/src/ofxButton.h
@@ -25,7 +25,7 @@ public:
 
 	template<class ListenerClass, typename ListenerMethod>
 	void removeListener(ListenerClass * listener, ListenerMethod method){
-		value.addListener(listener,method);
+		value.removeListener(listener,method);
 	}
 	virtual ofAbstractParameter & getParameter();
 