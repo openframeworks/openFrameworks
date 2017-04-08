b9ef7d22840e39e2d8099549001f4496da4fb541
Ingo Randolf <ingo.randolf@servus.at>
2017-04-08 22:16:06 +0200

    add videostreaming support

--- libs/openFrameworks/video/ofAVFoundationVideoPlayer.h
+++ libs/openFrameworks/video/ofAVFoundationVideoPlayer.h
@@ -81,6 +81,7 @@ typedef enum _playerLoopType{
     BOOL bSampleVideo; // default to YES
     BOOL bSampleAudio; // default to NO
 	BOOL bIsUnloaded;
+	BOOL bStream;
 	
 	NSLock* asyncLock;
 	NSCondition* deallocCond;
@@ -161,5 +162,7 @@ typedef enum _playerLoopType{
 - (BOOL)getAutoplay;
 - (void)setWillBeUpdatedExternally:(BOOL)value;
 - (void)close;
+- (void)setStreaming:(BOOL)value;
+
 
 @end
