517843280d6ba461b1b7ed3f496d2fa09c919fc0
arturo castro <arturo@openframeworks.cc>
2017-04-11 20:53:56 +0200

    ofTime & ofTimer: fixes for new time modes in several platforms

--- libs/openFrameworks/utils/ofUtils.h
+++ libs/openFrameworks/utils/ofUtils.h
@@ -91,12 +91,47 @@ struct ofTime{
 		FixedRate,
 	} mode = System;
 
-	uint64_t getAsMilliseconds();
-	uint64_t getAsMicroseconds();
-	uint64_t getAsNanoseconds();
-	double getAsSeconds();
-	std::chrono::time_point<std::chrono::nanoseconds> getAsTimePoint();
-	std::chrono::nanoseconds operator-(const ofTime&);
+	uint64_t getAsMilliseconds() const;
+	uint64_t getAsMicroseconds() const;
+	uint64_t getAsNanoseconds() const;
+	double getAsSeconds() const;
+#ifdef TARGET_LINUX
+	timespec getAsTimespec() const;
+#endif
+
+	std::chrono::time_point<std::chrono::nanoseconds> getAsTimePoint() const;
+	std::chrono::nanoseconds operator-(const ofTime&) const;
+	bool operator<(const ofTime&) const;
+	bool operator>(const ofTime&) const;
+	bool operator<=(const ofTime&) const;
+	bool operator>=(const ofTime&) const;
+
+	template<typename rep, typename ratio>
+	ofTime operator+(const std::chrono::duration<rep,ratio> & duration) const{
+		constexpr uint64_t NANOS_PER_SEC = 1000000000ll;
+		auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+		ofTime t = *this;
+		t.nanoseconds += ns.count();
+		if(this->nanoseconds>NANOS_PER_SEC){
+			uint64_t secs = this->nanoseconds / NANOS_PER_SEC;
+			t.nanoseconds -= NANOS_PER_SEC*secs;
+			t.seconds+=secs;
+		}
+		return t;
+	}
+
+	template<typename rep, typename ratio>
+	ofTime &operator+=(const std::chrono::duration<rep,ratio> & duration){
+		constexpr uint64_t NANOS_PER_SEC = 1000000000ll;
+		auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(duration);
+		this->nanoseconds += ns.count();
+		if(this->nanoseconds>NANOS_PER_SEC){
+			uint64_t secs = this->nanoseconds / NANOS_PER_SEC;
+			this->nanoseconds -= NANOS_PER_SEC*secs;
+			this->seconds+=secs;
+		}
+		return *this;
+	}
 };
 
 /// \brief Get the system time.
