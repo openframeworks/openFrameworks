0940d475f77fcc5a338c4e3f2892fbac62301054
arturo castro <arturo@openframeworks.cc>
2017-04-21 13:25:21 +0200

    ofxSlider: add an input field enabled by right pressing the slider

--- addons/ofxGui/src/ofxSlider.cpp
+++ addons/ofxGui/src/ofxSlider.cpp
@@ -21,6 +21,9 @@ ofxSlider<Type>::ofxSlider(ofParameter<Type> _val, float width, float height){
 
 template<typename Type>
 ofxSlider<Type>* ofxSlider<Type>::setup(ofParameter<Type> _val, float width, float height){
+	listener = input.leftFocus.newListener([this]{
+		state = Slider;
+	});
 	bUpdateOnReleaseOnly = false;
 	value.makeReferenceTo(_val);
 	b.x = 0;
@@ -32,6 +35,8 @@ ofxSlider<Type>* ofxSlider<Type>::setup(ofParameter<Type> _val, float width, flo
 
 	value.addListener(this,&ofxSlider::valueChanged);
 	registerMouseEvents();
+
+	input.setup(_val, width, height);
 	return this;
 }
 
@@ -62,44 +67,68 @@ Type ofxSlider<Type>::getMax(){
 }
 
 template<typename Type>
-bool ofxSlider<Type>::mouseMoved(ofMouseEventArgs & args){
-	mouseInside = isGuiDrawing() && b.inside(ofPoint(args.x,args.y));
+bool ofxSlider<Type>::mouseMoved(ofMouseEventArgs & mouse){
+	mouseInside = isGuiDrawing() && b.inside(mouse);
 	return mouseInside;
 }
 
 template<typename Type>
-bool ofxSlider<Type>::mousePressed(ofMouseEventArgs & args){
-	if(bUpdateOnReleaseOnly){
-		value.disableEvents();
-	}
-	if(setValue(args.x, args.y, true)){
-		return true;
+bool ofxSlider<Type>::mousePressed(ofMouseEventArgs & mouse){
+	if(state==Slider){
+		if(mouse.button == OF_MOUSE_BUTTON_RIGHT){
+			if(b.inside(mouse)){
+				state = Input;
+				auto mouseLeft = mouse;
+				mouseLeft.button = OF_MOUSE_BUTTON_LEFT;
+				input.mousePressed(mouseLeft);
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			if(bUpdateOnReleaseOnly){
+				value.disableEvents();
+			}
+			if(setValue(mouse.x, mouse.y, true)){
+				return true;
+			}else{
+				return false;
+			}
+		}
 	}else{
-		return false;
+		return isGuiDrawing() && input.mousePressed(mouse);
 	}
 }
 
 template<typename Type>
-bool ofxSlider<Type>::mouseDragged(ofMouseEventArgs & args){
-	if(setValue(args.x, args.y, false)){
-		return true;
+bool ofxSlider<Type>::mouseDragged(ofMouseEventArgs & mouse){
+	if(state==Slider){
+		if(setValue(mouse.x, mouse.y, false)){
+			return true;
+		}else{
+			return false;
+		}
 	}else{
-		return false;
+		return isGuiDrawing() && input.mouseDragged(mouse);
 	}
 }
 
 template<typename Type>
-bool ofxSlider<Type>::mouseReleased(ofMouseEventArgs & args){
-	if(bUpdateOnReleaseOnly){
-		value.enableEvents();
-	}
-	bool attended = setValue(args.x, args.y, false);
+bool ofxSlider<Type>::mouseReleased(ofMouseEventArgs & mouse){
+	if(state==Slider){
+		if(bUpdateOnReleaseOnly){
+			value.enableEvents();
+		}
+		bool attended = setValue(mouse.x, mouse.y, false);
 
-	bGuiActive = false;
-	if(attended){
-		return true;
+		bGuiActive = false;
+		if(attended){
+			return true;
+		}else{
+			return false;
+		}
 	}else{
-		return false;
+		return isGuiDrawing() && input.mouseReleased(mouse);
 	}
 }
 
@@ -121,16 +150,20 @@ getRange(Type min, Type max, float width){
 
 template<typename Type>
 bool ofxSlider<Type>::mouseScrolled(ofMouseEventArgs & args){
-	if(mouseInside){
-		if(args.scrollY>0 || args.scrollY<0){
-			double range = getRange(value.getMin(),value.getMax(),b.width);
-			Type newValue = value + ofMap(args.scrollY,-1,1,-range, range);
-			newValue = ofClamp(newValue,value.getMin(),value.getMax());
-			value = newValue;
+	if(state==Slider){
+		if(mouseInside){
+			if(args.scrollY>0 || args.scrollY<0){
+				double range = getRange(value.getMin(),value.getMax(),b.width);
+				Type newValue = value + ofMap(args.scrollY,-1,1,-range, range);
+				newValue = ofClamp(newValue,value.getMin(),value.getMax());
+				value = newValue;
+			}
+			return true;
+		}else{
+			return false;
 		}
-		return true;
 	}else{
-		return false;
+		return isGuiDrawing() && input.mouseScrolled(args);
 	}
 }
 
@@ -160,6 +193,7 @@ void ofxSlider<Type>::generateDraw(){
 	bar.rectangle(b.x+1, b.y+1, valAsPct, b.height-2);
 
 	generateText();
+	input.generateDraw();
 }
 
 
@@ -179,24 +213,28 @@ void ofxSlider<unsigned char>::generateText(){
 
 template<typename Type>
 void ofxSlider<Type>::render(){
-	ofColor c = ofGetStyle().color;
+	if(state==Slider){
+		ofColor c = ofGetStyle().color;
 
-	bg.draw();
-	bar.draw();
+		bg.draw();
+		bar.draw();
 
-	ofBlendMode blendMode = ofGetStyle().blendingMode;
-	if(blendMode!=OF_BLENDMODE_ALPHA){
-		ofEnableAlphaBlending();
-	}
-	ofSetColor(thisTextColor);
+		ofBlendMode blendMode = ofGetStyle().blendingMode;
+		if(blendMode!=OF_BLENDMODE_ALPHA){
+			ofEnableAlphaBlending();
+		}
+		ofSetColor(thisTextColor);
 
-	bindFontTexture();
-	textMesh.draw();
-	unbindFontTexture();
+		bindFontTexture();
+		textMesh.draw();
+		unbindFontTexture();
 
-	ofSetColor(c);
-	if(blendMode!=OF_BLENDMODE_ALPHA){
-		ofEnableBlendMode(blendMode);
+		ofSetColor(c);
+		if(blendMode!=OF_BLENDMODE_ALPHA){
+			ofEnableBlendMode(blendMode);
+		}
+	}else{
+		input.draw();
 	}
 }
 
@@ -235,6 +273,36 @@ void ofxSlider<Type>::setUpdateOnReleaseOnly(bool _bUpdateOnReleaseOnly){
 template<typename Type>
 void ofxSlider<Type>::valueChanged(Type & value){
     setNeedsRedraw();
+}
+
+template<typename Type>
+void ofxSlider<Type>::setPosition(const ofPoint & p){
+	ofxBaseGui::setPosition(p);
+	input.setPosition(p);
+}
+
+template<typename Type>
+void ofxSlider<Type>::setPosition(float x, float y){
+	ofxBaseGui::setPosition(x, y);
+	input.setPosition(x, y);
+}
+
+template<typename Type>
+void ofxSlider<Type>::setSize(float w, float h){
+	ofxBaseGui::setSize(w, h);
+	input.setSize(w, h);
+}
+
+template<typename Type>
+void ofxSlider<Type>::setShape(ofRectangle r){
+	ofxBaseGui::setShape(r);
+	input.setShape(r);
+}
+
+template<typename Type>
+void ofxSlider<Type>::setShape(float x, float y, float w, float h){
+	ofxBaseGui::setShape(x, y, w, h);
+	input.setShape(x, y, w, h);
 }
 
 template class ofxSlider<int8_t>;
