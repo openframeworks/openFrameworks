ce079adcb3c6eb9e44c0afc7d48da65d0c3a89f0
arturo castro <arturo@openframeworks.cc>
2018-01-08 15:36:32 +0100

    refactor ccache config for osx based targets to ccache.sh script

--- scripts/ci/ccache.sh
+++ scripts/ci/ccache.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
+echo "Using ccache"
+if [ "$TARGET" == "osx" ] || [ "$TARGET" == "ios" ] || [ "$TARGET" == "tvos" ]; then
+	# CCACHE_PATH="$(dirname $(which ccache gcc | sed -n 2p))"
+	# export PATH="$CCACHE_PATH:$PATH"
 
-if [ "$USE_CCACHE" = true ] ; then
-        echo "Using ccache"
-        if [ "$TARGET" == "osx" ] || [ "$TARGET" == "ios" ] || [ "$TARGET" == "tvos" ]; then
-		CCACHE_PATH="$(dirname $(which ccache gcc | sed -n 2p))"
-		export PATH="$CCACHE_PATH:$PATH"         
-	else
-		export PATH="/usr/lib/ccache:$PATH"
-	fi
+    CCACHE=$(which ccache)
+    CLANGXX=$(xcodebuild -find clang++)
+    CLANG=$(xcodebuild -find clang)
+    cat >$PWD/clang++ <<< EOF
+         $CCACHE $CLANGXX
+EOF
+    cat >$PWD/clang <<< EOF2
+        $CCACHE $CLANG
+EOF2
+
+    chmod 755 clang++
+    chmod 755 clang
+
+    export CXX="$PWD/clang++"
+    export CC="$PWD/clang"
+else
+	export PATH="/usr/lib/ccache:$PATH"
 fi
--- scripts/ci/ios/build.sh
+++ scripts/ci/ios/build.sh
@@ -2,10 +2,6 @@
 set -ev
 echo "Building openFrameworks - iOS Template Project"
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
-# source $ROOT/scripts/ci/ccache.sh
-export CXX="$(which ccache)"
-export CXXFLAGS="$(xcodebuild -find clang++)"
-export CC="$(which ccache)"
-export CFLAGS="$(xcodebuild -find clang)"
+source $ROOT/scripts/ci/ccache.sh
 
 xcodebuild -project "$ROOT/scripts/templates/ios/emptyExample.xcodeproj"  -target emptyExample -sdk iphonesimulator ONLY_ACTIVE_ARCH=NO
