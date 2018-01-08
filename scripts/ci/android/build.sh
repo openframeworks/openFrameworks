6c86a4579d697715fa26cd38f0f34c59ce5f5088
arturo castro <arturo@openframeworks.cc>
2018-01-08 16:17:38 +0100

    adding gradle cache for android

--- scripts/ci/android/build.sh
+++ scripts/ci/android/build.sh
@@ -11,6 +11,6 @@ gradle wrapper --gradle-version 3.3
 #~/projectGenerator/projectGenerator_linux -p"android" -o"$ROOT" $ROOT/examples/android/androidImageExample
 
 echo "ANDROID_NDK_HOME: $ANDROID_NDK_HOME"
-./gradlew -b $ROOT/examples/android/androidEmptyExample/build.gradle compileArm7DebugSources
+./gradlew --build-cache -b $ROOT/examples/android/androidEmptyExample/build.gradle compileArm7DebugSources
 #./gradlew -b $ROOT/examples/android/androidOpenCVExample/build.gradle compileArm7DebugSources
 #./gradlew -b $ROOT/examples/android/androidImageExample/build.gradle compileArm7DebugSources
