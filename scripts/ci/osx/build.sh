15fb105fc79140c104f5bb7528dab6dcaa01b214
arturo castro <arturo@openframeworks.cc>
2018-01-08 13:51:52 +0100

    fixing ccache for osx, ios, tvos and linux armv7

--- scripts/ci/osx/build.sh
+++ scripts/ci/osx/build.sh
@@ -2,8 +2,10 @@
 set -ev
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 # source $ROOT/scripts/ci/ccache.sh
-export CXX="$(which ccache) $(xcodebuild -find clang++)"
-export CC="$(which ccache) $(xcodebuild -find clang)"
+export CXX="$(which ccache)"
+export CXXFLAGS="$(xcodebuild -find clang++)"
+export CC="$(which ccache)"
+export CFLAGS="$(xcodebuild -find clang)"
 echo "**** Building oF + emptyExample - OSX Template Project ****"
 xcodebuild -configuration Release -target emptyExample -project "$ROOT/scripts/templates/osx/emptyExample.xcodeproj"
 echo "**** Done building emptyExample ****"
