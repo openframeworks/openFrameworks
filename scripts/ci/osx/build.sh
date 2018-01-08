c3c780d17358e7ce71da222bc78f187c323eec0f
arturo castro <arturo@openframeworks.cc>
2018-01-08 14:23:28 +0100

    fixing osx ccache

--- scripts/ci/osx/build.sh
+++ scripts/ci/osx/build.sh
@@ -2,10 +2,18 @@
 set -ev
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 # source $ROOT/scripts/ci/ccache.sh
-export CXX="$(which ccache)"
-export CXXFLAGS="$(xcodebuild -find clang++)"
-export CC="$(which ccache)"
-export CFLAGS="$(xcodebuild -find clang)"
+cat >$PWD/clang++ <<< EOF
+    $(which ccache) $(xcodebuild -find clang++)
+EOF
+cat >$PWD/clang <<< EOF
+    $(which ccache) $(xcodebuild -find clang)
+EOF
+
+chmod 755 clang++
+chmod 755 clang
+
+export CXX="$PWD/clang++"
+export CC="$PWD/clang"
 echo "**** Building oF + emptyExample - OSX Template Project ****"
 xcodebuild -configuration Release -target emptyExample -project "$ROOT/scripts/templates/osx/emptyExample.xcodeproj"
 echo "**** Done building emptyExample ****"
