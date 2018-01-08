0178a33599a57be63f931e363859fb476129635b
arturo castro <arturo@openframeworks.cc>
2018-01-08 15:36:32 +0100

    refactor ccache config for osx based targets to ccache.sh script

--- scripts/ci/osx/build.sh
+++ scripts/ci/osx/build.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 set -ev
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
-# source $ROOT/scripts/ci/ccache.sh
-CCACHE=$(which ccache)
-CLANGXX=$(xcodebuild -find clang++)
-CLANG=$(xcodebuild -find clang)
-cat >$PWD/clang++ <<< EOF
-     $CCACHE $CLANGXX
-EOF
-cat >$PWD/clang <<< EOF2
-    $CCACHE $CLANG
-EOF2
-
-chmod 755 clang++
-chmod 755 clang
-
-export CXX="$PWD/clang++"
-export CC="$PWD/clang"
+source $ROOT/scripts/ci/ccache.sh
 echo "**** Building oF + emptyExample - OSX Template Project ****"
 xcodebuild -configuration Release -target emptyExample -project "$ROOT/scripts/templates/osx/emptyExample.xcodeproj"
 echo "**** Done building emptyExample ****"
