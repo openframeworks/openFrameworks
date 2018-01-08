bffa0215c180d053bea396fc8951885ffe18df88
arturo castro <arturo@openframeworks.cc>
2018-01-08 12:40:16 +0100

    fixing ccache for ios, tvos and checking armv7

--- scripts/ci/tvos/build.sh
+++ scripts/ci/tvos/build.sh
@@ -3,7 +3,7 @@ set -ev
 echo "Building openFrameworks - tvOS Template Project"
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 # source $ROOT/scripts/ci/ccache.sh
-export CXX="ccache $(xcodebuild -find clang++)"
-export CC="ccache $(xcodebuild -find clang)"
+export CXX="$(which ccache) $(xcodebuild -find clang++)"
+export CC="$(which ccache) $(xcodebuild -find clang)"
 
 xcodebuild -project "$ROOT/scripts/templates/tvos/emptyExample.xcodeproj"  -target emptyExample -sdk appletvsimulator ONLY_ACTIVE_ARCH=NO
