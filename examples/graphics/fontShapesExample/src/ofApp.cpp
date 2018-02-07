8cbd8b9c5c730ba5d40276e61c5266347630c23e
arturo castro <arturo@openframeworks.cc>
2018-02-07 09:19:47 +0100

    fontShapesExample: get character as fill and contour.
    Fixes: #5863

--- examples/graphics/fontShapesExample/src/ofApp.cpp
+++ examples/graphics/fontShapesExample/src/ofApp.cpp
@@ -8,7 +8,14 @@ void ofApp::setup(){
 	testFont2.load("cooperBlack.ttf", 52, true, true, true);
 
 	letter = '$';
-	testChar = testFont.getCharacterAsPoints(letter);
+
+	bool vflip = true;
+	bool filled = true;
+	testChar = testFont.getCharacterAsPoints(letter, vflip, filled);
+
+	filled = false;
+	testCharContour = testFont.getCharacterAsPoints(letter, vflip, filled);
+
 	ofSetFullscreen(false);
 }
 
@@ -22,12 +29,6 @@ void ofApp::draw(){
 	ofSetColor(0, 90, 60);
 	ofFill();
 
-//	ofBeginShape();
-//		ofVertex(100, 100);
-//		ofVertex(200, 200);
-//		ofVertex(100, 200);
-//	ofEndShape(true);
-//
 	ofDrawBitmapString("fps: "+ofToString(ofGetFrameRate()), 10, 10);
 	ofDrawBitmapString("press a key to see it as a texture and as a vector. ", 10, 24);
 
@@ -41,19 +42,17 @@ void ofApp::draw(){
 	//lets draw the key pressed as a tex and a vector both fill and no fill
 	//here we show how easy it is to get
 
-	string str = "";
-	str += char(letter);
+	string str = ofUTF8ToString(letter);
 
 	testFont.drawString(str, 50, 250);
 
 	//okay lets get the character back as shapes
-	testChar.setFilled(true);
-    testChar.draw(200,250);
-    testChar.setFilled(false);
-    testChar.draw(350,250);
+	testChar.draw(200,250);
+
+	testCharContour.draw(350,250);
 
 
-    // we can also access the individual points
+	// we can also access the individual points
 	ofFill();
 	ofPushMatrix();
 		ofTranslate(550, 250, 0);
@@ -86,8 +85,14 @@ void ofApp::draw(){
 //--------------------------------------------------------------
 void ofApp::keyPressed  (int key){
 	if(key==OF_KEY_ESC) return;
-	testChar = testFont.getCharacterAsPoints(key);
 	letter = key;
+
+	bool vflip = true;
+	bool filled = true;
+	testChar = testFont.getCharacterAsPoints(letter, vflip, filled);
+
+	filled = false;
+	testCharContour = testFont.getCharacterAsPoints(letter, vflip, filled);
 }
 
 //--------------------------------------------------------------
