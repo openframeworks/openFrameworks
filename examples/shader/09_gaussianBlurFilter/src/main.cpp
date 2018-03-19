f11974679421f3e9cd592a45ec555b03f48aa836
arturo castro <arturo@openframeworks.cc>
2018-03-19 10:21:40 +0100

    shader/gaussianBlueExample: fix gaussian kernel

--- examples/shader/09_gaussianBlurFilter/src/main.cpp
+++ examples/shader/09_gaussianBlurFilter/src/main.cpp
@@ -5,10 +5,12 @@
 //========================================================================
 int main( ){
 
-	ofGLWindowSettings settings;
-	settings.setGLVersion(3,2);
-	ofCreateWindow(settings);
-    
+//	ofGLWindowSettings settings;
+//	settings.setGLVersion(3,2);
+//	ofCreateWindow(settings);
+
+	ofSetupOpenGL(1024, 768, OF_WINDOW);
+
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
