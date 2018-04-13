bd6dd3dc5eb947bdb3b34ecebd477056804d177a
oxillo <oxillo@hotmail.com>
2018-04-13 19:20:32 +0200

    Revert to std::locale(locale.c_str())

--- libs/openFrameworks/utils/ofUtils.cpp
+++ libs/openFrameworks/utils/ofUtils.cpp
@@ -904,26 +904,24 @@ utf8::iterator<std::string::const_reverse_iterator> ofUTF8Iterator::rend() const
 //--------------------------------------------------
 // helper method to get locale from name
 static std::locale getLocale(const string & locale) {
-    std::locale loc;
+std::locale loc;
 #if defined(TARGET_WIN32) && !_MSC_VER
-    static bool printonce = true;
-    if( printonce ){
-        std::string current( setlocale(LC_ALL,NULL) );
-        setlocale (LC_ALL,"");
-        ofLogWarning("ofUtils") << "std::locale not supported. Using C locale  :" << current ;
-        printonce = false;
-    }
-    //std::string current(setlocale(LC_ALL,locale.c_str()));
+	static bool printonce = true;
+	if( printonce ){
+		std::string current( setlocale(LC_ALL,NULL) );
+		setlocale (LC_ALL,"");
+		ofLogWarning("ofUtils") << "std::locale not supported. Using C locale  :" << current ;
+		printonce = false;
+	}
 #else
-    try {
-        loc = std::locale("fr_FR.ISO-8859-1");
-    }
-    catch (...) {
-        ofLogWarning("ofUtils") << "Couldn't create locale " << locale << " using default, " << loc.name();
-        //ofLogWarning("ofUtils") << "Couldn't create locale " << locale << " using default, " << current;
-    }
+	try {
+		loc = std::locale(locale.c_str());
+	}
+	catch (...) {
+		ofLogWarning("ofUtils") << "Couldn't create locale " << locale << " using default, " << loc.name();
+	}
 #endif
-    return loc;
+	return loc;
 }
 
 //--------------------------------------------------
