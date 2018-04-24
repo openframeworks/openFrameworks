32a7d2507998b7c56158ec55bdd9d65e087b037f
Roy Macdonald <macdonald.roy@gmail.com>
2018-04-24 13:25:44 -0300

    Revert "Made ofPoint a typedef of ofDefaultVec3 instead of ofVec3f. Updated includes where needed"
    This reverts commit 1f9c4d2c56a3b9b1d295d5ee766d89cd56f9e108.

--- libs/openFrameworks/types/ofPoint.h
+++ libs/openFrameworks/types/ofPoint.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "ofMathConstants.h"
+#include "ofVec3f.h"
 
 /// \file 
 /// ofPoint is a typedef (alias) of ofVec3f
 
 /// \brief Look at ofVec3f for documentation
-typedef ofDefaultVec3 ofPoint;
+typedef ofVec3f ofPoint;
