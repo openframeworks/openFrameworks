0e720336f16b663424d37fbf0c7649e1edae3a9b
arturo castro <arturo@openframeworks.cc>
2018-05-04 17:48:03 +0200

    use gold linker in linux
    makes link times much faster on big projects

--- libs/openFrameworksCompiled/project/makefileCommon/config.linux.common.mk
+++ libs/openFrameworksCompiled/project/makefileCommon/config.linux.common.mk
@@ -174,7 +174,7 @@ endif
 #   Note: Leave a leading space when adding list items with the += operator
 ################################################################################
 
-PLATFORM_LDFLAGS = -Wl,-rpath=./libs:./bin/libs -Wl,--as-needed -Wl,--gc-sections
+PLATFORM_LDFLAGS = -Wl,-rpath=./libs:./bin/libs -Wl,--as-needed -Wl,--gc-sections -fuse-ld=gold
 
 
 
