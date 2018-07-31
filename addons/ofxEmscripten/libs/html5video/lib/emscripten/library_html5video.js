c8061eb5f5264b1ed8e84ee3d0b801d2d13ff825
jpericas22 <jpericas22@gmail.com>
2018-07-31 17:34:56 +0200

    Fixes for html5 audio and video with emscripten (#6039)
    SoundPlayer.isLoaded() was giving a wrong output on html5 audio. It returned if the loading started, not if the loading was finished. This is caused by the way the audio is fetched (asynchronous).
    
    Video grabbing from webcam was outdated and did not work on IOS11 and Safari 11.x.
    
    This fixes problems, updates the deprecated navigator.getUserMedia() to navigator.mediaDevices.getUserMedia and includes a fix to get webcam support on both IOS and Safari.

--- addons/ofxEmscripten/libs/html5video/lib/emscripten/library_html5video.js
+++ addons/ofxEmscripten/libs/html5video/lib/emscripten/library_html5video.js
@@ -3,28 +3,28 @@ var LibraryHTML5Video = {
         players: [],
         playersContexts: [],
         playersCounter: 0,
-        
+
         getNewPlayerId: function() {
           var ret = VIDEO.playersCounter++;
           return ret;
         },
-        
+
         grabbers: [],
         grabbersContexts: [],
         grabbersCounter: 0,
-        
+
         getNewGrabberId: function() {
           var ret = VIDEO.grabbersCounter++;
           return ret;
         },
-        
+
         getUserMedia: function(){
         	return navigator.getUserMedia ||
         	    navigator.webkitGetUserMedia ||
         	    navigator.mozGetUserMedia ||
         	    navigator.msGetUserMedia;
         },
-        
+
         update: function(updatePixels, video, context, dstPixels){
         	if((updatePixels || video.pixelFormat!="RGBA") && video.width!=0 && video.height!=0 && dstPixels!=0){
         		try {
@@ -55,7 +55,7 @@ var LibraryHTML5Video = {
 		            		array[i++] = (((srcPixels[j++]|0) << 1) + ((srcPixels[j]|0) << 2) + (srcPixels[j++]|0) + (srcPixels[j++]|0)) >> 3;
 		            		++j;
 		            	}
-		            	
+
 		                GLctx.bindTexture(GLctx.TEXTURE_2D, GL.textures[video.textureId]);
 		                GLctx.texImage2D(GLctx.TEXTURE_2D, 0, GLctx.LUMINANCE, video.width, video.height, 0, GLctx.LUMINANCE, GLctx.UNSIGNED_BYTE, array);
 		                GLctx.bindTexture(GLctx.TEXTURE_2D, null);
@@ -69,20 +69,20 @@ var LibraryHTML5Video = {
         	}
         }
     },
-    
+
     html5video_player_create: function(){
         var video = document.createElement('video');
         video.loop = true;
         video.pixelFormat = "RGB";
         //video.crossOrigin = 'anonymous';
-        
+
         var player_id = VIDEO.getNewPlayerId();
         VIDEO.players[player_id] = video;
         video.onloadedmetadata = function (e){
         	console.log(this.videoWidth + 'x' + this.videoHeight);
         	VIDEO.players[player_id].width = this.videoWidth;
         	VIDEO.players[player_id].height = this.videoHeight;
-            
+
     	    var videoImage = document.createElement( 'canvas' );
     	    videoImage.width = this.videoWidth;
     	    videoImage.height = this.videoHeight;
@@ -91,13 +91,13 @@ var LibraryHTML5Video = {
     	    // background color if no video present
     	    videoImageContext.fillStyle = '#000000';
     	    videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );
-    	    
+
     	    VIDEO.playersContexts[player_id] = videoImageContext;
         };
-	    
+
         return player_id;
     },
-    
+
     html5video_player_delete: function(id){
     	VIDEO.players[id] = null;
     },
@@ -118,15 +118,15 @@ var LibraryHTML5Video = {
         VIDEO.players[id].textureId = texId;
         VIDEO.players[id].load();
     },
-    
+
     html5video_player_pixel_format: function(id){
         return allocate(intArrayFromString(VIDEO.players[id].pixelFormat), 'i8', ALLOC_STACK);
     },
-    
+
     html5video_player_set_pixel_format: function(id, format){
         VIDEO.players[id].pixelFormat = Pointer_stringify(format);
     },
-    
+
     html5video_player_update__deps: ['$GL'],
     html5video_player_update: function(id,update_pixels,pixels){
         var player = VIDEO.players[id];
@@ -140,93 +140,90 @@ var LibraryHTML5Video = {
         	return false;
         }
     },
-    
+
     html5video_player_texture_id: function(id){
         return VIDEO.players[id].textureId;
     },
-    
+
     html5video_player_width: function(id){
         return VIDEO.players[id].width;
     },
-    
+
     html5video_player_height: function(id){
         return VIDEO.players[id].height;
     },
-    
+
     html5video_player_play: function(id){
         console.log('play');
         VIDEO.players[id].play();
     },
-    
+
     html5video_player_pause: function(id){
         VIDEO.players[id].pause();
     },
-    
+
     html5video_player_stop: function(id){
         VIDEO.players[id].pause();
     },
-    
+
     html5video_player_is_paused: function(id){
         return VIDEO.players[id].paused;
     },
-    
+
     html5video_player_ready_state: function(id){
         return VIDEO.players[id].readyState;
     },
-    
+
     html5video_player_duration: function(id){
         return VIDEO.players[id].duration;
     },
-    
+
     html5video_player_current_time: function(id){
         return VIDEO.players[id].currentTime;
     },
-    
+
     html5video_player_set_current_time: function(id,time){
         VIDEO.players[id].currentTime = time;
     },
-    
+
     html5video_player_ended: function(id){
         return VIDEO.players[id].ended;
     },
-    
+
     html5video_player_playback_rate: function(id){
         return VIDEO.players[id].playbackRate;
     },
-    
+
     html5video_player_set_playback_rate: function(id,rate){
         VIDEO.players[id].playbackRate = rate;
     },
-    
+
     html5video_player_volume: function(id){
         return VIDEO.players[id].volume;
     },
-    
+
     html5video_player_set_volume: function(id,volume){
         VIDEO.players[id].volume = volume;
     },
-    
+
     html5video_player_set_loop: function(id,loop){
         VIDEO.players[id].loop = loop;
     },
-    
+
     html5video_player_loop: function(id){
         return VIDEO.players[id].loop;
     },
-    
+
     html5video_grabber_create: function(){
-    	if( VIDEO.getUserMedia() ){
+
 	        var video = document.createElement('video');
 			video.autoplay=true;
 			video.pixelFormat = "RGB";
-	        
+
 	        var grabber_id = VIDEO.getNewGrabberId();
 	        VIDEO.grabbers[grabber_id] = video;
 	        return grabber_id;
-    	}else{
-    		console.log("coudln't create grabber");
-    		return -1;
-    	}
+    	
     },
 
     html5video_grabber_init__deps: ['$GL'],
@@ -234,7 +231,7 @@ var LibraryHTML5Video = {
     	if(id!=-1){
         	VIDEO.grabbers[id].width = w;
         	VIDEO.grabbers[id].height = h;
-        	
+
     	    var videoImage = document.createElement( 'canvas' );
     	    videoImage.width = w;
     	    videoImage.height = h;
@@ -243,9 +240,9 @@ var LibraryHTML5Video = {
     	    // background color if no video present
     	    videoImageContext.fillStyle = '#000000';
     	    videoImageContext.fillRect( 0, 0, w, h );
-    	    
+
     	    VIDEO.grabbersContexts[id] = videoImageContext;
-        	
+
     		var errorCallback = function(e) {
     			console.log('Couldn\'t init grabber!', e);
     		};
@@ -266,38 +263,36 @@ var LibraryHTML5Video = {
 		    		        maxWidth: w,
 		    		        maxHeight: h,
 		    		    },
-    					optional: [ 
+    					optional: [
     					    { minFrameRate: framerate }
 		    		    ]
 	    		    }
     			};
     		}
 
-    		var getUserMedia = VIDEO.getUserMedia().bind(navigator);
-    		getUserMedia(constraints, function(stream) {
-				VIDEO.grabbers[id].src = window.URL.createObjectURL(stream);
-				var texId = GL.getNewId(GL.textures);
-		        var texture = GLctx.createTexture();
-		        texture.name = texId;
-		        GL.textures[texId] = texture;
-		        GLctx.bindTexture(GLctx.TEXTURE_2D, texture);
-		        GLctx.texParameteri(GLctx.TEXTURE_2D, GLctx.TEXTURE_MAG_FILTER, GLctx.LINEAR);
-		        GLctx.texParameteri(GLctx.TEXTURE_2D, GLctx.TEXTURE_MIN_FILTER, GLctx.LINEAR);
-		        GLctx.texParameteri(GLctx.TEXTURE_2D, GLctx.TEXTURE_WRAP_S, GLctx.CLAMP_TO_EDGE);
-		        GLctx.texParameteri(GLctx.TEXTURE_2D, GLctx.TEXTURE_WRAP_T, GLctx.CLAMP_TO_EDGE);
-		        VIDEO.grabbers[id].textureId = texId;
-			}, errorCallback);
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then(function(stream) {
+          window.stream = stream;
+          VIDEO.grabbers[id].srcObject = stream
+          VIDEO.grabbers[id].onloadedmetadata = function (e){
+            VIDEO.grabbers[id].play();
+          }
+        })
+        .catch(function(err) {
+          console.log(e);
+        });
+
     	}
     },
-    
+
     html5video_grabber_pixel_format: function(id){
         return allocate(intArrayFromString(VIDEO.grabbers[id].pixelFormat), 'i8', ALLOC_STACK);
     },
-    
+
     html5video_grabber_set_pixel_format: function(id, format){
         VIDEO.grabbers[id].pixelFormat = Pointer_stringify(format);
     },
-    
+
     html5video_grabber_update__deps: ['$GL'],
     html5video_grabber_update: function(id,update_pixels,pixels){
         var grabber = VIDEO.grabbers[id];
@@ -308,24 +303,24 @@ var LibraryHTML5Video = {
         	return false;
         }
     },
-    
+
     html5video_grabber_texture_id: function(id){
         return VIDEO.grabbers[id].textureId;
     },
-    
+
     html5video_grabber_width: function(id){
         return VIDEO.grabbers[id].width;
     },
-    
+
     html5video_grabber_height: function(id){
         return VIDEO.grabbers[id].height;
     },
-    
+
     html5video_grabber_ready_state: function(id){
         return VIDEO.grabbers[id].readyState;
     },
-    
-    
+
+
 }
 
 
