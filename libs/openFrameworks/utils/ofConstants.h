caf5177b4a6cd12c6d517837bad986b8b9a2554d
arturo castro <arturo@openframeworks.cc>
2018-07-31 18:02:40 +0200

    ofConstants: change version pre_release to master instead of stable

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -5,7 +5,7 @@
 #define OF_VERSION_MAJOR 0
 #define OF_VERSION_MINOR 11
 #define OF_VERSION_PATCH 0
-#define OF_VERSION_PRE_RELEASE "stable"
+#define OF_VERSION_PRE_RELEASE "master"
 
 // Set to 1 for compatibility with old projects using ofVec instead of glm
 #ifndef OF_USE_LEGACY_VECTOR_MATH
