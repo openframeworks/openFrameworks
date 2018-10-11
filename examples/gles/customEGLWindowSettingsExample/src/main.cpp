5dddfe29421f3c7ccea4326cd8f8d9e8ab1e5eee
Jason Van Cleave <jvcleave@gmail.com>
2018-10-11 11:17:50 -0400

    Update main.cpp (#6150)
    example was no longer working. These changes worked for me but unsure if this is the proper way

--- examples/gles/customEGLWindowSettingsExample/src/main.cpp
+++ examples/gles/customEGLWindowSettingsExample/src/main.cpp
@@ -4,16 +4,16 @@
 
 //========================================================================
 int main( ){
-	ofAppEGLWindowSettings settings;
-
+	ofGLESWindowSettings windowSettings;
+	windowSettings.setSize(1024, 768);
+    
+	ofAppEGLWindow::Settings settings(windowSettings);
+    
 	settings.eglWindowOpacity = 127;
 	settings.frameBufferAttributes[EGL_DEPTH_SIZE]   = 0; // 0 bits for depth
 	settings.frameBufferAttributes[EGL_STENCIL_SIZE] = 0; // 0 bits for stencil
 
-	settings.width = 1024;
-	settings.height = 768;
-	settings.windowMode = OF_FULLSCREEN;
-	ofCreateWindow(settings);			// <-------- setup the GL context
+	ofCreateWindow(settings);  			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
