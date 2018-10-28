b2e451b3f38660c94f0ef811a29bca29d7263701
Jason Van Cleave <jvcleave@gmail.com>
2018-10-28 09:48:57 -0400

    Revert "Update main.cpp (#6150)" (#6160)
    This reverts commit b2a835170f72b0286d6e5ef070adbb2f4ab8105b.

--- examples/gles/customEGLWindowSettingsExample/src/main.cpp
+++ examples/gles/customEGLWindowSettingsExample/src/main.cpp
@@ -4,16 +4,16 @@
 
 //========================================================================
 int main( ){
-	ofGLESWindowSettings windowSettings;
-	windowSettings.setSize(1024, 768);
-    
-	ofAppEGLWindow::Settings settings(windowSettings);
-    
+	ofAppEGLWindowSettings settings;
+
 	settings.eglWindowOpacity = 127;
 	settings.frameBufferAttributes[EGL_DEPTH_SIZE]   = 0; // 0 bits for depth
 	settings.frameBufferAttributes[EGL_STENCIL_SIZE] = 0; // 0 bits for stencil
 
-	ofCreateWindow(settings);  			// <-------- setup the GL context
+	settings.width = 1024;
+	settings.height = 768;
+	settings.windowMode = OF_FULLSCREEN;
+	ofCreateWindow(settings);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
