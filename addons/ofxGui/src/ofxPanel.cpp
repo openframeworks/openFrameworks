adaa0f74cbd1c67bd300d68b9697563c1de77d07
Roy Macdonald <macdonald.roy@gmail.com>
2019-02-03 12:18:46 +0100

    Fix ofx gui events propagation (#6203)
    When using an `ofEasyCam` and an `ofxPanel` or `ofxGuiGroup` object,if moving the camera by dragging the mouse and the mouse was released over the gui, then when trying to change something in the gui would also change the `ofEasyCam` object. 
    
    #changelog #addons #ofxGui

--- addons/ofxGui/src/ofxPanel.cpp
+++ addons/ofxGui/src/ofxPanel.cpp
@@ -119,12 +119,7 @@ bool ofxPanel::mousePressed(ofMouseEventArgs & args){
 }
 bool ofxPanel::mouseReleased(ofMouseEventArgs & args){
     this->bGrabbed = false;
-    if(ofxGuiGroup::mouseReleased(args)) return true;
-    if(isGuiDrawing() && b.inside(ofPoint(args.x,args.y))){
-    	return true;
-    }else{
-    	return false;
-    }
+	return ofxGuiGroup::mouseReleased(args);
 }
 
 bool ofxPanel::setValue(float mx, float my, bool bCheck){
