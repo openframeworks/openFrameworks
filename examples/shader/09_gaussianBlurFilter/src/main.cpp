8db8e45fccbc886f6749d45c5021f129d350f232
Antoine Villeret <antoine.villeret+dev@gmail.com>
2019-02-04 10:54:21 +0100

    fix shaders examples for OpenGL ES (#6223)
    #changelog #opengles

--- examples/shader/09_gaussianBlurFilter/src/main.cpp
+++ examples/shader/09_gaussianBlurFilter/src/main.cpp
@@ -5,11 +5,14 @@
 //========================================================================
 int main( ){
 
-//	ofGLWindowSettings settings;
-//	settings.setGLVersion(3,2);
-//	ofCreateWindow(settings);
-
-	ofSetupOpenGL(1024, 768, OF_WINDOW);
+#ifdef OF_TARGET_OPENGLES
+	ofGLESWindowSettings settings;
+	settings.glesVersion=2;
+#else
+	ofGLWindowSettings settings;
+	settings.setGLVersion(3,2);
+#endif
+	ofCreateWindow(settings);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
