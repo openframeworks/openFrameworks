c2e933ffbfcc858dd6102df505945b28650731fe
Arturo Castro <arturo@rin.rs>
2019-03-21 14:49:45 +0100

    emscripten: just run the build script using docker
    instead of chainging it to run everything through docerk

--- scripts/ci/emscripten/build.sh
+++ scripts/ci/emscripten/build.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
 set -ev
-# capture failing exits in commands obscured behind a pipe
-set -o pipefail
-
 ROOT=$(docker exec -i emscripten pwd)
 
-run(){
-    echo "TARGET=\"emscripten\" $@"
-    docker exec -it emscripten sh -c "TARGET=\"emscripten\" $@"
-}
-
 echo "**** Building OF core ****"
-
+cd $ROOT
 # this carries over to subsequent compilations of examples
-run "sed -i \"s/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = .*/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = -g0/\" libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk"
-run "cd libs/openFrameworksCompiled/project; emmake make Debug"
+#echo "PLATFORM_CFLAGS += $CUSTOMFLAGS" >> libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
+sed -i "s/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = .*/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = -g0/" libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
+cd libs/openFrameworksCompiled/project
+emmake make Debug # USE_CCACHE=1
 
 echo "**** Building emptyExample ****"
-run "cd $ROOT/scripts/templates/linux64; emmake make Debug"
+cd $ROOT/scripts/templates/linux64
+emmake make Debug # USE_CCACHE=1
 
 echo "**** Building allAddonsExample ****"
-run "cp $ROOT/scripts/templates/linux64/Makefile $ROOT/examples/templates/allAddonsExample/"
-run "cp $ROOT/scripts/templates/linux64/config.make $ROOT/examples/templates/allAddonsExample/"
-run "sed -i s/ofxOsc// $ROOT/examples/templates/allAddonsExample/addons.make"
-run "sed -i s/ofxNetwork// $ROOT/examples/templates/allAddonsExample/addons.make"
-run "sed -i s/ofxKinect// $ROOT/examples/templates/allAddonsExample/addons.make"
-run "sed -i s/ofxThreadedImageLoader// $ROOT/examples/templates/allAddonsExample/addons.make"
+cd $ROOT
+cp scripts/templates/linux64/Makefile examples/templates/allAddonsExample/
+cp scripts/templates/linux64/config.make examples/templates/allAddonsExample/
+cd examples/templates/allAddonsExample/
+sed -i s/ofxOsc// addons.make
+sed -i s/ofxNetwork// addons.make
+sed -i s/ofxKinect// addons.make
+sed -i s/ofxThreadedImageLoader// addons.make
 
-run "sed -i 's/#include \"ofxOsc.h\"//' $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
-run "sed -i 's/#include \"ofxNetwork.h\"//' $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
-run "sed -i 's/#include \"ofxKinect.h\"//' $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
-run "sed -i 's/#include \"ofxThreadedImageLoader.h\"//' $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
+sed -i "s/#include \"ofxOsc.h\"//" src/ofApp.h
+sed -i "s/#include \"ofxNetwork.h\"//" src/ofApp.h
+sed -i "s/#include \"ofxKinect.h\"//" src/ofApp.h
+sed -i "s/#include \"ofxThreadedImageLoader.h\"//" src/ofApp.h
 
-run "sed -i \"s/ofxTCPClient client;//\" $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
-run "sed -i \"s/ofxTCPServer server;//\" $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
-run "sed -i \"s/ofxOscSender osc_sender;//\" $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
-run "sed -i \"s/ofxKinect kinect;//\" $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
-run "sed -i \"s/ofxThreadedImageLoader .*;//\" $ROOT/examples/templates/allAddonsExample/src/ofApp.h"
+sed -i "s/ofxTCPClient client;//" src/ofApp.h
+sed -i "s/ofxTCPServer server;//" src/ofApp.h
+sed -i "s/ofxOscSender osc_sender;//" src/ofApp.h
+sed -i "s/ofxKinect kinect;//" src/ofApp.h
+sed -i "s/ofxThreadedImageLoader .*;//" src/ofApp.h
 
-run "cd $ROOT/examples/templates/allAddonsExample; emmake make Debug"
+emmake make Debug # USE_CCACHE=1
