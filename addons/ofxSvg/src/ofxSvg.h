9af6e13960a142e3d208889a2741ccc027d05819
Seb Lee-Delisle <seb@leedelisle.com>
2019-05-29 16:08:58 +0100

    ofxSvg improved compatibility
    ofxSVG
    - added support for the SVG "use" command, a common feature that allows multiple instances of the same graphic elements. Also fixed an issue where line weights less that 1 unit would not render. This increased compatibility can be disabled if required by using the ofxSVG::setImprovedCompatibilityMode(bool mode) method.
    
    ofXML - provided access to the underlying pugi::xml_node method "getParent()"

--- addons/ofxSvg/src/ofxSvg.h
+++ addons/ofxSvg/src/ofxSvg.h
@@ -3,31 +3,49 @@
 //#include "ofMain.h"
 #include "ofPath.h"
 #include "ofTypes.h"
+#include "ofXml.h"
 
 class ofxSVG {
 	public: ~ofxSVG();
 
-
-		float getWidth() const {
-			return width;
-		}
-		float getHeight() const {
-			return height;
-		}
+		float getWidth() const ;
+		float getHeight() const;
+	
+		/// \brief Loads an SVG file from the provided filename.
+		///
+		/// ~~~~
 		void load(std::string path);
+	
+		/// \brief Loads an SVG from a text string.
+		///
+		/// As the underlying SVG parsing library requires a url, this method
+		/// gives you the option of providing one.
+		///
+		/// ~~~~
+		void loadFromString(std::string data, std::string url="local");
+	
+		/// \brief Enabled or disable a mode that improves compatibility.
+		///
+		/// Set to true by default, this mode adds a number of improved
+		/// SVG parsing features. Turning it off will increase performance
+		/// but fewer SVG files will work.
+		///
+		/// ~~~~
+		void setImprovedCompatibilityMode(bool enabled);
+	
 		void draw();
 
-		int getNumPath(){
-			return paths.size();
-		}
-		ofPath & getPathAt(int n){
-			return paths[n];
-		}
-
+		int getNumPath();
+		ofPath & getPathAt(int n);
+	
 		const std::vector <ofPath> & getPaths() const;
 
 	private:
 
+		void fixSvgText(std::string& xmlstring);
+	
+		bool increaseCompatibility = true;
+	
 		float width, height;
 
 		std::vector <ofPath> paths;
