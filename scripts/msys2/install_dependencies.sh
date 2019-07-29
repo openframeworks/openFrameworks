ec9ba1508debe3872173446b06d6adb2a3f67e35
Olivier XILLO <oxillo@users.noreply.github.com>
2019-07-29 11:27:45 +0200

    MSYS2 : Remove setup of PATH env var in install_dependencies (#6349)
    Fix #5740 : It's better not to modify user environment without him knowing. Moreover, setting PATH is optional. So move instructions to documentation
    
    Fix #6211 : documentation typo

--- scripts/msys2/install_dependencies.sh
+++ scripts/msys2/install_dependencies.sh
@@ -29,14 +29,6 @@ while [[ $# > 0 ]] ; do
 	exit 1
 done
 
-NOT_HAS_PATH=$(cmd /c "echo %PATH%" | grep mingw32\\bin > /dev/null; echo $?)
-if [ "$NOT_HAS_PATH" -ne "0" ]; then
-	cd /
-	MSYS2_ROOT=$(pwd)
-	MSYS2_ROOT=$(cygpath -w $MSYS2_ROOT)
-	setx PATH "%PATH%;${MSYS2_ROOT}mingw32\\bin;${MSYS2_ROOT}usr\\bin\\"
-	echo "set path to ${MSYS2_ROOT}mingw32\\bin;${MSYS2_ROOT}usr\\bin\\"
-fi
 
 #Install packages
 if [ -z ${confirm+x} ]; then
