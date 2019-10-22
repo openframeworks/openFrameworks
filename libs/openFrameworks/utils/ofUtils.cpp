9140e4d6f1f38bf1d1dc92a93cc9d15f8588da71
oxillo <oxillo@hotmail.com>
2019-10-22 15:01:37 +0200

    fix misleading-indentation warning

--- libs/openFrameworks/utils/ofUtils.cpp
+++ libs/openFrameworks/utils/ofUtils.cpp
@@ -520,11 +520,13 @@ void ofSetDataPathRoot(const std::filesystem::path& newRoot){
 
 //--------------------------------------------------
 string ofToDataPath(const std::filesystem::path & path, bool makeAbsolute){
-    if (makeAbsolute && path.is_absolute())
+    if (makeAbsolute && path.is_absolute()){
         return path.string();
+    }
     
-	if (!enableDataPath)
+	if (!enableDataPath){
         return path.string();
+	}
 
     bool hasTrailingSlash = !path.empty() && path.generic_string().back()=='/';
 
