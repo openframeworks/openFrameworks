3b15266cd6e3e68bb301ad9c2aa811fafebaff9e
Roy Macdonald <macdonald.roy@gmail.com>
2019-11-04 14:31:50 -0500

    ofxGui: Updating events to allow multiwindow functioning

--- addons/ofxGui/src/ofxBaseGui.h
+++ addons/ofxGui/src/ofxBaseGui.h
@@ -71,8 +71,8 @@ class ofxBaseGui {
 		static void loadFont(const ofTrueTypeFontSettings & fontSettings);
 		static void setUseTTF(bool bUseTTF);
 
-		void registerMouseEvents();
-		void unregisterMouseEvents();
+		virtual void registerMouseEvents();
+		virtual void unregisterMouseEvents();
 
 		virtual void sizeChangedCB();
 		virtual void setParent(ofxBaseGui * parent);
@@ -89,6 +89,7 @@ class ofxBaseGui {
 		virtual void mouseExited(ofMouseEventArgs &){
 		}
 
+		void setEvents(ofCoreEvents & events);
 	protected:
 		virtual void render() = 0;
 		virtual bool setValue(float mx, float my, bool bCheckBounds) = 0;
@@ -135,10 +136,11 @@ class ofxBaseGui {
 		static void loadStencilFromHex(ofImage & img, unsigned char * data);
 
 		void setNeedsRedraw();
-
+		ofCoreEvents * events = nullptr;
 	private:
 		bool needsRedraw;
 		unsigned long currentFrame;
 		bool bRegisteredForMouseEvents;
+	
 		//std::vector<ofEventListener> coreListeners;
 };
