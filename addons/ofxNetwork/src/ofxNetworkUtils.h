379be1d4ec5f36ba5634db90a9d4db27bec9230b
oxillo <oxillo@hotmail.com>
2019-11-16 22:06:55 +0100

    Deprecate ofxNetworkCheckErrno
    Update  documentation

--- addons/ofxNetwork/src/ofxNetworkUtils.h
+++ addons/ofxNetwork/src/ofxNetworkUtils.h
@@ -24,24 +24,19 @@
  * 
  */
 #define ofxNetworkCheckError() ofxNetworkCheckErrno(__FILE__, __LINE__)
+
+/**
+ * @brief Log the latest network error and where it happened (file and line)
+ * 
+ */
 #define ofxNetworkLogLastError() ofxNetworkLogError(ofxNetworkGetLastError(), __FILE__, __LINE__)
 
 /**
  * @brief returns error code from the last network function
  * 
- * @return int 
+ * @return int the last network error
  */
 int ofxNetworkGetLastError();
-
-
-/**
- * @brief Logs the network error err using ofLogError.
- * 
- * @param err 
- * @param file 
- * @param line 
- * @return int 
- */
 
 
 /**
@@ -54,13 +49,14 @@ int ofxNetworkGetLastError();
 void ofxNetworkLogError(int err, const char* file=__FILE__, int line=__LINE__-1);
 
 /**
- * @brief 
+ * @brief Logs the last network error and returns it; 
  * 
- * @param file 
- * @param line 
- * @return int 
+ * @deprecated use ofxNetworkLogError(ofxNetworkGetLastError(), file, line) instead
+ * 
+ * @param file the file where the error happened, generally __FILE__
+ * @param line the line where the error happened, generally __LINE__
+ * @return int the last network error
  */
-//OF_DEPRECATED_MSG("use ofxNetworkLogError(ofxNetworkGetLastError(), file, line) instead", int ofxNetworkCheckErrno(const char* file, int line) );
-int ofxNetworkCheckErrno(const char* file, int line);
+OF_DEPRECATED_MSG("use ofxNetworkLogError(ofxNetworkGetLastError(), file, line) instead", int ofxNetworkCheckErrno(const char* file, int line) );
 
 
