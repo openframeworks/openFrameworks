8b7d96d3d9bacfe6425026fb92fd11d3c922db0a
Dan Rosser <danoli3@gmail.com>
2020-11-23 16:19:24 +1100

    Download Libs Script Fix

--- scripts/ios/download_libs.sh
+++ scripts/ios/download_libs.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 ../dev/download_libs.sh -p ios -n $@
-../dev/download_libs.sh -p tvos $@
+../dev/download_libs.sh -p tvos -n $@
