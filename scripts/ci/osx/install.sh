8441df6994b0e001fc37044c6f6303d01d938082
ofTheo <theo@openframeworks.cc>
2020-12-28 16:24:02 -0800

    osx

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -4,3 +4,7 @@ brew update
 brew install gdb
 brew install ccache
 brew upgrade coreutils
+
+SDK_PATH=$(xcrun --sdk macosx --show-sdk-path)
+echo "SDK PATH IS $SDK_PATH"
+export DEVELOPER_DIR=$SDK_PATH
