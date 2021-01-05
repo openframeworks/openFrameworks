ae26604802a6f76f6b2ab98be1fd5bc86080f1c1
ofTheo <theo@openframeworks.cc>
2021-01-05 13:49:45 -0800

    better fix and with debugging

--- libs/openFrameworksCompiled/project/makefileCommon/compile.project.mk
+++ libs/openFrameworksCompiled/project/makefileCommon/compile.project.mk
@@ -430,7 +430,7 @@ endif
 	@rm -rf bin/libs
 
 after: $(TARGET_NAME)
-	@for libfile in $(OF_LIBS_PATH)/*/lib/$(PLATFORM_LIB_SUBPATH)/*.$(SHARED_LIB_EXTENSION); do if [ -e  "$(libfile)" ]; then cp $(libfile) bin/; fi done
+	@for libfile in ${OF_LIBS_PATH}/*/lib/${PLATFORM_LIB_SUBPATH}/*.${SHARED_LIB_EXTENSION}; do echo "copying shared file ${libfile}"; if [ -e ${libfile} ]; then cp ${libfile} bin/; fi done
 	@echo
 	@echo "     compiling done"
 	@echo "     to launch the application"
