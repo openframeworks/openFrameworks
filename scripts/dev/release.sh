a15b20d831103338bf0e678fed59b7bef8e58e8f
ofTheo <theo@openframeworks.cc>
2021-01-12 15:37:21 -0800

    added for release script

--- scripts/dev/release.sh
+++ scripts/dev/release.sh
@@ -51,8 +51,8 @@ git commit scripts/apothecary apps/projectGenerator -m"update submodules to late
 
 cd $(cat ~/.ofprojectgenerator/config)/scripts/dev
 
-./create_package.sh linux64 $version $branch gcc4
-./create_package.sh linux64 $version $branch gcc5
+#./create_package.sh linux64 $version $branch gcc4
+#./create_package.sh linux64 $version $branch gcc5
 ./create_package.sh linux64 $version $branch gcc6
 ./create_package.sh linuxarmv6l $version $branch
 ./create_package.sh linuxarmv7l $version $branch
