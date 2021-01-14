76d738c33783461e9456a76ff633817b85dadf4b
ofTheo <theo@openframeworks.cc>
2021-01-14 10:29:36 -0800

    better fix for resetting OF_ROOT in config.make

--- scripts/dev/create_package.sh
+++ scripts/dev/create_package.sh
@@ -198,13 +198,7 @@ function createProjectFiles {
         
         #fix config.make because the project generator is putting in the full path to the OF_ROOT as it is designed to do.
         #in this case we actually don't want to set it as the default of ../../../ is fine.
-        for example_group in $pkg_ofroot/examples/*; do
-            for example in $example_group/*; do
-                if [ -d $example ]; then
-                    sed -i 's/^OF_ROOT =.*/# OF_ROOT = ..\/..\/..\//' $example/config.make
-                fi
-            done
-        done
+        find $pkg_ofroot/examples -name "config.make" -type f -exec sed -i 's/^OF_ROOT =.*/# OF_ROOT = ..\/..\/..\//' {} \;
     elif [ "$pkg_platform" == "linuxarmv6l" ] || [ "$pkg_platform" == "linuxarmv7l" ]; then
         for example_group in $pkg_ofroot/examples/*; do
             for example in $example_group/*; do
