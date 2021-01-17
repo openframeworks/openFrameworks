267d9bf3d1bb6a1bab203d442218e3b3c4c0b3d5
Olivier XILLO <oxillo@hotmail.com>
2021-01-17 21:50:25 +0100

    core(ofXml) : fix getChildren(name)
    getChildren(name) was returning ALL elements between
    first child and last child with correct name

--- libs/openFrameworks/utils/ofXml.h
+++ libs/openFrameworks/utils/ofXml.h
@@ -250,24 +250,40 @@ public:
 	}
 
 	const ofXmlIterator& operator++(){
-		this->xml = xml.getNextSibling();
+		if( std::is_same<Base, pugi::xml_named_node_iterator>::value ){
+			this->xml = xml.getNextSibling( xml.getName() );
+		}else{
+			this->xml = xml.getNextSibling();
+		}
 		return *this;
 	}
 
 	ofXmlIterator operator++(int){
 		auto now = xml;
-		this->xml = xml.getNextSibling();
+		if( std::is_same<Base, pugi::xml_named_node_iterator>::value ){
+			this->xml = xml.getNextSibling( xml.getName() );
+		}else{
+			this->xml = xml.getNextSibling();
+		}
 		return now;
 	}
 
 	const ofXmlIterator& operator--(){
-		this->xml = xml.getPreviousSibling();
+		if( std::is_same<Base, pugi::xml_named_node_iterator>::value ){
+			this->xml = xml.getPreviousSibling( xml.getName() );
+		}else{
+			this->xml = xml.getPreviousSibling();
+		}
 		return *this;
 	}
 
 	ofXmlIterator operator--(int){
 		auto now = xml;
-		this->xml = xml.getPreviousSibling();
+		if( std::is_same<Base, pugi::xml_named_node_iterator>::value ){
+			this->xml = xml.getPreviousSibling( xml.getName() );
+		}else{
+			this->xml = xml.getPreviousSibling();
+		}
 		return now;
 	}
 	typedef It Base;
