87be32d66dc09950a3e86a6cc164338b7ecb9a0f
ofTheo <theo@openframeworks.cc>
2021-01-25 14:01:07 -0800

    trying here

--- scripts/ci/emscripten/build.sh
+++ scripts/ci/emscripten/build.sh
@@ -13,7 +13,7 @@ echo "**** Building OF core ****"
 cd $ROOT
 # this carries over to subsequent compilations of examples
 #echo "PLATFORM_CFLAGS += $CUSTOMFLAGS" >> libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
-sed -i "s/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = .*/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = -g0/" libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
+sed -i "s/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = .*/PLATFORM_OPTIMIZATION_CFLAGS_DEBUG = -g0 -c -r/" libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
 cd libs/openFrameworksCompiled/project
 emmake make Debug # USE_CCACHE=1
 
