bd33990ae76c37403209443dbc0d5a4ca68c7b8e
Dan Rosser <danoli3@gmail.com>
2021-01-26 06:44:12 +1100

    Script Downloaders : if Wget then use else curl (#5848)

---
+++ scripts/dev/downloader.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+printDownloaderHelp(){
+cat << EOF
+    Usage: download.sh [URL] [OPTIONS]
+
+    Example: download.sh http://ci.openframeworks.cc/libs/file.zip -s
+
+    Options:
+    -s, --silent                Silent download progress
+    -h, --help                  Shows this message
+EOF
+}
+
+downloader() { 
+
+	if [ -z "$1" ]
+	then printDownloaderHelp; fi
+
+    	SILENTARGS="";
+    	if [ "$2" == "-s" ]; then
+		if command -v curl 2>/dev/null; then 
+    			curl -LO --retry 20 -O -s $@; 
+    		else 
+        		wget -q $@ 2> /dev/null; fi;
+	else
+		if command -v curl 2>/dev/null; then 
+    			curl -LO --retry 20 -O --progress-bar $@; 
+    		else 
+        		wget $@ 2> /dev/null; fi;
+	fi
+
+ 
+}
+
