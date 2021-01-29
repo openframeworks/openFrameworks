c828d528ae528571e262d5df88794adf0e0a7135
Theodore Watson <theo@openframeworks.cc>
2021-01-29 10:09:33 -0800

    bugfix ofNode::setOrientation euler angles. closes #6589 (#6674)
    #changelog #math

--- libs/openFrameworks/3d/ofNode.cpp
+++ libs/openFrameworks/3d/ofNode.cpp
@@ -227,7 +227,8 @@ void ofNode::setOrientation(const glm::quat& q) {
 
 //----------------------------------------
 void ofNode::setOrientation(const glm::vec3& eulerAngles) {
-	setOrientation(glm::angleAxis(ofDegToRad(eulerAngles.x), glm::vec3(1, 0, 0)) * glm::angleAxis(ofDegToRad(eulerAngles.z), glm::vec3(0, 0, 1)) * glm::angleAxis(ofDegToRad(eulerAngles.y), glm::vec3(0, 1, 0)));
+    glm::quat q(glm::radians(eulerAngles));
+    setOrientation(q);
 }
 
 //----------------------------------------
