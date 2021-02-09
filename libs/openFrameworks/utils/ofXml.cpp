a1277f26dd081c672837b407787cd2962fee17e5
Olivier XILLO <oxillo@hotmail.com>
2021-02-09 19:32:58 +0100

    improve log messages

--- libs/openFrameworks/utils/ofXml.cpp
+++ libs/openFrameworks/utils/ofXml.cpp
@@ -23,7 +23,7 @@ bool ofXml::load(const std::filesystem::path & file){
 		xml = doc->root();
 		return true;
 	}else{
-		ofLogWarning()<< res.description();
+		ofLogWarning("ofXml") << "Cannot load file " << file << ":" << res.description();
 		return false;
 	}
 }
@@ -50,10 +50,9 @@ bool ofXml::parse(const std::string & xmlStr){
 bool ofXml::save(const std::filesystem::path & file) const{
 	if(xml == doc->root()){
 		auto res = doc->save_file(ofToDataPath(file).c_str());
-		ofLogNotice()<<"XML Save : "<< res;
-		ofLogNotice()<<this->toString();
+		ofLogVerbose("ofXml")<<"ofXML Save : "<< res;
+		ofLogVerbose("ofXml")<<this->toString();
 		return res;
-		
 	}else{
 		pugi::xml_document doc;
 		if(doc.append_copy(xml.root())){
