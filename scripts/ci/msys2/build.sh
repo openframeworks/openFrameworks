25411cb470468a6468d9f6816a89366077518e5a
ofTheo <theo@openframeworks.cc>
2021-02-10 18:55:17 -0800

    bugfix for msys2 64bit failing. closes #6683

--- scripts/ci/msys2/build.sh
+++ scripts/ci/msys2/build.sh
@@ -16,4 +16,9 @@ cd $ROOT
 cp scripts/templates/msys2/Makefile examples/templates/allAddonsExample/
 cp scripts/templates/msys2/config.make examples/templates/allAddonsExample/
 cd examples/templates/allAddonsExample/
+
+# this is maybe a fix for some weird issues where the linker runs out of address space
+echo "PROJECT_CFLAGS = -fpic -mmode=medium" >> config.make
+echo "PROJECT_OPTIMIZATION_CFLAGS_DEBUG = -Os" >> config.make
+
 make ${USE_CCACHE} -j4 Debug
