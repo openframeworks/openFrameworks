5d601f04a051495278d550e37869c4b6a0009aa2
ofTheo <theo@openframeworks.cc>
2021-02-10 20:28:04 -0800

    try Release

--- scripts/ci/msys2/build.sh
+++ scripts/ci/msys2/build.sh
@@ -5,22 +5,15 @@ source $ROOT/scripts/ci/ccache.sh
 
 echo "**** Building OF core ****"
 cd $ROOT/libs/openFrameworksCompiled/project
-make ${USE_CCACHE} -j4 Debug
+make ${USE_CCACHE} -j4 Release
 
 echo "**** Building emptyExample ****"
 cd $ROOT/scripts/templates/msys2
-make ${USE_CCACHE} -j4 Debug
+make ${USE_CCACHE} -j4 Release
 
 echo "**** Building allAddonsExample ****"
 cd $ROOT
 cp scripts/templates/msys2/Makefile examples/templates/allAddonsExample/
 cp scripts/templates/msys2/config.make examples/templates/allAddonsExample/
 cd examples/templates/allAddonsExample/
-
-# this is maybe a fix for some weird issues where the linker runs out of address space
-if [ "$MSYSTEM" == "MINGW64" ]; then
-    echo "PROJECT_CFLAGS = -fpic -mcmodel=medium" >> config.make
-    echo "PROJECT_OPTIMIZATION_CFLAGS_DEBUG = -Os" >> config.make
-fi
-
-make ${USE_CCACHE} -j4 Debug
+make ${USE_CCACHE} -j4 Release
