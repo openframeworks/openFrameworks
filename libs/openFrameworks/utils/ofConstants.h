5aab96b21666580ca34b26bc3a28d9b499c07e7d
ofTheo <theo@openframeworks.cc>
2021-02-22 21:43:33 -0800

    update changelog.md, thanks.md and ofConstants for 0.11.1

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -4,7 +4,7 @@
 //-------------------------------
 #define OF_VERSION_MAJOR 0
 #define OF_VERSION_MINOR 11
-#define OF_VERSION_PATCH 0
+#define OF_VERSION_PATCH 1
 #define OF_VERSION_PRE_RELEASE "master"
 
 // Set to 1 for compatibility with old projects using ofVec instead of glm
