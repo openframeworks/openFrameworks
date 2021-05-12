68149a7d4f33e15aa021d2bba20a52bdfc4c149c
Dimitre <dimitre@users.noreply.github.com>
2021-05-11 23:44:22 -0300

    more precise deprecated message
    following example
    eg: OF_DEPRECATED_MSG("ofAVFoundationPlayer::loadMovie() is deprecated, use load() instead.",

--- addons/ofxAssimpModelLoader/src/ofxAssimpModelLoader.h
+++ addons/ofxAssimpModelLoader/src/ofxAssimpModelLoader.h
@@ -31,8 +31,8 @@ class ofxAssimpModelLoader{
 
 		bool load(std::string modelName, bool optimize=false);
 		bool load(ofBuffer & buffer, bool optimize=false, const char * extension="");
-		OF_DEPRECATED_MSG("Use load instead", bool loadModel(std::string modelName, bool optimize=false));
-		OF_DEPRECATED_MSG("Use load instead", bool loadModel(ofBuffer & buffer, bool optimize=false, const char * extension=""));
+		OF_DEPRECATED_MSG("ofxAssimpModelLoader::loadModel() is deprecated, use load() instead.", bool loadModel(std::string modelName, bool optimize=false));
+		OF_DEPRECATED_MSG("ofxAssimpModelLoader::loadModel() is deprecated, use load() instead.", bool loadModel(ofBuffer & buffer, bool optimize=false, const char * extension=""));
 
 		void createEmptyModel();
 		void createLightsFromAiModel();
