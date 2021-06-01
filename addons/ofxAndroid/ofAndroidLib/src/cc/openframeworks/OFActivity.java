656868f52d29b441fc8bcee828d81d407323b2b9
Dan Rosser <danoli3@gmail.com>
2021-06-01 23:32:42 +1000

    Framerates and Controllers

--- addons/ofxAndroid/ofAndroidLib/src/cc/openframeworks/OFActivity.java
+++ addons/ofxAndroid/ofAndroidLib/src/cc/openframeworks/OFActivity.java
@@ -14,10 +14,14 @@ import androidx.core.content.ContextCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.view.InputDevice;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+
+import java.util.ArrayList;
 
 public abstract class OFActivity extends Activity implements DisplayManager.DisplayListener{
 
@@ -163,8 +167,10 @@ public abstract class OFActivity extends Activity implements DisplayManager.Disp
 		OFAndroidLifeCycle.setActivity(this);
 		OFAndroidLifeCycle.init();
 		OFAndroidLifeCycle.glCreate();
-		OFAndroid.deviceHighestRefreshRate((int)highestRefreshRate);
+
 		OFAndroid.deviceRefreshRate((int)currentRefreshRate);
+		OFAndroid.deviceHighestRefreshRate((int)highestRefreshRate);
+
 		//create gesture listener
 		//register the two events
 		initView();
@@ -220,22 +226,22 @@ public abstract class OFActivity extends Activity implements DisplayManager.Disp
 					int height = displayMetrics.heightPixels;
 					int width = displayMetrics.widthPixels;
 					int bar = getNavigationBarHeight();
+					if(bar > 256) bar = 0;
 					int barWidth = getNavigationBarHeight();
+					if(barWidth > 256) barWidth = 0; // fixes bug in android display metrics
 					int heightBar = displayMetrics.heightPixels + bar;
 					int widthBar = displayMetrics.widthPixels + barWidth;
-
 					int width_px = Resources.getSystem().getDisplayMetrics().widthPixels;
 					int height_px = Resources.getSystem().getDisplayMetrics().heightPixels;
 					int pixeldpi = Resources.getSystem().getDisplayMetrics().densityDpi;
-
-					Log.i("SuperHexagon", "DisplayMetrics: w/h:" +width + "x" + height + " barHeight:" + heightBar + "x barWidth:" + widthBar + " bar:" + bar + " widthBar:" + barWidth + " densityDPI:"  +pixeldpi);
+					Log.i("OF", "DisplayMetrics: w/h:" +width + "x" + height + " barHeight:" + heightBar + "x barWidth:" + widthBar + " bar:" + bar + " widthBar:" + barWidth + " densityDPI:"  +pixeldpi);
+					Log.i("OF", "DisplayRealMetrics: w/h:" +width_px + "x" + height_px + " pixeldpi:" + pixeldpi);
 
 
 					glView.setWindowResize(widthBar, heightBar);
 				}
-
-				OFAndroid.deviceHighestRefreshRate((int)highestRefreshRate);
 				OFAndroid.deviceRefreshRate((int)currentRefreshRate);
+				OFAndroid.deviceHighestRefreshRate((int)highestRefreshRate);
 			}
 		} catch(Exception exception) {
 			Log.w("OF", "onDisplayChanged Display Could not get Window for Display ", exception);
@@ -295,8 +301,8 @@ public abstract class OFActivity extends Activity implements DisplayManager.Disp
 			int height_px = Resources.getSystem().getDisplayMetrics().heightPixels;
 			int pixeldpi = Resources.getSystem().getDisplayMetrics().densityDpi;
 
-			Log.i("SuperHexagon", "DisplayMetrics: w/h:" +width + "x" + height + " barHeight:" + heightBar + "x barWidth:" + widthBar + " bar:" + bar + " widthBar:" + barWidth + " densityDPI:"  +pixeldpi);
-
+			Log.i("OF", "DisplayMetrics: w/h:" +width + "x" + height + " barHeight:" + heightBar + "x barWidth:" + widthBar + " bar:" + bar + " widthBar:" + barWidth + " densityDPI:"  +pixeldpi);
+			Log.i("OF", "DisplayRealMetrics: w/h:" +width_px + "x" + height_px + " pixeldpi:" + pixeldpi);
 
 			glView.setWindowResize(widthBar, heightBar);
 		}
@@ -312,9 +318,8 @@ public abstract class OFActivity extends Activity implements DisplayManager.Disp
 	private int getNavigationBarHeight() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
 			DisplayMetrics metrics = new DisplayMetrics();
-			getWindowManager().getDefaultDisplay().getMetrics(metrics);
-			int usableHeight = metrics.heightPixels;
 			getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+			int usableHeight = metrics.heightPixels;
 			int realHeight = metrics.heightPixels;
 			if (realHeight > usableHeight)
 				return realHeight - usableHeight;
@@ -341,35 +346,39 @@ public abstract class OFActivity extends Activity implements DisplayManager.Disp
 	
 	@Override
 	protected void onStart() {
+		Log.i("OF", "onStart");
 		super.onStart();
 		OFAndroidLifeCycle.glStart();
 
-		OFAndroid.deviceHighestRefreshRate((int)highestRefreshRate);
 		OFAndroid.deviceRefreshRate((int)currentRefreshRate);
+		OFAndroid.deviceHighestRefreshRate((int)highestRefreshRate);
 	}
 	
 	@Override
 	protected void onStop() {
+		Log.i("OF", "onStop");
 		super.onStop();
 		OFAndroidLifeCycle.glStop();
 	}
 	
 	@Override
 	protected void onRestart() {
+		Log.i("OF", "onRestart");
 		super.onRestart();
 		OFAndroidLifeCycle.glRestart();
 	}
 	
 	@Override
 	protected void onResume() {
+		Log.i("OF", "onResume");
 		super.onResume();
 		OFAndroidLifeCycle.setActivity(this);
 		if(OFAndroidLifeCycle.isInit() && mOFGlSurfaceContainer == null) {
-			Log.e("OF", "onResume mOFGlSurfaceContainer is null");
+			Log.i("OF", "onResume mOFGlSurfaceContainer is null");
 		}
 		//resetView();
 		if(OFAndroidLifeCycle.isInit() && OFAndroidLifeCycle.getGLView() == null) {
-			Log.e("OF", "onResume getGLView is null - glCreateSurface");
+			Log.i("OF", "onResume getGLView is null - glCreateSurface");
 			OFAndroid.setupGL(OFAndroid.eglVersion, true);
 		}
 
@@ -401,7 +410,7 @@ public abstract class OFActivity extends Activity implements DisplayManager.Disp
 	}
 	@Override
 	protected void onPause() {
-		Log.e("OF", "onPause");
+		Log.i("OF", "onPause");
 		super.onPause();
 		OFAndroidLifeCycle.glPause();
 	}
@@ -418,7 +427,14 @@ public abstract class OFActivity extends Activity implements DisplayManager.Disp
 	
 	@Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (OFAndroid.keyDown(keyCode, event)) {
+		Log.i("OF", "onKeyDown" + keyCode + " event:" + event.toString());
+		if ((event.getSource() & InputDevice.SOURCE_GAMEPAD)
+				== InputDevice.SOURCE_GAMEPAD) {
+			int deviceId = event.getDeviceId();
+			OFAndroid.keyDown(keyCode+400, event);
+			return true;
+		}
+		else if (OFAndroid.keyDown(keyCode, event)) {
 		    return true;
 		} else {
 		    return super.onKeyDown(keyCode, event);
@@ -427,10 +443,156 @@ public abstract class OFActivity extends Activity implements DisplayManager.Disp
     
     @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
-		if (OFAndroid.keyUp(keyCode, event)) {
+		Log.i("OF", "onKeyUp" + keyCode + " event:" + event.toString());
+
+		if ((event.getSource() & InputDevice.SOURCE_GAMEPAD)
+				== InputDevice.SOURCE_GAMEPAD) {
+			int deviceId = event.getDeviceId();
+			OFAndroid.keyUp(keyCode+400, event);
+			return true;
+		}
+		else if (OFAndroid.keyUp(keyCode, event)) {
 		    return true;
 		} else {
 		    return super.onKeyUp(keyCode, event);
 		}
     }
+
+	@Override
+	public boolean dispatchGenericMotionEvent (MotionEvent event){
+		Log.i("OF", "dispatchGenericMotionEvent" + " event:" + event.toString());
+		if (event.isFromSource(InputDevice.SOURCE_CLASS_JOYSTICK)) {
+			if (event.getAction() == MotionEvent.ACTION_MOVE) {
+				// process the joystick movement...
+				final int historySize = event.getHistorySize();
+
+				// Process the movements starting from the
+				// earliest historical position in the batch
+				for (int i = 0; i < historySize; i++) {
+					// Process the event at historical position i
+					processJoystickInput(event, i);
+				}
+				//processJoystickInput(event, -1);
+				return true;
+			}
+		}
+		if (event.isFromSource(InputDevice.SOURCE_CLASS_POINTER)) {
+			switch (event.getAction()) {
+				case MotionEvent.ACTION_HOVER_MOVE:
+					// process the mouse hover movement...
+					return true;
+				case MotionEvent.ACTION_SCROLL:
+					// process the scroll wheel movement...
+					return true;
+			}
+		}
+		return super.onGenericMotionEvent(event);
+	}
+
+	private static float getCenteredAxis(MotionEvent event,
+										 InputDevice device, int axis, int historyPos) {
+		final InputDevice.MotionRange range =
+				device.getMotionRange(axis, event.getSource());
+
+		// A joystick at rest does not always report an absolute position of
+		// (0,0). Use the getFlat() method to determine the range of values
+		// bounding the joystick axis center.
+		if (range != null) {
+			final float flat = range.getFlat();
+			final float value =
+					historyPos < 0 ? event.getAxisValue(axis):
+							event.getHistoricalAxisValue(axis, historyPos);
+
+			// Ignore axis values that are within the 'flat' region of the
+			// joystick axis center.
+			if (Math.abs(value) > flat) {
+				return value;
+			}
+		}
+		return 0;
+	}
+
+	private void processJoystickInput(MotionEvent event,
+									  int historyPos) {
+
+		InputDevice inputDevice = event.getDevice();
+
+		// Calculate the horizontal distance to move by
+		// using the input value from one of these physical controls:
+		// the left control stick, hat axis, or the right control stick.
+		float x = getCenteredAxis(event, inputDevice,
+				MotionEvent.AXIS_X, historyPos);
+		if (x == 0) {
+			x = getCenteredAxis(event, inputDevice,
+					MotionEvent.AXIS_HAT_X, historyPos);
+		}
+		if (x == 0) {
+			x = getCenteredAxis(event, inputDevice,
+					MotionEvent.AXIS_Z, historyPos);
+		}
+
+		// Calculate the vertical distance to move by
+		// using the input value from one of these physical controls:
+		// the left control stick, hat switch, or the right control stick.
+		float y = getCenteredAxis(event, inputDevice,
+				MotionEvent.AXIS_Y, historyPos);
+		if (y == 0) {
+			y = getCenteredAxis(event, inputDevice,
+					MotionEvent.AXIS_HAT_Y, historyPos);
+		}
+		if (y == 0) {
+			y = getCenteredAxis(event, inputDevice,
+					MotionEvent.AXIS_RZ, historyPos);
+		}
+		Log.i("OF", "processJoystickInput: " + " x:" + x + " y::" + y);
+	}
+
+	@Override
+	public boolean dispatchKeyEvent (KeyEvent event){
+		int deviceId = event.getDeviceId();
+		Log.i("OF", "dispatchKeyEvent" + " event:" + event.toString() + " deviceID:" + deviceId);
+
+		if ((event.getSource() & InputDevice.SOURCE_DPAD) == InputDevice.SOURCE_DPAD ||
+				(event.getSource() & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD ||
+				(event.getSource() & InputDevice.SOURCE_CLASS_JOYSTICK) == InputDevice.SOURCE_CLASS_JOYSTICK ||
+				(event.getSource() & InputDevice.SOURCE_CLASS_BUTTON) == InputDevice.SOURCE_CLASS_BUTTON ||
+				(event.getSource() & InputDevice.SOURCE_DPAD) > InputDevice.SOURCE_DPAD - 13 && (event.getSource() & InputDevice.SOURCE_DPAD)  < InputDevice.SOURCE_GAMEPAD  + 500
+		) {
+			if(event.getAction() == KeyEvent.ACTION_DOWN)
+				return OFAndroid.keyDown(event.getKeyCode()+400, event);
+			if(event.getAction() == KeyEvent.ACTION_UP)
+				return OFAndroid.keyUp(event.getKeyCode()+400, event);
+			else
+				return true;
+		}  else if ((event.getSource() & InputDevice.SOURCE_TRACKBALL) == InputDevice.SOURCE_TRACKBALL) {
+			return true;
+		} else {
+			if(event.getAction() == KeyEvent.ACTION_DOWN)
+				return OFAndroid.keyDown(event.getKeyCode(), event);
+			if(event.getAction() == KeyEvent.ACTION_UP)
+				return OFAndroid.keyUp(event.getKeyCode(), event);
+			else
+				return super.dispatchKeyEvent(event);
+		}
+	}
+
+	public ArrayList<Integer> getGameControllerIds() {
+		ArrayList<Integer> gameControllerDeviceIds = new ArrayList<Integer>();
+		int[] deviceIds = InputDevice.getDeviceIds();
+		for (int deviceId : deviceIds) {
+			InputDevice dev = InputDevice.getDevice(deviceId);
+			int sources = dev.getSources();
+
+			// Verify that the device has gamepad buttons, control sticks, or both.
+			if (((sources & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD)
+					|| ((sources & InputDevice.SOURCE_JOYSTICK)
+					== InputDevice.SOURCE_JOYSTICK)) {
+				// This device is a game controller. Store its device ID.
+				if (!gameControllerDeviceIds.contains(deviceId)) {
+					gameControllerDeviceIds.add(deviceId);
+				}
+			}
+		}
+		return gameControllerDeviceIds;
+	}
 }
