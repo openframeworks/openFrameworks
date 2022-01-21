a69f132071506df0a77ec0fa45dcde5faab164d5
sadmb <sadam@annolab.com>
2022-01-21 23:36:35 +0900

    using TARGET_OS_TV instead of TARGET_OF_TVOS

--- addons/ofxiOS/src/sound/AVSoundPlayer.m
+++ addons/ofxiOS/src/sound/AVSoundPlayer.m
@@ -34,7 +34,7 @@
 		return;
 	}
 	NSString * playbackCategory = AVAudioSessionCategoryPlayAndRecord;
-#ifdef TARGET_OF_OS_TV
+#ifdef TARGET_OS_TV
 	playbackCategory = AVAudioSessionCategoryPlayback;
 #endif
 	[[AVAudioSession sharedInstance] setCategory:playbackCategory error: nil];
@@ -42,7 +42,7 @@
     NSError * err = nil;
     // need to configure set the audio category, and override to it route the audio to the speaker
     if([audioSession respondsToSelector:@selector(setCategory:withOptions:error:)]) {
-#if defined (TARGET_OF_OS_TV) || defined (TARGET_OF_OS_WATCH)
+#if defined (TARGET_OS_TV) || defined (TARGET_OS_WATCH)
         if(![audioSession setCategory:playbackCategory
                                        withOptions:(AVAudioSessionCategoryOptionMixWithOthers)
                                 error:&err]) { err = nil; }
