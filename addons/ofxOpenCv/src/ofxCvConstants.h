2eef960db5cdcd0fe557a63977d9805b603942c4
Juan Gonzalez <j-gonzalez@email.de>
2022-02-01 13:37:35 +0100

    workaround for xlib macro issue

--- addons/ofxOpenCv/src/ofxCvConstants.h
+++ addons/ofxOpenCv/src/ofxCvConstants.h
@@ -1,25 +1,27 @@
 #pragma once
 
-#ifdef MIN 
-	#undef MIN
-#endif 
-#ifdef MAX 
-	#undef MAX 
-#endif 
+#ifdef MIN
+#undef MIN
+#endif
+#ifdef MAX
+#undef MAX
+#endif
+
+#define Status _PrivateCvStatus
 
 #include "opencv2/core/version.hpp"
 #if CV_MAJOR_VERSION < 4
-    #include "cv.h"
-    #define USE_OLD_CV
+#include "cv.h"
+#define USE_OLD_CV
 #else
-    #include "opencv2/opencv.hpp"
-    #include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/opencv.hpp"
 #endif
 
 #include <vector>
+
 #include "ofMain.h"
 
-enum ofxCvRoiMode {
-    OFX_CV_ROI_MODE_INTERSECT,
-    OFX_CV_ROI_MODE_NONINTERSECT
-};
+#undef Status
+
+enum ofxCvRoiMode { OFX_CV_ROI_MODE_INTERSECT, OFX_CV_ROI_MODE_NONINTERSECT };
