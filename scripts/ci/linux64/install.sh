6ec6406d1a600a09267cf6f0c42133e4facea2a1
ofTheo <theo@openframeworks.cc>
2022-06-02 16:32:22 -0700

    try 7

--- scripts/ci/linux64/install.sh
+++ scripts/ci/linux64/install.sh
@@ -34,8 +34,8 @@ sudo $OF_ROOT/scripts/linux/ubuntu/install_dependencies.sh -y;
 gcc -v
 g++ -v
 
-sudo apt-get install -y gcc-8 g++-8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+sudo apt-get install -y gcc-7 g++-7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 800 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 
 gcc -v
 g++ -v
