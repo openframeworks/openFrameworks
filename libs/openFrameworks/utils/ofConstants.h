7d969bf3a8fcdc11e0538fee418f4aff95ce218a
ofTheo <theo@openframeworks.cc>
2022-06-02 14:56:44 -0700

    another dumb mistake

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -458,15 +458,15 @@ std::unique_ptr<T> make_unique(Args&&... args) {
 // This may be okay but we need to test for the way C++17 is including the filesystem
 #if  OF_USING_STD_FS && !defined(OF_USE_EXPERIMENTAL_FS)
     #if defined(__cpp_lib_experimental_filesystem)
-        #define OF_USE_EXPERIMENTAL_FS = 1
+        #define OF_USE_EXPERIMENTAL_FS 1
     #elif defined(__cpp_lib_filesystem)
-        #define OF_USE_EXPERIMENTAL_FS = 0
+        #define OF_USE_EXPERIMENTAL_FS 0
     #elif !defined(__has_include)
-        #define OF_USE_EXPERIMENTAL_FS = 1
+        #define OF_USE_EXPERIMENTAL_FS 1
     #elif __has_include(<filesystem>)
-        #define OF_USE_EXPERIMENTAL_FS = 0
+        #define OF_USE_EXPERIMENTAL_FS 0
     #else
-        #define OF_USE_EXPERIMENTAL_FS = 0
+        #define OF_USE_EXPERIMENTAL_FS 0
     #endif
 #endif
 
