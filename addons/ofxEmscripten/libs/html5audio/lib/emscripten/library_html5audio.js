92ad10cfa451eab8f8c19dbc9b784b79f2e5c628
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-09-12 03:32:44 +0200

    Update library_html5audio.js

--- addons/ofxEmscripten/libs/html5audio/lib/emscripten/library_html5audio.js
+++ addons/ofxEmscripten/libs/html5audio/lib/emscripten/library_html5audio.js
@@ -31,10 +31,7 @@ var LibraryHTML5Audio = {
     	try {
 			// Fix up for prefixing
 			window.AudioContext = window.AudioContext || window.webkitAudioContext;
-			var context = new AudioContext({
-				latencyHint: 'interactive',
-				sampleRate: 44100,
-			});
+			var context = new AudioContext();
 			var id = emscriptenRegisterAudioObject(context);
 			// Fix issue with chrome autoplay policy
 			document.addEventListener('mousedown', function cb(event) {
