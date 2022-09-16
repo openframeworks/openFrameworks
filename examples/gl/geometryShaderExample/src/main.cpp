a78d8856cd2c29ba7aa0f0e4e4c633eba03bdee8
ofTheo <theo@openframeworks.cc>
2022-09-16 12:15:50 -0700

    gl examples main.cpp

--- examples/gl/geometryShaderExample/src/main.cpp
+++ examples/gl/geometryShaderExample/src/main.cpp
@@ -2,16 +2,22 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
-	ofSetupOpenGL(1400,768, OF_WINDOW);			// <-------- setup the GL context
+int main(){
+
+	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
+	ofGLWindowSettings settings;
+	settings.setSize(1024, 768);
+	settings.setGLVersion(3,2);
+	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+
+	auto window = ofCreateWindow(settings);
+	
 	if(!ofGLCheckExtension("GL_ARB_geometry_shader4") && !ofGLCheckExtension("GL_EXT_geometry_shader4") && !ofIsGLProgrammableRenderer()){
 		ofLogFatalError() << "geometry shaders not supported on this graphics card";
 		return 1;
 	}
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new ofApp());
+	ofRunApp(window, make_shared<ofApp>());
+	ofRunMainLoop();
 
 }
