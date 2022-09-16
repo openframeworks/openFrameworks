97f7ac30c8002cf8d7fa434863af19383ca7df9a
NickHardeman <nickhardeman@gmail.com>
2022-09-16 12:36:20 -0400

    update area light example with of logo

--- examples/gl/areaLightExample/src/ofApp.cpp
+++ examples/gl/areaLightExample/src/ofApp.cpp
@@ -2,19 +2,14 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	ofEnableLighting();
-	ofEnableDepthTest();
+	
 	areaLight.setup();
 	areaLight.enable();
-	areaLight.setAreaLight(120,400);
-	//areaLight.setSpotlight(80,3);
-	areaLight.setAmbientColor(ofFloatColor(0.1,0.1,0.1));
-	areaLight.setAttenuation(1.0,0.0001,0.0001);
+	areaLight.setAreaLight(400,120);
+	areaLight.setAmbientColor(ofFloatColor(0.3,0.3,0.3));
+	areaLight.setAttenuation(1.0,0.00003,0.00001);
 	areaLight.setDiffuseColor(ofFloatColor(1,1,1));
 	areaLight.setSpecularColor(ofFloatColor(1,1,1));
-	areaLight.rotateDeg(-90,glm::vec3(1,0,0));
-	areaLight.rotateDeg(30,glm::vec3(0,0,1));
-	areaLight.setPosition(0,-200,0);
 
 	ofBackground(0);
 	plane.set(20000,20000,2,2);
@@ -28,40 +23,95 @@ void ofApp::setup(){
 
 	camera.setFarClip(20000);
 	camera.move(0,0,1000);
+    
+    logoMesh.load("oflogo.ply");
+    materialMesh.setAmbientColor(ofFloatColor(0.85, 0.16, 0.43, 0.4));
+    materialMesh.setDiffuseColor(ofFloatColor(0.85, 0.16, 0.43, 1.0));
+    materialMesh.setSpecularColor(ofFloatColor(1.0,1.0,1.0,1.0));
+    materialMesh.setShininess(10);
+    
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-	//areaLight.setPosition(0,-200,0);
+void ofApp::update() {
+    float etimef = ofGetElapsedTimef();
+    float deltaTime = ofClamp( ofGetLastFrameTime(), 1.f / 10000.0, 1.f / 5.f );
+    if( mode == 0 ) {
+        float speed = ofMap( ofGetMouseX(), 100, ofGetWidth()-100, 0.1, 24, true );
+        orbitSpeed += deltaTime * speed;
+        areaLight.setPosition(cos(orbitSpeed)*-600,(sin(etimef*1.1)*200), sin(orbitSpeed)*-600);
+        areaLight.lookAt(glm::vec3(0, -150, 0));
+        areaLight.setAmbientColor(ofFloatColor(0.3,0.3,0.3));
+    } else if( mode == 1 ) {
+        float hpct = ofMap( ofGetMouseX(), 100, ofGetWidth()-100, 0, 1, true );
+        highwaySpeed = ofMap( hpct, 0, 1, 500, 5000, true );
+        areaLight.setAmbientColor(ofFloatColor(0.0,0.0,0.0));
+        float tz = ofClamp(sin(etimef), 0, 1) * 500.0+200;
+        areaLight.setPosition( areaLight.getPosition().x + deltaTime * highwaySpeed, 350, tz );
+        if( areaLight.getPosition().x > 1500 ) {
+            areaLight.setPosition(-1500, areaLight.getPosition().y, tz );
+        }
+        areaLight.lookAt( glm::vec3(areaLight.getPosition().x, areaLight.getPosition().y-300, areaLight.getPosition().z) );
+        areaLight.rollDeg(90);
+        
+        float tnoise = ofSignedNoise(areaLight.getPosition().y * hpct * 0.001, areaLight.getPosition().x * hpct * 0.001, hpct * etimef );
+        if( tnoise > 1.0 - hpct * 0.5 && bump > 1.0 ) {
+            bump = 0.0;
+            bumpHeight = ofRandom(10, 80);
+        }
+        bump += deltaTime * (3.0 + (hpct+0.2));
+    } else if( mode == 2 ) {
+        float hpct = ofMap( ofGetMouseX(), 100, ofGetWidth()-100, 0, 1, true );
+        areaLight.setPosition(cos(etimef*2.0)*-600,fabs(sin(etimef*2.0)*600)-200,sin(etimef*3.4f)*200+400);
+//        areaLight.rotateDeg(-90,glm::vec3(1,0,0));
+        areaLight.tiltDeg(deltaTime*(hpct*360.0*3.0+5.0f));
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    
+    ofEnableLighting();
+    ofEnableDepthTest();
 	camera.begin();
 	materialPlane.begin();
-	plane.draw();
+    plane.draw();
 	ofDrawSphere(0,-300,0,10000);
 	materialPlane.end();
+    
+    materialMesh.begin(); {
+        ofPushMatrix(); {
+            ofTranslate(0,-300,0);
+            if( mode == 1 ) {
+                if( bump < 1.0 ) {
+                    ofTranslate(0.0, sin(bump * PI) * bumpHeight, 0.0 );
+                }
+            }
+            ofRotateXDeg(-90);
+            logoMesh.draw();
+        } ofPopMatrix();
+    } materialMesh.end();
+    
+    
 	areaLight.draw();
 	camera.end();
+    
+    ofDisableDepthTest();
+    ofDisableLighting();
+    
+    ofDrawBitmapStringHighlight("Mode (m): "+ofToString(mode,0), 15, 15);
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-	switch(key){
-	case OF_KEY_UP:
-		areaLight.move(0,10,0);
-		break;
-	case OF_KEY_DOWN:
-		areaLight.move(0,-10,0);
-		break;
-	case OF_KEY_LEFT:
-		areaLight.rotateDeg(1,0,0,1);
-		break;
-	case OF_KEY_RIGHT:
-		areaLight.rotateDeg(-1,0,0,1);
-		break;
-	}
+    if( key == 'm' ) {
+        mode ++;
+        if( mode > 2 ) {
+            mode = 0;
+        }
+    }
 }
 
 //--------------------------------------------------------------
