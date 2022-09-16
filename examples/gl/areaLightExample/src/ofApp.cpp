8a7bf2e949cef18c1c54973c4332c19456dfa182
NickHardeman <nickhardeman@gmail.com>
2022-09-16 13:18:24 -0400

    formatting

--- examples/gl/areaLightExample/src/ofApp.cpp
+++ examples/gl/areaLightExample/src/ofApp.cpp
@@ -34,38 +34,38 @@ void ofApp::setup(){
 }
 
 //--------------------------------------------------------------
-void ofApp::update() {
-    float etimef = ofGetElapsedTimef();
+void ofApp::update(){
+    float elapsedTime = ofGetElapsedTimef();
     float deltaTime = ofClamp( ofGetLastFrameTime(), 1.f / 10000.0, 1.f / 5.f );
-    if( mode == 0 ) {
+	
+    if( mode == 0 ){
         float speed = ofMap( ofGetMouseX(), 100, ofGetWidth()-100, 0.1, 24, true );
         orbitSpeed += deltaTime * speed;
-        areaLight.setPosition(cos(orbitSpeed)*-600,(sin(etimef*1.1)*200), sin(orbitSpeed)*-600);
+        areaLight.setPosition(cos(orbitSpeed)*-600,(sin(elapsedTime*1.1)*200), sin(orbitSpeed)*-600);
         areaLight.lookAt(glm::vec3(0, -150, 0));
         areaLight.setAmbientColor(ofFloatColor(0.3,0.3,0.3));
-    } else if( mode == 1 ) {
-        float hpct = ofMap( ofGetMouseX(), 100, ofGetWidth()-100, 0, 1, true );
-        highwaySpeed = ofMap( hpct, 0, 1, 500, 5000, true );
+    }else if( mode == 1 ){
+        float mousePercent = ofMap( ofGetMouseX(), 100, ofGetWidth()-100, 0, 1, true );
+        highwaySpeed = ofMap( mousePercent, 0, 1, 500, 5000, true );
         areaLight.setAmbientColor(ofFloatColor(0.0,0.0,0.0));
-        float tz = ofClamp(sin(etimef), 0, 1) * 500.0+200;
-        areaLight.setPosition( areaLight.getPosition().x + deltaTime * highwaySpeed, 350, tz );
+        float zPosition = ofClamp(sin(elapsedTime), 0, 1) * 500.0+200;
+        areaLight.setPosition( areaLight.getPosition().x + deltaTime * highwaySpeed, 350, zPosition );
         if( areaLight.getPosition().x > 1500 ) {
-            areaLight.setPosition(-1500, areaLight.getPosition().y, tz );
+            areaLight.setPosition(-1500, areaLight.getPosition().y, zPosition );
         }
         areaLight.lookAt( glm::vec3(areaLight.getPosition().x, areaLight.getPosition().y-300, areaLight.getPosition().z) );
         areaLight.rollDeg(90);
         
-        float tnoise = ofSignedNoise(areaLight.getPosition().y * hpct * 0.001, areaLight.getPosition().x * hpct * 0.001, hpct * etimef );
-        if( tnoise > 1.0 - hpct * 0.5 && bump > 1.0 ) {
+        float noiseValue = ofSignedNoise(areaLight.getPosition().y * mousePercent * 0.001, areaLight.getPosition().x * mousePercent * 0.001, mousePercent * elapsedTime );
+        if( noiseValue > 1.0 - mousePercent * 0.5 && bump > 1.0 ) {
             bump = 0.0;
             bumpHeight = ofRandom(10, 80);
         }
-        bump += deltaTime * (3.0 + (hpct+0.2));
-    } else if( mode == 2 ) {
-        float hpct = ofMap( ofGetMouseX(), 100, ofGetWidth()-100, 0, 1, true );
-        areaLight.setPosition(cos(etimef*2.0)*-600,fabs(sin(etimef*2.0)*600)-200,sin(etimef*3.4f)*200+400);
-//        areaLight.rotateDeg(-90,glm::vec3(1,0,0));
-        areaLight.tiltDeg(deltaTime*(hpct*360.0*3.0+5.0f));
+        bump += deltaTime * (3.0 + (mousePercent+0.2));
+    }else if( mode == 2 ){
+        float mousePercent = ofMap( ofGetMouseX(), 100, ofGetWidth()-100, 0, 1, true );
+        areaLight.setPosition(cos(elapsedTime*2.0)*-600,fabs(sin(elapsedTime*2.0)*600)-200,sin(elapsedTime*3.4f)*200+400);
+        areaLight.tiltDeg(deltaTime*(mousePercent*360.0*3.0+5.0f));
     }
 }
 
@@ -80,18 +80,18 @@ void ofApp::draw(){
 	ofDrawSphere(0,-300,0,10000);
 	materialPlane.end();
     
-    materialMesh.begin(); {
-        ofPushMatrix(); {
-            ofTranslate(0,-300,0);
-            if( mode == 1 ) {
-                if( bump < 1.0 ) {
-                    ofTranslate(0.0, sin(bump * PI) * bumpHeight, 0.0 );
-                }
-            }
-            ofRotateXDeg(-90);
-            logoMesh.draw();
-        } ofPopMatrix();
-    } materialMesh.end();
+    materialMesh.begin();
+	ofPushMatrix();
+	ofTranslate(0,-300,0);
+	if( mode == 1 ){
+		if( bump < 1.0 ){
+			ofTranslate(0.0, sin(bump * glm::pi<float>()) * bumpHeight, 0.0 );
+		}
+	}
+	ofRotateXDeg(-90);
+	logoMesh.draw();
+	ofPopMatrix();
+    materialMesh.end();
     
     
 	areaLight.draw();
@@ -106,9 +106,9 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-    if( key == 'm' ) {
+    if( key == 'm' ){
         mode ++;
-        if( mode > 2 ) {
+        if( mode > 2 ){
             mode = 0;
         }
     }
