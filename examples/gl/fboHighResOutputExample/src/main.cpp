ec7a71c3065aec10e9c2e09cb5d8f4759a3d4f0b
Nick Hardeman <nickhardeman@gmail.com>
2022-09-27 14:12:07 -0400

    Simple example of high resolution saving of an fbo (#7104)
    #changelog #examples

---
+++ examples/gl/fboHighResOutputExample/src/main.cpp
@@ -0,0 +1,17 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+
+	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
+	ofGLWindowSettings settings;
+	settings.setSize(1024, 768);
+	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+
+	auto window = ofCreateWindow(settings);
+
+	ofRunApp(window, make_shared<ofApp>());
+	ofRunMainLoop();
+
+}
