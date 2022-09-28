5f9e3217022e3605ca1124e8d43317ed16c4438c
Dimitre <dimitre@users.noreply.github.com>
2022-09-28 02:49:29 -0300

    arch

--- scripts/ci/osx/build.sh
+++ scripts/ci/osx/build.sh
@@ -3,5 +3,5 @@ set -ev
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 # source $ROOT/scripts/ci/ccache.sh
 echo "**** Building oF + emptyExample - OSX Template Project ****"
-xcodebuild -configuration Release -target emptyExample -project "$ROOT/scripts/templates/osx/emptyExample.xcodeproj"
+xcodebuild -arch x86_64 -configuration Release -target emptyExample -project "$ROOT/scripts/templates/osx/emptyExample.xcodeproj"
 echo "**** Done building emptyExample ****"
