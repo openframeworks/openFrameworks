c53220b972772a8c72031f9118842ed47e34e232
Dimitre <dimitre@users.noreply.github.com>
2022-10-05 12:10:57 -0300

    echo url so we know where from

--- scripts/dev/download_libs.sh
+++ scripts/dev/download_libs.sh
@@ -35,6 +35,7 @@ if [[ ! -d "$SCRIPT_DIR" ]]; then SCRIPT_DIR="$PWD"; fi
 download(){
     echo "Downloading $1"
     # downloader ci.openframeworks.cc/libs/$1 $SILENT_ARGS
+    echo downloader https://github.com/openframeworks/apothecary/releases/download/nightly/$1 $SILENT_ARGS
     downloader https://github.com/openframeworks/apothecary/releases/download/nightly/$1 $SILENT_ARGS
 }
 
