2a80c08b4d3028a7c1d31f218d33402aaedd6b46
themancalledjakob <jakob@jrkb.land>
2022-11-11 14:12:54 +0100

    option to compile all examples with multiple threads

--- scripts/linux/testAllExamples.sh
+++ scripts/linux/testAllExamples.sh
@@ -3,6 +3,11 @@
 export LC_ALL=C
 cd ../../examples
 
+THREADS=""
+if [[ "$1" == -j* ]]; then
+  THREADS="$1"
+fi
+
 for category in $( ls . )
 do
     if [ "$category" = "android" -o "$category" = "ios" -o "$category" = "gles" ]; then
@@ -18,13 +23,13 @@ do
         echo "-----------------------------------------------------------------"
         echo "building " + $example
         cd $example
-        make Debug
+        make Debug ${THREADS}
         ret=$?
         if [ $ret -ne 0 ]; then
           echo error compiling $example
           exit
         fi
-        make Release
+        make Release ${THREADS}
         ret=$?
         if [ $ret -ne 0 ]; then
           echo error compiling $example
