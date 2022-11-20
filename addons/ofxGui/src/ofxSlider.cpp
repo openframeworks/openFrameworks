8926be9856d073c28919baa0b98397bf6794dbe3
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-11-20 23:41:39 +0100

    Update ofxSlider.cpp

--- addons/ofxGui/src/ofxSlider.cpp
+++ addons/ofxGui/src/ofxSlider.cpp
@@ -377,7 +377,7 @@ template class ofxSlider<float>;
 template class ofxSlider<double>;
 
 //for some reason osx errors if this isn't defined 
-#ifdef TARGET_OSX
+#if defined TARGET_OSX || defined TARGET_EMSCRIPTEN
 template class ofxSlider<typename std::conditional<std::is_same<uint32_t, size_t>::value || std::is_same<uint64_t, size_t>::value, bool, size_t>::type>;
 #endif
 
