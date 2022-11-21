7d73a8e23f07c90fd04e1345c6f8bd90d71fb0ed
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-11-21 05:15:49 +0100

    Update ofxInputField.cpp

--- addons/ofxGui/src/ofxInputField.cpp
+++ addons/ofxGui/src/ofxInputField.cpp
@@ -808,5 +808,5 @@ template class ofxInputField<std::string>;
 
 //for some reason osx errors if this isn't defined
 #if defined TARGET_OSX || defined TARGET_EMSCRIPTEN
-template class ofxInputField<typename std::conditional<std::is_same<uint32_t, size_t>::value || std::is_same<uint64_t, size_t>::value, bool, size_t>::type>;
+	template class ofxInputField<typename std::conditional<std::is_same<uint32_t, size_t>::value || std::is_same<uint64_t, size_t>::value, bool, size_t>::type>;
 #endif
