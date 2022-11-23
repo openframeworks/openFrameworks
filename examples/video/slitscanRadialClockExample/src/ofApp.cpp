2a99982af0efa50779018b091fb6967f715af556
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-11-23 23:37:46 +0100

    Update ofApp.cpp

--- examples/video/slitscanRadialClockExample/src/ofApp.cpp
+++ examples/video/slitscanRadialClockExample/src/ofApp.cpp
@@ -53,6 +53,7 @@ void ofApp::setup(){
     vidGrabber.initGrabber(camWidth, camHeight); // set the width and height of the camera
     videoPixels.allocate(camWidth,camHeight, OF_PIXELS_RGB); // set up our pixel object to be the same size as our camera object
     videoTexture.allocate(videoPixels);
+    pixels.allocate(camWidth,camHeight, OF_PIXELS_RGB);
     
     ofSetBackgroundColor(0, 0, 0); // set the background colour to dark black
     ofDisableSmoothing();
