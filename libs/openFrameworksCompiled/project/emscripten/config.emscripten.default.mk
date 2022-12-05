1bbbc1dfb48d003eb4c51a6fc6e8bb0eb599c2f5
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-12-06 00:22:02 +0100

    Update config.emscripten.default.mk

--- libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
+++ libs/openFrameworksCompiled/project/emscripten/config.emscripten.default.mk
@@ -93,7 +93,7 @@ ifdef USE_CCACHE
 	endif
 endif
 
-PLATFORM_LDFLAGS = -Wl --gc-sections --preload-file bin/data@data --emrun --bind --profiling-funcs -s USE_FREETYPE=1 -s EXPORTED_RUNTIME_METHODS=allocate -s ALLOW_MEMORY_GROWTH=1 -s MAX_WEBGL_VERSION=2 -s ASYNCIFY=1
+PLATFORM_LDFLAGS = -Wl --gc-sections --preload-file bin/data@data --emrun --bind --profiling-funcs -s USE_FREETYPE=1 -s ALLOW_MEMORY_GROWTH=1 -s MAX_WEBGL_VERSION=2 -s ASYNCIFY=1
 PLATFORM_LDFLAGS += --js-library $(OF_ADDONS_PATH)/ofxEmscripten/libs/html5video/lib/emscripten/library_html5video.js
 PLATFORM_LDFLAGS += --js-library $(OF_ADDONS_PATH)/ofxEmscripten/libs/html5audio/lib/emscripten/library_html5audio.js
 
