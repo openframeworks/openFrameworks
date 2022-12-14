6eb9a6f0b38f5fd0081a5a83f1226a09425e0898
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2022-12-14 04:30:57 +0100

    Update ofxAppEmscriptenWindow.cpp

--- addons/ofxEmscripten/src/ofxAppEmscriptenWindow.cpp
+++ addons/ofxEmscripten/src/ofxAppEmscriptenWindow.cpp
@@ -68,7 +68,7 @@ void ofxAppEmscriptenWindow::loop(){
 	instance->events().notifySetup();
 
 	// Emulate loop via callbacks
-	emscripten_set_main_loop(display_cb, -1, 0);
+	emscripten_set_main_loop(display_cb, -1, 1);
 }
 
 void ofxAppEmscriptenWindow::update(){
