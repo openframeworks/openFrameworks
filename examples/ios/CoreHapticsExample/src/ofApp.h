039d3852e0e6124dc7928ccc0f9bee0c7ca11c32
alexandre burton <b@artificiel.org>
2023-02-21 20:57:35 -0500

    CoreHapticsExample: inherited method boilerplate

--- examples/ios/CoreHapticsExample/src/ofApp.h
+++ examples/ios/CoreHapticsExample/src/ofApp.h
@@ -6,8 +6,22 @@
 class ofApp : public ofxiOSApp {
     
     ofxiOSCoreHaptics haptics_;
-    void update() override;
-    
+
+	void setup() override;
+	void update() override;
+	void draw() override;
+	void exit() override;
+
+	void touchDown(ofTouchEventArgs & touch) override;
+	void touchMoved(ofTouchEventArgs & touch) override;
+	void touchUp(ofTouchEventArgs & touch) override;
+	void touchDoubleTap(ofTouchEventArgs & touch) override;
+	void touchCancelled(ofTouchEventArgs & touch) override;
+
+	void lostFocus() override;
+	void gotFocus() override;
+	void gotMemoryWarning() override;
+	void deviceOrientationChanged(int newOrientation) override;
+	void launchedWithURL(std::string url) override;
+	
 };
-
-
