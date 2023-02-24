61a045c58820384f474442547fd4462244d20ddf
Dimitre <dimitre@users.noreply.github.com>
2023-02-24 20:21:21 -0300

    General changes (namespace, deprecated loadFile function) (#7344)
    #changelog #core

--- addons/ofxThreadedImageLoader/src/ofxThreadedImageLoader.cpp
+++ addons/ofxThreadedImageLoader/src/ofxThreadedImageLoader.cpp
@@ -19,7 +19,7 @@ ofxThreadedImageLoader::~ofxThreadedImageLoader(){
 
 // Load an image from disk.
 //--------------------------------------------------------------
-void ofxThreadedImageLoader::loadFromDisk(ofImage& image, string filename) {
+void ofxThreadedImageLoader::loadFromDisk(ofImage& image, std::string filename) {
 	nextID++;
 	ofImageLoaderEntry entry(image);
 	entry.filename = filename;
@@ -32,7 +32,7 @@ void ofxThreadedImageLoader::loadFromDisk(ofImage& image, string filename) {
 
 // Load an url asynchronously from an url.
 //--------------------------------------------------------------
-void ofxThreadedImageLoader::loadFromURL(ofImage& image, string url) {
+void ofxThreadedImageLoader::loadFromURL(ofImage& image, std::string url) {
 	nextID++;
 	ofImageLoaderEntry entry(image);
 	entry.url = url;
