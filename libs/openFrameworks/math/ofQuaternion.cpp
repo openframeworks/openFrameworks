8886bebcf0908a4b7da9823b1f69a5d15bea72f3
Dimitre <dimitre@users.noreply.github.com>
2023-03-23 11:32:05 -0300

    quaternion

--- libs/openFrameworks/math/ofQuaternion.cpp
+++ libs/openFrameworks/math/ofQuaternion.cpp
@@ -285,12 +285,12 @@ ofVec3f ofQuaternion::getEuler() const {
 	float attitude;
 	float bank;
 	if (test > 0.499) { // singularity at north pole
-		heading = 2 * atan2(x(), w());
-		attitude = PI/2;
+		heading = 2.0f * atan2(x(), w());
+		attitude = glm::half_pi<float>();
 		bank = 0;
 	} else if (test < -0.499) { // singularity at south pole
-		heading = -2 * atan2(x(), w());
-		attitude = - PI/2;
+		heading = -2.0f * atan2(x(), w());
+		attitude = - glm::half_pi<float>();
 		bank = 0;
 	} else {
 		float sqx = x() * x();
