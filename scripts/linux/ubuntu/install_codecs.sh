f93179bcfae8b5ff178cc60c483ddf41f295394b
Theodore Watson <theo@openframeworks.cc>
2023-04-02 18:34:20 -0700

    added option FORCE_YES (#6560)
    #changelog #linux

--- scripts/linux/ubuntu/install_codecs.sh
+++ scripts/linux/ubuntu/install_codecs.sh
@@ -9,7 +9,14 @@ if [ $(id -u) != 0 ]; then
    exit 1
 fi
 
+
+if [ "$1" == "-y" ]; then
+    FORCE_YES=-y
+else
+    FORCE_YES=""
+fi
+
 GSTREAMER_VERSION=0.10
 apt-cache show libgstreamer1.0-dev && GSTREAMER_VERSION=1.0
 
-apt-get install libmpg123-dev gstreamer${GSTREAMER_VERSION}-plugins-ugly
+apt-get install ${FORCE_YES} libmpg123-dev gstreamer${GSTREAMER_VERSION}-plugins-ugly
