46a458067934a41b5cad8e67179fea23497d0f87
ofTheo <theo@openframeworks.cc>
2023-04-03 21:01:28 -0700

    rebase

--- scripts/dev/create_package.sh
+++ scripts/dev/create_package.sh
@@ -181,7 +181,7 @@ function createProjectFiles {
         #downloader http://ci.openframeworks.cc/openFrameworks_libs/linux64/libopenFrameworksDebug.a
 
         cd ${main_ofroot}/apps/projectGenerator
-        git pull $PG_REPO $PG_BRANCH --ff-only
+        git pull $PG_REPO $PG_BRANCH --rebase
         cd commandLine
         echo "Recompiling command line PG"
         if [ -d ~/logs ]; then
