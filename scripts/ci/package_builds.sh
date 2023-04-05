c8a8c26a3885c43137667340cd9ae7df634a4079
ofTheo <theo@openframeworks.cc>
2023-04-04 21:02:08 -0700

    test new line fix

--- scripts/ci/package_builds.sh
+++ scripts/ci/package_builds.sh
@@ -20,15 +20,25 @@ git pull origin master
 
 cd $OUTPUT_FOLDER
 
-$ROOT/scripts/dev/create_package.sh ios $lastversion master
-$ROOT/scripts/dev/create_package.sh osx $lastversion master
-$ROOT/scripts/dev/create_package.sh msys2 $lastversion master mingw64
-$ROOT/scripts/dev/create_package.sh vs $lastversion master
-$ROOT/scripts/dev/create_package.sh linuxarmv6l $lastversion master
-$ROOT/scripts/dev/create_package.sh linux64 $lastversion master gcc6
+#$ROOT/scripts/dev/create_package.sh ios $lastversion master
+#$ROOT/scripts/dev/create_package.sh osx $lastversion master
+#$ROOT/scripts/dev/create_package.sh msys2 $lastversion master mingw64
+#$ROOT/scripts/dev/create_package.sh vs $lastversion master
+#$ROOT/scripts/dev/create_package.sh linuxarmv6l $lastversion master
+#$ROOT/scripts/dev/create_package.sh linux64 $lastversion master gcc6
+
+echo "test 123" >> of_v20230405_ios_release.zip
+echo "test 123" >> of_v20230405_linux64gcc6_release.tar.gz
+echo "test 123" >> of_v20230405_linuxarmv6l_release.tar.gz
+echo "test 123" >> of_v20230405_msys2_mingw64_release.zip
+echo "test 123" >> of_v20230405_osx_release.zip
+echo "test 123" >> of_v20230405_vs_release.zip
 
 ls -la
 cd $ROOT
 
 #export a list of files for the next step - matching .zip or .tar anything and including the relative paths
-echo "FILES_OUT=$( (ls -t out/*.zip 2> /dev/null || true) && (ls -t out/*.tar* 2> /dev/null || true) )" >> $GITHUB_OUTPUT
+FILES_OUT=$( (ls -t out/*.zip 2> /dev/null || true) && (ls -t out/*.tar* 2> /dev/null || true) )
+#remove new lines
+FILES_OUT=$(echo $FILES_OUT | tr '\n' ' ')
+echo "FILES_OUT=$FILES_OUT" >> $GITHUB_OUTPUT
