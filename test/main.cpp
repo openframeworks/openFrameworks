06c5c923e3f71c6ee05082dc6cb9e60d73b0ffcd
Dimitre <dimitre@users.noreply.github.com>
2023-04-22 12:37:25 -0300

    up

--- test/main.cpp
+++ test/main.cpp
@@ -6,7 +6,7 @@ using std::endl;
 
 int main() {
 	cout << "cplusplus version : " << __cplusplus << endl;
-	cout << "__cpp_lib_filesystem " << __cpp_lib_filesystem << endl;
 	cout << "__cpp_lib_experimental_filesystem " << __cpp_lib_experimental_filesystem << endl;
+	cout << "__cpp_lib_filesystem " << __cpp_lib_filesystem << endl;
 	return 0;
 }
