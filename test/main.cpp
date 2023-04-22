6e5b3c93709c5056ec8514e545203ae654cd98ff
Dimitre <dimitre@users.noreply.github.com>
2023-04-22 12:38:43 -0300

    up

--- test/main.cpp
+++ test/main.cpp
@@ -8,7 +8,9 @@ int main() {
 	cout << "cplusplus version : " << __cplusplus << endl;
 	#ifdef __cpp_lib_experimental_filesystem
 		cout << "__cpp_lib_experimental_filesystem "  << endl;
+	#endif		
 	#ifdef __cpp_lib_filesystem
 		cout << "__cpp_lib_filesystem " << endl;
+	#endif		
 	return 0;
 }
