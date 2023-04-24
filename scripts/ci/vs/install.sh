b0781e2c3ae23d1116858bed2e1f61d7dafe6ef6
ofTheo <theo@openframeworks.cc>
2023-04-24 15:08:49 -0700

    try nightly from github ci

--- scripts/ci/vs/install.sh
+++ scripts/ci/vs/install.sh
@@ -12,10 +12,10 @@ mkdir -p ~/projectGenerator
 cd ~/projectGenerator
 
 echo "Downloading projectGenerator from ci server"
-downloader http://ci.openframeworks.cc/projectGenerator/projectGenerator-vs.zip 2> /dev/null
+downloader https://github.com/ofTheo/projectGenerator/releases/download/nightly/projectGenerator-vs.zip 2> /dev/null
 unzip projectGenerator-vs.zip 2> /dev/null
 rm projectGenerator-vs.zip
 
 cd $OF_ROOT
-~/projectGenerator/projectGenerator.exe -o./ examples/templates/emptyExample
-~/projectGenerator/projectGenerator.exe -o./ examples/templates/allAddonsExample
+PG_OF_PATH=$OF_ROOT ~/projectGenerator/projectGenerator.exe examples/templates/emptyExample
+PG_OF_PATH=$OF_ROOT ~/projectGenerator/projectGenerator.exe examples/templates/allAddonsExample
