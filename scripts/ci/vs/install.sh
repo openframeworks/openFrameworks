aae8bed3fa20bce731a0b6eef0dccf875cffbd4a
ofTheo <theo@openframeworks.cc>
2023-04-24 13:57:02 -0700

    test this approach

--- scripts/ci/vs/install.sh
+++ scripts/ci/vs/install.sh
@@ -17,5 +17,5 @@ unzip projectGenerator-vs.zip 2> /dev/null
 rm projectGenerator-vs.zip
 
 cd $OF_ROOT
-PG_OF_PATH=$OF_ROOT ~/projectGenerator/projectGenerator.exe examples/templates/emptyExample
-PG_OF_PATH=$OF_ROOT ~/projectGenerator/projectGenerator.exe examples/templates/allAddonsExample
+~/projectGenerator/projectGenerator.exe -o./ examples/templates/emptyExample
+~/projectGenerator/projectGenerator.exe -o./ examples/templates/allAddonsExample
