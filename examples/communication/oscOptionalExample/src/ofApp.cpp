512c8180f3ca7cb1c11b9786157386d1cb69868c
alexandre burton <b@artificiel.org>
2023-07-15 02:01:54 -0400

    ofxOsc Optional Example: better formatting

--- examples/communication/oscOptionalExample/src/ofApp.cpp
+++ examples/communication/oscOptionalExample/src/ofApp.cpp
@@ -5,8 +5,8 @@ void ofApp::setup() {
 }
 
 void ofApp::update() {
-	while (auto m = osc_receiver.getMessage()){
-		ofLogNotice("got message") << m->getAddress() << " = " << m->getArgAsInt(0) << " @ " << m->getArgAsFloat(1);
+	while (const auto m = osc_receiver.getMessage()) {
+		ofLogNotice(m->getAddress()) << m->getArgAsInt(0) << " @ " << m->getArgAsFloat(1);
 	}
 }
 
@@ -29,17 +29,14 @@ void ofApp::keyPressed(int key) {
 	}
 }
 
-/* boilerplate
- 
-void ofApp::keyReleased(int key){}
-void ofApp::mouseMoved(int x, int y){}
-void ofApp::mouseDragged(int x, int y, int button){}
-void ofApp::mousePressed(int x, int y, int button){}
-void ofApp::mouseReleased(int x, int y, int button){}
-void ofApp::mouseEntered(int x, int y){}
-void ofApp::mouseExited(int x, int y){}
-void ofApp::windowResized(int w, int h){}
-void ofApp::gotMessage(ofMessage msg){}
-void ofApp::dragEvent(ofDragInfo dragInfo){}
- 
-*/
+//void ofApp::keyReleased(int key){}
+//void ofApp::mouseMoved(int x, int y){}
+//void ofApp::mouseDragged(int x, int y, int button){}
+//void ofApp::mousePressed(int x, int y, int button){}
+//void ofApp::mouseReleased(int x, int y, int button){}
+//void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {};
+//void ofApp::mouseEntered(int x, int y){}
+//void ofApp::mouseExited(int x, int y){}
+//void ofApp::windowResized(int w, int h){}
+//void ofApp::gotMessage(ofMessage msg){}
+//void ofApp::dragEvent(ofDragInfo dragInfo){}
