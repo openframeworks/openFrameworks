df29e6d398a21725377497661b62a571c8c20210
alexandre burton <b@artificiel.org>
2023-07-15 00:53:58 -0400

    ofxOsc Optional Example: upgrade #define to const int

--- examples/communication/oscOptionalExample/src/ofApp.h
+++ examples/communication/oscOptionalExample/src/ofApp.h
@@ -6,12 +6,12 @@
 // demonstrates the optional pattern as well as constructors and method chaining
 
 // listening port
-#define PORT 12345
 
 class ofApp : public ofBaseApp{
 	
-	ofxOscReceiver osc_receiver { PORT };
-	ofxOscSender osc_sender { "127.0.0.1", PORT };
+	const int port { 12345 } ;
+	ofxOscReceiver osc_receiver { port };
+	ofxOscSender osc_sender { "127.0.0.1", port };
 	
 public:
 	
