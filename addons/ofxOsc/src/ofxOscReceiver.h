66a12da664a5bed33756d65af8c4af7cf08cb33a
alexandre burton <b@artificiel.org>
2023-07-16 13:38:03 -0400

    ofxOsc: documentation adjustments

--- addons/ofxOsc/src/ofxOscReceiver.h
+++ addons/ofxOsc/src/ofxOscReceiver.h
@@ -85,6 +85,8 @@ public:
 	bool getNextMessage(ofxOscMessage& msg);
 	OF_DEPRECATED_MSG("Pass a reference instead of a pointer", bool getNextMessage(ofxOscMessage *msg));
 	
+	/// if there is at tleast 1 message in the queue, pop and swap it into an optional message
+	/// \return the std::optional<ofxOscMessage> if there was a message; std::nullopt otherwise
     std::optional<const ofxOscMessage> getMessage();
 
 	/// try to get waiting message an ofParameter
