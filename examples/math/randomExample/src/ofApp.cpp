d5429f56b7b7433781df075f20a463e3578a0b89
alexandre burton <b@artificiel.org>
2023-08-02 12:03:47 -0400

    randomExample: deeper examples

--- examples/math/randomExample/src/ofApp.cpp
+++ examples/math/randomExample/src/ofApp.cpp
@@ -1,19 +1,32 @@
 #include "ofApp.h"
 #include "ofRandom.hpp"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
-	
-	panel_.setup();
-	panel_.add(uniform1_min_);
-	panel_.add(uniform1_max_);
-	panel_.add(normal1_mean_);
-	panel_.add(normal1_stddev_);
-	panel_.add(gamma1_alpha_);
-	panel_.add(gamma1_beta_);
-	panel_.add(poisson1_mean_);
-	panel_.add(exponential1_lambda_);
+// https://rovdownloads.com/blog/quick-overview-of-probability-distributions/
 
+
+void ofApp::setup(){
+
+	ofSetWindowShape(1920,1000);
+	ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+	
+	panel_.setup("Random Generators");
+	panel_.add(size_);
+	panel_.add(size_calc_);
+	panel_.setPosition(400,0);
+	size_t i = 0; // to spread hue
+	float chunk = 255.0/(dists_.size() + of_dists_.size());
+	for (auto & dist: dists_) {
+		panel_.add(dist->parameters_);
+		dist->color_.setHsb(chunk * i++, 192, 64);
+		panel_.getGroup(dist->parameters_.getName()).setHeaderBackgroundColor(dist->color_);
+	}
+	
+	for (auto & dist: of_dists_) {
+		panel_.add(dist->parameters_);
+		dist->color_.setHsb(chunk * i++, 192, 64);
+		panel_.getGroup(dist->parameters_.getName()).setHeaderBackgroundColor(dist->color_);
+	}
+	
 	std::vector<int> v {1,2,3,4,5,6,7,8};
 	ofRandomize(v);
 	for (auto & i: v) ofLogNotice("1") << i;
@@ -24,94 +37,69 @@ void ofApp::setup(){
 	of::random::shuffle(v);
 	for (auto & i: v) ofLogNotice("1") << i;
 
-	ofLogNotice("X") << ofRandomUniform<float>(3,10);
-	ofLogNotice("Y") << ofRandomUniform<glm::vec2>(3,10);
-	ofLogNotice("Z") << ofRandomUniform<glm::vec2>({3,10}, {-1,1});
-
-	// cheapo benchmark
-	auto now = ofGetElapsedTimef();
-	for (size_t i = 0; i < 10'000'000; i++) of::random::uniform(5,10);
-	ofLogNotice("a") << ofGetElapsedTimef() - now;
-	
-	now = ofGetElapsedTimef();
-	for (size_t i = 0; i < 5'000'000; i++) {
-		of::random::uniform(5,10);
-		of::random::uniform(1,2);
-	}
-	ofLogNotice("a'") << ofGetElapsedTimef() - now;
-	
-	now = ofGetElapsedTimef();
-	for (size_t i = 0; i < 10'000'000; i++) of::random::uniform<double>(5,10);
-	ofLogNotice("ofRandUniform<double>(5,10);") << ofGetElapsedTimef()  - now;
-	
-	now = ofGetElapsedTimef();
-	for (size_t i = 0; i < 10'000'000; i++) ofRandom(5,10);
-	ofLogNotice("plain ofRandome") << ofGetElapsedTimef()  - now;
-	
-	now = ofGetElapsedTimef();
-	for (size_t i = 0; i < 10'000'000; i++) of::random::uniform<glm::vec3>({0,10,20},{10,20,30});
-	ofLogNotice("d") << ofGetElapsedTimef()  - now;
-	
-
-
-	ofLogNotice("of::random::uniform())") << of::random::uniform(5,10);
-//	ofLogNotice("ofRandUniform())") << of::random::ofRandUniform();
-	ofLogNotice("ofRandUniform<float>") << of::random::uniform<float>(10,20);
-	ofLogNotice("ofRandUniform<double>") << of::random::uniform<double>(10,20);
-	ofLogNotice("ofRandUniform<int>") << of::random::uniform<int>(10,20);
-	ofLogNotice("ofRandUniform<glm::vec2>") << of::random::uniform<glm::vec2>(10,20);
-	ofLogNotice("ofRandUniform<glm::vec3>") << of::random::uniform<glm::vec3>(10,20);
-	ofLogNotice("ofRandUniform<glm::vec3> with custom ranges") << of::random::uniform<glm::vec3>({0,10,20},{10,20,30});
-	
-//	of::random::seed(666);
-	ofLogNotice("of::random::uniform())") << of::random::uniform(5,10);
-	
-//	ofSetRandomSeed(1984);
-	ofLogNotice("of::random::uniform()) 1") << of::random::uniform(5,10);
-	ofLogNotice("of::random::uniform()) 2") << of::random::uniform(5,10);
-	ofLogNotice("of::random::uniform()) 3") << of::random::uniform(5,10);
-
-	ofLogNotice("of::random::normal(100,15))") << of::random::normal<int>(100.0,15.0);
-	ofLogNotice("of::random::normal(0,1))") << of::random::normal(0,1);
-	ofLogNotice("of::random::normal<glm::vec2>())") << of::random::normal<glm::vec2>({100, 0},{15, 5});
-	
-	for (std::size_t i=0;i<10;i++) ofLogNotice("ofRandomNormalLimits(20,30)") << ofRandomNormalLimits<glm::vec2>({100, 0},{15, 5});
-
-
-	ofLogNotice("done") << 1;
-
 }
 
-//--------------------------------------------------------------
 void ofApp::update(){
-	uniform1_.clear();
-	normal1_.clear();
-	gamma1_.clear();
-	poisson1_.clear();
-	exponential1_.clear();
-	for (std::size_t i=0;i<10000;i++) uniform1_.push_back(of::random::uniform<int>(uniform1_min_,uniform1_max_));
-	for (std::size_t i=0;i<10000;i++) normal1_.push_back(of::random::normal<int>(normal1_mean_, normal1_stddev_));
-	for (std::size_t i=0;i<10000;i++) gamma1_.push_back(of::random::gamma<int>(gamma1_alpha_, gamma1_beta_));
-	for (std::size_t i=0;i<10000;i++) poisson1_.push_back(of::random::poisson<int>(poisson1_mean_));
-	for (std::size_t i=0;i<10000;i++) exponential1_.push_back(of::random::exponential<int>(exponential1_lambda_));
-	uniform1_.compile();
-	normal1_.compile();
-	gamma1_.compile();
-	poisson1_.compile();
-	exponential1_.compile();
+	
+	// section seed
+	// NON-DETERMINISTIC  vs  DETERMINISTIC
+	// shuffle
+	// barbo
+	
+	
+	// DISTRIBUTIONS
+	auto num_samples = pow(size_.get(),3);
+	size_calc_ = ofToString(num_samples);
+	for (auto & group: {dists_, of_dists_}) {
+		for (const auto & d: group) {
+			d->clear();
+			float start = ofGetElapsedTimef();
+			for (std::size_t i=0; i < num_samples; i++) d->gen();
+			d->cost_ = ofGetElapsedTimef() - start;
+			d->compile();
+		}
+	}
+	
+	// VEC2 THINGS
+	// truc vec2d avec params animés
 }
 
-//--------------------------------------------------------------
 void ofApp::draw(){
-	uniform1_.draw(220,20,100,100);
-	normal1_.draw(220,140,100,100);
-	gamma1_.draw(220,260,100,100);
-	poisson1_.draw(220,380,100,100);
-	exponential1_.draw(220,500,100,100);
-
+	ofClear(20);
 	panel_.draw();
+
+	ofPushMatrix();
+	{
+		ofTranslate(panel_.getPosition());
+		ofPushMatrix();
+		{
+			ofTranslate(220,15);
+			ofDrawBitmapString("C++ fundamental numerics pseudorandom generators",0,0);
+			ofTranslate(0,15);
+			for (const auto & dist: dists_) {
+				dist->draw(0,0,100,100);
+				ofTranslate(0,110);
+			}
+		}
+		ofPopMatrix();
+		
+		ofPushMatrix();
+		{
+			ofTranslate(800,15);
+			ofDrawBitmapString("OF-centric generator wrappers",0,0);
+			ofTranslate(0,15);
+			for (const auto & dist: of_dists_) {
+				dist->draw(0,0,100,100);
+				ofTranslate(0,110);
+			}
+		}
+		ofPopMatrix();
+	}
+	ofPopMatrix();
 }
 
+// MARK: ofApp boilerplate
+/*
 //--------------------------------------------------------------
 void ofApp::exit(){
 
@@ -176,3 +164,4 @@ void ofApp::gotMessage(ofMessage msg){
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
+*/
--- examples/math/randomExample/src/ofApp.h
+++ examples/math/randomExample/src/ofApp.h
@@ -3,59 +3,140 @@
 #include "ofMain.h"
 
 struct Dist {
-	std::vector<int> data_;
-	std::array<size_t, 101> bins_;
-	size_t max_ {0};
 	
-	std::string label_;
+	ofParameterGroup parameters_;
+	ofColor color_ {128,128,128};
+	std::string info_;
+	glm::vec2 range_ { };
+	bool discrete_ { };
+	size_t underflow_;
+	size_t overflow_;
+	std::size_t max_ { 0 };
+	float cost_ { 0.0f };
 	
-	Dist(std::string label): label_(label) {}
+	virtual auto gen() -> void = 0;
+	virtual auto clear() -> void = 0;
+	virtual auto compile() -> void = 0;
+	virtual auto draw(float x, float y, float w, float h) -> void = 0;
 	
-	auto clear() {
+	Dist() {};
+};
+
+template <typename T>
+struct ConcreteDist: public Dist {
+	
+	std::vector<T> data_;
+	std::function<T()> gen_;
+	std::vector<T> bins_;
+	int autorot_ { 0 };
+
+	ConcreteDist(std::string label,
+				 std::string info,
+				 std::vector<ofAbstractParameter *> params,
+				 std::function<T()> gen,
+				 std::size_t num_bins = 101,
+				 glm::vec2 range = {-1.0, 1.0},
+				 bool discrete = false )
+	: gen_(gen)
+	{
+		info_ = info;
+		range_ = range;
+		discrete_ = discrete;
+		bins_.resize(num_bins);
+		parameters_.setName(label);
+		for (auto & p: params) parameters_.add(*p);
+	}
+	
+	auto gen() -> void  {
+		data_.push_back(gen_());
+	}
+	
+	auto clear() -> void  {
+		overflow_ = 0;
+		underflow_ = 0;
 		data_.clear();
-		bins_ = {0};
+		std::fill(bins_.begin(), bins_.end(), T{0});
 		max_ = 0;
 	}
 	
-	auto push_back(size_t i) {
-		data_.push_back(i);
-	}
-	
-	auto compile() {
+	auto compile() -> void  {
 		for (auto & v: data_) {
-			if (v < bins_.size()) {
-				bins_[v] = bins_[v]+1;
-				if (bins_[v] > max_) max_ = bins_[v];
-			} else {
-				ofLogNotice("ouf") << v;
+			if constexpr (std::is_arithmetic_v<T>) {
+				if (v < range_.x) {
+					underflow_++;
+				} else if (v >= bins_.size()) {
+					overflow_++;
+				} else {
+					bins_.at(v) = bins_.at(v)+1;
+					if (bins_.at(v) > max_) max_ = bins_.at(v);
+				}
 			}
 		}
 	}
 	
-	auto draw(float x, float y, float w, float h) {
+	auto draw(float x, float y, float w, float h) -> void  {
 		ofPushStyle();
 		ofPushMatrix();
-		
-		ofTranslate(x,y);
-		ofSetColor(0,0,0);
-		ofDrawRectangle(0,0,w,h);
-		
-		ofSetColor(255,255,255,255);
-		
-		ofPolyline line;
-		int p = 0;
-		auto fact = 100.0f/float(max_);
-		line.addVertex(0,100-bins_[0]*fact);
-		for (auto y: bins_) {
-			line.lineTo(p++, 100-float(y)*fact);
-		}
-		line.draw();
-		
-		ofDrawBitmapStringHighlight(label_, w+5, h-5);
-		
-		ofPopStyle();
-		ofPopMatrix();
+		{
+			ofTranslate(x,y);
+			ofSetColor(color_);
+			ofDrawRectangle(0,0,w,h);
 
+			ofSetColor(ofColor::darkRed);
+			if (underflow_) ofDrawBitmapString("undershoot: " + ofToString(underflow_), w+5, 48);
+			if (overflow_) ofDrawBitmapString("overshoot: " + ofToString(overflow_), w+5, 64);
+
+			ofSetColor(255,255,255,255);
+			ofDrawBitmapStringHighlight(parameters_.getName() + " " + ofToString(cost_*1000, 2, 5)+"ms", w+5, 12);
+			ofDrawBitmapString(info_, w+5, 30);
+	
+			if constexpr (std::is_arithmetic_v<T>) {
+
+				auto p = 0.0f;
+				auto incr = w/bins_.size();
+				auto fact = h/max_;
+				if (discrete_) {
+					ofTranslate(incr/2, 0);
+					for (auto y: bins_) {
+						ofDrawLine(0, h, 0, h-float(y)*fact);
+						ofDrawCircle(0, h-float(y)*fact, 3);
+						ofTranslate(int(incr), 0);
+					}
+				} else {
+					ofPolyline line;
+					line.addVertex(0,h-bins_[0]*fact);
+					for (auto y: bins_) {
+						line.lineTo(p, h-float(y)*fact);
+						p+=incr;
+					}
+					line.draw();
+				}
+			} else if constexpr (std::is_same_v<T, glm::vec2>) {
+				ofSetColor(255,255,255,96);
+				for (const auto & d: data_) {
+					ofDrawCircle(d, .5);
+				}
+				
+			} else if constexpr (std::is_same_v<T, glm::vec3>) {
+				ofSetColor(255,255,255,32);
+				of3dPrimitive prim;
+				prim.getMesh().getVertices() = data_;
+				auto x = ofMap(sin(M_PI*2*fmod(ofGetElapsedTimef(),4.13)/4.13), -1, 1, 0, 1);
+				auto y = ofMap(sin(M_PI*2*fmod(ofGetElapsedTimef(),2.13)/2.13), -1, 1, 0, 1);
+				auto z = ofMap(sin(M_PI*2*fmod(ofGetElapsedTimef(),3.13)/3.13), -1, 1, 0, 1);
+				auto d = ofMap(sin(M_PI*2*fmod(ofGetElapsedTimef(),11.1)/11.1), -1, 1, 33, 70);
+				prim.rotateDeg(d,{x,y,z});
+				ofPushMatrix();
+				{
+					ofTranslate(w/3, h/3);
+					prim.drawWireframe();
+					prim.drawAxes(25);
+				}
+				ofPopMatrix();
+			}
+		}
+		ofPopMatrix();
+		ofPopStyle();
 	}
 };
 
@@ -64,37 +145,65 @@ struct Dist {
 class ofApp : public ofBaseApp{
 
 	ofxPanel panel_;
-	ofParameter<float> uniform1_min_{"uniform1 min", 0, 0, 100};
-	ofParameter<float> uniform1_max_{"uniform1 max", 100, 0, 100};
-	ofParameter<float> normal1_mean_{"normal1 mean", 50, 0, 100};
-	ofParameter<float> normal1_stddev_{"normal1 stddev", 10, 0, 50};
-	ofParameter<float> gamma1_alpha_{"gamma1 alpha", 1, 0, 100};
-	ofParameter<float> gamma1_beta_{"gamma1 beta", 12, 0, 50};
-	ofParameter<float> poisson1_mean_{"poisson1 mean", 12, 0, 50};
-	ofParameter<float> exponential1_lambda_{"exponential1 lambda", .1, 0.01, 0.5};
+	ofParameter<size_t> size_{"size cube root", 25, 1, 50};
+	ofParameter<std::string> size_calc_{"actual num"};
 
-	public:
-		void setup() override;
-		void update() override;
-		void draw() override;
-		void exit() override;
+	ofParameter<float> 		rand_min_	{"min", 0, 0, 100};
+	ofParameter<float> 		rand_max_	{"max", 100, 0, 100};
+	ofParameter<float> 		uni_min_	{"min", 0, 0, 100};
+	ofParameter<float> 		uni_max_	{"max", 100, 0, 100};
+	ofParameter<int> 		uni_int_min_{"min", 1, 0, 9};
+	ofParameter<int> 		uni_int_max_{"max", 6, 1, 10};
+	ofParameter<float> 		norm_mean_	{"mean", 100, 0, 100};
+	ofParameter<float> 		norm_dev_	{"stddev", 15, 0, 50};
+	ofParameter<float> 		bn_min_		{"min", 35, 0, 200};
+	ofParameter<float> 		bn_max_		{"max", 165, 0, 200};
+	ofParameter<float> 		bn_focus_	{"focus", 1.75, 0.01, 20};
+	ofParameter<float> 		gamma_alpha_{"alpha", 3, 1, 20};
+	ofParameter<float> 		gamma_beta_	{"beta", 5, 0, 50};
+	ofParameter<float> 		poiss_mean_	{"mean", 4, 0, 20};
+	ofParameter<float> 		exp_lambda_	{"lambda", .1, 0.01, 0.5};
+	ofParameter<glm::vec2> 	vec_min_	{"min", {40,10}, {0,0}, {200,200}};
+	ofParameter<glm::vec2> 	vec_max_	{"max", {60,90}, {0,0}, {200,200}};
+	ofParameter<glm::vec2> 	vec_mean_	{"mean", {50,50}, {0,0}, {100,100}};
+	ofParameter<glm::vec2> 	vec_dev_	{"stddev", {50,15}, {0,0}, {50,50}};
+	ofParameter<float> 		vec_gamma_a_{"alpha", 3, 1, 20};
+	ofParameter<float> 		vec_gamma_b_{"beta", 5, 0, 50};
 
-		void keyPressed(int key) override;
-		void keyReleased(int key) override;
-		void mouseMoved(int x, int y ) override;
-		void mouseDragged(int x, int y, int button) override;
-		void mousePressed(int x, int y, int button) override;
-		void mouseReleased(int x, int y, int button) override;
-		void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
-		void mouseEntered(int x, int y) override;
-		void mouseExited(int x, int y) override;
-		void windowResized(int w, int h) override;
-		void dragEvent(ofDragInfo dragInfo) override;
-		void gotMessage(ofMessage msg) override;
+	std::vector<std::shared_ptr<Dist>> dists_ {
+		std::make_shared<ConcreteDist<float>>("rand()", "old-skool (thread risky) based on srand()", 		std::vector<ofAbstractParameter *>{&rand_min_, &rand_max_},		[&]{ return ofRandom(rand_min_, rand_max_); }),
+		std::make_shared<ConcreteDist<float>>("uniform <real>", "modern, thread safe with seeded mt19937",	std::vector<ofAbstractParameter *>{&uni_min_, &uni_max_},		[&]{ return of::random::uniform<float>(uni_min_, uni_max_); }),
+		std::make_shared<ConcreteDist<int>>("uniform <int>", "unambiguously includes bounds", 				std::vector<ofAbstractParameter *>{&uni_int_min_, &uni_int_max_},[&]{ return of::random::uniform<int>(uni_int_min_, uni_int_max_); }, 	 11, glm::vec2{0,  10}, true),
+		std::make_shared<ConcreteDist<float>>("normal", "aka gaussian", 									std::vector<ofAbstractParameter *>{&norm_mean_, &norm_dev_},	[&]{ return of::random::normal<float>(norm_mean_, norm_dev_); }, 		201, glm::vec2{0, 200}),
+		std::make_shared<ConcreteDist<float>>("gamma", "with an alpha of 1, ~equivalent to exponential",	std::vector<ofAbstractParameter *>{&gamma_alpha_, &gamma_beta_},[&]{ return of::random::gamma<int>(gamma_alpha_, gamma_beta_); }),
+		std::make_shared<ConcreteDist<int>>("poisson", "for sparse, positive events; mean>12 is ~normal", 	std::vector<ofAbstractParameter *>{&poiss_mean_},				[&]{ return of::random::poisson<int>(poiss_mean_); }, 					 21, glm::vec2{0,  20}, true),
+		std::make_shared<ConcreteDist<float>>("exponential", "", 											std::vector<ofAbstractParameter *>{&exp_lambda_},				[&]{ return of::random::exponential<int>(exp_lambda_); })
+	};
 	
-	Dist uniform1_ {"uniform"};
-	Dist normal1_ {"normal"};
-	Dist gamma1_ {"gamma"};
-	Dist poisson1_ {"poisson"};
-	Dist exponential1_ {"exponential"};
+	std::vector<std::shared_ptr<Dist>> of_dists_ {
+		std::make_shared<ConcreteDist<float>>("bound normal", "practical min/max, with enforced limits",std::vector<ofAbstractParameter *>{&bn_min_, &bn_max_, &bn_focus_},[&]{ return ofRandomNormalLimits<int>(bn_min_, bn_max_, bn_focus_); }, 	201, glm::vec2{0, 200}),
+		std::make_shared<ConcreteDist<glm::vec2>>("vec2 uniform/refined", "all generators can be typed vec2, 3 or 4",std::vector<ofAbstractParameter *>{&vec_min_, &vec_max_},[&]{ return of::random::uniform<glm::vec2>(vec_min_, vec_max_); }, 	201, glm::vec2{0, 200}),
+		std::make_shared<ConcreteDist<glm::vec2>>("vec2 normal/refined", "vecN version can be refined to vecN params",std::vector<ofAbstractParameter *>{&vec_mean_, &vec_dev_},[&]{ return of::random::normal<glm::vec2>(vec_mean_, vec_dev_); }, 	201, glm::vec2{0, 200}),
+		std::make_shared<ConcreteDist<glm::vec3>>("vec3 gamma", "",std::vector<ofAbstractParameter *>{&vec_gamma_a_, &vec_gamma_b_},[&]{ return of::random::gamma<glm::vec3>(vec_gamma_a_, vec_gamma_b_); }, 	201, glm::vec2{0, 200})
+	};
+	
+public:
+	void setup() override;
+	void update() override;
+	void draw() override;
+
+// MARK: ofApp boilerplate
+//	void exit() override;
+//	void keyPressed(int key) override;
+//	void keyReleased(int key) override;
+//	void mouseMoved(int x, int y ) override;
+//	void mouseDragged(int x, int y, int button) override;
+//	void mousePressed(int x, int y, int button) override;
+//	void mouseReleased(int x, int y, int button) override;
+//	void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
+//	void mouseEntered(int x, int y) override;
+//	void mouseExited(int x, int y) override;
+//	void windowResized(int w, int h) override;
+//	void dragEvent(ofDragInfo dragInfo) override;
+//	void gotMessage(ofMessage msg) override;
 };
