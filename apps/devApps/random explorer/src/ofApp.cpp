3b1d2d15d63c07b4c59a972625ca9493865ce7e7
alexandre burton <burton@artificiel.org>
2023-08-05 15:41:20 -0400

    Random Explorer: much rework

--- apps/devApps/random explorer/src/ofApp.cpp
+++ apps/devApps/random explorer/src/ofApp.cpp
@@ -1,105 +1,84 @@
 #include "ofApp.h"
 #include "ofRandom.hpp"
 
-// https://rovdownloads.com/blog/quick-overview-of-probability-distributions/
-
-
 void ofApp::setup(){
     
-    ofColor(255,255,255);
-    ofRectangle(10,10,200,200);
+    ofSetWindowShape(1920,1000);
+    ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+    ofSetWindowTitle("Random Explorer");
     
-	ofSetWindowShape(1920,1000);
-	ofEnableBlendMode(OF_BLENDMODE_ALPHA);
-    
-	panel_.setup("Random Generators");
-	panel_.add(size_);
+    panel_.setup("Global state");
+    panel_.add(size_);
     panel_.add(size_calc_);
     panel_.add(seed_);
     panel_.add(reinit_);
-	panel_.setPosition(400,10);
+        
+    size_t sum = 0;
+    for (auto& group : {dists_["core"], dists_["special"], dists_["of"]}) sum+=group->dists_.size();
+    auto chunk = 255.0/sum;
     
-	size_t i = 0; // to spread hue
-	auto chunk = 255.0/(dists_.size() + of_dists_.size());
-	for (auto & dist: dists_) {
-		panel_.add(dist->parameters_);
-		dist->color_.setHsb(chunk * i++, 192, 64);
-		panel_.getGroup(dist->parameters_.getName()).setHeaderBackgroundColor(dist->color_);
-	}
-	
-	for (auto & dist: of_dists_) {
-		panel_.add(dist->parameters_);
-		dist->color_.setHsb(chunk * i++, 192, 64);
-		panel_.getGroup(dist->parameters_.getName()).setHeaderBackgroundColor(dist->color_);
-	}
+    size_t i = 0; // to spread the hue; order below matters
+    for (auto & group: {dists_["core"], dists_["special"], dists_["of"], dists_["old"]}) {
+                
+        group->panel_.setup();
+//        group->panel_.disableHeader();
+        for (auto & dist: group->dists_) {
+            group->panel_.add(dist->parameters_);
+            if (i>=sum) {
+                dist->color_ = {48,48,48,255}; // sad
+            } else {
+                dist->color_.setHsb(chunk * i++, 192, 64);
+            }
+            group->panel_.getGroup(dist->parameters_.getName()).setHeaderBackgroundColor(dist->color_);
+        }
+    }
     
+    auto y = panel_.getPosition().y + panel_.getHeight() + 50;
+
+    // because ofxpanel does not track ofTranslate()
+    dists_["core"]->panel_.setPosition(10,y);
+    dists_["special"]->panel_.setPosition(10+col_w_,y);
+    dists_["of"]->panel_.setPosition(10+col_w_*2,y);
+    dists_["old"]->panel_.setPosition(10+col_w_*2,y+((square_+gap_)*(dists_["of"]->dists_.size()+1)));
+
     seed_.addListener(this, &ofApp::seed);
     reinit_.addListener(this, &ofApp::reinit);
-    reinit(); // not required, but keeps the demo together
-
+    reinit(); // not required from the random perspective, but keeps the demo together
+    
 }
 
 void ofApp::update(){
-	
-	// section seed
-	// NON-DETERMINISTIC  vs  DETERMINISTIC
-	// shuffle
-	// barbo
-	
-	// DISTRIBUTIONS
-	auto num_samples = pow(size_.get(),3);
-	size_calc_ = ofToString(num_samples);
-	for (auto & group: {dists_, of_dists_}) {
-		for (const auto & d: group) {
-			d->clear();
-			float start = ofGetElapsedTimef();
-			for (std::size_t i=0; i < num_samples; i++) d->gen();
-			d->cost_ = ofGetElapsedTimef() - start;
-			d->compile();
-		}
-	}
+    
+    // DISTRIBUTIONS
+    auto num_samples = pow(size_.get(),3);
+    size_calc_ = ofToString(num_samples);
+    for (auto & [name, group]: dists_) {
+        for (const auto & d: group->dists_) {
+            d->clear();
+            float start = ofGetElapsedTimef();
+            for (std::size_t i=0; i < num_samples; i++) d->gen();
+            d->cost_ = ofGetElapsedTimef() - start;
+            d->compile();
+        }
+    }
 }
 
 void ofApp::draw(){
-	ofClear(20);
+    ofClear(20);
+    
+    auto x =  220;
+    ofDrawBitmapStringHighlight(dna_string_, x, 60);
+    ofDrawBitmapStringHighlight(shuffle_string_, x, 80);
     
     if (of::random::Engine::instance()->is_deterministic()) {
-        ofDrawBitmapStringHighlight("engine is deterministic (seeded)", 20, 20, ofColor::black, ofColor::green);
+        ofDrawBitmapStringHighlight("engine is deterministic (seeded)", x, 20, ofColor::black, ofColor::green);
     } else {
-        ofDrawBitmapStringHighlight("engine is non-deterministic", 20, 20, ofColor::black, ofColor::white);
+        ofDrawBitmapStringHighlight("engine is non-deterministic", x, 20, ofColor::black, ofColor::white);
     }
-
-    ofDrawBitmapStringHighlight(dna_string_, 20, 40);
-    ofDrawBitmapStringHighlight(shuffle_string_, 20, 60);
-
-	panel_.draw();
-
-	ofPushMatrix();
-	{
-		ofTranslate(panel_.getPosition());
-		ofPushMatrix();
-		{
-			ofTranslate(220,15);
-			ofDrawBitmapString("C++ fundamental numerics pseudorandom generators",0,0);
-			ofTranslate(0,15);
-			for (const auto & dist: dists_) {
-				dist->draw(0,0,100,100);
-				ofTranslate(0,110);
-			}
-		}
-		ofPopMatrix();
-		
-		ofPushMatrix();
-		{
-			ofTranslate(800,15);
-			ofDrawBitmapString("OF-centric generator wrappers",0,0);
-			ofTranslate(0,15);
-			for (const auto & dist: of_dists_) {
-				dist->draw(0,0,100,100);
-				ofTranslate(0,110);
-			}
-		}
-		ofPopMatrix();
-	}
-	ofPopMatrix();
+    panel_.draw();
+    dists_["core"]->draw("C++ fundamental distributions", square_, gap_);
+    dists_["special"]->draw("more specialized distributions", square_, gap_);
+    dists_["of"]->draw("OF/art-centric wrappers/utils", square_, gap_);
+    dists_["old"]->draw("Previous implementation (reference)", square_, gap_);
+    
 }
