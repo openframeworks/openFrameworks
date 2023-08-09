d61e0c7dfdb5991475bfff24dd3a9c86b4ac0f4e
ofTheo <theo@openframeworks.cc>
2023-08-09 09:01:13 -0700

    force yes

--- scripts/linux/debian/install_dependencies.sh
+++ scripts/linux/debian/install_dependencies.sh
@@ -9,6 +9,12 @@ if [ $EUID != 0 ]; then
    exit 1
 fi
 
+if [ "$1" == "-y" ]; then
+    FORCE_YES=-y
+else
+    FORCE_YES=""
+fi
+
 ROOT=$(cd $(dirname $0); pwd -P)
 
 apt-get update
@@ -37,7 +43,7 @@ fi
 
 
 echo "installing OF dependencies"
-apt-get install freeglut3-dev libasound2-dev libxmu-dev libxxf86vm-dev g++ libgl1-mesa-dev libglu1-mesa-dev libraw1394-dev libudev-dev libdrm-dev libglew-dev libopenal-dev libsndfile-dev libfreeimage-dev libcairo2-dev libfreetype6-dev libssl-dev libpulse-dev libusb-1.0-0-dev libgtk${GTK_VERSION}-dev libopencv-dev libegl1-mesa-dev libgles1 libgles2-mesa-dev libassimp-dev librtaudio-dev libboost-filesystem-dev libglfw3-dev  liburiparser-dev libcurl4-openssl-dev libpugixml-dev libpoco-dev libgconf-2-4
+apt-get install ${FORCE_YES} freeglut3-dev libasound2-dev libxmu-dev libxxf86vm-dev g++ libgl1-mesa-dev libglu1-mesa-dev libraw1394-dev libudev-dev libdrm-dev libglew-dev libopenal-dev libsndfile-dev libfreeimage-dev libcairo2-dev libfreetype6-dev libssl-dev libpulse-dev libusb-1.0-0-dev libgtk${GTK_VERSION}-dev libopencv-dev libegl1-mesa-dev libgles1 libgles2-mesa-dev libassimp-dev librtaudio-dev libboost-filesystem-dev libglfw3-dev  liburiparser-dev libcurl4-openssl-dev libpugixml-dev libpoco-dev libgconf-2-4
 exit_code=$?
 if [ $exit_code != 0 ]; then
     echo "error installing dependencies, there could be an error with your internet connection"
@@ -46,7 +52,7 @@ if [ $exit_code != 0 ]; then
 fi
 
 echo "installing gstreamer"
-apt-get install libgstreamer${GSTREAMER_VERSION}-dev libgstreamer-plugins-base${GSTREAMER_VERSION}-dev  ${GSTREAMER_FFMPEG} gstreamer${GSTREAMER_VERSION}-pulseaudio gstreamer${GSTREAMER_VERSION}-x gstreamer${GSTREAMER_VERSION}-plugins-bad gstreamer${GSTREAMER_VERSION}-alsa gstreamer${GSTREAMER_VERSION}-plugins-base gstreamer${GSTREAMER_VERSION}-plugins-good
+apt-get install ${FORCE_YES} libgstreamer${GSTREAMER_VERSION}-dev libgstreamer-plugins-base${GSTREAMER_VERSION}-dev  ${GSTREAMER_FFMPEG} gstreamer${GSTREAMER_VERSION}-pulseaudio gstreamer${GSTREAMER_VERSION}-x gstreamer${GSTREAMER_VERSION}-plugins-bad gstreamer${GSTREAMER_VERSION}-alsa gstreamer${GSTREAMER_VERSION}-plugins-base gstreamer${GSTREAMER_VERSION}-plugins-good
 exit_code=$?
 if [ $exit_code != 0 ]; then
 	echo "error installing gstreamer, there could be an error with your internet connection"
@@ -57,14 +63,14 @@ fi
 if [ -f /opt/vc/include/bcm_host.h ]; then
     echo "detected Raspberry Pi"
     echo "installing gstreamer omx"
-    apt-get install  gstreamer${GSTREAMER_VERSION}-omx
+    apt-get ${FORCE_YES} install  gstreamer${GSTREAMER_VERSION}-omx
 fi
 
 OS_CODENAME=$(cat /etc/os-release | grep VERSION= | sed "s/VERSION\=\"\(.*\)\"/\1/")
 
 if [ "$OS_CODENAME" = "7 (wheezy)" ]; then
     echo "detected wheezy, installing g++4.8 for c++11 compatibility"
-    apt-get install g++-4.8
+    apt-get install ${FORCE_YES} g++-4.8
 	echo "setting gcc-${CXX_VER} as default compiler"
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 1 --force
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 1 --force
