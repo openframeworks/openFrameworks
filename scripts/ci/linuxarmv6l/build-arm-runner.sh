01b5dfac391b6b3ba8f98397d8c5bb14a8e51034
ofTheo <theo@openframeworks.cc>
2023-08-09 18:59:11 -0700

    final test

--- scripts/ci/linuxarmv6l/build-arm-runner.sh
+++ scripts/ci/linuxarmv6l/build-arm-runner.sh
@@ -3,7 +3,6 @@ set -ev
 pwd
 OF_ROOT=$( cd "$(dirname "$0")/../../.." ; pwd -P )
 PROJECTS=$OF_ROOT/libs/openFrameworksCompiled/project
-# source $OF_ROOT/scripts/ci/ccache.sh
 
 # Add compiler flag to reduce memory usage to enable builds to complete
 # see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56746#c7
@@ -12,14 +11,11 @@ export CXXFLAGS="${CXXFLAGS} -ftrack-macro-expansion=0"
 
 echo "**** Building emptyExample ****"
 cd $OF_ROOT/scripts/templates/linuxarmv6l
-make Debug -j3
+make Debug -j4
 
 echo "**** Building allAddonsExample ****"
 cd $OF_ROOT
 cp scripts/templates/linuxarmv6l/Makefile examples/templates/allAddonsExample/
 cp scripts/templates/linuxarmv6l/config.make examples/templates/allAddonsExample/
 cd examples/templates/allAddonsExample/
-make Debug -j3
-
-git checkout $PROJECTS/makefileCommon/config.linux.common.mk
-git checkout $PROJECTS/linuxarmv6l/config.linuxarmv6l.default.mk
+make Debug -j4
