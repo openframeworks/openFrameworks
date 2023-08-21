c9b813bdae876e2d6c03e003e9f075117ea1b44e
ofTheo <theo@openframeworks.cc>
2023-08-21 11:29:26 -0700

    udev

--- scripts/ci/linuxarmv6l/install.sh
+++ scripts/ci/linuxarmv6l/install.sh
@@ -16,6 +16,7 @@ installPackages(){
     sudo apt-get -y install multistrap unzip
     #workaround for https://bugs.launchpad.net/ubuntu/+source/multistrap/+bug/1313787
     sudo sed -i s/\$forceyes//g /usr/sbin/multistrap
+    sudo apt-get -y install libudev-dev:armhf
 }
 
 createRaspbianImg(){
