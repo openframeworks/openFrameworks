a73a0deab9b9d14619d928edb68f548943a62d68
ofTheo <theo@openframeworks.cc>
2023-08-22 17:47:09 -0700

    try no copy libs

--- scripts/ci/linuxarmv6l/install.sh
+++ scripts/ci/linuxarmv6l/install.sh
@@ -70,10 +70,6 @@ createRaspbianImg
 downloadToolchain
 downloadFirmware
 
-cp -rn rpi_toolchain/arm-linux-gnueabihf/libc/lib/* $ROOT/raspbian/usr/lib/
-cp -rn rpi_toolchain/arm-linux-gnueabihf/libc/usr/lib/* $ROOT/raspbian/usr/lib/
-cp -rn rpi_toolchain/arm-linux-gnueabihf/lib/* $ROOT/raspbian/usr/lib/
-
 cd $ROOT/raspbian/usr/lib
 relativeSoftLinks
 cd $ROOT/raspbian/usr/lib/arm-linux-gnueabihf
