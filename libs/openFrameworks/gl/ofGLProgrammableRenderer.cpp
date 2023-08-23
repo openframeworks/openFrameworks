afdae819ee330dffb629d7108869c38c316f2f92
Dan Rosser <danoli3@gmail.com>
2023-08-23 16:39:20 +1000

    Update ofGLProgrammableRenderer.cpp
    fix for not bound texture for font

--- libs/openFrameworks/gl/ofGLProgrammableRenderer.cpp
+++ libs/openFrameworks/gl/ofGLProgrammableRenderer.cpp
@@ -1892,17 +1892,16 @@ void ofGLProgrammableRenderer::drawString(string textString, float x, float y, f
 
 //----------------------------------------------------------
 void ofGLProgrammableRenderer::drawString(const ofTrueTypeFont & font, string text, float x, float y) const{
-		const_cast<ofGLProgrammableRenderer*>(this)->setAttributes(true,true,true,false);
+		ofGLProgrammableRenderer * mutThis = const_cast<ofGLProgrammableRenderer*>(this);
+		mutThis->setAttributes(true,true,true,false);
 		const ofTexture& tex = font.getFontTexture();
 		ofBlendMode blendMode = currentStyle.blendingMode;
 		if(tex.isAllocated()) {
-			const_cast<ofGLProgrammableRenderer*>(this)->setBlendMode(OF_BLENDMODE_ALPHA);
-			const_cast<ofGLProgrammableRenderer*>(this)->bind(tex,0);
-			//draw(font.getStringMesh(text,x,y,isVFlipped()),OF_MESH_FILL);
-			font.getStringMesh(text, x,y).draw();
-			//draw(font.getStringMesh(text,x,y),OF_MESH_FILL);
-			const_cast<ofGLProgrammableRenderer*>(this)->unbind(tex,0);
-			const_cast<ofGLProgrammableRenderer*>(this)->setBlendMode(blendMode);
+		 	mutThis->setBlendMode(OF_BLENDMODE_ALPHA);
+		 	mutThis->bind(font.getFontTexture(),0);
+		 	draw(font.getStringMesh(text,x,y,isVFlipped()),OF_MESH_FILL);
+		 	mutThis->unbind(font.getFontTexture(),0);
+		 	mutThis->setBlendMode(blendMode);
 		} else {
 			ofLogWarning("ofGLProgrammableRenderer") << "draw(): texture is not allocated";
 		}
