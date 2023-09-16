f93db3772b21bffe6e7023857ca424151e01d534
alexandre burton <b@artificiel.org>
2023-09-16 13:23:57 -0400

    ofRandomDistributions: proper #includes

--- libs/openFrameworks/utils/ofRandomDistributions.h
+++ libs/openFrameworks/utils/ofRandomDistributions.h
@@ -2,7 +2,7 @@
 #define OF_RANDOM_DISTRIBUTIONS_H_
 
 #include <glm/glm.hpp>
-#include <random>
+#include "ofRandomEngine.h"
 
 // https://gist.github.com/imneme/540829265469e673d045
 // https://github.com/effolkronium/random/tree/master
