f627bfba756f70d7896c8f629c48203ea1b2af09
alexandre burton <b@artificiel.org>
2023-09-28 16:17:00 -0400

    ofxOscReceiver: fix #includes

--- addons/ofxOsc/src/ofxOscReceiver.cpp
+++ addons/ofxOsc/src/ofxOscReceiver.cpp
@@ -1,6 +1,6 @@
 // copyright (c) openFrameworks team 2010-2023
 // copyright (c) Damian Stewart 2007-2009
-#include "ofxOscMessage.h"
+#include "ofxOscReceiver.h"
 
 //--------------------------------------------------------------
 ofxOscReceiver::~ofxOscReceiver() {
