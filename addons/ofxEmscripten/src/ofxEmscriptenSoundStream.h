65c2f3e6bebc325a9c865664006dccd16035f9f7
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2023-10-20 18:39:58 +0200

    Update ofxEmscriptenSoundStream.h

--- addons/ofxEmscripten/src/ofxEmscriptenSoundStream.h
+++ addons/ofxEmscripten/src/ofxEmscriptenSoundStream.h
@@ -32,13 +32,13 @@ public:
 	int getNumOutputChannels() const;
 	int getSampleRate() const;
 	int getBufferSize() const;
-	ofSoundStreamSettings settings;
-	ofSoundBuffer inbuffer;
-	ofSoundBuffer outbuffer;
 
 private:
 	static void audio_cb(int bufferSize, int inputChannels, int outputChannels, void * userData);
 	void audioCB(int bufferSize, int inputChannels, int outputChannels);
 	int context;
 	unsigned long long tickCount;
+	ofSoundStreamSettings settings;
+	ofSoundBuffer inbuffer;
+	ofSoundBuffer outbuffer;
 };
