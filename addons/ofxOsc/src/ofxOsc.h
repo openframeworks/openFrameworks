5af7247e5c9284872ea161d46a9504c6002aa5a8
alexandre burton <burton@artificiel.org>
2023-11-15 14:54:07 -0500

    ofxOsc.h: superfluous #includes

--- addons/ofxOsc/src/ofxOsc.h
+++ addons/ofxOsc/src/ofxOsc.h
@@ -1,8 +1,6 @@
-// copyright (c) openFrameworks team 2010-2017
+// copyright (c) openFrameworks team 2010-2023
 // copyright (c) Damian Stewart 2007-2009
 #pragma once
 
-#include "ofxOscArg.h"
-#include "ofxOscMessage.h"
 #include "ofxOscSender.h"
 #include "ofxOscReceiver.h"
