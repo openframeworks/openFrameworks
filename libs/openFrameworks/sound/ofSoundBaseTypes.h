71a092a1e99f2c2094939c581717fadd1d18ef9b
Dan Wilcox <danomatika@gmail.com>
2023-11-20 16:53:14 +0100

    docs_low_level: cleanups for ofBaseTypes, ofMaterial, ofURLFileLoader, take 2 (#7744)
    #changelog #documentation 
    
    * fix ofMatrixStack error message spacing
    * fix missing whitespace in ofXml load warning
    * fix missing whitespace in ofFile copyTo and moveTo errors
    * fix missing whitespace after colon in numerous ofLogs, some log format consistency updates (mainly ofOpenALSoundPlayer
    * documentation: low level cleanups from 2016 for base types

--- libs/openFrameworks/sound/ofSoundBaseTypes.h
+++ libs/openFrameworks/sound/ofSoundBaseTypes.h
@@ -5,33 +5,43 @@
 
 class ofSoundBuffer;
 
+/// \class ofBaseSoundInput
 /// \brief A base class representing a sound input stream.
 class ofBaseSoundInput{
 	public:
+
 		/// \brief Destroy the ofBaseSoundInput.
 		virtual ~ofBaseSoundInput() {};
 
 		/// \brief Receive an audio buffer.
+		///
 		/// \param buffer An audio buffer.
 		virtual void audioIn( ofSoundBuffer& buffer );
 
+		/// \brief Use void audioIn(ofSoundBuffer& buffer) instead.
+		///
 		/// \deprecated This legacy method is deprecated and will be removed.
 		/// Use void audioIn(ofSoundBuffer& buffer) instead.
 		virtual void audioIn( float * input, int bufferSize, int nChannels, int deviceID, long unsigned long tickCount );
 
+		/// \brief Use void audioIn(ofSoundBuffer& buffer) instead.
+		///
 		/// \deprecated This legacy method is deprecated and will be removed.
 		/// Use void audioIn(ofSoundBuffer& buffer) instead.
 		virtual void audioIn( float * input, int bufferSize, int nChannels );
 
+		/// \brief Use void audioIn(ofSoundBuffer& buffer) instead.
+		///
 		/// \deprecated This legacy method is deprecated and will be removed.
 		/// Use void audioIn(ofSoundBuffer& buffer) instead.
 		virtual void audioReceived( float * input, int bufferSize, int nChannels ){}
 };
 
-
+/// \class ofBaseSoundOutput
 /// \brief A base class representing a sound output stream.
 class ofBaseSoundOutput{
 	public:
+
 		/// \brief Destroy the ofBaseSoundOutput.
 		virtual ~ofBaseSoundOutput() {};
 
