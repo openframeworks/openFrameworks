23669759b41fbd8f5941aaeb18440ed01e010cca
tiamo68 <124882934+tiamo68@users.noreply.github.com>
2023-12-04 13:34:04 +0000

    Update install_template.sh
    updating qt creator build script

--- scripts/qtcreator/install_template.sh
+++ scripts/qtcreator/install_template.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-# default path for Linux
+# Set the default path for Linux
 QTCREATOR_WIZARDS=${HOME}/.config/QtProject/qtcreator/templates/wizards/
 
-# if running MSYS2, templates are located in $APPDATA
-OS=`uname`
-if [ "${OS:0:5}" == "MINGW" -o "${OS:0:7}" == "MSYS_NT" ]; then
-	QTCREATOR_WIZARDS=`cygpath ${APPDATA}/QtProject/qtcreator/templates/wizards/`
+# If running MSYS2, templates are located in $APPDATA
+OS=$(uname)
+if [[ "${OS:0:5}" == "MINGW" || "${OS:0:7}" == "MSYS_NT" ]]; then
+	QTCREATOR_WIZARDS=$(cygpath "${APPDATA}/QtProject/qtcreator/templates/wizards/")
 fi
 
+# Ensure the destination directory exists
+mkdir -p "${QTCREATOR_WIZARDS}"
 
-mkdir -p ${QTCREATOR_WIZARDS}
-cp -r templates/wizards/openFrameworks ${QTCREATOR_WIZARDS}
-cp -r templates/wizards/openFrameworksUpdate ${QTCREATOR_WIZARDS}
-
+# Copy OpenFrameworks templates to the Qt Creator wizards directory
+cp -r templates/wizards/openFrameworks "${QTCREATOR_WIZARDS}"
+cp -r templates/wizards/openFrameworksUpdate "${QTCREATOR_WIZARDS}"
