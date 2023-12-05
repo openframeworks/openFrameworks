c8570f186ba6df63e26541c1e0d5ff914b3802c4
ofTheo <theo@openframeworks.cc>
2023-12-05 07:42:02 -0800

    try quotes

--- scripts/ci/emscripten/examples_to_build.sh
+++ scripts/ci/emscripten/examples_to_build.sh
@@ -13,52 +13,52 @@ folders=(
 echo "RUNNING IN CI ${GH_BRANCH} ${GH_ACTIONS}"
 echo "TRYING USER/SERVER ${GA_EXAMPLES_USER}@${GA_EXAMPLES_SERVER}" 
 
-cur_root=$(pwd);
-cd $cur_root;
-mkdir -p out
-out_folder="$cur_root/out"
-
-# Iterate through the folder paths
-for folder in "${folders[@]}"; do
-    # Check if the folder exists
-    if [ -d "$folder" ]; then
-
-        # Change to the directory
-        cd $folder
-        cp ../../../scripts/templates/emscripten/Makefile .
-		cp ../../../scripts/templates/emscripten/config.make .
-        emmake make -j2 Release
-        
-        errorcode=$?
-		if [[ $errorcode -ne 0 ]]; then
-			echo "Couldn't build emscripten example: $folder"
-		else
-			folder_name=$(basename "$folder")
-			cp -r "bin/em/$folder_name" "$out_folder/"
-		fi
-		
-		cd $cur_root
-    else
-        echo "Folder does not exist: $folder"
-    fi
-done
-
-cd $cur_root;
-DO_UPLOAD="false"
-
-#if [[ "$GH_ACTIONS" = true && "${GH_BRANCH}" == "master" && -z "${GH_HEAD_REF}" ]]; then
-if [[ "$GH_ACTIONS" = "true" ]]; then
-	echo "upload 1/2 - make key file"
-    # Temporary file to store the private key
-	key_file=$(mktemp)
-	echo -e "$GA_EXAMPLES_KEY" > "$key_file"
-	chmod 600 "$key_file"
-    DO_UPLOAD="true";
-fi
-
-if [ "$DO_UPLOAD" = "true" ]; then
-	echo "upload 2/2 - time for rsync"
-	remote_path="/home/ofadmin/openFrameworks.cc/examples/"
-	rsync -avz -e "ssh -i $key_file" "$out_folder/" "$GA_EXAMPLES_USER@$GA_EXAMPLES_SERVER:$remote_path"
-    rm -f "$key_file"
-fi
+#cur_root=$(pwd);
+#cd $cur_root;
+#mkdir -p out
+#out_folder="$cur_root/out"
+#
+## Iterate through the folder paths
+#for folder in "${folders[@]}"; do
+#    # Check if the folder exists
+#    if [ -d "$folder" ]; then
+#
+#        # Change to the directory
+#        cd $folder
+#        cp ../../../scripts/templates/emscripten/Makefile .
+#		cp ../../../scripts/templates/emscripten/config.make .
+#        emmake make -j2 Release
+#        
+#        errorcode=$?
+#		if [[ $errorcode -ne 0 ]]; then
+#			echo "Couldn't build emscripten example: $folder"
+#		else
+#			folder_name=$(basename "$folder")
+#			cp -r "bin/em/$folder_name" "$out_folder/"
+#		fi
+#		
+#		cd $cur_root
+#    else
+#        echo "Folder does not exist: $folder"
+#    fi
+#done
+#
+#cd $cur_root;
+#DO_UPLOAD="false"
+#
+##if [[ "$GH_ACTIONS" = true && "${GH_BRANCH}" == "master" && -z "${GH_HEAD_REF}" ]]; then
+#if [[ "$GH_ACTIONS" = "true" ]]; then
+#	echo "upload 1/2 - make key file"
+#    # Temporary file to store the private key
+#	key_file=$(mktemp)
+#	echo -e "$GA_EXAMPLES_KEY" > "$key_file"
+#	chmod 600 "$key_file"
+#    DO_UPLOAD="true";
+#fi
+#
+#if [ "$DO_UPLOAD" = "true" ]; then
+#	echo "upload 2/2 - time for rsync"
+#	remote_path="/home/ofadmin/openFrameworks.cc/examples/"
+#	rsync -avz -e "ssh -i $key_file" "$out_folder/" "$GA_EXAMPLES_USER@$GA_EXAMPLES_SERVER:$remote_path"
+#    rm -f "$key_file"
+#fi
