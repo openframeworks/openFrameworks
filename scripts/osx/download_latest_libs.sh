44e6426b3ca1364b7375691b60dd6ea7d2670df9
Dan Rosser <danoli3@gmail.com>
2023-12-27 06:26:59 +1100

    override logic fix -n for extra targets

--- scripts/osx/download_latest_libs.sh
+++ scripts/osx/download_latest_libs.sh
@@ -3,8 +3,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 cd $SCRIPT_DIR
 if [ ! -z ${BITS+x} ]; then
-    ../dev/download_libs.sh -p osx -b -a $BITS -n $@
+    ../dev/download_libs.sh -p osx -b -a $BITS $@
 else
-    ../dev/download_libs.sh -p osx -b -a x86_64 -n $@
+    ../dev/download_libs.sh -p osx -b -a x86_64 $@
     ../dev/download_libs.sh -p osx -b -a arm64 -n $@
 fi
--- scripts/vs/download_latest_libs.sh
+++ scripts/vs/download_latest_libs.sh
@@ -3,9 +3,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 cd $SCRIPT_DIR
 if [ ! -z ${BITS+x} ]; then
-    ../dev/download_libs.sh -p vs -b -a $BITS -n $@
+    ../dev/download_libs.sh -p vs -b -a $BITS $@
 else
-    ../dev/download_libs.sh -p vs -b -a 64 -n $@
+    ../dev/download_libs.sh -p vs -b -a 64 $@
     ../dev/download_libs.sh -p vs -b -a arm64 -n $@
     ../dev/download_libs.sh -p vs -b -a arm64ec -n $@
 fi
