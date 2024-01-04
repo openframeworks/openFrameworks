8a37ce5a8d2280c7a6dafa0f56eb9572424a4eab
Dimitre <dimitre@users.noreply.github.com>
2024-01-04 15:07:02 -0300

    remove ccache

--- scripts/ci/osx/run_tests.sh
+++ scripts/ci/osx/run_tests.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -ev
 ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
-source $ROOT/scripts/ci/ccache.sh
-export CXX="$(which ccache) $(xcodebuild -find clang++)"
-export CC="$(which ccache) $(xcodebuild -find clang)"
+# source $ROOT/scripts/ci/ccache.sh
 
 trap 'for f in ~/Library/Logs/DiagnosticReports/*; do cat $f; done' 11
 
