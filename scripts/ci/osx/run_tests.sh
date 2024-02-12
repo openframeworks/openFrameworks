19813bc4087091ad3948a94b3bcac07116e05b8c
Dimitre <dimitre@users.noreply.github.com>
2024-02-12 11:21:38 -0300

    osx to macos on template folder

--- scripts/ci/osx/run_tests.sh
+++ scripts/ci/osx/run_tests.sh
@@ -7,15 +7,13 @@ trap 'for f in ~/Library/Logs/DiagnosticReports/*; do cat $f; done' 11
 
 echo "**** Building emptyExample ****"
 cd $ROOT
-cp scripts/templates/osx/Makefile examples/templates/emptyExample/
-cp scripts/templates/osx/config.make examples/templates/emptyExample/
+cp scripts/templates/macos/{Makefile,config.make} examples/templates/emptyExample/
 cd examples/templates/emptyExample/
 make -j Debug
 
 echo "**** Building allAddonsExample ****"
 cd $ROOT
-cp scripts/templates/osx/Makefile examples/templates/allAddonsExample/
-cp scripts/templates/osx/config.make examples/templates/allAddonsExample/
+cp scripts/templates/macos/{Makefile,config.make} examples/templates/emptyExample/
 cd examples/templates/allAddonsExample/
 make -j Debug
 
@@ -26,8 +24,7 @@ for group in *; do
         for test in $group/*; do
             if [ -d $test ]; then
                 cd $test
-                cp ../../../scripts/templates/osx/Makefile .
-                cp ../../../scripts/templates/osx/config.make .
+                cp ../../../scripts/templates/macos/{Makefile,config.make} .
                 make -j Debug
                 make RunDebug
 				errorcode=$?
