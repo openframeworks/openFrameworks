a18d2111268723d578bd9dc23113c1e0df98694e
Dimitre <dimitre@users.noreply.github.com>
2024-03-10 13:34:48 -0300

    downloader in parallel
    (cherry picked from commit 6e8819c740a8d66f4762801530e180acb56799f3)

--- scripts/dev/downloader.sh
+++ scripts/dev/downloader.sh
@@ -13,24 +13,37 @@ EOF
 }
 
 downloader() { 
-
 	if [ -z "$1" ]
 	then printDownloaderHelp; fi
 
-    	SILENTARGS="";
-        if [ $# -ge 2  ]; then
-            SILENTARGS=$2
-        fi
-        if [[ "${SILENTARGS}" == "-s" ]]; then
-		if command -v curl 2>/dev/null; then 
-    			curl -LO --retry 20 -O -s $@; 
-    		else 
-        		wget -q $@ 2> /dev/null; fi;
+	SILENTARGS="";
+	if [ $# -ge 2  ]; then
+		SILENTARGS=$2
+	fi
+	if [[ "${SILENTARGS}" == "-s" ]]; then
+		if command -v wget2 2>/dev/null; then 
+			wget2 -q $@;
+		elif command -v curl 2>/dev/null; then 
+			for PKG in $@; do
+				curl -LO --retry 20 -O -s $PKG; 
+			done
+		else 
+			for PKG in $@; do
+				wget -q $PKG@ 2> /dev/null; 
+			done
+		fi;
 	else
-		if command -v curl 2>/dev/null; then 
-    			curl -LO --retry 20 -O --progress-bar $@; 
-    		else 
-        		wget $@ 2> /dev/null; fi;
+		if command -v wget2 2>/dev/null; then 
+			wget2 $@;
+		elif command -v curl 2>/dev/null; then 
+			for PKG in $@; do
+				curl -LO --retry 20 -O --progress-bar $PKG;
+			done
+		else 
+			for PKG in $@; do
+				wget $PKG 2> /dev/null; 
+			done
+		fi;
 	fi
 
  
