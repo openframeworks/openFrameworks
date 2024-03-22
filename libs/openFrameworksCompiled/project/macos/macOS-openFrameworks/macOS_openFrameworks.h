ddb841b4ff19321b10c467ec3c06bde5701fb4b2
Dan Rosser <danoli3@gmail.com>
2024-03-22 23:00:31 +1100

    macOS Super Mega Framework Project all Targets

---
+++ libs/openFrameworksCompiled/project/macos/macOS-openFrameworks/macOS_openFrameworks.h
@@ -0,0 +1,18 @@
+//
+//  macOS_openFrameworks.h
+//  macOS-openFrameworks
+//
+//  Created by One4Seven on 22/3/2024.
+//
+
+#import <Foundation/Foundation.h>
+//
+////! Project version number for macOS_openFrameworks.
+FOUNDATION_EXPORT double macOS_openFrameworksVersionNumber;
+//
+////! Project version string for macOS_openFrameworks.
+FOUNDATION_EXPORT const unsigned char macOS_openFrameworksVersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <macOS_openFrameworks/PublicHeader.h>
+//
+//
