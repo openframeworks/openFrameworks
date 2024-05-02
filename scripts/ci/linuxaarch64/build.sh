3cf1d1e8cc41158c2aa2f9618b32216faaef9ff1
Dimitre <dimitre@users.noreply.github.com>
2024-05-02 20:29:50 -0300

    up

--- scripts/ci/linuxaarch64/build.sh
+++ scripts/ci/linuxaarch64/build.sh
@@ -9,11 +9,12 @@ export PLATFORM_OS=Linux
 export PLATFORM_ARCH=aarch64
 export PKG_CONFIG_LIBDIR=${RPI_ROOT}/usr/include:${RPI_ROOT}/usr/lib/pkgconfig:${RPI_ROOT}/usr/lib/${GCC_PREFIX}/pkgconfig:${RPI_ROOT}/usr/share/pkgconfig
 export PKG_CONFIG_PATH=${RPI_ROOT}/usr/include:${RPI_ROOT}/usr/lib/pkgconfig:${RPI_ROOT}/usr/lib/${GCC_PREFIX}/pkgconfig:${RPI_ROOT}/usr/share/pkgconfig
-export TOOLCHAIN_ROOT=/usr/bin
-export CXX="${TOOLCHAIN_ROOT}/${GCC_PREFIX}-g++"
-export CC="${TOOLCHAIN_ROOT}/${GCC_PREFIX}-gcc"
-export AR=${TOOLCHAIN_ROOT}/${GCC_PREFIX}-ar
-export LD=${TOOLCHAIN_ROOT}/${GCC_PREFIX}-ld
+# export TOOLCHAIN_ROOT=/usr/bin/
+export TOOLCHAIN_ROOT=
+export CXX="${TOOLCHAIN_ROOT}${GCC_PREFIX}-g++"
+export CC="${TOOLCHAIN_ROOT}${GCC_PREFIX}-gcc"
+export AR=${TOOLCHAIN_ROOT}${GCC_PREFIX}-ar
+export LD=${TOOLCHAIN_ROOT}${GCC_PREFIX}-ld
 
 # export PATH=${RPI_ROOT}/bin/:$PATH
 # export LD_LIBRARY_PATH=${RPI_ROOT}/lib:$LD_LIBRARY_PATH
