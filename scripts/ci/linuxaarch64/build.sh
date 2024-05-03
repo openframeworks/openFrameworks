a101e5f044a836e1c0d87cb7fc1227976d3be995
Dimitre <dimitre@users.noreply.github.com>
2024-05-02 21:34:33 -0300

    fixes

--- scripts/ci/linuxaarch64/build.sh
+++ scripts/ci/linuxaarch64/build.sh
@@ -18,10 +18,10 @@ export PLATFORM_ARCH=${ARCH}
 export PKG_CONFIG_LIBDIR=${RPI_ROOT}/usr/include:${RPI_ROOT}/usr/lib/pkgconfig:${RPI_ROOT}/usr/lib/${GCC_PREFIX}/pkgconfig:${RPI_ROOT}/usr/share/pkgconfig
 export PKG_CONFIG_PATH=${RPI_ROOT}/usr/include:${RPI_ROOT}/usr/lib/pkgconfig:${RPI_ROOT}/usr/lib/${GCC_PREFIX}/pkgconfig:${RPI_ROOT}/usr/share/pkgconfig
 export TOOLCHAIN_ROOT=/usr
-# export CXX="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-g++"
-# export CC="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-gcc"
-# export AR=${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-ar
-# export LD=${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-ld
+export CXX="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-g++"
+export CC="${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-gcc"
+export AR=${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-ar
+export LD=${TOOLCHAIN_ROOT}/bin/${GCC_PREFIX}-ld
 
 echo "**** Building emptyExample ****"
 cd $OF_ROOT
