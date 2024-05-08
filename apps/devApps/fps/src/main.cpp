bca2635e5ec7c2e38a06961e9269ccf9c5f84ddf
Dimitre <dimitre@users.noreply.github.com>
2024-05-08 17:08:43 -0300

    ok

--- apps/devApps/fps/src/main.cpp
+++ apps/devApps/fps/src/main.cpp
@@ -6,11 +6,10 @@ int main( ){
 	ofGLFWWindowSettings settings;
 	settings.setSize(600, 400);
 	settings.windowMode = OF_WINDOW;
-	settings.doubleBuffering = false;
+//	settings.doubleBuffering = false;
 	auto mainApp { make_shared<ofApp>() };
 	auto window { ofCreateWindow(settings) };
-	
+
 	ofRunApp(window, mainApp);
 	ofRunMainLoop();
-
 }
