28f507c6ef51600ec306a5b7962b244219075989
Dimitre <dimitre@users.noreply.github.com>
2024-05-09 10:48:32 -0300

    up

--- libs/openFrameworksCompiled/project/makefileCommon/config.linux.common.mk
+++ libs/openFrameworksCompiled/project/makefileCommon/config.linux.common.mk
@@ -141,7 +141,7 @@ PLATFORM_CXXVER = -std=c++17
 GCC_MAJOR := $(shell expr `gcc -dumpversion | cut -f1 -d.`)
 GCC_MINOR := $(shell expr `gcc -dumpversion | cut -f2 -d.`)
 
-# $(info $(GCC_MAJOR))
+$(info GCC VERSION $(GCC_MAJOR).$(GCC_MINOR))
 # $(info $(GCC_MINOR))
 # $(info ($(shell expr $(GCC_MAJOR) \>= 8), 1))
 # $(info ($(shell expr $(GCC_MINOR) \< 7), 1))
