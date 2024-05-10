b01f3e8ef2ce1d2ca9e94451a299ac282dd4a17d
Dimitre <dimitre@users.noreply.github.com>
2024-05-10 02:27:01 -0300

    xml

--- libs/openFrameworks/utils/ofXml.cpp
+++ libs/openFrameworks/utils/ofXml.cpp
@@ -17,7 +17,8 @@ ofXml::ofXml(std::shared_ptr<pugi::xml_document> doc, const pugi::xml_node & xml
 
 bool ofXml::load(const of::filesystem::path & file){
 	auto auxDoc = std::make_shared<pugi::xml_document>();
-	auto res = auxDoc->load_file(ofToDataPath(file).c_str());
+	// auto res = auxDoc->load_file(ofToDataPath(file).c_str());
+	auto res = auxDoc->load_file(ofToDataPath(file));
 	if( res ){
 		doc = auxDoc;
 		xml = doc->root();
@@ -34,11 +35,13 @@ bool ofXml::load(const ofBuffer & buffer){
 
 bool ofXml::parse(const std::string & xmlStr){
 	auto auxDoc = std::make_shared<pugi::xml_document>();
-    #if ( defined(PUGIXML_VERSION) && PUGIXML_VERSION >= 150 )
-        if(auxDoc->load_string(xmlStr.c_str())){
-    #else
-        if(auxDoc->load(xmlStr.c_str())){
-    #endif
+	#if ( defined(PUGIXML_VERSION) && PUGIXML_VERSION >= 150 )
+		// if(auxDoc->load_string(xmlStr.c_str())){
+		if(auxDoc->load_string(xmlStr)){
+	#else
+		// if(auxDoc->load(xmlStr.c_str())){
+		if(auxDoc->load(xmlStr)){
+	#endif
 		doc = auxDoc;
 		xml = doc->root();
 		return true;
@@ -49,14 +52,16 @@ bool ofXml::parse(const std::string & xmlStr){
 
 bool ofXml::save(const of::filesystem::path & file) const{
 	if(xml == doc->root()){
-		auto res = doc->save_file(ofToDataPath(file).c_str());
+		// auto res = doc->save_file(ofToDataPath(file).c_str());
+		auto res = doc->save_file(ofToDataPath(file));
 		ofLogVerbose("ofXml")<<"save: "<< res;
 		ofLogVerbose("ofXml")<<this->toString();
 		return res;
 	}else{
 		pugi::xml_document doc;
 		if(doc.append_copy(xml.root())){
-			return doc.save_file(ofToDataPath(file).c_str());
+			// return doc.save_file(ofToDataPath(file).c_str());
+			return doc.save_file(ofToDataPath(file));
 		}
 	}
 	return false;
