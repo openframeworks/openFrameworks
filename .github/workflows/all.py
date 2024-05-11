49f97cda33f0d1a76a07bf0af0bbd845401c81d6
Dimitre <dimitre@users.noreply.github.com>
2024-05-11 17:01:46 -0300

    update

--- .github/workflows/all.py
+++ .github/workflows/all.py
@@ -6,13 +6,23 @@ print(lista)
 contagem = 0
 alllines = ''
 lines = []
+first = True
 for a in lista:
     print(a)
     f = open(a,'r')
     # alllines += (f.read() + '\n\n\n\n')
-    lines += f.readlines()[2:]
+
+
+    if first:
+        first = False
+        lines += 'name: OF'
+        lines += ''
+        lines += f.readlines()[1:]
+    else:
+        lines += f.readlines()[23:]
     # lines.append(f.readlines()[2:])
-    lines += '\n\n\n\n'
+    lines += '\n\n'
+    lines += '# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n\n'
     # lines.append('\n\n\n\n')
     f.close() 
 
