2a4ac9da25695de02cf4a46c29fe9b2f1a062c48
Dimitre <dimitre@users.noreply.github.com>
2024-05-11 10:19:12 -0300

    group / endgroup in run_tests

--- scripts/ci/osx/run_tests.sh
+++ scripts/ci/osx/run_tests.sh
@@ -5,24 +5,27 @@ ROOT=${TRAVIS_BUILD_DIR:-"$( cd "$(dirname "$0")/../../.." ; pwd -P )"}
 
 trap 'for f in ~/Library/Logs/DiagnosticReports/*; do cat $f; done' 11
 
-echo "**** Building emptyExample ****"
+echo "##[group] **** Building emptyExample ****"
 cd $ROOT
 cp scripts/templates/osx/Makefile examples/templates/emptyExample/
 cp scripts/templates/osx/config.make examples/templates/emptyExample/
 cd examples/templates/emptyExample/
 make -j Debug
+echo "##[endgroup]"
 
-echo "**** Building allAddonsExample ****"
+echo "##[group]**** Building allAddonsExample ****"
 cd $ROOT
 cp scripts/templates/osx/Makefile examples/templates/allAddonsExample/
 cp scripts/templates/osx/config.make examples/templates/allAddonsExample/
 cd examples/templates/allAddonsExample/
 make -j Debug
+echo "##[endgroup]"
 
-echo "**** Running unit tests ****"
+echo "##[group]**** Running unit tests ****"
 cd $ROOT/tests
 for group in *; do
     if [ -d $group ]; then
+        echo "##[group] $group"
         for test in $group/*; do
             if [ -d $test ]; then
                 cd $test
@@ -37,5 +40,7 @@ for group in *; do
                 cd $ROOT/tests
             fi
         done
+        echo "##[endgroup]"
     fi
 done
+echo "##[endgroup]"
