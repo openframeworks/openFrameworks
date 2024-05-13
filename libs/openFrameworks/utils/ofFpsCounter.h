3f069c2e9b7a4675e8ed86bde63a32170e9c4d00
Dimitre <dimitre@users.noreply.github.com>
2024-05-13 13:49:51 -0300

    fps counter back original

--- libs/openFrameworks/utils/ofFpsCounter.h
+++ libs/openFrameworks/utils/ofFpsCounter.h
@@ -4,7 +4,6 @@
 #include "ofUtils.h"
 #include <queue>
 
-
 class ofFpsCounter {
 public:
 	ofFpsCounter();
@@ -34,52 +33,4 @@ private:
 	std::chrono::nanoseconds filteredTime;
 	double filterAlpha;
 	std::queue<double> timestamps;
-};
-
-
-
-
-
-#include <chrono>
-using namespace std::chrono;
-using namespace std::chrono_literals;
-
-struct fpsCounterX {
-public:
-	int nAverages = 20;
-	using space = std::chrono::duration<long double, std::nano>;
-	time_point<steady_clock> lastTick;
-	steady_clock::duration onesec = 1s;
-	std::vector <space> intervals;
-	space interval;
-	space average;
-	bool firstTick = true;
-	int cursor = 0;
-
-	void tick() {
-		if (firstTick) {
-			firstTick = false;
-			lastTick = steady_clock::now();
-			return;
-		}
-
-		interval = steady_clock::now() - lastTick;
-		lastTick = steady_clock::now();
-		if (intervals.size() < nAverages) {
-			intervals.emplace_back(interval);
-		} else {
-			intervals[cursor] = interval;
-			cursor = (cursor+1)%nAverages;
-		}
-	}
-	
-	double getFps()  {
-		average = std::reduce(intervals.begin(), intervals.end());
-		return (double)intervals.size() * onesec / average;
-	}
-	
-	float get() {
-		average = std::reduce(intervals.begin(), intervals.end());
-		return (float)intervals.size() * onesec / average;
-	}
 };
