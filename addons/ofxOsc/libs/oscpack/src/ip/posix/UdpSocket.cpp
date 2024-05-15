9ec04e937b7d567091be6e693711b4e0164db2f8
alexandre burton <b@artificiel.org>
2024-05-15 18:10:02 -0400

    UdpSocket: upgrade volatile to std::atomic<bool>

--- addons/ofxOsc/libs/oscpack/src/ip/posix/UdpSocket.cpp
+++ addons/ofxOsc/libs/oscpack/src/ip/posix/UdpSocket.cpp
@@ -356,7 +356,8 @@ class SocketReceiveMultiplexer::Implementation{
 	std::vector< std::pair< PacketListener*, UdpSocket* > > socketListeners_;
 	std::vector< AttachedTimerListener > timerListeners_;
 
-	volatile bool break_;
+	std::atomic<bool> break_ = false;
+
 	int breakPipe_[2]; // [0] is the reader descriptor and [1] the writer
 
 	double GetCurrentTimeMs() const
