3f710cfe258352c544053450d4276822f4b10bd9
Dimitre <dimitre@users.noreply.github.com>
2024-05-20 12:46:28 -0300

    up

--- scripts/msys2/install_dependencies.sh
+++ scripts/msys2/install_dependencies.sh
@@ -32,7 +32,7 @@ done
 
 # List of MSYS packages to be installed
 # msyspackages="make rsync zip unzip wget"
-msyspackages="unzip make"
+msyspackages="unzip make lld"
 
 # List of MINGW packages to be installed (without prefix)
 mingwPackages="gcc assimp cairo curl freeglut FreeImage \
