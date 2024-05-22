33c300f40186b5efc013245175a313ae5cf96ca2
Dimitre <dimitre@users.noreply.github.com>
2024-05-22 14:41:18 -0300

    commented out .h files removed

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -99,7 +99,6 @@ enum ofTargetPlatform{
 #elif defined( __APPLE_CC__)
     #define __ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES 0
     #include <TargetConditionals.h>
-	// #include <unistd.h>
 
 	#if (TARGET_OS_IPHONE || TARGET_OS_IOS || TARGET_OS_SIMULATOR || TARGET_IPHONE_SIMULATOR) && !TARGET_OS_TV && !TARGET_OS_WATCH
         #define TARGET_OF_IPHONE
@@ -191,7 +190,6 @@ enum ofTargetPlatform{
 		#define __MACOSX_CORE__
 	#endif
 	#include "GL/glew.h"
-	// #include <ApplicationServices/ApplicationServices.h>
 
 	#if defined(__LITTLE_ENDIAN__)
 		#define TARGET_LITTLE_ENDIAN		// intel cpu
@@ -203,8 +201,6 @@ enum ofTargetPlatform{
 #endif
 
 #ifdef TARGET_LINUX
-
-	// #include <unistd.h>
 
 	#ifdef TARGET_LINUX_ARM
 		#ifdef TARGET_RASPBERRY_PI
@@ -259,7 +255,6 @@ enum ofTargetPlatform{
 
 #ifdef TARGET_ANDROID
 	#include <typeinfo>
-	// #include <unistd.h>
 	#include <GLES/gl.h>
 	#define GL_GLEXT_PROTOTYPES
 	#include <GLES/glext.h>
