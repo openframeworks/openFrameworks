26b7bb6f9a9c64d3bda4b38f5c5e97907a54e29d
Dan Rosser <danoli3@gmail.com>
2024-07-12 22:02:56 +1000

    vs ci fix

--- scripts/ci/vs/install_bleeding.sh
+++ scripts/ci/vs/install_bleeding.sh
@@ -17,11 +17,20 @@ echo "Downloading projectGenerator from Github Bleeding"
 downloader https://github.com/openframeworks/projectGenerator/releases/download/nightly/projectGenerator-vs.zip 2> /dev/null
 unzip projectGenerator-vs.zip 2> /dev/null
 rm projectGenerator-vs.zip
+ls
+cd ../
 
 # downloader https://github.com/openframeworks/projectGenerator/releases/download/nightly/projectGenerator-vs-gui.zip 2> /dev/null
 # unzip projectGenerator-vs-gui.zip 2> /dev/null
 # rm projectGenerator-vs-gui.zip
 
-cd $OF_ROOT
-PG_OF_PATH=$OF_ROOT/projectGenerator/projectGenerator.exe examples/templates/emptyExample
-PG_OF_PATH=$OF_ROOT/projectGenerator/projectGenerator.exe examples/templates/allAddonsExample
+PG_OF_PATH=$OF_ROOT/projectGenerator/projectGenerator.exe
+
+PROJECTS=(
+    "examples/templates/emptyExample"
+    "examples/templates/allAddonsExample"
+)
+
+for PROJECT in "${PROJECTS[@]}"; do
+    "$PG_OF_PATH" -o"$OF_ROOT" -a"$PROJECT"
+done
