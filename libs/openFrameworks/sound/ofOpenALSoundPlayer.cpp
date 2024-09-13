1b4cff112938b8bb9456692786c1984e49986d43
Dimitre <dimitre@users.noreply.github.com>
2024-09-13 13:17:13 -0300

    ok

--- libs/openFrameworks/sound/ofOpenALSoundPlayer.cpp
+++ libs/openFrameworks/sound/ofOpenALSoundPlayer.cpp
@@ -498,11 +498,7 @@ bool ofOpenALSoundPlayer::readFile(const fs::path & fileName, std::vector<short>
 }
 
 //------------------------------------------------------------
-<<<<<<< HEAD
 bool ofOpenALSoundPlayer::load(const fs::path& _fileName, bool is_stream){
-=======
-bool ofOpenALSoundPlayer::load(const of::filesystem::path & _fileName, bool is_stream){
->>>>>>> master
 
 	auto fileName = ofToDataPath(_fileName);
 
