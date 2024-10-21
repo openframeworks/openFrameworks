f09851ea011065f215210237835ed584c8883f61
Dimitre <dimitre@users.noreply.github.com>
2024-10-21 11:41:57 -0300

    err invalid chars

--- libs/openFrameworks/utils/ofSystemUtils.h
+++ libs/openFrameworks/utils/ofSystemUtils.h
@@ -44,14 +44,14 @@ std::string ofSystemTextBoxDialog(std::string question, std::string text="");
 
 #ifdef TARGET_WIN32
 std::string ConvertWideToUtf8(const std::wstring & wstr) {
-	int count = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), wstr.length(), NULL, 0, NULL, NULL);
+	int count = WideCharToMultiByte(CP_UTF8, MB_ERR_INVALID_CHARS, wstr.c_str(), wstr.length(), NULL, 0, NULL, NULL);
 	std::string str(count, 0);
 	WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &str[0], count, NULL, NULL);
 	return str;
 }
 
 std::wstring ConvertUtf8ToWide(const std::string & str) {
-	int count = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), NULL, 0);
+	int count = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, str.c_str(), str.length(), NULL, 0);
 	std::wstring wstr(count, 0);
 	MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.length(), &wstr[0], count);
 	return wstr;
