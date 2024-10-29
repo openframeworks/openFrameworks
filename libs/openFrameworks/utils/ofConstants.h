987fb8f78b5b13b9cab386c40014e36e74f9f225
alexandre burton <burton@artificiel.org>
2024-10-29 00:44:21 -0400

    rename

--- libs/openFrameworks/utils/ofConstants.h
+++ libs/openFrameworks/utils/ofConstants.h
@@ -443,7 +443,7 @@ typedef TESSindex ofIndexType;
 			namespace filesystem = std::experimental::filesystem;
 		}
     #else
-		#include "ofFilesystemPath.h"
+		#include "ofFilesystem.h"
     #endif
 #else //not OF_USING_STD_FS
     // No experimental or c++17 filesytem support use boost
