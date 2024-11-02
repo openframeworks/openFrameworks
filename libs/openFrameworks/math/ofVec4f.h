530a29c3c57a9e8a108b12bd36c8945b8b3fc6e0
Dimitre <dimitre@users.noreply.github.com>
2024-11-02 19:50:25 -0300

    reducing .h includes in .h files

--- libs/openFrameworks/math/ofVec4f.h
+++ libs/openFrameworks/math/ofVec4f.h
@@ -1,11 +1,13 @@
 #pragma once
 
-#define GLM_FORCE_CTOR_INIT
-#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/detail/qualifier.hpp>
+namespace glm {
+	typedef vec<2, float, defaultp>		vec2;
+	typedef vec<3, float, defaultp>		vec3;
+}
+
 #include <glm/vec4.hpp>
-#include <glm/fwd.hpp>
 #include <iostream>
-#include <cmath>
 
 class ofVec2f;
 class ofVec3f;
