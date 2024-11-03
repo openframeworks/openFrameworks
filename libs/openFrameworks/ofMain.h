4c9dfa07297d19e4f1ae7b11e43683cbff820b75
Dimitre <dimitre@users.noreply.github.com>
2024-11-03 01:01:26 -0300

    up

--- libs/openFrameworks/ofMain.h
+++ libs/openFrameworks/ofMain.h
@@ -36,7 +36,7 @@
 // communication
 #if !defined(TARGET_OF_IOS) & !defined(TARGET_ANDROID) & !defined(TARGET_EMSCRIPTEN)
     #include "ofSerial.h"
-    #include "ofArduino.h"
+//    #include "ofArduino.h"
 #endif
 
 //--------------------------
