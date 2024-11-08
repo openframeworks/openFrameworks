e0394d82ceb7e24ccea21debb535982ec7b89336
alexandre burton <b@artificiel.org>
2024-11-08 18:51:04 -0500

    ofSIngleton: tweaks/updates

--- libs/openFrameworks/utils/ofSingleton.h
+++ libs/openFrameworks/utils/ofSingleton.h
@@ -4,6 +4,15 @@
 // atomic C++17 DCLP CRTP singleton adapted by burton@artificiel.org from
 // https://github.com/jimmy-park/singleton/blob/main/include/singleton_dclp.hpp (as of df5e4a2)
 
+// the atomic CRTP Singleton is explicitely constructed (not simply as a side effect
+// of being the first call to the instance) which makes it easier to audit and
+// configure (arguments are explictely passed as required))
+//
+// using the Singleton make the code thread-safe and better contained than
+// arbitrarily allocated pointers in the global namespace
+//
+// this implements abstract class injection to prevent construction at compile time
+
 #include <atomic>
 #include <cassert>
 #include <shared_mutex>
@@ -18,9 +27,9 @@ public:
 	/// \brief constructs the instance
 	template <typename... Args>
 	static void construct(Args &&... args) {
-		struct Dummy : public Derived {
+		struct Dummy final : Derived {
 			using Derived::Derived;
-			void prohibit_construct_from_derived() const override { }
+			void prohibit_construct_from_derived() const noexcept override { }
 		};
 
 		using Instance = Dummy;
@@ -49,7 +58,6 @@ public:
 		if (!the_instance) {
 			std::shared_lock lock { mutex_ };
 			the_instance = instance_.load(std::memory_order_relaxed);
-			assert(the_instance);
 		}
 		return the_instance;
 	}
@@ -57,13 +65,13 @@ public:
 protected:
 	Singleton() = default;
 	Singleton(const Singleton &) = delete;
-	Singleton(Singleton &&) noexcept = delete;
+	Singleton(Singleton &&) = delete;
 	Singleton & operator=(const Singleton &) = delete;
-	Singleton & operator=(Singleton &&) noexcept = delete;
+	Singleton & operator=(Singleton &&) = delete;
 	virtual ~Singleton() = default;
 
 private:
-	virtual void prohibit_construct_from_derived() const = 0;
+	virtual void prohibit_construct_from_derived() const noexcept = 0;
 	inline static std::atomic<Derived *> instance_ { nullptr };
 	inline static std::shared_mutex mutex_;
 };
