b9b866bd6e08ec825c008ded58e3525553217ae6
Dimitre <dimitre@users.noreply.github.com>
2024-11-20 03:50:53 -0300

    pixels memcpy

--- libs/openFrameworks/graphics/ofPixels.cpp
+++ libs/openFrameworks/graphics/ofPixels.cpp
@@ -1,7 +1,7 @@
 #include "ofGraphicsConstants.h"
 #include "ofPixels.h"
 #include "ofColor.h"
-#include <string> // memcpy
+#include <string.h> // memcpy
 
 static ofImageType getImageTypeFromChannels(size_t channels){
 	switch(channels){
