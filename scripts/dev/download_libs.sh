3f2b5fdcb19ff481aa2a526f33c57cce472da8f3
Dan Rosser <danoli3@gmail.com>
2025-02-26 06:51:48 +1100

    oF script / linux downloader

--- scripts/dev/download_libs.sh
+++ scripts/dev/download_libs.sh
@@ -251,15 +251,9 @@ if [ "$ARCH" == "" ]; then
         if [ "$ARCH" == "x86_64" ]; then
             ARCH=64
         elif [ "$ARCH" == "arm64" ]; then
-            # ARCH=arm64 # need to fix
-            ARCH=64
-            if [ -f /opt/vc/include/bcm_host.h ]; then # RPi
-                ARCH=aarch64
-            fi
-        elif [ "$ARCH" == "armv7l" ]; then
-            if [ -f /opt/vc/include/bcm_host.h ]; then # Check for Raspberry Pi
-                ARCH=armv6l #this makes no sense
-            fi
+            ARCH=arm64
+        elif [ "$ARCH" == "aarch64" ]; then
+            ARCH=arm64
         elif [ "$ARCH" == "i686" ] || [ "$ARCH" == "i386" ]; then
             echo "32bit linux is not officially supported anymore but compiling the libraries using the build script in apothecary/scripts should compile all the dependencies without problem"
             exit 1
@@ -316,7 +310,7 @@ if [ "$PLATFORM" == "linux" ]; then
     elif [ "$ARCH" == "arm64" ]; then
 		OPT="_${GCC_VERSION}"
 	elif [ "$ARCH" == "aarch64" ]; then
-		OPT=""
+		OPT="_${GCC_VERSION}"
 	elif [ "$ARCH" == "armv8l" ]; then
 	    OPT=""
 	elif [ "$ARCH" == "armv7l" ]; then
