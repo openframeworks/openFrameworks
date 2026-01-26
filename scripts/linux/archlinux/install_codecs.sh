5eb5f94caafd819dd48773f39426bf2901a76f82
Carlo <freevibes@gmail.com>
2026-01-26 18:02:55 +0100

    Fix Arch install scripts (#8497)
    * Add missing dependencies for Arch Linux installation scripts:
    Previously compilation of oF and PG would fail otherwise.
    
    * comments
    
    * Update install_dependencies.sh

--- scripts/linux/archlinux/install_codecs.sh
+++ scripts/linux/archlinux/install_codecs.sh
@@ -1,2 +1,10 @@
-pacman -S --needed mpg123 gst-plugins-ugly
+if [ $EUID != 0 ]; then
+	echo "this script must be run as root"
+	echo ""
+	echo "usage:"
+	echo "sudo ./install_codecs.sh"
+	exit $exit_code
+   exit 1
+fi
 
+pacman -S --needed mpg123 gst-plugins-ugly
