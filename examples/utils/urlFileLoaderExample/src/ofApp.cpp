4198602810cce3424ae5cfe97f563a4a09d8ec73
Dan Rosser <danoli3@gmail.com>
2026-08-14 03:53:23 +1000

    Update for https://github.com/openframeworks/openFrameworks/pull/2043 urlFileLoader example

--- examples/utils/urlFileLoaderExample/src/ofApp.cpp
+++ examples/utils/urlFileLoaderExample/src/ofApp.cpp
@@ -5,8 +5,8 @@ void ofApp::setup() {
 	ofSetWindowTitle("URL file loader progress");
 	ofSetBackgroundColor(30);
 
-	const std::string url = "https://github.com/openframeworks/openFrameworks/archive/refs/heads/master.zip";
-	ofHttpRequest request(url, ofToDataPath("openFrameworks-master.zip"), true);
+	const std::string url = "https://raw.githubusercontent.com/openframeworks/openFrameworks/master/examples/graphics/fontShapesExample/bin/data/Batang.ttf";
+	ofHttpRequest request(url, ofToDataPath("Batang.ttf"), true);
 	request.progressCallback = [this](const ofHttpRequest &, float value) {
 		progress.store(value);
 	};
@@ -28,7 +28,7 @@ void ofApp::draw() {
 	ofDrawRectangle(40, 100, width * value, 24);
 
 	ofSetColor(255);
-	ofDrawBitmapString("Downloading openFrameworks-master.zip", 40, 70);
+	ofDrawBitmapString("Downloading Batang.ttf", 40, 70);
 	ofDrawBitmapString(ofToString(value * 100.0f, 1) + "%", 40, 150);
 	if (finished) {
 		ofDrawBitmapString("Finished with HTTP status " + ofToString(status), 40, 185);
