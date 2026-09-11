14fc6f3963e90c8ff7d5748719e7350b374be9e9
Dan Rosser <danoli3@gmail.com>
2026-09-11 21:18:00 +1000

    Fix typo in emscripten template: tofGLWindowSettings -> ofGLWindowSettings
    Follow-up to ae054c2 which introduced the typo.

--- scripts/templates/emscripten/src/main.cpp
+++ scripts/templates/emscripten/src/main.cpp
@@ -16,7 +16,7 @@ int main( ){
 #endif
 
 	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
-	tofGLWindowSettings settings;
+	ofGLWindowSettings settings;
 	settings.setSize(1024, 768);
 	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
 
